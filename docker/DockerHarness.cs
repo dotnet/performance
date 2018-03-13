92389d990ee20a611f7ab67fd1e293afd499bf06
Victor Nate Graf <vigraf@microsoft.com>
2018-03-13 12:10:57 -0700

    Add Docker harnessing code

---
+++ docker/DockerHarness.cs
@@ -0,0 +1,516 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace DockerHarness
+{
+    public class Identifier : IEquatable<Identifier>
+    {
+        public string Name { get; set; }
+        public string Tag { get; set; }
+        public Platform Platform { get; set; }
+
+        public Identifier(string name, string tag, Platform platform)
+        {
+            Name = name;
+            Tag = tag;
+            Platform = platform;
+        }
+
+        public bool Equals(Identifier other)
+        {
+            return (
+                other != null &&
+                other.Name == this.Name &&
+                other.Tag == this.Tag &&
+                (other.Platform?.Equals(this.Platform) ?? false)
+            );
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked {
+                return (
+                    ((Name?.GetHashCode() ?? 0) * 29) ^
+                    ((Tag?.GetHashCode() ?? 0) * 83) ^
+                    ((Platform?.GetHashCode() ?? 0) * 131)
+                );
+            }
+        }
+    }
+
+    public class Platform : IEquatable<Platform>
+    {
+        public string OS { get; set; }
+        public string Architecture { get; set; }
+
+        public bool Equals(Platform other)
+        {
+            return (
+                other != null &&
+                other.OS == this.OS &&
+                other.Architecture == this.Architecture
+            );
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked {
+                return (
+                    ((OS?.GetHashCode() ?? 0) * 163) ^
+                    ((Architecture?.GetHashCode() ?? 0) * 197)
+                );
+            }
+        }
+    }
+
+    public struct Reference<T> {
+        public T Value { get; set; }
+        public int Count { get; set; }
+
+        public Reference(T val)
+        {
+            Value = val;
+            Count = 1;
+        }
+    }
+
+    public class Git : IDisposable, IEquatable<Git>
+    {
+        public string Url { get; private set; }
+        public string Branch { get; private set; }
+        public string Commit { get; private set; }
+        public DirectoryInfo Location { get; private set; }
+
+        private static Dictionary<Git, Reference<DirectoryInfo>> clones = new Dictionary<Git, Reference<DirectoryInfo>>();
+
+        public Git(string url, string branch=null, string commit=null)
+        {
+            Url = url;
+            Branch = branch;
+            Commit = commit;
+        }
+
+        public DirectoryInfo Checkout()
+        {
+            if (Location == null)
+            {
+                if (clones.TryGetValue(this, out var reference))
+                {
+                    Location = reference.Value;
+                    reference.Count++;
+                }
+                else
+                {
+                    Location = new DirectoryInfo(Path.Combine(Path.GetTempPath(), Util.RandomString(16)));
+                    using (var command = Util.Command("git", $"clone {Url} {Location}"))
+                    {
+                        command.WaitForExit();
+                    }
+
+                    if (Branch != null)
+                    {
+                        using (var command = Util.Command("git", $"fetch origin {Branch}", Location))
+                        {
+                            command.WaitForExit();
+                        }
+
+                        if (Commit == null) {
+                            using (var command = Util.Command("git", $"checkout {Branch}", Location))
+                            {
+                                command.WaitForExit();
+                            }
+                        }
+                    }
+
+                    if (Commit != null)
+                    {
+                        using (var command = Util.Command("git", $"checkout {Commit}", Location))
+                        {
+                            command.WaitForExit();
+                        }
+                    }
+
+                    clones.Add(this, new Reference<DirectoryInfo>(Location));
+                }
+            }
+
+            return Location;
+        }
+
+        public void Dispose()
+        {
+           Dispose(true);
+           GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing) {
+                if (Location == null)
+                {
+                    if (clones.TryGetValue(this, out var reference))
+                    {
+                        Debug.Assert(this.Location.FullName == reference.Value.FullName);
+                        reference.Count--;
+                        if (reference.Count <= 0) {
+                            Util.DeleteDirectory(reference.Value);
+                            clones.Remove(this);
+                        }
+                        Location = null;
+                    }
+                }
+            }
+        }
+
+        public bool Equals(Git other)
+        {
+            return (
+                other != null &&
+                other.Url == this.Url &&
+                other.Branch == this.Branch &&
+                other.Commit == this.Commit
+            );
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked {
+                return (
+                    ((Url?.GetHashCode() ?? 0) * 29) ^
+                    ((Branch?.GetHashCode() ?? 0) * 83) ^
+                    ((Commit?.GetHashCode() ?? 0) * 131)
+                );
+            }
+        }
+    }
+
+    public class Repository
+    {
+        public string Name { get; set; }
+        public Dictionary<Identifier, Image> Images = new Dictionary<Identifier, Image>();
+    }
+
+    public class Image
+    {
+        public string Dockerfile { get; set; }
+        public Git Git { get; set; }
+        public Repository Repository { get; set; }
+        public Platform Platform { get; set; }
+        public HashSet<string> Tags { get; } = new HashSet<string>();
+        public Identifier Base { get; set; }
+    }
+
+    public class DockerHarness : IDisposable
+    {
+        private HashSet<Identifier> pulledImages = new HashSet<Identifier>();
+        private List<Git> gitRepos = new List<Git>();
+
+        private Repository ParseManifest(string manifest, Git git) {
+            // Read the manifest into a JSON object and pull out the repo
+            var repoJson = JObject.Parse(manifest)["repos"][0];
+
+            var repo = new Repository {
+                Name = repoJson["name"].ToString()
+            };
+
+            // Parse the manifest to pull out image information
+            foreach (var imageJson in repoJson["images"])
+            {
+                foreach (var platformJson in imageJson["platforms"])
+                {
+                    // Get the platform specification
+                    var plat = new Platform {
+                        OS = platformJson["os"].ToString(),
+                        Architecture = platformJson["architecture"]?.ToString() ?? "amd64"
+                    };
+
+                    // If there is more specification on OS or Arch add it after a ':'
+                    if (platformJson["osVersion"] != null) {
+                        plat.OS = String.Join(":", plat.OS, platformJson["osVersion"].ToString());
+                    }
+                    if (platformJson["variant"] != null) {
+                        plat.Architecture = String.Join(":", plat.Architecture, platformJson["variant"].ToString());
+                    }
+
+                    var img = new Image {
+                        Platform = plat,
+                        Dockerfile = platformJson["dockerfile"].ToString(),
+                        Repository = repo,
+                        Git = git
+                    };
+
+                    // Assign all the tags that map to this image
+                    foreach (var tag in (platformJson["tags"] as JObject).Properties())
+                    {
+                        img.Tags.Add(tag.Name);
+                        repo.Images.Add(new Identifier(repo.Name, tag.Name, plat), img);
+                    }
+
+                    if (imageJson["sharedTags"] != null)
+                    {
+                        foreach (var tag in (imageJson["sharedTags"] as JObject).Properties())
+                        {
+                            img.Tags.Add(tag.Name);
+                            repo.Images.Add(new Identifier(repo.Name, tag.Name, plat), img);
+                        }
+                    }
+                }
+            }
+
+            return repo;
+        }
+
+        private string PlatformKeyCombine(Platform platform, string key)
+        {
+            if (platform.OS == "linux")
+            {
+                if (platform.Architecture == "amd64")
+                {
+                    return key;
+                }
+                else
+                {
+                    return String.Join("-", platform.Architecture, key);
+                }
+            }
+            else
+            {
+                return String.Join("-", platform.OS, platform.Architecture, key);
+            }
+        }
+
+        private Repository ParseLibrary(string library, string name)
+        {
+            var blocks = Rfc2822.Parse(library);
+
+            if (!(blocks.Count > 0)) {
+                throw new InvalidOperationException("Input is not a proper library. No blocks were found");
+            }
+
+            var repo = new Repository {
+                Name = name,
+            };
+
+            string defaultUrl = null;
+            string defaultBranch = null;
+            string defaultCommit = null;
+            blocks[0].TryGetValue("GitRepo", out defaultBranch);
+            blocks[0].TryGetValue("GitFetch", out defaultBranch);
+            blocks[0].TryGetValue("GitCommit", out defaultCommit);
+
+            foreach (var block in blocks.Skip(1))
+            {
+                // Tags is mandatory
+                var tags = block["Tags"].Split(new char[]{',', ' '});
+                List<Platform> platforms = null;
+                if (!block.TryGetValue("Architectures", out var platStrs))
+                {
+                    platforms = platStrs.Split(new char[]{',', ' '}).Select(platStr => {
+                        var split = platStr.Split(new char[]{'-'});
+                        if (split.Length == 1)
+                        {
+                            return new Platform {
+                                OS = "linux",
+                                Architecture = split[0]
+                            };
+                        }
+                        else
+                        {
+                            Debug.Assert(split.Length == 2);
+                            return new Platform {
+                                OS = split[0],
+                                Architecture = split[1]
+                            };
+                        }
+                    }).ToList();
+                }
+                else
+                {
+                    platforms = new List<Platform> { new Platform { OS = "linux", Architecture = "arm64" } };
+                }
+
+                foreach (var platform in platforms) {
+                    string url, branch, commit;
+                    if (!block.TryGetValue(PlatformKeyCombine(platform, "GitRepo"), out url))
+                    {
+                        url = defaultUrl;
+                    }
+                    if (!block.TryGetValue(PlatformKeyCombine(platform, "GitFetch"), out branch))
+                    {
+                        branch = defaultBranch;
+                    }
+                    if (!block.TryGetValue(PlatformKeyCombine(platform, "GitCommit"), out commit))
+                    {
+                        commit = defaultCommit;
+                    }
+
+                    string dockerfile = "Dockerfile";
+                    if (block.TryGetValue(PlatformKeyCombine(platform, "Directory"), out var dir))
+                    {
+                        dockerfile = Path.Combine(dir, "Dockerfile");
+                    }
+
+                    var image = new Image {
+                        Dockerfile = dockerfile,
+                        Git = new Git(url, branch, commit),
+                        Repository = repo,
+                        Platform = platform,
+                    };
+
+                    foreach (var tag in tags)
+                    {
+                        image.Tags.Add(tag);
+                        repo.Images.Add(new Identifier(repo.Name, tag, platform), image);
+                    }
+                }
+            }
+
+            return repo;
+        }
+
+        public Repository LoadRepository(Git git, string manifestPath = "manifest.json")
+        {
+            var gitDir = git.Checkout();
+            gitRepos.Add(git);
+
+            // Create a Repository object from a manifest
+            var repo = ParseManifest(File.ReadAllText(Path.Combine(gitDir.FullName, manifestPath)), git);
+
+            // Parse each Dockerfile to extract the base image tag
+            // This will result in a form of tree with root nodes being a out-of-repo images (including 'scratch')
+            foreach (var image in repo.Images.Values)
+            {
+                string filename = Path.Combine(gitDir.FullName, image.Dockerfile);
+                if (File.GetAttributes(filename).HasFlag(FileAttributes.Directory))
+                {
+                    filename = Path.Combine(filename, "Dockerfile");
+                }
+
+                using (var dockerfile = new StreamReader(filename))
+                {
+                    // Regex to match a 'FROM' line and pull out the image name and tag
+                    // Assumption: The base image is not specified in any ARG options
+                    var pattern = new Regex(@"^FROM\s+(?<name>(?:\S+/)?(?:[^:\s]+))(?::(?<tag>\S+))?");
+                    string baseName = null;
+                    string baseTag = null;
+                    string line = null;
+
+                    // Read every line because the last ocurrance of 'FROM' determines the base image
+                    while ((line = dockerfile.ReadLine()) != null)
+                    {
+                        var match = pattern.Match(line);
+                        if (match.Success)
+                        {
+                            baseName = match.Groups["name"].Value;
+                            if (match.Groups["tag"].Success) {
+                                baseTag = match.Groups["tag"].Value;
+                            }
+                            else {
+                                baseTag = "latest";
+                            }
+                        }
+                    }
+
+                    Debug.Assert(baseName != null && baseTag != null, $"Failed to find the base image from {filename}");
+
+                    image.Base = new Identifier(baseName, baseTag, image.Platform);
+                }
+            }
+
+            return repo;
+        }
+
+        public string SupportedOS()
+        {
+            using (var command = Util.Command("docker", "info --format \"{{ .OSType }}\""))
+            {
+                return command.StandardOutput.ReadToEnd().Trim();
+            }
+        }
+
+        public JArray Inspect(Identifier identifier)
+        {
+            if (!pulledImages.Contains(identifier))
+            {
+                using (var command = Util.Command("docker", $"pull {identifier.Name}:{identifier.Tag}"))
+                {
+                    command.WaitForExit();
+                }
+                pulledImages.Add(identifier);
+            }
+
+            using (var command = Util.Command("docker", $"image inspect {identifier.Name}:{identifier.Tag}"))
+            {
+                return JArray.Parse(command.StandardOutput.ReadToEnd());
+            }
+        }
+
+        public IEnumerable<string> InstalledPackages(Identifier identifier, string baseName=null)
+        {
+            string listCommand;
+            string listFormat;
+
+            switch (baseName ?? identifier.Name)
+            {
+                case "debian":
+                case "ubuntu":
+                case "buildpack-deps":
+                    listCommand = "dpkg -l";
+                    listFormat = @"^\w{2,3}\s+(?<name>[^:\s]+)*";
+                    break;
+                case "alpine":
+                    listCommand = "apk info";
+                    listFormat = @"^(?<name>\S+)";
+                    break;
+                case "centos":
+                    listCommand = "yum list installed";
+                    listFormat = @"^(?<name>[^.\s]+).*@";
+                    break;
+                default:
+                    throw new NotSupportedException($"Unable to list packages for {identifier.Name}");
+            }
+
+            using (var command = Util.Command("docker", $"run --rm {identifier.Name}:{identifier.Tag} {listCommand}"))
+            {
+                string line = null;
+                bool yielded = false;
+
+                // Read every line because the last ocurrance of 'FROM' determines the base image
+                while ((line = command.StandardOutput.ReadLine()) != null)
+                {
+                    var match = Regex.Match(line, listFormat);
+                    if (match.Success) {
+                        yield return match.Groups["name"].Value;
+                        yielded = true;
+                    }
+                }
+                Debug.Assert(yielded);
+            }
+        }
+
+        public void Dispose()
+        {
+           Dispose(true);
+           GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing) {
+                foreach (var git in gitRepos)
+                {
+                    git.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ docker/Util.cs
@@ -0,0 +1,214 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace DockerHarness
+{
+    // https://stackoverflow.com/a/8946825
+    internal sealed class IdentityEqualityComparer<T> : IEqualityComparer<T> where T : class
+    {
+        public int GetHashCode(T value)
+        {
+            return RuntimeHelpers.GetHashCode(value);
+        }
+
+        public bool Equals(T left, T right)
+        {
+            return left == right; // Reference identity comparison
+        }
+    }
+
+    // A simple class for escaping strings for CSV writing
+    // https://stackoverflow.com/a/769713
+    // Used instead of a package because only these < 20 lines of code are needed
+    internal static class Csv
+    {
+        public static string Escape(string s)
+        {
+            if (s.Contains( QUOTE ))
+                s = s.Replace(QUOTE, ESCAPED_QUOTE);
+
+            if (s.IndexOfAny(CHARACTERS_THAT_MUST_BE_QUOTED) > -1)
+                s = QUOTE + s + QUOTE;
+
+            return s;
+        }
+
+        public static StringBuilder AppendCsvRow(this StringBuilder sb, params object[] objects)
+        {
+            for (int i = 0; i < objects.Length; i++)
+            {
+                sb.Append(Escape(objects[i].ToString()));
+
+                if (i != objects.Length - 1)
+                {
+                    sb.Append(',');
+                }
+            }
+            sb.AppendLine();
+
+            return sb;
+        }
+
+        private const string QUOTE = "\"";
+        private const string ESCAPED_QUOTE = "\"\"";
+        private static char[] CHARACTERS_THAT_MUST_BE_QUOTED = { ',', '"', '\n' };
+    }
+
+    internal static class Rfc2822
+    {
+        private static char[] KeySeperator = new char[]{ ':' };
+
+        public static List<Dictionary<string, string>> Parse(string document)
+        {
+            var blocks = new List<Dictionary<string, string>>();
+            var reader = new StringReader(document);
+            string lastKey = null;
+            string line = null;
+            int lineNum = 0;
+            Dictionary<string, string> block = null;
+
+            while((line = reader.ReadLine()) != null)
+            {
+                // Ignore leading/trailing whitespace
+                line = line.Trim();
+
+                if (line != String.Empty && line[0] != '#')
+                {
+                    if (block == null)
+                    {
+                        block = new Dictionary<string,string>();
+                    }
+
+                    var split = line.Split(KeySeperator, 2);
+                    if (split.Length == 2) {
+                        var key = split[0].Trim();
+                        var val = split[1].Trim();
+                        block[key] = val;
+                        lastKey = key;
+                    }
+                    else {
+                        Debug.Assert(split.Length == 1);
+                        if (lastKey != null) {
+                            // This line is a continuation
+                            block[lastKey] += split[0];
+                        }
+                        else {
+                            throw new InvalidOperationException($"RFC2822 syntax error. Line {lineNum} is has no key and is not a continuation");
+                        }
+                    }
+                }
+                else if (line == String.Empty && block != null)
+                {
+                    blocks.Add(block);
+                    block = null;
+                    lastKey = null;
+                }
+
+                lineNum++;
+            }
+
+            return blocks;
+        }
+    }
+
+    internal static class Util
+    {
+        public static void DeleteDirectory(DirectoryInfo dir, bool setAttr=false, uint maxWait=10000)
+        {
+            dir.Refresh();
+
+            if (!dir.Exists)
+            {
+                return;
+            }
+
+            foreach (var subdir in dir.GetDirectories())
+            {
+                bool _setAttr = setAttr;
+                if (subdir.Name == ".git") {
+                    // Let's git aggressive
+                    _setAttr = true;
+                }
+                DeleteDirectory(subdir, setAttr: _setAttr, maxWait: maxWait);
+            }
+
+            // Give it time to actually delete all the files
+            var files = dir.GetFiles();
+            bool wait = true;
+            uint waitTime = 0;
+            while (wait)
+            {
+                wait = false;
+
+                foreach (var f in files)
+                {
+                    if (File.Exists(f.FullName))
+                    {
+                        try
+                        {
+                            if (setAttr) {
+                                // Allows us to delete read-only files (e.g. git files)
+                                f.Attributes = FileAttributes.Normal;
+                            }
+                            File.Delete(f.FullName);
+                        }
+                        catch (IOException) { if (waitTime > maxWait) throw; }
+                        catch (UnauthorizedAccessException) { if (waitTime > maxWait) throw; }
+
+                        if (File.Exists(f.FullName))
+                        {
+                            wait = true;
+
+                            // Print a message every 3 seconds if the thread is stuck
+                            if (waitTime != 0 && waitTime % 3000 == 0)
+                            {
+                                Console.WriteLine($"Waiting to delete {f.FullName}");
+                            }
+                        }
+                    }
+                }
+
+                // Try again in 100ms
+                if (wait)
+                {
+                    Thread.Sleep(100);
+                    waitTime += 100;
+                }
+            }
+
+            Directory.Delete(dir.FullName);
+        }
+
+        private static Random Generator = new Random();
+        internal static string RandomString(int length)
+        {
+            // All alphanumerics except O, 0, 1, and l
+            const string chars = "abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ23456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[Generator.Next(s.Length)]).ToArray());
+        }
+
+        internal static Process Command(string executable, string args, DirectoryInfo workingDir=null)
+        {
+            Console.WriteLine(String.Join(" ", executable, args));
+            return Process.Start(
+                new ProcessStartInfo {
+                    FileName = executable,
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = false,
+                    WorkingDirectory = workingDir?.FullName ?? ""
+                }
+            );
+        }
+    }
+}
