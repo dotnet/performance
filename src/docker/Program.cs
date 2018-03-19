c269b0fedde465a60cfc2d7b3d92395a103433bc
Victor Nate Graf <vigraf@microsoft.com>
2018-03-19 12:05:09 -0700

    Adjust reported metrics and repos

--- src/docker/Program.cs
+++ src/docker/Program.cs
@@ -24,28 +24,28 @@ namespace DockerHarness
                 harness.LoadManifest("https://github.com/docker-library/official-images/", "library/openjdk");
                 harness.LoadManifest("https://github.com/docker-library/official-images/", "library/mono");
                 harness.LoadManifest("https://github.com/docker-library/official-images/", "library/python");
+                harness.LoadManifest("https://github.com/docker-library/official-images/", "library/pypy");
                 harness.LoadManifest("https://github.com/docker-library/official-images/", "library/golang");
                 harness.LoadManifest("https://github.com/docker-library/official-images/", "library/ruby");
                 harness.LoadManifest("https://github.com/docker-library/official-images/", "library/buildpack-deps");
-                //harness.LoadManifest("https://github.com/docker-library/official-images/", "library/php");
+                harness.LoadManifest("https://github.com/docker-library/official-images/", "library/php");
 
                 var seen = new HashSet<Image>(new IdentityEqualityComparer<Image>());
 
-                report.AppendCsvRow("Name","Tag","BaseName","BaseTag","Foundation Name","Foundation Tag","Size","DiffSize","NumPackages","Packages");
+                report.AppendCsvRow("Name","Tag","ParentName","ParentTag","BaseName","BaseTag","Size","AddedSize","NumAddedPackages","AddedPackages","Inspect", "AllTags");
                 foreach (var repo in harness.Repositories)
                 {
                     foreach (var image in repo.Images.Values)
                     {
-                        if (!seen.Contains(image))
+                        if (seen.Add(image))
                         {
-                            seen.Add(image);
                             if (image.Platform.Equals(harness.Platform))
                             {
                                 var id = new Identifier(repo.Name, image.Tags.OrderBy(tag => tag.Length).First(), image.Platform);
                                 var imageInfo = harness.Inspect(id)[0];
-                                var baseInfo = harness.Inspect(image.Parent)[0];
+                                var parentInfo = harness.Inspect(image.Parent)[0];
                                 var imageSize = Int64.Parse(imageInfo["Size"].ToString());
-                                var baseSize = Int64.Parse(baseInfo["Size"].ToString());
+                                var parentSize = Int64.Parse(parentInfo["Size"].ToString());
 
                                 var foundation = id;
                                 while (harness.Images.TryGetValue(foundation, out var img)) {
@@ -58,7 +58,14 @@ namespace DockerHarness
                                     packages = harness.InstalledPackages(id, foundation.Name).Except(harness.InstalledPackages(image.Parent, foundation.Name)).ToArray();
                                 }
 
-                                report.AppendCsvRow(id.Name, id.Tag, image.Parent.Name, image.Parent.Tag, foundation.Name, foundation.Tag, imageSize, imageSize - baseSize, packages.Length, String.Join(" ", packages));
+                                report.AppendCsvRow(
+                                    id.Name, id.Tag,
+                                    image.Parent.Name, image.Parent.Tag,
+                                    foundation.Name, foundation.Tag,
+                                    imageSize, imageSize - parentSize,
+                                    packages.Length, String.Join(" ", packages),
+                                    imageInfo.ToString(), String.Join(" ", image.Tags)
+                                );
                             }
                         }
                     }
