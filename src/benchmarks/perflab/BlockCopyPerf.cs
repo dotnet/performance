a79729ce1e6ba7a54e95eb554aa2f0dbaed81382
Adam Sitnik <adam.sitnik@gmail.com>
2018-05-30 14:21:43 +0200

    port BlockCopyPerf

--- src/benchmarks/perflab/BlockCopyPerf.cs
+++ src/benchmarks/perflab/BlockCopyPerf.cs
@@ -1,28 +1,26 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.Xunit.Performance;
 using System;
-using Xunit;
+using BenchmarkDotNet.Attributes;
 
 namespace PerfLabTests
 {
     public class BlockCopyPerf
     {
-        [Benchmark(InnerIterationCount = 1000000)]
-        [InlineData(0)]
-        [InlineData(10)]
-        [InlineData(100)]
-        [InlineData(1000)]
-        public static void CallBlockCopy(int numElements)
-        {
-            byte[] bytes = new byte[numElements * 2];
-            Buffer.BlockCopy(bytes, 0, bytes, numElements, numElements);
+        private byte[] bytes;
 
-            foreach (var iteration in Benchmark.Iterations)
-                using (iteration.StartMeasurement())
-                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        Buffer.BlockCopy(bytes, 0, bytes, numElements, numElements);
+        [Params(0, 10, 100, 1000)]
+        public int NumElements;
+
+        [GlobalSetup]
+        public void Setup(int numElements)
+        {
+            bytes = new byte[numElements * 2];
+            Buffer.BlockCopy(bytes, 0, bytes, numElements, numElements);
         }
+
+        [Benchmark]
+        public void CallBlockCopy() => Buffer.BlockCopy(bytes, 0, bytes, NumElements, NumElements);
     }
 }
