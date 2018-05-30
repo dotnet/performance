0ad84ad0c6055df01c44cb792d1e3b7480c5c8af
Adam Sitnik <adam.sitnik@gmail.com>
2018-05-30 17:02:41 +0200

    port ThreadingPerf

--- src/benchmarks/perflab/ThreadingPerf.cs
+++ src/benchmarks/perflab/ThreadingPerf.cs
@@ -1,9 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.Xunit.Performance;
-using System;
 using System.Threading;
+using BenchmarkDotNet.Attributes;
 
 namespace PerfLabTests
 {
@@ -12,66 +11,34 @@ namespace PerfLabTests
         private static int s_i;
         private static string s_s;
 
-        [Benchmark(InnerIterationCount = 100000)]
-        public static void CompareExchangeIntNoMatch()
-        {
-            s_i = 0;
-            foreach (var iteration in Benchmark.Iterations)
-                using (iteration.StartMeasurement())
-                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        Interlocked.CompareExchange(ref s_i, 5, -1);
-        }
+        [GlobalSetup(Target = nameof(CompareExchangeIntNoMatch))]
+        public void SetupCompareExchangeIntNoMatch() => s_i = 0;
 
-        [Benchmark(InnerIterationCount = 100000)]
-        public static void CompareExchangeIntMatch()
-        {
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                s_i = 1;
-                using (iteration.StartMeasurement())
-                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        Interlocked.CompareExchange(ref s_i, 5, 1);
-            }
-        }
+        [Benchmark]
+        public void CompareExchangeIntNoMatch() => Interlocked.CompareExchange(ref s_i, 5, -1);
 
-        [Benchmark(InnerIterationCount = 100000)]
-        public static void CompareExchangeObjNoMatch()
-        {
-            s_s = "Hello";
-            foreach (var iteration in Benchmark.Iterations)
-                using (iteration.StartMeasurement())
-                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        Interlocked.CompareExchange(ref s_s, "World", "What?");
-        }
+        [GlobalSetup(Target = nameof(CompareExchangeIntMatch))]
+        public void SetupCompareExchangeIntMatch() => s_i = 1;
+        
+        [Benchmark]
+        public void CompareExchangeIntMatch() => Interlocked.CompareExchange(ref s_i, 5, 1);
 
-        [Benchmark(InnerIterationCount = 100000)]
-        public static void CompareExchangeObjMatch()
-        {
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                s_s = "What?";
-                using (iteration.StartMeasurement())
-                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        Interlocked.CompareExchange(ref s_s, "World", "What?");
-            }
-        }
+        [GlobalSetup(Target = nameof(CompareExchangeObjNoMatch))]
+        public void SetupCompareExchangeObjNoMatch() => s_s = "Hello";
+        
+        [Benchmark]
+        public void CompareExchangeObjNoMatch() => Interlocked.CompareExchange(ref s_s, "World", "What?");
+        
+        [GlobalSetup(Target = nameof(CompareExchangeObjMatch))]
+        public void SetupCompareExchangeObjMatch() => s_s = "Hello";
+        
+        [Benchmark]
+        public void CompareExchangeObjMatch() => Interlocked.CompareExchange(ref s_s, "World", "What?");
 
-        [Benchmark(InnerIterationCount = 100000)]
-        public static void InterlockedIncrement()
-        {
-            foreach (var iteration in Benchmark.Iterations)
-                using (iteration.StartMeasurement())
-                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        Interlocked.Increment(ref s_i);
-        }
+        [Benchmark]
+        public void InterlockedIncrement() => Interlocked.Increment(ref s_i);
 
-        [Benchmark(InnerIterationCount = 100000)]
-        public static void InterlockedDecrement()
-        {
-            foreach (var iteration in Benchmark.Iterations)
-                using (iteration.StartMeasurement())
-                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        Interlocked.Decrement(ref s_i);
-        }
+        [Benchmark]
+        public void InterlockedDecrement() =>  Interlocked.Decrement(ref s_i);
     }
 }
