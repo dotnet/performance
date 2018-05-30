902832a3681b6de3ed7e08ba5f3c07e16c5edec6
Adam Sitnik <adam.sitnik@gmail.com>
2018-05-30 15:31:32 +0200

    port DelegatePerf

--- src/benchmarks/perflab/DelegatePerf.cs
+++ src/benchmarks/perflab/DelegatePerf.cs
@@ -1,89 +1,82 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.Xunit.Performance;
 using System;
-using Xunit;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
 
 namespace PerfLabTests
 {
-
-    internal delegate long DelegateLong(Object obj, long x, long y);
-    internal delegate void MultiDelegate(Object obj, long x, long y);
+    public delegate long DelegateLong(Object obj, long x, long y);
+    public delegate void MultiDelegate(Object obj, long x, long y);
 
     internal delegate int SerializeDelegate();
 
     public class DelegatePerf
     {
-        [Benchmark(InnerIterationCount = 200000)]
-        public void DelegateInvoke()
+        [Benchmark]
+        [ArgumentsSource(nameof(DelegateInvokeArguments))]
+        public void DelegateInvoke(DelegateLong dl, Object obj) => dl(obj, 100, 100);
+
+        public IEnumerable<object[]> DelegateInvokeArguments()
         {
-            DelegateLong dl = new DelegateLong(this.Invocable1);
-            Object obj = new Object();
-
-            long ret = dl(obj, 100, 100);
-
-            foreach (var iteration in Benchmark.Iterations)
-                using (iteration.StartMeasurement())
-                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        ret = dl(obj, 100, 100);
+            yield return new object[] { new DelegateLong(this.Invocable1), new Object() };
         }
 
-        [Benchmark(InnerIterationCount = 1000)]
-        public void MulticastDelegateCombineInvoke()
+        [Benchmark]
+        [ArgumentsSource(nameof(MulticastDelegateCombineInvokerAguments))]
+        public MultiDelegate MulticastDelegateCombineInvoke(MultiDelegate md1, MultiDelegate md2, MultiDelegate md3, MultiDelegate md4, MultiDelegate md5, 
+            MultiDelegate md6, MultiDelegate md7, MultiDelegate md8, MultiDelegate md9, MultiDelegate md10)
         {
             MultiDelegate md = null;
-            Object obj = new Object();
+            
+            md = (MultiDelegate)Delegate.Combine(md1, md);
+            md = (MultiDelegate)Delegate.Combine(md2, md);
+            md = (MultiDelegate)Delegate.Combine(md3, md);
+            md = (MultiDelegate)Delegate.Combine(md4, md);
+            md = (MultiDelegate)Delegate.Combine(md5, md);
+            md = (MultiDelegate)Delegate.Combine(md6, md);
+            md = (MultiDelegate)Delegate.Combine(md7, md);
+            md = (MultiDelegate)Delegate.Combine(md8, md);
+            md = (MultiDelegate)Delegate.Combine(md9, md);
+            md = (MultiDelegate)Delegate.Combine(md10, md);
 
-            foreach (var iteration in Benchmark.Iterations)
+            return md;
+        }
+        
+        public IEnumerable<object[]> MulticastDelegateCombineInvokerAguments()
+        {
+            yield return new object[]
             {
-                MultiDelegate md1 = new MultiDelegate(this.Invocable2);
-                MultiDelegate md2 = new MultiDelegate(this.Invocable2);
-                MultiDelegate md3 = new MultiDelegate(this.Invocable2);
-                MultiDelegate md4 = new MultiDelegate(this.Invocable2);
-                MultiDelegate md5 = new MultiDelegate(this.Invocable2);
-                MultiDelegate md6 = new MultiDelegate(this.Invocable2);
-                MultiDelegate md7 = new MultiDelegate(this.Invocable2);
-                MultiDelegate md8 = new MultiDelegate(this.Invocable2);
-                MultiDelegate md9 = new MultiDelegate(this.Invocable2);
-                MultiDelegate md10 = new MultiDelegate(this.Invocable2);
-
-                using (iteration.StartMeasurement())
-                {
-                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                    {
-                        md = (MultiDelegate)Delegate.Combine(md1, md);
-                        md = (MultiDelegate)Delegate.Combine(md2, md);
-                        md = (MultiDelegate)Delegate.Combine(md3, md);
-                        md = (MultiDelegate)Delegate.Combine(md4, md);
-                        md = (MultiDelegate)Delegate.Combine(md5, md);
-                        md = (MultiDelegate)Delegate.Combine(md6, md);
-                        md = (MultiDelegate)Delegate.Combine(md7, md);
-                        md = (MultiDelegate)Delegate.Combine(md8, md);
-                        md = (MultiDelegate)Delegate.Combine(md9, md);
-                        md = (MultiDelegate)Delegate.Combine(md10, md);
-                    }
-                }
-            }
-
-            md(obj, 100, 100);
+                new MultiDelegate(this.Invocable2), 
+                new MultiDelegate(this.Invocable2), 
+                new MultiDelegate(this.Invocable2), 
+                new MultiDelegate(this.Invocable2), 
+                new MultiDelegate(this.Invocable2), 
+                new MultiDelegate(this.Invocable2), 
+                new MultiDelegate(this.Invocable2),
+                new MultiDelegate(this.Invocable2), 
+                new MultiDelegate(this.Invocable2), 
+                new MultiDelegate(this.Invocable2)
+            };
         }
 
-        [Benchmark(InnerIterationCount = 10000)]
-        [InlineData(100)]
-        [InlineData(1000)]
-        public void MulticastDelegateInvoke(int length)
+        [Benchmark]
+        [ArgumentsSource(nameof(MulticastDelegateInvokeArguments))]
+        public void MulticastDelegateInvoke(int length, MultiDelegate md, Object obj) => md(obj, 100, 100);
+        
+        public IEnumerable<object[]> MulticastDelegateInvokeArguments()
         {
-            MultiDelegate md = null;
-            Object obj = new Object();
+            foreach (var length in new[] { 100, 1000 })
+            {
+                MultiDelegate md = null;
+                Object obj = new Object();
 
-            for (long i = 0; i < length; i++)
-                md = (MultiDelegate)Delegate.Combine(new MultiDelegate(this.Invocable2), md);
-
-            foreach (var iteration in Benchmark.Iterations)
-                using (iteration.StartMeasurement())
-                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        md(obj, 100, 100);
+                for (long i = 0; i < length; i++)
+                    md = (MultiDelegate)Delegate.Combine(new MultiDelegate(this.Invocable2), md);
+                
+                yield return new object[] { length, md, obj };
+            }
         }
 
         internal virtual long Invocable1(Object obj, long x, long y)
