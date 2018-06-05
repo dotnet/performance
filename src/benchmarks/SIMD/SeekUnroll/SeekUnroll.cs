2eb2ea1652873595baa53fabfe38b0cddfa692f2
Adam Sitnik <adam.sitnik@gmail.com>
2018-06-05 14:37:29 +0200

    port SeekUnroll

--- src/benchmarks/SIMD/SeekUnroll/SeekUnroll.cs
+++ src/benchmarks/SIMD/SeekUnroll/SeekUnroll.cs
@@ -2,20 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.Xunit.Performance;
 using System;
 using System.Numerics;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Reflection;
 using System.Collections.Generic;
-using Xunit;
+using BenchmarkDotNet.Attributes;
 
-[assembly: OptimizeForBenchmarks]
-
-public static class SeekUnroll
+public class SeekUnroll
 {
-
     // The purpose of this micro-benchmark is to measure the effect of unrolling
     // on this loop (taken from https://github.com/aspnet/KestrelHttpServer/pull/1138)
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -58,44 +53,11 @@ public static class SeekUnroll
         }
     }
 
-    // Iteration counts for inner loop set to have each call take 1 or
-    // 2 seconds or so in release, finish quickly in debug.
-#if DEBUG
-    static int InnerIterations = 1;
-#else
     static int InnerIterations = 1000000000;
-#endif
 
-    // Function to meaure InnerLoop using the xunit-perf benchmark measurement facilities
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    static void XunitBenchmarkLoop(ref int foundIndex, ref Vector<Byte> vector)
-    {
-        foreach (var iteration in Benchmark.Iterations)
-        {
-            using (iteration.StartMeasurement())
-            {
-                InnerLoop(ref foundIndex, ref vector);
-            }
-        }
-    }
-
-    // Function to measure InnerLoop with manual use of a stopwatch timer
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    static void ManualTimerLoop(ref int foundIndex, ref Vector<Byte> vector)
-    {
-        for (int iteration = 0; iteration < ManualLoopTimes.Length; ++iteration)
-        {
-            var timer = System.Diagnostics.Stopwatch.StartNew();
-            InnerLoop(ref foundIndex, ref vector);
-            timer.Stop();
-            ManualLoopTimes[iteration] = timer.ElapsedMilliseconds;
-        }
-    }
-    static long[] ManualLoopTimes;
-
-    // Function that tests one input, dispatching to either the xunit-perf
-    // loop or the manual timer loop
-    static bool Test(int index, bool isXunitBenchmark)
+    [Benchmark(Description = nameof(SeekUnroll))]
+    [ArgumentsSource(nameof(ArrayedBoxedIndicesToTest))]
+    public bool Test(int index)
     {
         if (index >= Vector<Byte>.Count)
         {
@@ -108,70 +70,10 @@ public static class SeekUnroll
 
         int foundIndex = -1;
 
-        if (isXunitBenchmark)
-        {
-            XunitBenchmarkLoop(ref foundIndex, ref vector);
-        }
-        else
-        {
-            ManualTimerLoop(ref foundIndex, ref vector);
-        }
+        InnerLoop(ref foundIndex, ref vector);
 
-        Assert.Equal(index, foundIndex);
         return (index == foundIndex);
     }
 
-    // Set of indices to pass to Test(int, bool)
-    static int[] IndicesToTest = new int[] { 1, 3, 11, 19, 27 };
-
-    // Entrypoint for xunit-perf to call the benchmark
-    [Benchmark]
-    [MemberData(nameof(ArrayedBoxedIndicesToTest))]
-    public static bool Test(object boxedIndex)
-    {
-        return Test((int)boxedIndex, true);
-    }
-
-    // IndicesToTest wrapped up in arrays of boxes since that's
-    // what xunit-perf needs
-    public static IEnumerable<object[]> ArrayedBoxedIndicesToTest =
-        IndicesToTest.Select((int index) => new object[] { index });
-
-
-    // Main method entrypoint runs the manual timer loop
-    public static int Main(string[] args)
-    {
-        int failures = 0;
-
-        // On non-hardware accelerated platforms, the test times out because it runs for too long.
-        // In those cases, we decrease InnerIterations so the test doesn't time out.
-        if (!Vector.IsHardwareAccelerated)
-        {
-            InnerIterations = 100000;
-        }
-
-        int manualLoopCount = 1;
-        if (args == null || args.Length == 0)
-        {
-            Console.WriteLine("Warning: no iteration count specified; defaulting to 1 iteration per case");
-            Console.WriteLine("To use multiple iterations per case, pass the desired number of iterations as the first command-line argument to this test");
-        }
-        else
-        {
-            manualLoopCount = int.Parse(args[0]);
-        }
-
-        foreach(int index in IndicesToTest)
-        {
-            ManualLoopTimes = new long[manualLoopCount];
-            bool passed = Test(index, false);
-            if (!passed)
-            {
-                ++failures;
-            }
-            Console.WriteLine("Index {0}, times (ms) [{1}]", index, String.Join(", ", ManualLoopTimes));
-        }
-
-        return 100 + failures;
-    }
+    public static IEnumerable<object> ArrayedBoxedIndicesToTest => new object[] { 1, 3, 11, 19, 27 };
 }
