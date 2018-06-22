0e5897efb5e0320da6ad2a939cd6e0a2d8b7f789
Adam Sitnik <adam.sitnik@gmail.com>
2018-06-22 12:57:54 +0200

    initial solution structure

---
+++ src/ArtifactsUploader/ArtifactsUploader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArtifactsUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
