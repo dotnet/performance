6ccd7835d6855b527216b5c5b637d6f5542d1eb2
Adam Sitnik <adam.sitnik@gmail.com>
2018-06-22 14:07:03 +0200

    configure static analysis tool

---
+++ src/ArtifactsUploader/ArtifactsUploader/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives should be placed correctly", Justification = "We prefer using outside namespaces")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "It's not a library, we don't need xml doc")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpecialRules", "SA0001:XmlCommentAnalysisDisabled", Justification = "It's not a library, we don't need xml doc")]
