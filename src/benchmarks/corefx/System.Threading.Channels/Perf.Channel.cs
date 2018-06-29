d380f0ae515f711a2e2694530a2e98179172533b
Adam Sitnik <adam.sitnik@gmail.com>
2018-06-29 20:17:39 +0200

    port

--- src/benchmarks/corefx/System.Threading.Channels/Perf.Channel.cs
+++ src/benchmarks/corefx/System.Threading.Channels/Perf.Channel.cs
@@ -3,128 +3,115 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Threading.Tasks;
-using Microsoft.Xunit.Performance;
-using System.Threading.Channels;
+using BenchmarkDotNet.Attributes;
 
 namespace System.Threading.Channels.Tests
 {
-    public sealed class UnboundedChannelPerfTests : PerfTests
+    public class UnboundedChannelPerfTests : PerfTests
     {
-        public override Channel<int> CreateChannel() => Channel.CreateUnbounded<int>();
+        public sealed override Channel<int> CreateChannel() => Channel.CreateUnbounded<int>();
     }
 
-    public sealed class SpscUnboundedChannelPerfTests : PerfTests
+    public class SpscUnboundedChannelPerfTests : PerfTests
     {
-        public override Channel<int> CreateChannel() => Channel.CreateUnbounded<int>(new UnboundedChannelOptions { SingleReader = true, SingleWriter = true });
+        public sealed override Channel<int> CreateChannel() => Channel.CreateUnbounded<int>(new UnboundedChannelOptions { SingleReader = true, SingleWriter = true });
     }
 
-    public sealed class BoundedChannelPerfTests : PerfTests
+    public class BoundedChannelPerfTests : PerfTests
     {
-        public override Channel<int> CreateChannel() => Channel.CreateBounded<int>(10);
+        public sealed override Channel<int> CreateChannel() => Channel.CreateBounded<int>(10);
     }
 
     public abstract class PerfTests
     {
+        private Channel<int> _channel, _channel1, _channel2;
+        private ChannelReader<int> _reader;
+        private ChannelWriter<int> _writer;
+
         public abstract Channel<int> CreateChannel();
 
-        [Benchmark(InnerIterationCount = 1_000_000), MeasureGCAllocations]
+        [GlobalSetup(Target = nameof(TryWriteThenTryRead) + "," + nameof(WriteAsyncThenReadAsync) + "," + nameof(ReadAsyncThenWriteAsync))]
+        public void SingleChannelSetup()
+        {
+            _channel = CreateChannel();
+            _reader = _channel.Reader;
+            _writer = _channel.Writer;
+        }
+
+        [Benchmark]
         public void TryWriteThenTryRead()
         {
-            Channel<int> channel = CreateChannel();
-            ChannelReader<int> reader = channel.Reader;
-            ChannelWriter<int> writer = channel.Writer;
+            ChannelReader<int> reader = _reader;
+            ChannelWriter<int> writer = _writer;
 
-            foreach (BenchmarkIteration iteration in Benchmark.Iterations)
+            for (int i = 0; i < 1_000_000; i++)
             {
-                long iters = Benchmark.InnerIterationCount;
-                using (iteration.StartMeasurement())
-                {
-                    for (int i = 0; i < iters; i++)
-                    {
-                        writer.TryWrite(i);
-                        reader.TryRead(out _);
-                    }
-                }
+                writer.TryWrite(i);
+                reader.TryRead(out _);
             }
         }
 
-        [Benchmark(InnerIterationCount = 1_000_000), MeasureGCAllocations]
+        [Benchmark]
         public async Task WriteAsyncThenReadAsync()
         {
-            Channel<int> channel = CreateChannel();
-            ChannelReader<int> reader = channel.Reader;
-            ChannelWriter<int> writer = channel.Writer;
+            ChannelReader<int> reader = _reader;
+            ChannelWriter<int> writer = _writer;
 
-            foreach (BenchmarkIteration iteration in Benchmark.Iterations)
+            for (int i = 0; i < 1_000_000; i++)
             {
-                long iters = Benchmark.InnerIterationCount;
-                using (iteration.StartMeasurement())
+                await writer.WriteAsync(i);
+                await reader.ReadAsync();
+            }
+        }
+
+        [Benchmark]
+        public async Task ReadAsyncThenWriteAsync()
+        {
+            ChannelReader<int> reader = _reader;
+            ChannelWriter<int> writer = _writer;
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                ValueTask<int> r = reader.ReadAsync();
+                await writer.WriteAsync(42);
+                await r;
+            }
+        }
+
+        [GlobalSetup(Target = nameof(PingPong))]
+        public void SetupTwoChannels()
+        {
+            _channel1 = CreateChannel();
+            _channel2 = CreateChannel();
+        }
+
+        [Benchmark]
+        public async Task PingPong()
+        {
+            Channel<int> channel1 = _channel1;
+            Channel<int> channel2 = _channel2;
+
+            await Task.WhenAll(
+                Task.Run(async () =>
                 {
-                    for (int i = 0; i < iters; i++)
+                    ChannelReader<int> reader = channel1.Reader;
+                    ChannelWriter<int> writer = channel2.Writer;
+                    for (int i = 0; i < 1_000_000; i++)
                     {
                         await writer.WriteAsync(i);
                         await reader.ReadAsync();
                     }
-                }
-            }
-        }
-
-        [Benchmark(InnerIterationCount = 1_000_000), MeasureGCAllocations]
-        public async Task ReadAsyncThenWriteAsync()
-        {
-            Channel<int> channel = CreateChannel();
-            ChannelReader<int> reader = channel.Reader;
-            ChannelWriter<int> writer = channel.Writer;
-
-            foreach (BenchmarkIteration iteration in Benchmark.Iterations)
-            {
-                long iters = Benchmark.InnerIterationCount;
-                using (iteration.StartMeasurement())
+                }),
+                Task.Run(async () =>
                 {
-                    for (int i = 0; i < iters; i++)
+                    ChannelWriter<int> writer = channel1.Writer;
+                    ChannelReader<int> reader = channel2.Reader;
+                    for (int i = 0; i < 1_000_000; i++)
                     {
-                        ValueTask<int> r = reader.ReadAsync();
-                        await writer.WriteAsync(42);
-                        await r;
+                        await reader.ReadAsync();
+                        await writer.WriteAsync(i);
                     }
-                }
-            }
-        }
-
-        [Benchmark(InnerIterationCount = 1_000_000), MeasureGCAllocations]
-        public async Task PingPong()
-        {
-            Channel<int> channel1 = CreateChannel();
-            Channel<int> channel2 = CreateChannel();
-
-            foreach (BenchmarkIteration iteration in Benchmark.Iterations)
-            {
-                long iters = Benchmark.InnerIterationCount;
-                using (iteration.StartMeasurement())
-                {
-                    await Task.WhenAll(
-                        Task.Run(async () =>
-                        {
-                            ChannelReader<int> reader = channel1.Reader;
-                            ChannelWriter<int> writer = channel2.Writer;
-                            for (int i = 0; i < iters; i++)
-                            {
-                                await writer.WriteAsync(i);
-                                await reader.ReadAsync();
-                            }
-                        }),
-                        Task.Run(async () =>
-                        {
-                            ChannelWriter<int> writer = channel1.Writer;
-                            ChannelReader<int> reader = channel2.Reader;
-                            for (int i = 0; i < iters; i++)
-                            {
-                                await reader.ReadAsync();
-                                await writer.WriteAsync(i);
-                            }
-                        }));
-                }
-            }
+                }));
         }
     }
 }
