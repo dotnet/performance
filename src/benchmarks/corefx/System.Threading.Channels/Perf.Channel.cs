48990a75c7a7bc7f4714217484f1f5e9f52114a9
Adam Sitnik <adam.sitnik@gmail.com>
2018-07-03 14:49:53 +0200

    Port Channels benchmarks (#79), fixes #72
    * copy
    
    * port
    
    * add the category
    
    * move magic number to a const

---
+++ src/benchmarks/corefx/System.Threading.Channels/Perf.Channel.cs
@@ -0,0 +1,121 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Benchmarks;
+
+namespace System.Threading.Channels.Tests
+{
+    public class UnboundedChannelPerfTests : PerfTests
+    {
+        public sealed override Channel<int> CreateChannel() => Channel.CreateUnbounded<int>();
+    }
+
+    public class SpscUnboundedChannelPerfTests : PerfTests
+    {
+        public sealed override Channel<int> CreateChannel() => Channel.CreateUnbounded<int>(new UnboundedChannelOptions { SingleReader = true, SingleWriter = true });
+    }
+
+    public class BoundedChannelPerfTests : PerfTests
+    {
+        public sealed override Channel<int> CreateChannel() => Channel.CreateBounded<int>(10);
+    }
+
+    [BenchmarkCategory(Categories.CoreFX)]
+    public abstract class PerfTests
+    {
+        private const int InnerIterationCount = 1_000_000;
+
+        private Channel<int> _channel, _channel1, _channel2;
+        private ChannelReader<int> _reader;
+        private ChannelWriter<int> _writer;
+
+        public abstract Channel<int> CreateChannel();
+
+        [GlobalSetup(Target = nameof(TryWriteThenTryRead) + "," + nameof(WriteAsyncThenReadAsync) + "," + nameof(ReadAsyncThenWriteAsync))]
+        public void SingleChannelSetup()
+        {
+            _channel = CreateChannel();
+            _reader = _channel.Reader;
+            _writer = _channel.Writer;
+        }
+
+        [Benchmark]
+        public void TryWriteThenTryRead()
+        {
+            ChannelReader<int> reader = _reader;
+            ChannelWriter<int> writer = _writer;
+
+            for (int i = 0; i < InnerIterationCount; i++)
+            {
+                writer.TryWrite(i);
+                reader.TryRead(out _);
+            }
+        }
+
+        [Benchmark]
+        public async Task WriteAsyncThenReadAsync()
+        {
+            ChannelReader<int> reader = _reader;
+            ChannelWriter<int> writer = _writer;
+
+            for (int i = 0; i < InnerIterationCount; i++)
+            {
+                await writer.WriteAsync(i);
+                await reader.ReadAsync();
+            }
+        }
+
+        [Benchmark]
+        public async Task ReadAsyncThenWriteAsync()
+        {
+            ChannelReader<int> reader = _reader;
+            ChannelWriter<int> writer = _writer;
+
+            for (int i = 0; i < InnerIterationCount; i++)
+            {
+                ValueTask<int> r = reader.ReadAsync();
+                await writer.WriteAsync(42);
+                await r;
+            }
+        }
+
+        [GlobalSetup(Target = nameof(PingPong))]
+        public void SetupTwoChannels()
+        {
+            _channel1 = CreateChannel();
+            _channel2 = CreateChannel();
+        }
+
+        [Benchmark]
+        public async Task PingPong()
+        {
+            Channel<int> channel1 = _channel1;
+            Channel<int> channel2 = _channel2;
+
+            await Task.WhenAll(
+                Task.Run(async () =>
+                {
+                    ChannelReader<int> reader = channel1.Reader;
+                    ChannelWriter<int> writer = channel2.Writer;
+                    for (int i = 0; i < InnerIterationCount; i++)
+                    {
+                        await writer.WriteAsync(i);
+                        await reader.ReadAsync();
+                    }
+                }),
+                Task.Run(async () =>
+                {
+                    ChannelWriter<int> writer = channel1.Writer;
+                    ChannelReader<int> reader = channel2.Reader;
+                    for (int i = 0; i < InnerIterationCount; i++)
+                    {
+                        await reader.ReadAsync();
+                        await writer.WriteAsync(i);
+                    }
+                }));
+        }
+    }
+}
