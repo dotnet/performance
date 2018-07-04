47c009b815bafa8bf161530b3e7746d82b87392d
Adam Sitnik <adam.sitnik@gmail.com>
2018-07-04 14:38:42 +0200

    port 1:1

--- src/benchmarks/corefx/System.Console/Perf.Console.cs
+++ src/benchmarks/corefx/System.Console/Perf.Console.cs
@@ -2,13 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using Xunit;
-using Microsoft.Xunit.Performance;
+using BenchmarkDotNet.Attributes;
 
 namespace System.ConsoleTests
 {
@@ -20,132 +15,100 @@ namespace System.ConsoleTests
     /// </summary>
     public class Perf_Console
     {
+        const int StreamInnerIterations = 50;
+        const int ColorInnerIterations = 1000;
+
+        private Stream[] _streams;
+
+        [GlobalSetup(Target = nameof(OpenStandardInput) + "," + nameof(OpenStandardOutput) + "," + nameof(OpenStandardError))]
+        public void SetupStreams() => _streams = new Stream[StreamInnerIterations * 4];
+
+        [GlobalCleanup(Target = nameof(OpenStandardInput) + "," + nameof(OpenStandardOutput) + "," + nameof(OpenStandardError))]
+        public void CleanupStreams()
+        {
+            foreach (var stream in _streams)
+                stream.Dispose();
+        }
+        
         [Benchmark]
         public void OpenStandardInput()
         {
-            // We preserve copies of each opened Stream so that the perf area doesn't 
-            // cover the disposal of those streams.
-            const int innerIterations = 50;
-            Stream[] streams = new Stream[innerIterations * 4];
-            foreach (var iteration in Benchmark.Iterations)
+            Stream[] streams = _streams;
+            
+            for (int i = 0; i < StreamInnerIterations; i++)
             {
-                using (iteration.StartMeasurement())
-                {
-                    for (int i = 0; i < innerIterations; i++)
-                    {
-                        streams[0 + i * 4] = Console.OpenStandardInput(); streams[1 + i * 4] = Console.OpenStandardInput();
-                        streams[2 + i * 4] = Console.OpenStandardInput(); streams[3 + i * 4] = Console.OpenStandardInput();
-                    }
-                }
-                foreach (Stream s in streams)
-                    s.Dispose();
+                streams[0 + i * 4] = Console.OpenStandardInput(); streams[1 + i * 4] = Console.OpenStandardInput();
+                streams[2 + i * 4] = Console.OpenStandardInput(); streams[3 + i * 4] = Console.OpenStandardInput();
             }
         }
 
         [Benchmark]
         public void OpenStandardOutput()
         {
-            // We preserve copies of each opened Stream so that the perf area doesn't 
-            // cover the disposal of those streams.
-            const int innerIterations = 50;
-            Stream[] streams = new Stream[innerIterations * 4];
-            foreach (var iteration in Benchmark.Iterations)
+            Stream[] streams = _streams;
+            
+            for (int i = 0; i < StreamInnerIterations; i++)
             {
-                using (iteration.StartMeasurement())
-                {
-                    for (int i = 0; i < innerIterations; i++)
-                    {
-                        streams[0 + i * 4] = Console.OpenStandardOutput(); streams[1 + i * 4] = Console.OpenStandardOutput();
-                        streams[2 + i * 4] = Console.OpenStandardOutput(); streams[3 + i * 4] = Console.OpenStandardOutput();
-                    }
-                }
-                foreach (Stream s in streams)
-                    s.Dispose();
+                streams[0 + i * 4] = Console.OpenStandardOutput(); streams[1 + i * 4] = Console.OpenStandardOutput();
+                streams[2 + i * 4] = Console.OpenStandardOutput(); streams[3 + i * 4] = Console.OpenStandardOutput();
             }
         }
 
         [Benchmark]
         public void OpenStandardError()
         {
-            // We preserve copies of each opened Stream so that the perf area doesn't 
-            // cover the disposal of those streams.
-            const int innerIterations = 50;
-            Stream[] streams = new Stream[innerIterations * 4];
-            foreach (var iteration in Benchmark.Iterations)
+            Stream[] streams = _streams;
+            
+            for (int i = 0; i < StreamInnerIterations; i++)
             {
-                using (iteration.StartMeasurement())
-                {
-                    for (int i = 0; i < innerIterations; i++)
-                    {
-                        streams[0 + i * 4] = Console.OpenStandardError(); streams[1 + i * 4] = Console.OpenStandardError();
-                        streams[2 + i * 4] = Console.OpenStandardError(); streams[3 + i * 4] = Console.OpenStandardError();
-                    }
-                }
-                foreach (Stream s in streams)
-                    s.Dispose();
+                streams[0 + i * 4] = Console.OpenStandardError(); streams[1 + i * 4] = Console.OpenStandardError();
+                streams[2 + i * 4] = Console.OpenStandardError(); streams[3 + i * 4] = Console.OpenStandardError();
             }
         }
 
         [Benchmark]
         public void ForegroundColor()
         {
-            const int innerIterations = 1000;
-            foreach (var iteration in Benchmark.Iterations)
+            for (int i = 0; i < ColorInnerIterations; i++)
             {
-                using (iteration.StartMeasurement())
-                {
-                    for (int i = 0; i < innerIterations; i++)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.ForegroundColor = ConsoleColor.Cyan; Console.ForegroundColor = ConsoleColor.DarkBlue;
-                        Console.ForegroundColor = ConsoleColor.DarkGray; Console.ForegroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.DarkGreen; Console.ForegroundColor = ConsoleColor.White;
-                    }
-                }
+                Console.ForegroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Cyan; Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.ForegroundColor = ConsoleColor.DarkGray; Console.ForegroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.DarkGreen; Console.ForegroundColor = ConsoleColor.White;
             }
-            Console.ResetColor();
         }
+        
+        [GlobalCleanup(Target = nameof(ForegroundColor))]
+        public void ForegroundColorCleanup() => Console.ResetColor();
 
         [Benchmark]
         public void BackgroundColor()
         {
-            const int innerIterations = 1000;
-            foreach (var iteration in Benchmark.Iterations)
+            for (int i = 0; i < ColorInnerIterations; i++)
             {
-                using (iteration.StartMeasurement())
-                {
-                    for (int i = 0; i < innerIterations; i++)
-                    {
-                        Console.BackgroundColor = ConsoleColor.Black; Console.BackgroundColor = ConsoleColor.Blue;
-                        Console.BackgroundColor = ConsoleColor.Cyan; Console.BackgroundColor = ConsoleColor.DarkBlue;
-                        Console.BackgroundColor = ConsoleColor.DarkGray; Console.BackgroundColor = ConsoleColor.Red;
-                        Console.BackgroundColor = ConsoleColor.DarkGreen; Console.BackgroundColor = ConsoleColor.White;
-                    }
-                }
+                Console.BackgroundColor = ConsoleColor.Black; Console.BackgroundColor = ConsoleColor.Blue;
+                Console.BackgroundColor = ConsoleColor.Cyan; Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.BackgroundColor = ConsoleColor.DarkGray; Console.BackgroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.DarkGreen; Console.BackgroundColor = ConsoleColor.White;
             }
-            Console.ResetColor();
-}
+        }
+
+        [GlobalCleanup(Target = nameof(BackgroundColor))]
+        public void BackgroundColorCleanup() => Console.ResetColor();
 
         [Benchmark]
         public void ResetColor()
         {
-            const int innerIterations = 1000;
-            foreach (var iteration in Benchmark.Iterations)
+            for (int i = 0; i < ColorInnerIterations; i++)
             {
-                using (iteration.StartMeasurement())
-                {
-                    for (int i = 0; i < innerIterations; i++)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed; Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ResetColor();
-                        Console.ForegroundColor = ConsoleColor.DarkRed; Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ResetColor();
-                        Console.ForegroundColor = ConsoleColor.DarkRed; Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ResetColor();
-                        Console.ForegroundColor = ConsoleColor.DarkRed; Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ResetColor();
-                    }
-                }
+                Console.ForegroundColor = ConsoleColor.DarkRed; Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkRed; Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkRed; Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkRed; Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ResetColor();
             }
         }
     }
