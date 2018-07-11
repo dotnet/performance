85f98c1dcd55aacc685c22ee18a80dd28f8455df
Adam Sitnik <adam.sitnik@gmail.com>
2018-07-11 21:00:24 +0200

    ctor given size all collections benchmarks

---
+++ src/benchmarks/corefx/System.Collections/CtorGivenSize.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Benchmarks;
+
+namespace System.Collections
+{
+    [BenchmarkCategory(Categories.CoreFX, Categories.Collections)]
+    [GenericTypeArguments(typeof(int), typeof(int))] // value type
+    [GenericTypeArguments(typeof(string), typeof(string))] // reference type
+    public class CtorGiventSize<TKey, TValue>
+    {
+        private const int ConcurrencyLevel = 4;
+        
+        [Params(100)]
+        public int Size;
+        
+        [Benchmark]
+        public TKey[] Array() => new TKey[Size];
+
+        [Benchmark]
+        public List<TKey> List() => new List<TKey>(Size);
+
+#if !NET461 // API added in .NET Core 2.0
+        [Benchmark]
+        public HashSet<TKey> HashSet() => new HashSet<TKey>(Size);
+#endif
+        [Benchmark]
+        public Dictionary<TKey, TValue> Dictionary() => new Dictionary<TKey, TValue>(Size);
+
+        [Benchmark]
+        public Queue<TKey> Queue() => new Queue<TKey>(Size);
+
+        [Benchmark]
+        public Stack<TKey> Stack() => new Stack<TKey>(Size);
+
+        [Benchmark]
+        public SortedList<TKey, TValue> SortedList() => new SortedList<TKey, TValue>(Size);
+
+        [Benchmark]
+        public ConcurrentDictionary<TKey, TValue> ConcurrentDictionary() => new ConcurrentDictionary<TKey, TValue>(ConcurrencyLevel, Size);
+    }
+}
---
+++ src/benchmarks/corefx/System.Collections/CtorGivenSizeNonGeneric.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace System.Collections
+{
+    public class CtorGivenSizeNonGeneric
+    {
+        [Params(100)]
+        public int Size;
+        
+        [Benchmark]
+        public ArrayList ArrayList() => new ArrayList(Size);
+
+        [Benchmark]
+        public Hashtable Hashtable() => new Hashtable(Size);
+
+        [Benchmark]
+        public Queue Queue() => new Queue(Size);
+
+        [Benchmark]
+        public Stack Stack() => new Stack(Size);
+
+        [Benchmark]
+        public SortedList SortedList() => new SortedList(Size);
+    }
+}
