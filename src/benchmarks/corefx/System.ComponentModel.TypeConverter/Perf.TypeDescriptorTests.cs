7d69ea7e3497a345b3a4902abbd624fba8a8281b
Adam Sitnik <adam.sitnik@gmail.com>
2018-07-11 13:09:03 +0200

    change benchmark name and remove inner iterations to use BDN features

--- src/benchmarks/corefx/System.ComponentModel.TypeConverter/Perf.TypeDescriptorTests.cs
+++ src/benchmarks/corefx/System.ComponentModel.TypeConverter/Perf.TypeDescriptorTests.cs
@@ -9,27 +9,17 @@ namespace System.ComponentModel.Tests
     public class Perf_TypeDescriptorTests
     {
         [Benchmark]
-        [Arguments(typeof(int), typeof(Int32Converter))] // primitive
-        [Arguments(typeof(int?), typeof(NullableConverter))] // nullable primitive
-        [Arguments(typeof(Enum), typeof(EnumConverter))] // enum
-        [Arguments(typeof(SomeEnum), typeof(EnumConverter))] // custom enum
-        [Arguments(typeof(Guid), typeof(GuidConverter))] // built-in value type
-        [Arguments(typeof(SomeValueType?), typeof(NullableConverter))] // nullable custom value type
-        [Arguments(typeof(string), typeof(StringConverter))] // built-in reference type
-        [Arguments(typeof(ClassWithNoConverter), typeof(TypeConverter))] // no converter
-        [Arguments(typeof(DerivedClass), typeof(DerivedClassConverter))] // derived class converter
-        [Arguments(typeof(IDerived), typeof(IBaseConverter))] // derived interface
-        [Arguments(typeof(ClassIDerived), typeof(IBaseConverter))] // class which implements derived interface 
-        public TypeConverter GetConverter(Type typeToConvert, Type expectedConverter) // the expectedConverter argument is not used anymore, but kept to remain BenchView ID, do NOT remove
-        {
-            TypeConverter converter = default;
-            
-            for (int i = 0; i < 100; i++)
-            {
-                converter = TypeDescriptor.GetConverter(typeToConvert);
-            }
-
-            return converter;
-        }
+        [Arguments(typeof(int))] // primitive
+        [Arguments(typeof(int?))] // nullable primitive
+        [Arguments(typeof(Enum))] // enum
+        [Arguments(typeof(SomeEnum))] // custom enum
+        [Arguments(typeof(Guid))] // built-in value type
+        [Arguments(typeof(SomeValueType?))] // nullable custom value type
+        [Arguments(typeof(string))] // built-in reference type
+        [Arguments(typeof(ClassWithNoConverter))] // no converter
+        [Arguments(typeof(DerivedClass))] // derived class converter
+        [Arguments(typeof(IDerived))] // derived interface
+        [Arguments(typeof(ClassIDerived))] // class which implements derived interface 
+        public TypeConverter GetConverter(Type typeToConvert) => TypeDescriptor.GetConverter(typeToConvert);
     }
 }
