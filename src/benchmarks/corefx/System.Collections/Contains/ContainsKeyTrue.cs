aff1094c2db2f74b58ee9b49d692404fad74e0d8
Adam Sitnik <adam.sitnik@gmail.com>
2018-07-12 13:31:54 +0200

    ContainsKey for all generic collections

---
+++ src/benchmarks/corefx/System.Collections/Contains/ContainsKeyTrue.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using Benchmarks;
+using Helpers;
+
+namespace System.Collections
+{
+    [BenchmarkCategory(Categories.CoreFX, Categories.Collections, Categories.GenericCollections)]
+    [GenericTypeArguments(typeof(int), typeof(int))] // value type
+    [GenericTypeArguments(typeof(string), typeof(string))] // reference type
+    public class ContainsKeyTrue<TKey, TValue>
+    {
+        private TKey[] _array;
+        private Dictionary<TKey, TValue> _source;
+        
+        private Dictionary<TKey, TValue> _dictionary;
+        private SortedList<TKey, TValue> _sortedlist;
+        private SortedDictionary<TKey, TValue> _sorteddictionary;
+        private ConcurrentDictionary<TKey, TValue> _concurrentdictionary;
+        private ImmutableDictionary<TKey, TValue> _immutabledictionary;
+        private ImmutableSortedDictionary<TKey, TValue> _immutablesorteddictionary;
+
+        [Params(100)]
+        public int Size;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _array = UniqueValuesGenerator.GenerateArray<TKey>(Size);
+            _source = _array.ToDictionary(item => item, item => (TValue)(object)item);
+            _dictionary = new Dictionary<TKey, TValue>(_source);
+            _sortedlist = new SortedList<TKey, TValue>(_source);
+            _sorteddictionary = new SortedDictionary<TKey, TValue>(_source);
+            _concurrentdictionary = new ConcurrentDictionary<TKey, TValue>(_source);
+            _immutabledictionary = Immutable.ImmutableDictionary.CreateRange<TKey, TValue>(_source);
+            _immutablesorteddictionary = Immutable.ImmutableSortedDictionary.CreateRange<TKey, TValue>(_source);
+        }
+
+        [Benchmark]
+        public bool Dictionary()
+        {
+            bool result = default;
+            var collection = _dictionary;
+            var found = _array;
+            for (int i = 0; i < found.Length; i++)
+                result = collection.ContainsKey(found[i]);
+            return result;
+        }
+
+        [Benchmark]
+        public bool SortedList()
+        {
+            bool result = default;
+            var collection = _sortedlist;
+            var found = _array;
+            for (int i = 0; i < found.Length; i++)
+                result = collection.ContainsKey(found[i]);
+            return result;
+        }
+
+        [Benchmark]
+        public bool SortedDictionary()
+        {
+            bool result = default;
+            var collection = _sorteddictionary;
+            var found = _array;
+            for (int i = 0; i < found.Length; i++)
+                result = collection.ContainsKey(found[i]);
+            return result;
+        }
+
+        [Benchmark]
+        public bool ConcurrentDictionary()
+        {
+            bool result = default;
+            var collection = _concurrentdictionary;
+            var found = _array;
+            for (int i = 0; i < found.Length; i++)
+                result = collection.ContainsKey(found[i]);
+            return result;
+        }
+
+        [Benchmark]
+        public bool ImmutableDictionary()
+        {
+            bool result = default;
+            var collection = _immutabledictionary;
+            var found = _array;
+            for (int i = 0; i < found.Length; i++)
+                result = collection.ContainsKey(found[i]);
+            return result;
+        }
+
+        [Benchmark]
+        public bool ImmutableSortedDictionary()
+        {
+            bool result = default;
+            var collection = _immutablesorteddictionary;
+            var found = _array;
+            for (int i = 0; i < found.Length; i++)
+                result = collection.ContainsKey(found[i]);
+            return result;
+        }
+    }
+}
