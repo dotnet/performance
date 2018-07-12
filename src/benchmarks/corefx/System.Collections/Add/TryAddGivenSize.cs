a95457695cdca2937e489a3c7cd587569ab01111
Adam Sitnik <adam.sitnik@gmail.com>
2018-07-12 14:44:39 +0200

    try add benchmarks

---
+++ src/benchmarks/corefx/System.Collections/Add/TryAddGivenSize.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Collections.Concurrent;
+using BenchmarkDotNet.Attributes;
+using Benchmarks;
+using Helpers;
+
+namespace System.Collections
+{
+    [BenchmarkCategory(Categories.CoreFX, Categories.Collections, Categories.GenericCollections)]
+    [GenericTypeArguments(typeof(int))] // value type
+    [GenericTypeArguments(typeof(string))] // reference type
+    public class TryAddGiventSize<T>
+    {
+        private T[] _values;
+
+        [Params(100)]
+        public int Count;
+
+        [GlobalSetup]
+        public void Setup() => _values = UniqueValuesGenerator.GenerateArray<T>(Count);
+
+#if !NET461 // API added in .NET Core 2.0
+        [Benchmark]
+        public Dictionary<T, T> Dictionary()
+        {
+            var result = new Dictionary<T, T>(Count);
+            var values = _values;
+            for(int i = 0; i < values.Length; i++)
+                result.TryAdd(values[i], values[i]);
+            return result;
+        }
+#endif
+
+        [Benchmark]
+        public ConcurrentDictionary<T, T> ConcurrentDictionary()
+        {
+            var result = new ConcurrentDictionary<T, T>(concurrencyLevel: 4, capacity: Count);
+            var values = _values;
+            for(int i = 0; i < values.Length; i++)
+                result.TryAdd(values[i], values[i]);
+            return result;
+        }
+    }
+}
