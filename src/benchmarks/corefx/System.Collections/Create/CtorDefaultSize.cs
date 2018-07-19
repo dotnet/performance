fb87ea39b2efc6fce0585c078ae64691b33513b1
Adam Sitnik <adam.sitnik@gmail.com>
2018-07-19 16:30:46 +0200

    remove the array benchmark

--- src/benchmarks/corefx/System.Collections/Create/CtorDefaultSize.cs
+++ src/benchmarks/corefx/System.Collections/Create/CtorDefaultSize.cs
@@ -11,9 +11,6 @@ namespace System.Collections
     [GenericTypeArguments(typeof(string))] // reference type
     public class CtorDefaultSize<T>
     {
-        [Benchmark] // we keep this benchmark to compare the cost of creating array vs cost of array wrappers (List, Queue, Stack)
-        public T[] Array() => new T[4]; // array has no default size, List has = 4 so I decided to use 4 here
-
         [Benchmark]
         public List<T> List() => new List<T>();
 
