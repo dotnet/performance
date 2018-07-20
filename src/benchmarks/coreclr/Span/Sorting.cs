a84b4b3e00b6a529e6afd9745cebfc67eb233ec9
Adam Sitnik <adam.sitnik@gmail.com>
2018-07-20 18:23:54 +0200

    cleanup Sorting benchmarks

---
+++ src/benchmarks/coreclr/Span/Sorting.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using Benchmarks;
+
+namespace Span
+{
+    [BenchmarkCategory(Categories.CoreCLR)]
+    [InvocationCount(InvocationsPerIteration)]
+    public class Sorting
+    {
+        private const int InvocationsPerIteration = 1000;
+
+        [Params(Utils.DefaultCollectionSize)]
+        public int Size;
+
+        private int _iterationIndex = 0;
+        private int[] _values;
+        
+        private int[][] _arrays;
+        
+        [GlobalSetup]
+        public void Setup() => _values = UniqueValuesGenerator.GenerateArray<int>(Size);
+        
+        [IterationSetup]
+        public void SetupSpanIteration() => Utils.FillArrays(ref _arrays, InvocationsPerIteration, _values);
+        
+        [IterationCleanup]
+        public void CleanupIteration() => _iterationIndex = 0; // after every iteration end we set the index to 0
+        
+        [BenchmarkCategory(Categories.Span)]
+        [Benchmark]
+        public void QuickSortSpan() => TestQuickSortSpan(new Span<int>(_arrays[_iterationIndex++]));
+
+        [BenchmarkCategory(Categories.Span)]
+        [Benchmark]
+        public void BubbleSortSpan() => TestBubbleSortSpan(new Span<int>(_arrays[_iterationIndex++]));
+
+        [Benchmark]
+        public void QuickSortArray() => TestQuickSortArray(_arrays[_iterationIndex++], 0, Size);
+
+        [Benchmark]
+        public void BubbleSortArray() => TestBubbleSortArray(_arrays[_iterationIndex++]);
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void TestQuickSortArray(int[] data, int lo, int hi)
+        {
+            if (lo >= hi)
+            {
+                return;
+            }
+
+            int i, j;
+            int pivot, temp;
+            for (i = lo, j = hi, pivot = data[hi]; i < j;)
+            {
+                while (i < j && data[i] <= pivot)
+                {
+                    ++i;
+                }
+                while (j > i && data[j] >= pivot)
+                {
+                    --j;
+                }
+                if (i < j)
+                {
+                    temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                }
+            }
+            if (i != hi)
+            {
+                temp = data[i];
+                data[i] = pivot;
+                data[hi] = temp;
+            }
+
+            TestQuickSortArray(data, lo, i - 1);
+            TestQuickSortArray(data, i + 1, hi);
+        }
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void TestQuickSortSpan(Span<int> data)
+        {
+            if (data.Length <= 1)
+            {
+                return;
+            }
+
+            int lo = 0;
+            int hi = data.Length - 1;
+            int i, j;
+            int pivot, temp;
+            for (i = lo, j = hi, pivot = data[hi]; i < j;)
+            {
+                while (i < j && data[i] <= pivot)
+                {
+                    ++i;
+                }
+                while (j > i && data[j] >= pivot)
+                {
+                    --j;
+                }
+                if (i < j)
+                {
+                    temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                }
+            }
+            if (i != hi)
+            {
+                temp = data[i];
+                data[i] = pivot;
+                data[hi] = temp;
+            }
+
+            TestQuickSortSpan(data.Slice(0, i));
+            TestQuickSortSpan(data.Slice(i + 1));
+        }
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void TestBubbleSortArray(int[] data)
+        {
+            bool swap;
+            int temp;
+            int n = data.Length - 1;
+            do
+            {
+                swap = false;
+                for (int i = 0; i < n; i++)
+                {
+                    if (data[i] > data[i + 1])
+                    {
+                        temp = data[i];
+                        data[i] = data[i + 1];
+                        data[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+                --n;
+            }
+            while (swap);
+        }
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void TestBubbleSortSpan(Span<int> span)
+        {
+            bool swap;
+            int temp;
+            int n = span.Length - 1;
+            do
+            {
+                swap = false;
+                for (int i = 0; i < n; i++)
+                {
+                    if (span[i] > span[i + 1])
+                    {
+                        temp = span[i];
+                        span[i] = span[i + 1];
+                        span[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+                --n;
+            }
+            while (swap);
+        }
+    }
+}
--- src/benchmarks/coreclr/Span/SpanBench.cs
+++ src/benchmarks/coreclr/Span/SpanBench.cs
@@ -22,8 +22,6 @@ namespace Span
         private string[] strings;
         private string randomString;
 
-        private TestClass<byte> testClassByte; 
-        private TestClass<string> testClassString; 
 
         [GlobalSetup]
         public void Setup()
@@ -32,45 +30,9 @@ namespace Span
             ints = new int[length];
             strings = new string[length];
             randomString = GetRandomString();
-            testClassByte = CreateTestClass<byte>();
-            testClassString = CreateTestClass<string>();
         }
-
-        private TestClass<T> CreateTestClass<T>()
-        {
-            TestClass<T> testClass = new TestClass<T>();
-            testClass.C0 = new T[length];
-            return testClass;
-        }
-
-        class Destination<T>
-        {
-            public T[] array = new T[SpanBench.length]; // always more than length
-            
-            public static Destination<T> Instance = new Destination<T>();
-        }
-
         // Helpers
         #region Helpers
-        
-        [StructLayout(LayoutKind.Sequential)]
-        public sealed class TestClass<T>
-        {
-            private double _d;
-            public T[] C0;
-        }
-
-        // Helper for the sort tests to get some pseudo-random input
-        static int[] GetUnsortedData(int length)
-        {
-            int[] unsortedData = new int[length];
-            Random r = new Random(42);
-            for (int i = 0; i < unsortedData.Length; ++i)
-            {
-                unsortedData[i] = r.Next();
-            }
-            return unsortedData;
-        }
         
         private string GetRandomString()
         {
@@ -87,162 +49,6 @@ namespace Span
             return s;
         }
         #endregion // helpers
-
-        // Tests that implement some vary basic algorithms (fill/sort) over spans and arrays
-        #region Algorithm tests
-        #region TestQuickSortSpan
-        
-        int[] _unsortedData;
-        
-        [IterationSetup(Target = nameof(QuickSortSpan) + "," + nameof(BubbleSortSpan) + "," + nameof(QuickSortArray) + "," + nameof(BubbleSortArray))]
-        public void SetupSort() => _unsortedData = GetUnsortedData(length); 
-        
-        [Benchmark]
-        public void QuickSortSpan() => TestQuickSortSpan(_unsortedData);
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        static void TestQuickSortSpan(Span<int> data)
-        {
-            if (data.Length <= 1)
-            {
-                return;
-            }
-
-            int lo = 0;
-            int hi = data.Length - 1;
-            int i, j;
-            int pivot, temp;
-            for (i = lo, j = hi, pivot = data[hi]; i < j;)
-            {
-                while (i < j && data[i] <= pivot)
-                {
-                    ++i;
-                }
-                while (j > i && data[j] >= pivot)
-                {
-                    --j;
-                }
-                if (i < j)
-                {
-                    temp = data[i];
-                    data[i] = data[j];
-                    data[j] = temp;
-                }
-            }
-            if (i != hi)
-            {
-                temp = data[i];
-                data[i] = pivot;
-                data[hi] = temp;
-            }
-
-            TestQuickSortSpan(data.Slice(0, i));
-            TestQuickSortSpan(data.Slice(i + 1));
-        }
-        #endregion
-
-        #region TestBubbleSortSpan
-        [Benchmark]
-        public void BubbleSortSpan() => TestBubbleSortSpan(_unsortedData);
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        static void TestBubbleSortSpan(Span<int> span)
-        {
-            bool swap;
-            int temp;
-            int n = span.Length - 1;
-            do
-            {
-                swap = false;
-                for (int i = 0; i < n; i++)
-                {
-                    if (span[i] > span[i + 1])
-                    {
-                        temp = span[i];
-                        span[i] = span[i + 1];
-                        span[i + 1] = temp;
-                        swap = true;
-                    }
-                }
-                --n;
-            }
-            while (swap);
-        }
-        #endregion
-
-        #region TestQuickSortArray
-        [Benchmark]
-        public void QuickSortArray() => TestQuickSortArray(_unsortedData, 0, _unsortedData.Length - 1);
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        static void TestQuickSortArray(int[] data, int lo, int hi)
-        {
-            if (lo >= hi)
-            {
-                return;
-            }
-
-            int i, j;
-            int pivot, temp;
-            for (i = lo, j = hi, pivot = data[hi]; i < j;)
-            {
-                while (i < j && data[i] <= pivot)
-                {
-                    ++i;
-                }
-                while (j > i && data[j] >= pivot)
-                {
-                    --j;
-                }
-                if (i < j)
-                {
-                    temp = data[i];
-                    data[i] = data[j];
-                    data[j] = temp;
-                }
-            }
-            if (i != hi)
-            {
-                temp = data[i];
-                data[i] = pivot;
-                data[hi] = temp;
-            }
-
-            TestQuickSortArray(data, lo, i - 1);
-            TestQuickSortArray(data, i + 1, hi);
-        }
-        #endregion
-
-        #region TestBubbleSortArray
-        [Benchmark]
-        public void BubbleSortArray() => TestBubbleSortArray(_unsortedData);
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        static void TestBubbleSortArray(int[] data)
-        {
-            bool swap;
-            int temp;
-            int n = data.Length - 1;
-            do
-            {
-                swap = false;
-                for (int i = 0; i < n; i++)
-                {
-                    if (data[i] > data[i + 1])
-                    {
-                        temp = data[i];
-                        data[i] = data[i + 1];
-                        data[i + 1] = temp;
-                        swap = true;
-                    }
-                }
-                --n;
-            }
-            while (swap);
-        }
-        #endregion
-
-        #endregion // Algorithm tests
 
         [Benchmark]
         public ref byte TestMemoryMarshalGetReferenceByte() => ref MemoryMarshal.GetReference(new Span<byte>(bytes));
