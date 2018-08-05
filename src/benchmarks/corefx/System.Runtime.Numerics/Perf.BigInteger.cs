52390c1764bc50fff336ca43e5a735135c53fc28
Adam Sitnik <adam.sitnik@gmail.com>
2018-08-05 12:49:53 +0200

    merge BigInteger benchmarks into single file

--- src/benchmarks/corefx/System.Runtime.Numerics/Perf.BigInteger.cs
+++ src/benchmarks/corefx/System.Runtime.Numerics/Perf.BigInteger.cs
@@ -10,13 +10,91 @@ namespace System.Numerics.Tests
 {
     public class Perf_BigInteger
     {
-        public class BigIntegerDataWrapper
+        public IEnumerable<object> NumberStrings()
+        {
+            yield return new BigIntegerData("123");
+            yield return new BigIntegerData(int.MinValue.ToString());
+            yield return new BigIntegerData(string.Concat(Enumerable.Repeat("1234567890", 20)));
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(NumberStrings))]
+        public BigInteger Ctor_ByteArray(BigIntegerData numberString) // the argument name is "numberString" to preserve the benchmark ID
+            => new BigInteger(numberString.Bytes);
+
+        [Benchmark]
+        [ArgumentsSource(nameof(NumberStrings))]
+        public byte[] ToByteArray(BigIntegerData numberString) 
+            => numberString.Value.ToByteArray();
+
+        [Benchmark]
+        [ArgumentsSource(nameof(NumberStrings))]
+        public BigInteger Parse(BigIntegerData numberString) 
+            => BigInteger.Parse(numberString.Text);
+        
+        public IEnumerable<object> ValuesSameSize()
+        {
+            yield return new BigIntegers(new[] { 16, 16 });
+            yield return new BigIntegers(new[] { 1024, 1024 });
+            yield return new BigIntegers(new[] { 65536, 65536 });
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ValuesSameSize))]
+        public BigInteger Add(BigIntegers arguments)
+            => BigInteger.Add(arguments.Left, arguments.Right);
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ValuesSameSize))]
+        public BigInteger Subtract(BigIntegers arguments)
+            => BigInteger.Subtract(arguments.Left, arguments.Right);
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ValuesSameSize))]
+        public BigInteger Multiply(BigIntegers arguments)
+            => BigInteger.Multiply(arguments.Left, arguments.Right);
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ValuesSameSize))]
+        public BigInteger GreatestCommonDivisor(BigIntegers arguments)
+            => BigInteger.GreatestCommonDivisor(arguments.Left, arguments.Right);
+
+        public IEnumerable<object> ValuesHalfSize()
+        {
+            yield return new BigIntegers(new[] { 16, 16 / 2 });
+            yield return new BigIntegers(new[] { 1024, 1024 / 2 });
+            yield return new BigIntegers(new[] { 65536, 65536 / 2 });
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ValuesHalfSize))]
+        public BigInteger Divide(BigIntegers arguments)
+            => BigInteger.Divide(arguments.Left, arguments.Right);
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ValuesHalfSize))]
+        public BigInteger Remainder(BigIntegers arguments)
+            => BigInteger.Remainder(arguments.Left, arguments.Right);
+
+        public IEnumerable<object> ModPowValues()
+        {
+            yield return new BigIntegers(new[] { 16, 16, 16 });
+            yield return new BigIntegers(new[] { 1024, 1024, 64 });
+            yield return new BigIntegers(new[] { 16384, 16384, 64 });
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ModPowValues))]
+        public BigInteger ModPow(BigIntegers arguments)
+            => BigInteger.ModPow(arguments.Left, arguments.Right, arguments.Other);
+
+        public class BigIntegerData
         {
             public string Text { get; }
             public byte[] Bytes { get; }
             public BigInteger Value { get; }
 
-            public BigIntegerDataWrapper(string numberString)
+            public BigIntegerData(string numberString)
             {
                 Text = numberString;
                 Value = BigInteger.Parse(numberString);
@@ -25,27 +103,57 @@ namespace System.Numerics.Tests
 
             public override string ToString() => Text;
         }
-
-        public static IEnumerable<object> NumberStrings()
+        
+        public class BigIntegers
         {
-            yield return new BigIntegerDataWrapper("123");
-            yield return new BigIntegerDataWrapper(int.MinValue.ToString());
-            yield return new BigIntegerDataWrapper(string.Concat(Enumerable.Repeat("1234567890", 20)));
+            private readonly int[] _bitCounts;
+            
+            public BigInteger Left { get; }
+            public BigInteger Right { get; }
+            public BigInteger Other { get; }
+
+            public BigIntegers(int[] bitCounts)
+            {
+                _bitCounts = bitCounts;
+                var values = GenerateBigIntegers(bitCounts);
+
+                Left = values[0];
+                Right = values[1];
+                Other = values.Length == 3 ? values[2] : BigInteger.Zero;
+            }
+
+            public override string ToString() => $"{string.Join(",", _bitCounts)} bits";
+
+            private BigInteger[] GenerateBigIntegers(int[] bitCounts)
+            {
+                Random random = new Random(1138); // we always use the same seed to have repeatable results!
+
+                BigInteger[] result = new BigInteger[bitCounts.Length];
+
+                for (int i = 0; i < bitCounts.Length; i++)
+                    result[i] = CreateRandomInteger(random, bitCounts[i]);
+
+                return result;
+            }
+
+            private static BigInteger CreateRandomInteger(Random random, int bits)
+            {
+                byte[] value = new byte[(bits + 8) / 8];
+                BigInteger result = BigInteger.Zero;
+
+                while (result.IsZero)
+                {
+                    random.NextBytes(value);
+
+                    // ensure actual bit count (remaining bits not set)
+                    // ensure positive value (highest-order bit not set)
+                    value[value.Length - 1] &= (byte) (0xFF >> 8 - bits % 8);
+
+                    result = new BigInteger(value);
+                }
+
+                return result;
+            }
         }
-
-        [Benchmark]
-        [ArgumentsSource(nameof(NumberStrings))]
-        public BigInteger Ctor_ByteArray(BigIntegerDataWrapper numberString) // the argument name is "numberString" to preserve the benchmark ID
-            => new BigInteger(numberString.Bytes);
-
-        [Benchmark]
-        [ArgumentsSource(nameof(NumberStrings))]
-        public byte[] ToByteArray(BigIntegerDataWrapper numberString) 
-            => numberString.Value.ToByteArray();
-
-        [Benchmark]
-        [ArgumentsSource(nameof(NumberStrings))]
-        public BigInteger Parse(BigIntegerDataWrapper numberString) 
-            => BigInteger.Parse(numberString.Text);
     }
 }
