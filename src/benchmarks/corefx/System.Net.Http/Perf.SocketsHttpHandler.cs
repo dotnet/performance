e5e52d925e9176be4dd6bcef5ff5acec3471d5a2
Adam Sitnik <adam.sitnik@gmail.com>
2018-08-06 16:37:21 +0200

    port

--- src/benchmarks/corefx/System.Net.Http/Perf.SocketsHttpHandler.cs
+++ src/benchmarks/corefx/System.Net.Http/Perf.SocketsHttpHandler.cs
@@ -2,112 +2,121 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
+#if NETCOREAPP2_1 // the benchmark uses .NET Core 2.1 APIs (which consume ReadOnlySpan)
+
 using System.IO;
-using System.Linq;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Authentication;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Xunit.Performance;
-using Xunit;
+using BenchmarkDotNet.Attributes;
 
 namespace System.Net.Http.Tests
 {
-    public sealed class SocketsHttpHandlerPerfTest
+    public class SocketsHttpHandlerPerfTest
     {
         const int InnerIterationCount = 1000;
 
-        public static IEnumerable<object[]> Get_MemberData() =>
-            from ssl in new[] { false, true }
-            from chunkedResponse in new[] { false, true }
-            from responseLength in new[] { 1, 100_000 }
-            select new object[] { ssl, chunkedResponse, responseLength };
+        // the field names start with lowercase to keep to benchmark ID! do not change it
+        [Params(false, true)] public bool ssl;
+        [Params(false, true)] public bool chunkedResponse;
+        [Params(1, 100_000)] public int responseLength;
 
-        [Benchmark(InnerIterationCount = InnerIterationCount)]
-        [MemberData(nameof(Get_MemberData))]
-        public async Task Get(bool ssl, bool chunkedResponse, int responseLength)
+        private System.Security.Cryptography.X509Certificates.X509Certificate2 _serverCert;
+        private Socket _listener;
+        private Task _serverTask;
+        private SocketsHttpHandler _handler;
+        private HttpMessageInvoker _invoker;
+        private HttpRequestMessage _request;
+
+        [GlobalSetup]
+        public void Setup()
         {
-            using (var serverCert = System.Net.Test.Common.Configuration.Certificates.GetServerCertificate())
-            using (var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            _serverCert = Test.Common.Configuration.Certificates.GetServerCertificate();
+            _listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            _listener.Bind(new IPEndPoint(IPAddress.Loopback, 0));
+            _listener.Listen(int.MaxValue);
+            string responseText =
+                "HTTP/1.1 200 OK\r\n" + (chunkedResponse ?
+                $"Transfer-Encoding: chunked\r\n\r\n{responseLength.ToString("X")}\r\n{new string('a', responseLength)}\r\n0\r\n\r\n" :
+                $"Content-Length: {responseLength}\r\n\r\n{new string('a', responseLength)}");
+            ReadOnlyMemory<byte> responseBytes = Encoding.UTF8.GetBytes(responseText);
+
+            _serverTask = Task.Run(async () =>
             {
-                #region Server
-                listener.Bind(new IPEndPoint(IPAddress.Loopback, 0));
-                listener.Listen(int.MaxValue);
-                string responseText =
-                    "HTTP/1.1 200 OK\r\n" + (chunkedResponse ?
-                    $"Transfer-Encoding: chunked\r\n\r\n{responseLength.ToString("X")}\r\n{new string('a', responseLength)}\r\n0\r\n\r\n" :
-                    $"Content-Length: {responseLength}\r\n\r\n{new string('a', responseLength)}");
-                ReadOnlyMemory<byte> responseBytes = Encoding.UTF8.GetBytes(responseText);
-
-                Task serverTask = Task.Run(async () =>
+                try
                 {
-                    try
+                    while (true)
                     {
-                        while (true)
+                        using (Socket s = await _listener.AcceptAsync())
                         {
-                            using (Socket s = await listener.AcceptAsync())
+                            try
                             {
-                                try
+                                Stream stream = new NetworkStream(s);
+                                if (ssl)
                                 {
-                                    Stream stream = new NetworkStream(s);
-                                    if (ssl)
-                                    {
-                                        var sslStream = new SslStream(stream, false, delegate { return true; });
-                                        await sslStream.AuthenticateAsServerAsync(serverCert, false, SslProtocols.None, false);
-                                        stream = sslStream;
-                                    }
+                                    var sslStream = new SslStream(stream, false, delegate { return true; });
+                                    await sslStream.AuthenticateAsServerAsync(_serverCert, false, SslProtocols.None, false);
+                                    stream = sslStream;
+                                }
 
-                                    using (var reader = new StreamReader(stream, Encoding.ASCII, detectEncodingFromByteOrderMarks: false, bufferSize: 100))
+                                using (var reader = new StreamReader(stream, Encoding.ASCII, detectEncodingFromByteOrderMarks: false, bufferSize: 100))
+                                {
+                                    while (true)
                                     {
-                                        while (true)
-                                        {
-                                            while (!string.IsNullOrEmpty(await reader.ReadLineAsync()));
-                                            await stream.WriteAsync(responseBytes);
-                                        }
+                                        while (!string.IsNullOrEmpty(await reader.ReadLineAsync())) ;
+                                        await stream.WriteAsync(responseBytes);
                                     }
                                 }
-                                catch (SocketException e) when (e.SocketErrorCode == SocketError.ConnectionAborted) { }
                             }
-                        }
-                    }
-                    catch { }
-                });
-                #endregion
-
-                var ep = (IPEndPoint)listener.LocalEndPoint;
-                var uri = new Uri($"http{(ssl?"s":"")}://{ep.Address}:{ep.Port}/");
-                using (var handler = new SocketsHttpHandler())
-                using (var invoker = new HttpMessageInvoker(handler))
-                {
-                    if (ssl)
-                    {
-                        handler.SslOptions.RemoteCertificateValidationCallback = delegate { return true; };
-                    }
-
-                    var req = new HttpRequestMessage(HttpMethod.Get, uri);
-
-                    foreach (BenchmarkIteration iteration in Benchmark.Iterations)
-                    {
-                        using (iteration.StartMeasurement())
-                        {
-                            for (int i = 0; i < InnerIterationCount; i++)
-                            {
-                                using (HttpResponseMessage resp = await invoker.SendAsync(req, CancellationToken.None))
-                                using (Stream respStream = await resp.Content.ReadAsStreamAsync())
-                                {
-                                    await respStream.CopyToAsync(Stream.Null);
-                                }
-                            }
+                            catch (SocketException e) when (e.SocketErrorCode == SocketError.ConnectionAborted) { }
                         }
                     }
                 }
+                catch { }
+            });
 
-                listener.Dispose();
-                await serverTask;
+            var ep = (IPEndPoint)_listener.LocalEndPoint;
+            var uri = new Uri($"http{(ssl ? "s" : "")}://{ep.Address}:{ep.Port}/");
+            _handler = new SocketsHttpHandler();
+            _invoker = new HttpMessageInvoker(_handler);
+
+            if (ssl)
+            {
+                _handler.SslOptions.RemoteCertificateValidationCallback = delegate { return true; };
             }
+
+            _request = new HttpRequestMessage(HttpMethod.Get, uri);
+        }
+
+        [Benchmark]
+        public async Task Get()
+        {
+            var invoker = _invoker;
+            var req = _request;
+
+            for (int i = 0; i < InnerIterationCount; i++)
+            {
+                using (HttpResponseMessage resp = await invoker.SendAsync(req, CancellationToken.None))
+                using (Stream respStream = await resp.Content.ReadAsStreamAsync())
+                {
+                    await respStream.CopyToAsync(Stream.Null);
+                }
+            }
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            _invoker.Dispose();
+            _handler.Dispose();
+            _listener.Dispose();
+            _serverTask.GetAwaiter().GetResult();
+            _serverCert.Dispose();
         }
     }
 }
+#endif
