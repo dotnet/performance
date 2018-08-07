28ee45b451cce76235adbe0a1750ff67188ea7db
Adam Sitnik <adam.sitnik@gmail.com>
2018-08-07 16:14:29 +0200

    reduce the number of permutations! 300 -> 60 benchmarks

--- src/benchmarks/corefx/System.IO.Compression/CompressionStreamPerfTestBase.cs
+++ src/benchmarks/corefx/System.IO.Compression/CompressionStreamPerfTestBase.cs
@@ -38,21 +38,21 @@ namespace System.IO.Compression
 
         private IEnumerable<string> UncompressedTestFileNames()
         {
-            yield return "TestDocument.doc"; // 44.5 KB small test document with repeated paragraph
-            yield return "TestDocument.docx"; // 17.2 KB small test document with repeated paragraph
-            yield return "TestDocument.pdf"; // 199 KB small test document with repeated paragraph
-            yield return "TestDocument.txt"; // 21.1 KB small test document with repeated paragraph
-            yield return "alice29.txt"; // 145 KB, copy of "ALICE'S ADVENTURES IN WONDERLAND" book
-            yield return "asyoulik.txt"; // 122 KB, copy if "As You Like It" by William Shakespeare
-            yield return "cp.html"; // 24 KB, small HTML file
-            yield return "fields.c"; // 10.8 KB, 430 lines of C code
-            yield return "grammar.lsp"; // 3.63 KB, 90 lines of Lisp code
-            yield return "kennedy.xls"; // 0.98 MB, invalid excel file..
-            yield return "lcet10.txt"; // 409 KB, "The Project Gutenberg Etext of LOC WORKSHOP ON ELECTRONIC TEXTS", 7500 lines of text
-            yield return "plrabn12.txt"; // 460 KB, "Paradise Lost by John Milton", 10700 lines of text
-            yield return "ptt5"; // 501 KB, some binary content
-            yield return "sum"; // 37.3 KB, some binary content
-            yield return "xargs.1"; // 4.12 KB, output of --help of some Linux tool
+            // yield return "TestDocument.doc"; // 44.5 KB small test document with repeated paragraph
+            // yield return "TestDocument.docx"; // 17.2 KB small test document with repeated paragraph
+            yield return "TestDocument.pdf"; // 199 KB small test document with repeated paragraph, PDF are common
+            // yield return "TestDocument.txt"; // 21.1 KB small test document with repeated paragraph
+            yield return "alice29.txt"; // 145 KB, copy of "ALICE'S ADVENTURES IN WONDERLAND" book, an example of text file
+            // yield return "asyoulik.txt"; // 122 KB, copy if "As You Like It" by William Shakespeare
+            // yield return "cp.html"; // 24 KB, small HTML file
+            // yield return "fields.c"; // 10.8 KB, 430 lines of C code
+            // yield return "grammar.lsp"; // 3.63 KB, 90 lines of Lisp code
+            // yield return "kennedy.xls"; // 0.98 MB, invalid excel file..
+            // yield return "lcet10.txt"; // 409 KB, "The Project Gutenberg Etext of LOC WORKSHOP ON ELECTRONIC TEXTS", 7500 lines of text
+            // yield return "plrabn12.txt"; // 460 KB, "Paradise Lost by John Milton", 10700 lines of text
+            // yield return "ptt5"; // 501 KB, some binary content
+            yield return "sum"; // 37.3 KB, some binary content, an example of binary file
+            // yield return "xargs.1"; // 4.12 KB, output of --help of some Linux tool
         }
 
         [Benchmark]
