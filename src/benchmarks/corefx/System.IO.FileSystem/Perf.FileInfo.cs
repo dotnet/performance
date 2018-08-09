adb98f7dfc776c782b1c8b9a819b53ea66ce2da5
Adam Sitnik <adam.sitnik@gmail.com>
2018-08-09 22:18:41 +0200

    File system (#107)
    * copy
    
    * port 1:1
    
    * rewrite Perf_FileStream because one benchmark was checking 3 different things...
    
    * change the scaling so the benchmarks run in 2,5 minute, not 10
    
    * apply the scaling of the results to all the file benchmarks (if I had to do it for some of them I should do it for all of them)
    
    * remove old comment
    
    * make sure the recursive benchmarks does recursive thing ;)

---
+++ src/benchmarks/corefx/System.IO.FileSystem/Perf.FileInfo.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+
+namespace System.IO.Tests
+{
+    public class Perf_FileInfo
+    {
+        private readonly string _path = FileUtils.GetTestFilePath();
+
+        [Benchmark]
+        public FileInfo ctor_str() => new FileInfo(_path);
+    }
+}
