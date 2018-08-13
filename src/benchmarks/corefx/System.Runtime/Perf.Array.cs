25e960c258664bf20ed0dc691473a5f0bd783e13
Adam Sitnik <adam.sitnik@gmail.com>
2018-08-13 15:38:37 +0200

    port remaining Array benchmarks

--- src/benchmarks/corefx/System.Runtime/Perf.Array.cs
+++ src/benchmarks/corefx/System.Runtime/Perf.Array.cs
@@ -12,170 +12,134 @@ namespace System.Tests
         private static Array s_arr1;
         private static Array s_arr2;
         private static Array s_arr3;
+        private static Array _destinationArray;
+        private byte[][] _byteArrays;  
 
         private const int MAX_ARRAY_SIZE = 4096;
 
+        private const int OldSize = 42;
+        private const int NewSize = 41;
+        private const int ByteArraysCount = 2_000_000;
+
         private static readonly int s_DIM_1 = MAX_ARRAY_SIZE;
-        private static readonly int s_DIM_2 = (int)Math.Pow(MAX_ARRAY_SIZE, (1.0 / 2.0));
-        private static readonly int s_DIM_3 = (int)(Math.Pow(MAX_ARRAY_SIZE, (1.0 / 3.0)) + .001);
+        private static readonly int s_DIM_2 = (int) Math.Pow(MAX_ARRAY_SIZE, (1.0 / 2.0));
+        private static readonly int s_DIM_3 = (int) (Math.Pow(MAX_ARRAY_SIZE, (1.0 / 3.0)) + .001);
 
         [Benchmark]
-        public static void ArrayCreate1D()
+        public Array ArrayCreate1D() => Array.CreateInstance(typeof(int), s_DIM_1);
+
+        [Benchmark]
+        public Array ArrayCreate2D() => Array.CreateInstance(typeof(int), s_DIM_2, s_DIM_2);
+
+        [Benchmark]
+        public Array ArrayCreate3D() => Array.CreateInstance(typeof(int), s_DIM_3, s_DIM_3, s_DIM_3);
+
+        [GlobalSetup(Target = nameof(ArrayAssign1D))]
+        public void SetupArrayAssign1D() => s_arr1 = Array.CreateInstance(typeof(int), s_DIM_1);
+
+        [Benchmark]
+        public void ArrayAssign1D()
         {
-            foreach (var iteration in Benchmark.Iterations)
+            for (int j = 0; j < s_DIM_1; j++)
             {
-                using (iteration.StartMeasurement())
+                s_arr1.SetValue(j, j);
+                s_arr1.SetValue(j, j);
+                s_arr1.SetValue(j, j);
+                s_arr1.SetValue(j, j);
+                s_arr1.SetValue(j, j);
+                s_arr1.SetValue(j, j);
+                s_arr1.SetValue(j, j);
+                s_arr1.SetValue(j, j);
+                s_arr1.SetValue(j, j);
+                s_arr1.SetValue(j, j);
+            }
+        }
+
+        [GlobalSetup(Target = nameof(ArrayAssign2D))]
+        public void SetupArrayAssign2D() => s_arr2 = Array.CreateInstance(typeof(int), s_DIM_2, s_DIM_2);
+
+        [Benchmark]
+        public void ArrayAssign2D()
+        {
+            for (int j = 0; j < s_DIM_2; j++)
+            {
+                for (int k = 0; k < s_DIM_2; k++)
                 {
-                    s_arr1 = Array.CreateInstance(typeof(int), s_DIM_1);
+                    s_arr2.SetValue(j + k, j, k);
+                    s_arr2.SetValue(j + k, j, k);
+                    s_arr2.SetValue(j + k, j, k);
+                    s_arr2.SetValue(j + k, j, k);
+                    s_arr2.SetValue(j + k, j, k);
+                    s_arr2.SetValue(j + k, j, k);
+                    s_arr2.SetValue(j + k, j, k);
+                    s_arr2.SetValue(j + k, j, k);
+                    s_arr2.SetValue(j + k, j, k);
+                    s_arr2.SetValue(j + k, j, k);
                 }
             }
         }
 
-        [Benchmark]
-        public static void ArrayCreate2D()
-        {
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                using (iteration.StartMeasurement())
-                {
-                    s_arr2 = Array.CreateInstance(typeof(int), s_DIM_2, s_DIM_2);
-                }
-            }
-        }
+        [GlobalSetup(Target = nameof(ArrayAssign3D))]
+        public void SetupArrayAssign3D() => s_arr3 = Array.CreateInstance(typeof(int), s_DIM_3, s_DIM_3, s_DIM_3);
 
         [Benchmark]
-        public static void ArrayCreate3D()
+        public void ArrayAssign3D()
         {
-            foreach (var iteration in Benchmark.Iterations)
+            for (int j = 0; j < s_DIM_3; j++)
             {
-                using (iteration.StartMeasurement())
+                for (int k = 0; k < s_DIM_3; k++)
                 {
-                    s_arr3 = Array.CreateInstance(typeof(int), s_DIM_3, s_DIM_3, s_DIM_3);
-                }
-            }
-        }
-
-        [Benchmark]
-        public static void ArrayAssign1D()
-        {
-            s_arr1 = Array.CreateInstance(typeof(int), s_DIM_1);
-
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                using (iteration.StartMeasurement())
-                {
-                    for (int j = 0; j < s_DIM_1; j++)
+                    for (int l = 0; l < s_DIM_3; l++)
                     {
-                        s_arr1.SetValue(j, j);
-                        s_arr1.SetValue(j, j);
-                        s_arr1.SetValue(j, j);
-                        s_arr1.SetValue(j, j);
-                        s_arr1.SetValue(j, j);
-                        s_arr1.SetValue(j, j);
-                        s_arr1.SetValue(j, j);
-                        s_arr1.SetValue(j, j);
-                        s_arr1.SetValue(j, j);
-                        s_arr1.SetValue(j, j);
+                        s_arr3.SetValue(j + k + l, j, k, l);
+                        s_arr3.SetValue(j + k + l, j, k, l);
+                        s_arr3.SetValue(j + k + l, j, k, l);
+                        s_arr3.SetValue(j + k + l, j, k, l);
+                        s_arr3.SetValue(j + k + l, j, k, l);
+                        s_arr3.SetValue(j + k + l, j, k, l);
+                        s_arr3.SetValue(j + k + l, j, k, l);
+                        s_arr3.SetValue(j + k + l, j, k, l);
+                        s_arr3.SetValue(j + k + l, j, k, l);
+                        s_arr3.SetValue(j + k + l, j, k, l);
                     }
                 }
             }
         }
 
-        [Benchmark]
-        public static void ArrayAssign2D()
+        [GlobalSetup(Target = nameof(ArrayRetrieve1D))]
+        public void SetupArrayRetrieve1D()
         {
-            s_arr2 = Array.CreateInstance(typeof(int), s_DIM_2, s_DIM_2);
-
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                using (iteration.StartMeasurement())
-                {
-                    for (int j = 0; j < s_DIM_2; j++)
-                    {
-                        for (int k = 0; k < s_DIM_2; k++)
-                        {
-                            s_arr2.SetValue(j + k, j, k);
-                            s_arr2.SetValue(j + k, j, k);
-                            s_arr2.SetValue(j + k, j, k);
-                            s_arr2.SetValue(j + k, j, k);
-                            s_arr2.SetValue(j + k, j, k);
-                            s_arr2.SetValue(j + k, j, k);
-                            s_arr2.SetValue(j + k, j, k);
-                            s_arr2.SetValue(j + k, j, k);
-                            s_arr2.SetValue(j + k, j, k);
-                            s_arr2.SetValue(j + k, j, k);
-                        }
-                    }
-                }
-            }
-        }
-
-        [Benchmark]
-        public static void ArrayAssign3D()
-        {
-            s_arr3 = Array.CreateInstance(typeof(int), s_DIM_3, s_DIM_3, s_DIM_3);
-
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                using (iteration.StartMeasurement())
-                {
-                    for (int j = 0; j < s_DIM_3; j++)
-                    {
-                        for (int k = 0; k < s_DIM_3; k++)
-                        {
-                            for (int l = 0; l < s_DIM_3; l++)
-                            {
-                                s_arr3.SetValue(j + k + l, j, k, l);
-                                s_arr3.SetValue(j + k + l, j, k, l);
-                                s_arr3.SetValue(j + k + l, j, k, l);
-                                s_arr3.SetValue(j + k + l, j, k, l);
-                                s_arr3.SetValue(j + k + l, j, k, l);
-                                s_arr3.SetValue(j + k + l, j, k, l);
-                                s_arr3.SetValue(j + k + l, j, k, l);
-                                s_arr3.SetValue(j + k + l, j, k, l);
-                                s_arr3.SetValue(j + k + l, j, k, l);
-                                s_arr3.SetValue(j + k + l, j, k, l);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        [Benchmark]
-        public static void ArrayRetrieve1D()
-        {
-            int value;
             s_arr1 = Array.CreateInstance(typeof(int), s_DIM_1);
 
             for (int i = 0; i < s_DIM_1; i++)
                 s_arr1.SetValue(i, i);
-
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                using (iteration.StartMeasurement())
-                {
-                    for (int j = 0; j < s_DIM_1; j++)
-                    {
-                        value = (int)s_arr1.GetValue(j);
-                        value = (int)s_arr1.GetValue(j);
-                        value = (int)s_arr1.GetValue(j);
-                        value = (int)s_arr1.GetValue(j);
-                        value = (int)s_arr1.GetValue(j);
-                        value = (int)s_arr1.GetValue(j);
-                        value = (int)s_arr1.GetValue(j);
-                        value = (int)s_arr1.GetValue(j);
-                        value = (int)s_arr1.GetValue(j);
-                        value = (int)s_arr1.GetValue(j);
-                    }
-                }
-            }
         }
 
         [Benchmark]
-        public static void ArrayRetrieve2D()
+        public int ArrayRetrieve1D()
         {
-            int value;
+            int value = default;
+
+            for (int j = 0; j < s_DIM_1; j++)
+            {
+                value = (int) s_arr1.GetValue(j);
+                value = (int) s_arr1.GetValue(j);
+                value = (int) s_arr1.GetValue(j);
+                value = (int) s_arr1.GetValue(j);
+                value = (int) s_arr1.GetValue(j);
+                value = (int) s_arr1.GetValue(j);
+                value = (int) s_arr1.GetValue(j);
+                value = (int) s_arr1.GetValue(j);
+                value = (int) s_arr1.GetValue(j);
+                value = (int) s_arr1.GetValue(j);
+            }
+
+            return value;
+        }
+
+        [GlobalSetup(Target = nameof(ArrayRetrieve2D))]
+        public void SetupArrayRetrieve2D()
+        {
             s_arr2 = Array.CreateInstance(typeof(int), s_DIM_2, s_DIM_2);
 
             for (int i = 0; i < s_DIM_2; i++)
@@ -183,35 +147,36 @@ namespace System.Tests
                 for (int j = 0; j < s_DIM_2; j++)
                     s_arr2.SetValue(i + j, i, j);
             }
-
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                using (iteration.StartMeasurement())
-                {
-                    for (int j = 0; j < s_DIM_2; j++)
-                    {
-                        for (int k = 0; k < s_DIM_2; k++)
-                        {
-                            value = (int)s_arr2.GetValue(j, k);
-                            value = (int)s_arr2.GetValue(j, k);
-                            value = (int)s_arr2.GetValue(j, k);
-                            value = (int)s_arr2.GetValue(j, k);
-                            value = (int)s_arr2.GetValue(j, k);
-                            value = (int)s_arr2.GetValue(j, k);
-                            value = (int)s_arr2.GetValue(j, k);
-                            value = (int)s_arr2.GetValue(j, k);
-                            value = (int)s_arr2.GetValue(j, k);
-                            value = (int)s_arr2.GetValue(j, k);
-                        }
-                    }
-                }
-            }
         }
 
         [Benchmark]
-        public static void ArrayRetrieve3D()
+        public int ArrayRetrieve2D()
         {
-            int value;
+            int value = default;
+
+            for (int j = 0; j < s_DIM_2; j++)
+            {
+                for (int k = 0; k < s_DIM_2; k++)
+                {
+                    value = (int) s_arr2.GetValue(j, k);
+                    value = (int) s_arr2.GetValue(j, k);
+                    value = (int) s_arr2.GetValue(j, k);
+                    value = (int) s_arr2.GetValue(j, k);
+                    value = (int) s_arr2.GetValue(j, k);
+                    value = (int) s_arr2.GetValue(j, k);
+                    value = (int) s_arr2.GetValue(j, k);
+                    value = (int) s_arr2.GetValue(j, k);
+                    value = (int) s_arr2.GetValue(j, k);
+                    value = (int) s_arr2.GetValue(j, k);
+                }
+            }
+
+            return value;
+        }
+
+        [GlobalSetup(Target = nameof(ArrayRetrieve3D))]
+        public void SetupArrayRetrieve3D()
+        {
             s_arr3 = Array.CreateInstance(typeof(int), s_DIM_3, s_DIM_3, s_DIM_3);
 
             for (int i = 0; i < s_DIM_3; i++)
@@ -222,41 +187,41 @@ namespace System.Tests
                         s_arr3.SetValue(i + j + k, i, j, k);
                 }
             }
-
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                using (iteration.StartMeasurement())
-                {
-                    for (int j = 0; j < s_DIM_3; j++)
-                    {
-                        for (int k = 0; k < s_DIM_3; k++)
-                        {
-                            for (int l = 0; l < s_DIM_3; l++)
-                            {
-                                value = (int)s_arr3.GetValue(j, k, l);
-                                value = (int)s_arr3.GetValue(j, k, l);
-                                value = (int)s_arr3.GetValue(j, k, l);
-                                value = (int)s_arr3.GetValue(j, k, l);
-                                value = (int)s_arr3.GetValue(j, k, l);
-                                value = (int)s_arr3.GetValue(j, k, l);
-                                value = (int)s_arr3.GetValue(j, k, l);
-                                value = (int)s_arr3.GetValue(j, k, l);
-                                value = (int)s_arr3.GetValue(j, k, l);
-                                value = (int)s_arr3.GetValue(j, k, l);
-                                value = (int)s_arr3.GetValue(j, k, l);
-                            }
-                        }
-                    }
-                }
-            }
         }
 
         [Benchmark]
-        public static void ArrayCopy2D()
+        public int ArrayRetrieve3D()
         {
-            int arrayLen = (int)Math.Pow(s_DIM_2, 2);
+            int value = default;
 
-            Array dummy = Array.CreateInstance(typeof(int), s_DIM_2, s_DIM_2);
+            for (int j = 0; j < s_DIM_3; j++)
+            {
+                for (int k = 0; k < s_DIM_3; k++)
+                {
+                    for (int l = 0; l < s_DIM_3; l++)
+                    {
+                        value = (int) s_arr3.GetValue(j, k, l);
+                        value = (int) s_arr3.GetValue(j, k, l);
+                        value = (int) s_arr3.GetValue(j, k, l);
+                        value = (int) s_arr3.GetValue(j, k, l);
+                        value = (int) s_arr3.GetValue(j, k, l);
+                        value = (int) s_arr3.GetValue(j, k, l);
+                        value = (int) s_arr3.GetValue(j, k, l);
+                        value = (int) s_arr3.GetValue(j, k, l);
+                        value = (int) s_arr3.GetValue(j, k, l);
+                        value = (int) s_arr3.GetValue(j, k, l);
+                        value = (int) s_arr3.GetValue(j, k, l);
+                    }
+                }
+            }
+
+            return value;
+        }
+
+        [GlobalSetup(Target = nameof(ArrayCopy2D))]
+        public void SetupArrayCopy2D()
+        {
+            _destinationArray = Array.CreateInstance(typeof(int), s_DIM_2, s_DIM_2);
             s_arr2 = Array.CreateInstance(typeof(int), s_DIM_2, s_DIM_2);
 
             for (int i = 0; i < s_DIM_2; i++)
@@ -264,22 +229,15 @@ namespace System.Tests
                 for (int j = 0; j < s_DIM_2; j++)
                     s_arr2.SetValue(i + j, i, j);
             }
-
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                using (iteration.StartMeasurement())
-                {
-                    Array.Copy(s_arr2, dummy, arrayLen);
-                }
-            }
         }
 
         [Benchmark]
-        public static void ArrayCopy3D()
-        {
-            int arrayLen = (int)Math.Pow(s_DIM_3, 3);
+        public void ArrayCopy2D() => Array.Copy(s_arr2, _destinationArray, s_DIM_2 * s_DIM_2);
 
-            Array dummy = Array.CreateInstance(typeof(int), s_DIM_3, s_DIM_3, s_DIM_3);
+        [GlobalSetup(Target = nameof(ArrayCopy3D))]
+        public void SetupArrayCopy3D()
+        {
+            _destinationArray = Array.CreateInstance(typeof(int), s_DIM_3, s_DIM_3, s_DIM_3);
             s_arr3 = Array.CreateInstance(typeof(int), s_DIM_3, s_DIM_3, s_DIM_3);
 
             for (int i = 0; i < s_DIM_3; i++)
@@ -292,32 +250,24 @@ namespace System.Tests
                     }
                 }
             }
-
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                using (iteration.StartMeasurement())
-                {
-                    Array.Copy(s_arr3, dummy, arrayLen);
-                }
-            }
         }
 
         [Benchmark]
-        public static void ArrayResize()
+        public void ArrayCopy3D() => Array.Copy(s_arr3, _destinationArray, s_DIM_3 * s_DIM_3 * s_DIM_3);
+
+        [IterationSetup(Target = nameof(ArrayResize))]
+        public void SetupArrayResizeIteration()
         {
-            // Test copying a subarray
-            const int oldSize = 42;
-            const int newSize = 41;
-
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                byte[] ary = new byte[oldSize];
-
-                using (iteration.StartMeasurement())
-                {
-                    Array.Resize<byte>(ref ary, newSize);
-                }
-            }
+            _byteArrays = new byte[ByteArraysCount][];
+            for (int i = 0; i < _byteArrays.Length; i++)
+                _byteArrays[i] = new byte[OldSize];
+        }
+        
+        [Benchmark(OperationsPerInvoke = ByteArraysCount)]
+        public void ArrayResize()
+        {
+            for (int i = 0; i < _byteArrays.Length; i++)
+                Array.Resize<byte>(ref _byteArrays[i], NewSize);
         }
     }
 }
