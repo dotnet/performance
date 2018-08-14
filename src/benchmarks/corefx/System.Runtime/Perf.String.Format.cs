e93ea261dd970fd05c3a4c1413686d594f7e0537
Adam Sitnik <adam.sitnik@gmail.com>
2018-08-14 15:48:53 +0200

    reduce the number of Format benchmark cases, -11 benchmarks

--- src/benchmarks/corefx/System.Runtime/Perf.String.Format.cs
+++ src/benchmarks/corefx/System.Runtime/Perf.String.Format.cs
@@ -1,31 +1,12 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 
 namespace System.Tests
 {
     public partial class Perf_String
     {
-        public static IEnumerable<object[]> FormatArgs1 => Permutations(
-            new object[] {"Testing {0}, {0:C}, {0:D5}, {0:E} - {0:F4}{0:G}{0:N}  {0:X} !!"},
-            new object[] {8, 0}
-        );
-
-        public static IEnumerable<object[]> FormatArgs2 => Permutations(
-            new object[] {"Testing {0}, {0:C}, {0:E} - {0:F4}{0:G}{0:N} , !!"},
-            new object[] {0, -2, 3.14159, 11000000}
-        );
-
-        public static IEnumerable<object[]> FormatArgs3 => Permutations(
-            new object[] {"More testing: {0}"},
-            new object[]
-                {0, -2, 3.14159, 11000000, "Foo", 'a', "Testing {0}, {0:C}, {0:D5}, {0:E} - {0:F4}{0:G}{0:N}  {0:X} !!"}
-        );
-
-        public static IEnumerable<object[]> AllFormatArgs => FormatArgs1.Concat(FormatArgs2).Concat(FormatArgs3);
-
         [Benchmark]
-        [ArgumentsSource(nameof(AllFormatArgs))]
+        [Arguments("Testing {0}, {0:C}, {0:D5}, {0:E} - {0:F4}{0:G}{0:N}  {0:X} !!", 8)]
+        [Arguments("Testing {0}, {0:C}, {0:E} - {0:F4}{0:G}{0:N} , !!", 3.14159)]
         public string Format_OneArg(string s, object o)
             => string.Format(s, o);
 
