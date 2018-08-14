fa30bbeda213174a3f7f32490b773a2a695080b7
Adam Sitnik <adam.sitnik@gmail.com>
2018-08-14 16:36:58 +0200

    reduce the number of Replace benchmark cases and some better ones, MINUS TWO HUNDRED TWELVE BENCHMARKS

--- src/benchmarks/corefx/System.Runtime/Perf.String.Replace.cs
+++ src/benchmarks/corefx/System.Runtime/Perf.String.Replace.cs
@@ -1,48 +1,19 @@
-﻿using System.Collections.Generic;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 
 namespace System.Tests
 {
     public partial class Perf_String
     {
-        private static readonly char[][] s_replaceCharPairs = new char[][]
-        {
-            new char[] {' ', 'T'},
-            new char[] {'T', 'd'},
-            new char[] {'d', 'a'},
-            new char[] {'a', (char) 192},
-            new char[] {(char) 192, (char) 8197},
-            new char[] {(char) 8197, "\u0400"[0]},
-            new char[] {"\u0400"[0], '\t'},
-            new char[] {'\t', (char) 768},
-            new char[] {(char) 768, ' '},
-        };
-
-        public static IEnumerable<object[]> ReplaceCharArgs => Permutations(s_trimStrings, s_replaceCharPairs);
+        [Benchmark]
+        [Arguments("This is a very nice sentence", 'z', 'y')] // 'z' does not exist in the string
+        [Arguments("This is a very nice sentence", 'i', 'I')] // 'i' occuress 3 times in the string
+        public string Replace_Char(string text, char oldChar, char newChar)
+            => text.Replace(oldChar, newChar);
 
         [Benchmark]
-        [ArgumentsSource(nameof(ReplaceCharArgs))]
-        public string Replace_Char(string s, char[] chars)
-            => s.Replace(chars[0], chars[1]);
-
-        private static readonly string[][] s_replaceStringPairs = new string[][]
-        {
-            new string[] {"  ", "T"},
-            new string[] {"T", "dd"},
-            new string[] {"dd", "a"},
-            new string[] {"a", "\u00C0"},
-            new string[] {"\u00C0", "a\u0300"},
-            new string[] {"a\u0300", "\u0400"},
-            new string[] {"\u0400", "\u0300"},
-            new string[] {"\u0300", "\u00A0\u2000"},
-            new string[] {"\u00A0\u2000", "  "},
-        };
-
-        public static IEnumerable<object[]> ReplaceStringArgs => Permutations(s_trimStrings, s_replaceStringPairs);
-
-        [Benchmark]
-        [ArgumentsSource(nameof(ReplaceStringArgs))]
-        public string Replace_String(string s0, string[] strings)
-            => s0.Replace(strings[0], strings[1]);
+        [Arguments("This is a very nice sentence", "bad", "nice")] // there are no "bad" words in the string
+        [Arguments("This is a very nice sentence", "nice", "bad")] // there are is one "nice" word in the string
+        public string Replace_String(string text, string oldValue, string newValue)
+            => text.Replace(oldValue, newValue);
     }
 }
