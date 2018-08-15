a691f42f4accd293679e1a9d8262b62a40b484d7
Adam Sitnik <adam.sitnik@gmail.com>
2018-08-15 22:06:35 +0200

    Globalization (#115)
    * copy
    
    * port 1:1

---
+++ src/benchmarks/corefx/System.Globalization/Perf.DateTimeCultureInfo.cs
@@ -0,0 +1,37 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace System.Globalization.Tests
+{
+    /// <summary>
+    /// Performance tests for converting DateTime to different CultureInfos
+    /// 
+    /// Primary methods affected: Parse, ToString
+    /// </summary>
+    public class Perf_DateTimeCultureInfo
+    {
+        private readonly DateTime _time = DateTime.Now;
+        
+        public IEnumerable<object> Cultures()
+        {
+            yield return new CultureInfo("fr");
+            yield return new CultureInfo("da");
+            yield return new CultureInfo("ja");
+            yield return new CultureInfo("");
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Cultures))]
+        public string ToString(CultureInfo culturestring) // the argument is called "culturestring" to keep benchmark ID, do NOT rename it
+            => _time.ToString(culturestring);
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Cultures))]
+        public DateTime Parse(CultureInfo culturestring)
+            => DateTime.Parse("10/10/2010 12:00:00 AM", culturestring);
+    }
+}
