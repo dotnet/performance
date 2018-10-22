74043245f68779e3d0e0c35e395d7ab4a1a1d3dc
Adam Sitnik <adam.sitnik@gmail.com>
2018-10-22 17:43:07 +0200

    bring back the ArtifactsUploader which remained in the ignored "tools" folder (#141)
    * bring back the ArtifactsUploader which remained in the ignored "tools" folder
    
    * apply the code review suggestion

---
+++ src/tools/ArtifactsUploader/ArtifactsUploader/FilesHelper.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ArtifactsUploader
+{
+    internal static class FilesHelper
+    {
+        public static FileInfo GetNonExistingArchiveFile(DirectoryInfo workplace, string jobName)
+        {
+            var fileInfo = new FileInfo(Path.Combine(workplace.FullName, $"{jobName}.zip")); // I assume that job name is unique
+
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+
+            return fileInfo;
+        }
+
+        public static IEnumerable<FileInfo> GetFilesToArchive(DirectoryInfo artifactsDirectory, IEnumerable<string> searchPatterns)
+            => searchPatterns.SelectMany(searchPattern =>
+                    artifactsDirectory.EnumerateFiles(searchPattern, SearchOption.AllDirectories));
+    }
+}
