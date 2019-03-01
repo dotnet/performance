c2214df2e1d78b39861ff10e092e10c24aa02d36
Adam Sitnik <adam.sitnik@gmail.com>
2019-03-01 12:51:43 +0100

    port Perf_Graphics_Transforms

--- src/benchmarks/micro/corefx/System.Drawing/Perf_Graphics_Transforms.cs
+++ src/benchmarks/micro/corefx/System.Drawing/Perf_Graphics_Transforms.cs
@@ -2,42 +2,47 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
 using System.Drawing.Drawing2D;
-using Microsoft.Xunit.Performance;
-using Microsoft.DotNet.XUnitExtensions;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Extensions;
+using MicroBenchmarks;
 
 namespace System.Drawing.Tests
 {
-    public class Perf_Graphics_Transforms : RemoteExecutorTestBase
+    [BenchmarkCategory(Categories.CoreFX)]
+    public class Perf_Graphics_Transforms
     {
-        [Benchmark(InnerIterationCount = 10000)]
-        [ConditionalBenchmark(typeof(Helpers), nameof(Helpers.GetIsDrawingSupported), nameof(Helpers.IsNotUnix))] // Graphics.TransformPoints is not implemented in libgdiplus yet. See dotnet/corefx 20884
-        public void TransformPoints()
+        private Bitmap _image;
+        private Graphics _graphics;
+        private Point[] _points;
+
+        [GlobalSetup]
+        public void Setup()
         {
-            Point[] points =
+            _points = new []
             {
                 new Point(10, 10), new Point(20, 1), new Point(35, 5), new Point(50, 10),
                 new Point(60, 15), new Point(65, 25), new Point(50, 30)
             };
 
-            using (Bitmap image = new Bitmap(100, 100))
-            using (Graphics graphics = Graphics.FromImage(image))
-            {
-                foreach (var iteration in Benchmark.Iterations)
-                {
+            _image = new Bitmap(100, 100);
+            _graphics = Graphics.FromImage(_image);
+        }
 
-                    using (iteration.StartMeasurement())
-                    {
-                        for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        {
-                            graphics.TransformPoints(CoordinateSpace.World, CoordinateSpace.Page, points);
-                            graphics.TransformPoints(CoordinateSpace.Device, CoordinateSpace.World, points);
-                            graphics.TransformPoints(CoordinateSpace.Page, CoordinateSpace.Device, points);
-                        }
-                    }
-                }
-            }
+        [Benchmark]
+        [AllowedOperatingSystems("Graphics.TransformPoints is not implemented in libgdiplus yet. See dotnet/corefx 20884", OS.Windows)]
+        public void TransformPoints()
+        {
+            _graphics.TransformPoints(CoordinateSpace.World, CoordinateSpace.Page, _points);
+            _graphics.TransformPoints(CoordinateSpace.Device, CoordinateSpace.World, _points);
+            _graphics.TransformPoints(CoordinateSpace.Page, CoordinateSpace.Device, _points);
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            _graphics.Dispose();
+            _image.Dispose();
         }
     }
 }
