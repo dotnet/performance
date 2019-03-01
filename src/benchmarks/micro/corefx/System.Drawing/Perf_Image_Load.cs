f6af04d5b55426fd24b3fdb79c93382a35fea1b6
Adam Sitnik <adam.sitnik@gmail.com>
2019-03-01 14:51:20 +0100

    port Perf_Image_Load tests

--- src/benchmarks/micro/corefx/System.Drawing/Perf_Image_Load.cs
+++ src/benchmarks/micro/corefx/System.Drawing/Perf_Image_Load.cs
@@ -2,149 +2,91 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.Drawing.Imaging;
 using System.IO;
-using Microsoft.Xunit.Performance;
-using Microsoft.DotNet.XUnitExtensions;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Extensions;
+using MicroBenchmarks;
 
 namespace System.Drawing.Tests
 {
-    public class Perf_Image_Load : RemoteExecutorTestBase
+    [BenchmarkCategory(Categories.CoreFX)]
+    public class Perf_Image_Load
     {
-        [Benchmark(InnerIterationCount = 100)]
-        [ConditionalBenchmark(typeof(Helpers), nameof(Helpers.GetIsDrawingSupported))]
-        public void Bitmap_FromStream()
-        {
-            var files = CreateTestImageFiles();
+        private static readonly ImageTestData[] TestCases = {
+            new ImageTestData(ImageFormat.Bmp),
+            new ImageTestData(ImageFormat.Jpeg),
+            new ImageTestData(ImageFormat.Png),
+            new ImageTestData(ImageFormat.Gif)
+        };
 
-            using (FileStream bmpStream = new FileStream(files.BitmapPath, FileMode.Open, FileAccess.Read))
-            using (FileStream jpgStream = new FileStream(files.JpegPath, FileMode.Open, FileAccess.Read))
-            using (FileStream pngStream = new FileStream(files.PngPath, FileMode.Open, FileAccess.Read))
+        public IEnumerable<object> ImageFormats() => TestCases;
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ImageFormats))]
+        public void Bitmap_FromStream(ImageTestData format)
+        {
+            using (new Bitmap(format.Stream))
             {
-                foreach (var iteration in Benchmark.Iterations)
+            }
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ImageFormats))]
+        public void Image_FromStream(ImageTestData format)
+        {
+            using (Image.FromStream(format.Stream))
+            {
+            }
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ImageFormats))]
+        public void Image_FromStream_NoValidation(ImageTestData format)
+        {
+            using (Image.FromStream(format.Stream, false, false))
+            {
+            }
+        }
+
+        public class ImageTestData
+        {
+            public Stream Stream { get; }
+            private string FormatName { get; }
+
+            public ImageTestData(ImageFormat format)
+            {
+                Stream = CreateTestImage(format);
+                FormatName = format.ToString();
+            }
+
+            // the value returned by ToString is used in the text representation of Benchmark ID in our reporting system
+            public override string ToString() => FormatName;
+
+            private static Stream CreateTestImage(ImageFormat format)
+            {
+                Random r = new Random(1066); // the seed must not be changed
+
+                const int Size = 1000;
+                Point RandomPoint() => new Point(r.Next(Size), r.Next(Size));
+
+                var result = new MemoryStream();
+
+                using (Bitmap bitmap = new Bitmap(Size, Size))
+                using (Pen pen = new Pen(Color.Blue))
+                using (Graphics graphics = Graphics.FromImage(bitmap))
                 {
-                    using (iteration.StartMeasurement())
+                    for (int i = 0; i < 100; i++)
                     {
-                        for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        {
-                            using (new Bitmap(bmpStream))
-                            using (new Bitmap(jpgStream))
-                            using (new Bitmap(pngStream))
-                            {
-                            }
-                        }
+                        graphics.DrawBezier(pen, RandomPoint(), RandomPoint(), RandomPoint(), RandomPoint());
                     }
-                }
-            }
-        }
 
-        [Benchmark(InnerIterationCount = 100)]
-        [ConditionalBenchmark(typeof(Helpers), nameof(Helpers.GetIsDrawingSupported))]
-        public void Image_FromStream()
-        {
-            var files = CreateTestImageFiles();
-
-            using (FileStream bmpStream = new FileStream(files.BitmapPath, FileMode.Open, FileAccess.Read))
-            using (FileStream jpgStream = new FileStream(files.JpegPath, FileMode.Open, FileAccess.Read))
-            using (FileStream pngStream = new FileStream(files.PngPath, FileMode.Open, FileAccess.Read))
-            {
-                foreach (var iteration in Benchmark.Iterations)
-                {
-                    using (iteration.StartMeasurement())
-                    {
-                        for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        {
-                            using (Image.FromStream(bmpStream))
-                            using (Image.FromStream(jpgStream))
-                            using (Image.FromStream(pngStream))
-                            {
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        [Benchmark(InnerIterationCount = 100)]
-        [ConditionalBenchmark(typeof(Helpers), nameof(Helpers.GetIsDrawingSupported))]
-        public void Image_FromStream_NoValidation()
-        {
-            var files = CreateTestImageFiles();
-
-            using (FileStream bmpStream = new FileStream(files.BitmapPath, FileMode.Open, FileAccess.Read))
-            using (FileStream jpgStream = new FileStream(files.JpegPath, FileMode.Open, FileAccess.Read))
-            using (FileStream pngStream = new FileStream(files.PngPath, FileMode.Open, FileAccess.Read))
-            {
-                foreach (var iteration in Benchmark.Iterations)
-                {
-                    using (iteration.StartMeasurement())
-                    {
-                        for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        {
-                            using (Image.FromStream(bmpStream, false, false))
-                            using (Image.FromStream(jpgStream, false, false))
-                            using (Image.FromStream(pngStream, false, false))
-                            {
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        [Benchmark(InnerIterationCount = 100)]
-        [ConditionalBenchmark(typeof(Helpers), nameof(Helpers.GetIsDrawingSupported))]
-        public void Image_FromStream_NoValidation_Gif()
-        {
-            // GIF has extra logic looking for animated GIFs
-            string gifPath = CreateTestImageFile(ImageFormat.Gif);
-
-            using (FileStream gifStream = new FileStream(gifPath, FileMode.Open, FileAccess.Read))
-            {
-                foreach (var iteration in Benchmark.Iterations)
-                {
-                    using (iteration.StartMeasurement())
-                    {
-                        for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        {
-                            using (Image.FromStream(gifStream, false, false))
-                            {
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        private (string BitmapPath, string JpegPath, string PngPath) CreateTestImageFiles()
-        {
-            return (CreateTestImageFile(ImageFormat.Bmp), CreateTestImageFile(ImageFormat.Bmp), CreateTestImageFile(ImageFormat.Bmp));
-        }
-
-        private string CreateTestImageFile(ImageFormat format)
-        {
-            string path = GetTestFilePath();
-            path = $"{path}.{format.ToString().ToLowerInvariant()}";
-
-            Random r = new Random(1066);
-
-            const int Size = 1000;
-            Point RandomPoint() => new Point(r.Next(Size), r.Next(Size));
-
-            using (Bitmap bitmap = new Bitmap(Size, Size))
-            using (Pen pen = new Pen(Color.Blue))
-            using (Graphics graphics = Graphics.FromImage(bitmap))
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    graphics.DrawBezier(pen, RandomPoint(), RandomPoint(), RandomPoint(), RandomPoint());
+                    bitmap.Save(result, format);
                 }
 
-                bitmap.Save(path, format);
+                return result;
             }
-
-            return path;
         }
     }
 }
