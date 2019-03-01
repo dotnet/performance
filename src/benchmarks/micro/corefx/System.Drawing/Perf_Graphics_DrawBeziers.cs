c30166d83c71d972a2f6ef2c219d1003e89115b0
Adam Sitnik <adam.sitnik@gmail.com>
2019-03-01 15:01:59 +0100

    port Perf_Graphics_DrawBeziers

--- src/benchmarks/micro/corefx/System.Drawing/Perf_Graphics_DrawBeziers.cs
+++ src/benchmarks/micro/corefx/System.Drawing/Perf_Graphics_DrawBeziers.cs
@@ -2,63 +2,48 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
-using Microsoft.Xunit.Performance;
-using Microsoft.DotNet.XUnitExtensions;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Extensions;
+using MicroBenchmarks;
 
 namespace System.Drawing.Tests
 {
-    public class Perf_Graphics_DrawBeziers : RemoteExecutorTestBase
+    [BenchmarkCategory(Categories.CoreFX)]
+    public class Perf_Graphics_DrawBeziers
     {
-        [Benchmark(InnerIterationCount = 10000)]
-        [ConditionalBenchmark(typeof(Helpers), nameof(Helpers.GetIsDrawingSupported))]
-        public void DrawBezier_Point()
-        {
-            Random r = new Random(1942);
+        private Bitmap _image;
+        private Pen _pen;
+        private Graphics _graphics;
+        private Point _point1, _point2, _point3, _point4;
+        private Point[] _points;
 
-            using (Bitmap image = new Bitmap(100, 100))
-            using (Pen pen = new Pen(Color.White))
-            using (Graphics graphics = Graphics.FromImage(image))
-            {
-                foreach (var iteration in Benchmark.Iterations)
-                {
-                    using (iteration.StartMeasurement())
-                    {
-                        for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        {
-                            graphics.DrawBezier(pen, new Point(r.Next(100), r.Next(100)), new Point(r.Next(100), r.Next(100)), new Point(r.Next(100), r.Next(100)), new Point(r.Next(100), r.Next(100)));
-                        }
-                    }
-                }
-            }
+        [GlobalSetup]
+        public void Setup()
+        {
+            _image = new Bitmap(100, 100);
+            _pen = new Pen(Color.White);
+            _graphics = Graphics.FromImage(_image);
+
+            _point1 = new Point(10, 10);
+            _point2 = new Point(20, 1);
+            _point3 = new Point(35, 5);
+            _point4 = new Point(50, 10);
+
+            _points = new[] {_point1, _point2, _point3, _point4};
         }
 
-        [Benchmark(InnerIterationCount = 10000)]
-        [ConditionalBenchmark(typeof(Helpers), nameof(Helpers.GetIsDrawingSupported))]
-        public void DrawBezier_Points()
+        [GlobalCleanup]
+        public void Cleanup()
         {
-            Point[] points =
-            {
-                new Point(10, 10), new Point(20, 1), new Point(35, 5), new Point(50, 10),
-                new Point(60, 15), new Point(65, 25), new Point(50, 30)
-            };
-
-            using (Bitmap image = new Bitmap(100, 100))
-            using (Pen pen = new Pen(Color.Blue))
-            using (Graphics graphics = Graphics.FromImage(image))
-            {
-                foreach (var iteration in Benchmark.Iterations)
-                {
-
-                    using (iteration.StartMeasurement())
-                    {
-                        for (int i = 0; i < Benchmark.InnerIterationCount; i++)
-                        {
-                            graphics.DrawBeziers(pen, points);
-                        }
-                    }
-                }
-            }
+            _graphics.Dispose();
+            _pen.Dispose();
+            _image.Dispose();
         }
+
+        [Benchmark]
+        public void DrawBezier_Point() => _graphics.DrawBezier(_pen, _point1, _point2, _point3, _point4);
+
+        [Benchmark]
+        public void DrawBezier_Points() => _graphics.DrawBeziers(_pen, _points);
     }
 }
