b1c71f5520998dd1c7ce35e9b20f5f41dbda0c77
Stephen Toub <stoub@microsoft.com>
2019-03-22 10:38:41 -0400

    Add benchmark for Process.Start and WaitForExit

--- src/benchmarks/micro/corefx/System.Diagnostics/Perf_Process.cs
+++ src/benchmarks/micro/corefx/System.Diagnostics/Perf_Process.cs
@@ -39,5 +39,38 @@ namespace System.Diagnostics
                 process.Dispose();
             }
         }
+
+        private static ProcessStartInfo s_startProcessStartInfo = new ProcessStartInfo() {
+            FileName = "whoami", // exists on both Windows and Unix, and has very short output
+            RedirectStandardOutput = true // avoid visible output
+        };
+
+        private Process _startedProcess;
+
+        [Benchmark]
+        public void Start()
+        {
+            _startedProcess = Process.Start(s_startProcessStartInfo);
+        }
+
+        [IterationCleanup(Target = nameof(Start))]
+        public void CleanupStart()
+        {
+            if (_startedProcess != null)
+            {
+                _startedProcess.WaitForExit();
+                _startedProcess.Dispose();
+                _startedProcess = null;
+            }
+        }
+
+        [Benchmark]
+        public void StartAndWaitForExit()
+        {
+            using (Process p = Process.Start(s_startProcessStartInfo))
+            {
+                p.WaitForExit();
+            }
+        }
     }
 }
