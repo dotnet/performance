9808ca317d4bec0b9ac19a27ddc9e8ac247da8b1
Ahson Khan <ahkha@microsoft.com>
2019-05-07 21:43:29 -0700

    Add (de)serializer performance tests for System.Text.Json (#456)
    * Add serializer perf tests for System.Text.Json
    
    * Clean up serializer tests and remove types that aren't relevant to Json
    (or dont work yet)
    
    * Remove data generator duplicate.
    
    * Try to measure in console app using stop watch.
    
    * Clean up perf tests.
    
    * Clean up the S.T.Json perf tests and revert changes to serializers
    directory.
    
    * Update benchmark name and revert local csproj changes.
    
    * Remove the perf tests for TFMs other than netcoreapp3.0.
    
    * Address PR feedback.

---
+++ src/benchmarks/micro/corefx/System.Text.Json/Serializer/ReadJson.cs
@@ -0,0 +1,56 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+using MicroBenchmarks.Serializers;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    [GenericTypeArguments(typeof(LoginViewModel))]
+    [GenericTypeArguments(typeof(Location))]
+    [GenericTypeArguments(typeof(IndexViewModel))]
+    [GenericTypeArguments(typeof(MyEventsListerViewModel))]
+    public class ReadJson<T>
+    {
+        private string _serialized;
+        private byte[] _utf8Serialized;
+        private MemoryStream _memoryStream;
+
+        [GlobalSetup]
+        public async Task Setup()
+        {
+            T value = DataGenerator.Generate<T>();
+
+            _serialized = JsonSerializer.ToString(value);
+
+            _utf8Serialized = Encoding.UTF8.GetBytes(_serialized);
+
+            _memoryStream = new MemoryStream(capacity: short.MaxValue);
+            await JsonSerializer.WriteAsync(value, _memoryStream);
+        }
+
+        [BenchmarkCategory(Categories.CoreFX, Categories.JSON)]
+        [Benchmark]
+        public T DeserializeFromString() => JsonSerializer.Parse<T>(_serialized);
+
+        [BenchmarkCategory(Categories.CoreFX, Categories.JSON)]
+        [Benchmark]
+        public T DeserializeFromUtf8Bytes() => JsonSerializer.Parse<T>(_utf8Serialized);
+
+        [BenchmarkCategory(Categories.CoreFX, Categories.JSON)]
+        [Benchmark]
+        public async Task<T> DeserializeFromStream()
+        {
+            _memoryStream.Position = 0;
+            T value = await JsonSerializer.ReadAsync<T>(_memoryStream);
+            return value;
+        }
+
+        [GlobalCleanup]
+        public void Cleanup() => _memoryStream.Dispose();
+    }
+}
---
+++ src/benchmarks/micro/corefx/System.Text.Json/Serializer/WriteJson.cs
@@ -0,0 +1,50 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+using MicroBenchmarks.Serializers;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    [GenericTypeArguments(typeof(LoginViewModel))]
+    [GenericTypeArguments(typeof(Location))]
+    [GenericTypeArguments(typeof(IndexViewModel))]
+    [GenericTypeArguments(typeof(MyEventsListerViewModel))]
+    public class WriteJson<T>
+    {
+        private T _value;
+        private MemoryStream _memoryStream;
+
+        [GlobalSetup]
+        public async Task Setup()
+        {
+            _value = DataGenerator.Generate<T>();
+
+            _memoryStream = new MemoryStream(capacity: short.MaxValue);
+            await JsonSerializer.WriteAsync(_value, _memoryStream);
+        }
+
+        [BenchmarkCategory(Categories.CoreFX, Categories.JSON)]
+        [Benchmark]
+        public string SerializeToString() => JsonSerializer.ToString(_value);
+
+        [BenchmarkCategory(Categories.CoreFX, Categories.JSON)]
+        [Benchmark]
+        public byte[] SerializeToUtf8Bytes() => JsonSerializer.ToBytes(_value);
+
+        [BenchmarkCategory(Categories.CoreFX, Categories.JSON)]
+        [Benchmark]
+        public async Task SerializeToStream()
+        {
+            _memoryStream.Position = 0;
+            await JsonSerializer.WriteAsync(_value, _memoryStream);
+        }
+
+        [GlobalCleanup]
+        public void Cleanup() => _memoryStream.Dispose();
+    }
+}
