e92f9ff38958acce41788d61fdb9bebd1b473684
ntr <ntr@ihfood.dk>
2019-05-24 13:45:20 +0200

    S.C.Sort<T> add extra custom types to microbenchmark

--- src/benchmarks/micro/corefx/System.Collections/Sort.cs
+++ src/benchmarks/micro/corefx/System.Collections/Sort.cs
@@ -10,9 +10,31 @@ using MicroBenchmarks;
 
 namespace System.Collections
 {
+    // TODO: Where should the below types reside?
+
+    public readonly struct IntStruct : IComparable<IntStruct>
+    {
+        readonly int _value;
+
+        public IntStruct(int value) => _value = value;
+
+        public int CompareTo(IntStruct other) => _value.CompareTo(other._value);
+    }
+
+    public class IntClass : IComparable<IntClass>
+    {
+        readonly int _value;
+
+        public IntClass(int value) => _value = value;
+
+        public int CompareTo(IntClass other) => _value.CompareTo(other._value);
+    }
+
     [BenchmarkCategory(Categories.CoreCLR, Categories.Collections, Categories.GenericCollections)]
-    [GenericTypeArguments(typeof(int))] // value type
-    [GenericTypeArguments(typeof(string))] // reference type
+    [GenericTypeArguments(typeof(int))] // value type, Array sort in native code
+    [GenericTypeArguments(typeof(IntStruct))] // custom value type, sort in managed code
+    [GenericTypeArguments(typeof(IntClass))] // custom reference type, sort in managed code, compare fast
+    [GenericTypeArguments(typeof(string))] // reference type, compare slow
     [InvocationCount(InvocationsPerIteration)]
     public class Sort<T>
     {
@@ -23,13 +45,13 @@ namespace System.Collections
 
         private int _iterationIndex = 0;
         private T[] _values;
-        
+
         private T[][] _arrays;
         private List<T>[] _lists;
 
         [GlobalSetup]
-        public void Setup() => _values = ValuesGenerator.ArrayOfUniqueValues<T>(Size);
-        
+        public void Setup() => _values = GenerateValues();
+
         [IterationCleanup]
         public void CleanupIteration() => _iterationIndex = 0; // after every iteration end we set the index to 0
 
@@ -63,6 +85,24 @@ namespace System.Collections
             foreach (var _ in _values.OrderBy(value => value)) // we can't use .Count here because it has been optimized for icollection.OrderBy().Count()
                 count++;
             return count;
+        }
+
+        T[] GenerateValues()
+        {
+            if (typeof(T) == typeof(IntStruct))
+            {
+                var values = ValuesGenerator.ArrayOfUniqueValues<int>(Size);
+                return (T[])(object)values.Select(v => new IntStruct(v)).ToArray();
+            }
+            else if (typeof(T) == typeof(IntClass))
+            {
+                var values = ValuesGenerator.ArrayOfUniqueValues<int>(Size);
+                return (T[])(object)values.Select(v => new IntClass(v)).ToArray();
+            }
+            else
+            {
+                return ValuesGenerator.ArrayOfUniqueValues<T>(Size);
+            }
         }
     }
 }
