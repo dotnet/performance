c8677b528fd4e305b439abc4c70d7e5fd5b9aa9e
ntr <ntr@ihfood.dk>
2019-05-28 21:46:40 +0200

    remove using

--- src/benchmarks/micro/corefx/System.Collections/Sort.cs
+++ src/benchmarks/micro/corefx/System.Collections/Sort.cs
@@ -5,7 +5,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Diagnostics.Windows.Configs;
 using BenchmarkDotNet.Extensions;
 using MicroBenchmarks;
 
