dbe726c8789ef85632f9582d0e737281c8eefbf6
Alexander Soldatov <soldatov.a@samsung.com>
2019-05-29 20:04:09 +0300

    [Linux/ARM] Set proper name for ARM architecture
    DotNet Cli accepts 'arm' value for ARM32 architecture. While scripts from benchmark
    pass incorrect value which cause crash on installation.

--- scripts/dotnet.py
+++ scripts/dotnet.py
@@ -533,7 +533,7 @@ def add_arguments(parser: ArgumentParser) -> ArgumentParser:
     SUPPORTED_ARCHITECTURES = [
         'x64',  # Default architecture
         'x86',
-        'arm32',
+        'arm',
         'arm64',
     ]
     parser.add_argument(
