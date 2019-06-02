f3447d4072f100c80991549f46028c357a87e5a4
Adam Sitnik <adam.sitnik@gmail.com>
2019-06-02 11:29:13 +0200

    initial implementation of StringComparer benchmarks

---
+++ src/benchmarks/micro/corefx/System.Runtime/Perf.StringComparer.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Extensions;
+using MicroBenchmarks;
+
+namespace System.Tests
+{
+    [BenchmarkCategory(Categories.CoreFX, Categories.CoreCLR)]
+    public class Perf_StringComparer
+    {
+        [Params(10, 10_000_000)] // bigger than the biggest array in ArrayPool.Shared
+        public int Count { get; set; }
+
+        [Params(StringComparison.OrdinalIgnoreCase, StringComparison.InvariantCultureIgnoreCase)]
+        public StringComparison Comparison { get; set; }
+
+        private string _input, _same, _lastCharacterDifferent;
+        private StringComparer _comparer;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _comparer = GetStringComparer();
+            char[] characters = ValuesGenerator.Array<char>(Count);
+            _input = new string(characters);
+            _same = new string(characters);
+            characters[characters.Length - 1] = characters[characters.Length - 1]++;
+            _lastCharacterDifferent = new string(characters);
+        }
+
+        [Benchmark]
+        public int GetStringHashCode() => _comparer.GetHashCode(_input);
+
+        [Benchmark]
+        public int CompareSame() => _comparer.Compare(_input, _same);
+
+        [Benchmark]
+        public int CompareLastCharDifferent() => _comparer.Compare(_input, _lastCharacterDifferent);
+
+        private StringComparer GetStringComparer()
+        {
+            switch (Comparison)
+            {
+                case StringComparison.CurrentCulture:
+                    return StringComparer.CurrentCulture;
+                case StringComparison.CurrentCultureIgnoreCase:
+                    return StringComparer.CurrentCultureIgnoreCase;
+                case StringComparison.InvariantCulture:
+                    return StringComparer.InvariantCulture;
+                case StringComparison.InvariantCultureIgnoreCase:
+                    return StringComparer.InvariantCultureIgnoreCase;
+                case StringComparison.Ordinal:
+                    return StringComparer.Ordinal;
+                case StringComparison.OrdinalIgnoreCase:
+                    return StringComparer.OrdinalIgnoreCase;
+                default:
+                    throw new NotSupportedException($"{Comparison} is not supported");
+            }
+        }
+    }
+}
