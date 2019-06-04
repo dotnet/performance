4c447c314d91c0351a9c114e046707e7c0f48c79
Layomi Akinrinade <laakinri@microsoft.com>
2019-06-04 11:18:58 -0400

    Add benchmarks for ISet<T> (de)serialization and general framework for generic collections

---
+++ src/benchmarks/micro/corefx/System.Text.Json/Serializer/ReadJson.GenericCollection.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    public class WriteGenericCollection
+    {
+        private static readonly ISet<string> _iset = new HashSet<string>();
+
+        [Params(2, 50, 100)]
+        public int ElementCount;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            for (int i = 0; i < ElementCount; i++)
+            {
+                _iset.Add($"hello{i}");
+            }
+        }
+
+        [Benchmark]
+        public string SerializeISet_ToBytes()
+        {
+            return JsonSerializer.ToString(_iset);
+        }
+    }
+}
---
+++ src/benchmarks/micro/corefx/System.Text.Json/Serializer/WriteJson.GenericCollection.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    public class ReadGenericCollection
+    {
+        private static string _jsonString;
+
+        [Params(2, 50, 100)]
+        public int ElementCount;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            ISet<string> _iset = new HashSet<string>();
+            for (int i = 0; i < ElementCount; i++)
+            {
+                _iset.Add($"hello{i}");
+            }
+
+            _jsonString = JsonSerializer.ToString(_iset);
+        }
+
+        [Benchmark]
+        public ISet<string> DeserializeISet()
+        {
+            return JsonSerializer.Parse<ISet<string>>(_jsonString);
+        }
+    }
+}
