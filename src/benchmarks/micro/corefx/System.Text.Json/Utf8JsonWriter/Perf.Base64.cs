c3297928bdefb5e244ce8d7cfbcb8ff17d79e5f0
Günther Foidl <gue@korporal.at>
2019-06-07 15:34:10 +0200

    Removed some benchmarks
    as suggested by adamsitnik
    Cf. https://github.com/dotnet/performance/pull/542#pullrequestreview-246779884

--- src/benchmarks/micro/corefx/System.Text.Json/Utf8JsonWriter/Perf.Base64.cs
+++ src/benchmarks/micro/corefx/System.Text.Json/Utf8JsonWriter/Perf.Base64.cs
@@ -5,7 +5,6 @@
 using System.Buffers;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Extensions;
 using MicroBenchmarks;
 
 namespace System.Text.Json.Tests
@@ -14,24 +13,16 @@ namespace System.Text.Json.Tests
     public class Perf_Base64
     {
         private ArrayBufferWriter<byte> _arrayBufferWriter;
-        private byte[] _data;
         private byte[] _dataWithNoEscaping;
         private byte[] _dataWithEscaping;
 
-        [Params(10, 100, 1000)]
+        [Params(100, 1000)]
         public int NumberOfBytes { get; set; }
-
-        [Params(true, false)]
-        public bool Formatted;
-
-        [Params(true, false)]
-        public bool SkipValidation;
 
         [GlobalSetup]
         public void Setup()
         {
             _arrayBufferWriter = new ArrayBufferWriter<byte>();
-            _data = ValuesGenerator.Array<byte>(NumberOfBytes);
 
             // Results in a number of A plus padding
             _dataWithNoEscaping = new byte[NumberOfBytes];
@@ -44,9 +35,6 @@ namespace System.Text.Json.Tests
         }
 
         [Benchmark]
-        public void WriteByteArrayAsBase64() => WriteByteArrayAsBase64Core(_data);
-
-        [Benchmark]
         public void WriteByteArrayAsBase64_NoEscaping() => WriteByteArrayAsBase64Core(_dataWithNoEscaping);
 
         [Benchmark]
@@ -55,7 +43,7 @@ namespace System.Text.Json.Tests
         private void WriteByteArrayAsBase64Core(byte[] data)
         {
             _arrayBufferWriter.Clear();
-            using (var json = new Utf8JsonWriter(_arrayBufferWriter, new JsonWriterOptions { Indented = Formatted, SkipValidation = SkipValidation }))
+            using (var json = new Utf8JsonWriter(_arrayBufferWriter))
             {
                 json.WriteBase64StringValue(data);
                 json.Flush();
