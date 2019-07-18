f086248500bbcd5a4d06a334aef3980358de66d0
Bill Wert <billwert@microsoft.com>
2019-07-17 17:14:56 -0700

    Startup measurement tools (#669)
    * Startup measurement tools
    
    This tool is the mechanical part of enabling startup time testing.
    
    There are a handful of things it still needs. I'm checking this MVP in
    to unblock teammates.
    
    * review feedback

---
+++ src/tools/ScenarioMeasurement/Startup/TimeToMainParser.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Session;
+using Reporting;
+using System;
+using System.Collections.Generic;
+
+namespace ScenarioMeasurement
+{
+    internal class TimeToMainParser : IParser
+    {
+        public void EnableKernelProvider(TraceEventSession kernel)
+        {
+            kernel.EnableKernelProvider((KernelTraceEventParser.Keywords)(KernelTraceEventParser.Keywords.Process | KernelTraceEventParser.Keywords.Thread | KernelTraceEventParser.Keywords.ContextSwitch));
+        }
+
+        public void EnableUserProviders(TraceEventSession user)
+        {
+            user.EnableProvider(ClrPrivateTraceEventParser.ProviderGuid, TraceEventLevel.Verbose, (ulong)ClrPrivateTraceEventParser.Keywords.Startup);
+        }
+
+        public IList<Counter> Parse(string mergeTraceFile, string processName)
+        {
+            var results = new List<double>();
+            double threadTime = 0;
+            var threadTimes = new List<double>();
+            var ins = new Dictionary<int, double>();
+            double start = -1;
+            int? pid = null;
+            using (var source = new ETWTraceEventSource(mergeTraceFile))
+            {
+                
+                source.Kernel.ProcessStart += evt =>
+                {
+                    if(evt.ProcessName.Equals(processName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        if(pid.HasValue)
+                        {
+                            // Processes might be reentrant. For now this traces the first (outermost) process of a given name.
+                            return; 
+                        }
+                        pid = evt.ProcessID;
+                        start = evt.TimeStampRelativeMSec;
+                    }
+                };
+
+                source.Kernel.ThreadCSwitch += evt =>
+                {
+                    if (!pid.HasValue) // we're currently in a measurement interval
+                        return;
+
+                    if (evt.NewProcessID != pid && evt.OldProcessID != pid) 
+                        return; // but this isn't our process
+
+                    if (evt.OldProcessID == pid) // this is a switch out from our process
+                    {
+                        if (ins.TryGetValue(evt.OldThreadID, out var value)) // had we ever recorded a switch in for this thread? 
+                        {
+                            threadTime += evt.TimeStampRelativeMSec - value;
+                            ins.Remove(evt.OldThreadID);
+                        }
+                    }
+                    else // this is a switch in to our process
+                    {
+                        ins[evt.NewThreadID] = evt.TimeStampRelativeMSec;
+                    }
+                };
+
+                ClrPrivateTraceEventParser clrpriv = new ClrPrivateTraceEventParser(source);
+                clrpriv.StartupMainStart += evt =>
+                {
+                    if(pid.HasValue && evt.ProcessID == pid && evt.ProcessName.Equals(processName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        results.Add(evt.TimeStampRelativeMSec - start);
+                        pid = null;
+                        threadTimes.Add(threadTime);
+                        threadTime = 0;
+                        start = 0;
+                    }
+                };
+                source.Process();
+            }
+            return new[] { new Counter() { Name = "Time To Main", Results = results.ToArray(), TopCounter = true, DefaultCounter = true, HigherIsBetter = false, MetricName = "ms"},
+                           new Counter() { Name = "Time on Thread", Results = threadTimes.ToArray(), TopCounter = true, DefaultCounter = false, HigherIsBetter = false, MetricName = "ms" }
+            };
+
+        }
+    }
+}
---
+++ src/tools/ScenarioMeasurement/Startup/WPFParser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Diagnostics.Tracing.Session;
+using Reporting;
+using System.Collections.Generic;
+
+namespace ScenarioMeasurement
+{
+    internal class WPFParser : IParser
+    {
+        public void EnableKernelProvider(TraceEventSession kernel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EnableUserProviders(TraceEventSession user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Counter> Parse(string mergeTraceFile, string processName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
