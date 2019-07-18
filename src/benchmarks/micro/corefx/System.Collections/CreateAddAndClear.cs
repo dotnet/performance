ecb31a2ef903de1736d336bc7ae33100fd600e77
Adam Sitnik <adam.sitnik@gmail.com>
2019-07-18 07:54:55 +0200

    pedantic rename

--- src/benchmarks/micro/corefx/System.Collections/CreateAddAndClear.cs
+++ src/benchmarks/micro/corefx/System.Collections/CreateAddAndClear.cs
@@ -19,19 +19,19 @@ namespace System.Collections
         [Params(Utils.DefaultCollectionSize)]
         public int Size;
 
-        private T[] _keys;
+        private T[] _uniqueValues;
 
         [GlobalSetup]
-        public void Setup() => _keys = ValuesGenerator.ArrayOfUniqueValues<T>(Size);
+        public void Setup() => _uniqueValues = ValuesGenerator.ArrayOfUniqueValues<T>(Size);
 
         [BenchmarkCategory(Categories.CoreCLR)]
         [Benchmark]
         public T[] Array()
         {
-            T[] array = new T[_keys.Length];
-            for (int i = 0; i < _keys.Length; i++)
+            T[] array = new T[_uniqueValues.Length];
+            for (int i = 0; i < _uniqueValues.Length; i++)
             {
-                array[i] = _keys[i];
+                array[i] = _uniqueValues[i];
             }
             System.Array.Clear(array, 0, array.Length);
             return array;
@@ -41,10 +41,10 @@ namespace System.Collections
         [Benchmark]
         public Span<T> Span()
         {
-            Span<T> span = new T[_keys.Length];
-            for (int i = 0; i < _keys.Length; i++)
+            Span<T> span = new T[_uniqueValues.Length];
+            for (int i = 0; i < _uniqueValues.Length; i++)
             {
-                span[i] = _keys[i];
+                span[i] = _uniqueValues[i];
             }
             span.Clear();
             return span;
@@ -54,9 +54,9 @@ namespace System.Collections
         public List<T> List()
         {
             List<T> list = new List<T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                list.Add(uniqueKey);
+                list.Add(value);
             }
             list.Clear();
             return list;
@@ -69,9 +69,9 @@ namespace System.Collections
         [MethodImpl(MethodImplOptions.NoInlining)] // we want to prevent from inlining this particular method to make sure that JIT does not find out that ICollection is always List
         private ICollection<T> ICollection(ICollection<T> collection)
         {
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                collection.Add(uniqueKey);
+                collection.Add(value);
             }
             collection.Clear();
             return collection;
@@ -81,9 +81,9 @@ namespace System.Collections
         public LinkedList<T> LinkedList()
         {
             LinkedList<T> linkedList = new LinkedList<T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                linkedList.AddLast(uniqueKey);
+                linkedList.AddLast(value);
             }
             linkedList.Clear();
             return linkedList;
@@ -93,9 +93,9 @@ namespace System.Collections
         public HashSet<T> HashSet()
         {
             HashSet<T> hashSet = new HashSet<T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                hashSet.Add(uniqueKey);
+                hashSet.Add(value);
             }
             hashSet.Clear();
             return hashSet;
@@ -105,9 +105,9 @@ namespace System.Collections
         public Dictionary<T, T> Dictionary()
         {
             Dictionary<T, T> dictionary = new Dictionary<T, T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                dictionary.Add(uniqueKey, uniqueKey);
+                dictionary.Add(value, value);
             }
             dictionary.Clear();
             return dictionary;
@@ -120,9 +120,9 @@ namespace System.Collections
         [MethodImpl(MethodImplOptions.NoInlining)]
         private IDictionary<T, T> IDictionary(IDictionary<T, T> dictionary)
         {
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                dictionary.Add(uniqueKey, uniqueKey);
+                dictionary.Add(value, value);
             }
             dictionary.Clear();
             return dictionary;
@@ -132,9 +132,9 @@ namespace System.Collections
         public SortedList<T, T> SortedList()
         {
             SortedList<T, T> sortedList = new SortedList<T, T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                sortedList.Add(uniqueKey, uniqueKey);
+                sortedList.Add(value, value);
             }
             sortedList.Clear();
             return sortedList;
@@ -144,9 +144,9 @@ namespace System.Collections
         public SortedSet<T> SortedSet()
         {
             SortedSet<T> sortedSet = new SortedSet<T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                sortedSet.Add(uniqueKey);
+                sortedSet.Add(value);
             }
             sortedSet.Clear();
             return sortedSet;
@@ -156,9 +156,9 @@ namespace System.Collections
         public SortedDictionary<T, T> SortedDictionary()
         {
             SortedDictionary<T, T> sortedDictionary = new SortedDictionary<T, T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                sortedDictionary.Add(uniqueKey, uniqueKey);
+                sortedDictionary.Add(value, value);
             }
             sortedDictionary.Clear();
             return sortedDictionary;
@@ -168,9 +168,9 @@ namespace System.Collections
         public ConcurrentDictionary<T, T> ConcurrentDictionary()
         {
             ConcurrentDictionary<T, T> concurrentDictionary = new ConcurrentDictionary<T, T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                concurrentDictionary.TryAdd(uniqueKey, uniqueKey);
+                concurrentDictionary.TryAdd(value, value);
             }
             concurrentDictionary.Clear();
             return concurrentDictionary;
@@ -180,9 +180,9 @@ namespace System.Collections
         public Stack<T> Stack()
         {
             Stack<T> stack = new Stack<T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                stack.Push(uniqueKey);
+                stack.Push(value);
             }
             stack.Clear();
             return stack;
@@ -192,9 +192,9 @@ namespace System.Collections
         public ConcurrentStack<T> ConcurrentStack()
         {
             ConcurrentStack<T> stack = new ConcurrentStack<T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                stack.Push(uniqueKey);
+                stack.Push(value);
             }
             stack.Clear();
             return stack;
@@ -204,9 +204,9 @@ namespace System.Collections
         public Queue<T> Queue()
         {
             Queue<T> queue = new Queue<T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                queue.Enqueue(uniqueKey);
+                queue.Enqueue(value);
             }
             queue.Clear();
             return queue;
@@ -217,9 +217,9 @@ namespace System.Collections
         public ConcurrentQueue<T> ConcurrentQueue()
         {
             ConcurrentQueue<T> concurrentQueue = new ConcurrentQueue<T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                concurrentQueue.Enqueue(uniqueKey);
+                concurrentQueue.Enqueue(value);
             }
             concurrentQueue.Clear();
             return concurrentQueue;
@@ -229,9 +229,9 @@ namespace System.Collections
         public ConcurrentBag<T> ConcurrentBag()
         {
             ConcurrentBag<T> concurrentBag = new ConcurrentBag<T>();
-            foreach (T uniqueKey in _keys)
+            foreach (T value in _uniqueValues)
             {
-                concurrentBag.Add(uniqueKey);
+                concurrentBag.Add(value);
             }
             concurrentBag.Clear();
             return concurrentBag;
