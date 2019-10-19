8ee7a1934c9735e3eedbab6db8ddb7c93ce60998
Emmanuel André <2341261+manandre@users.noreply.github.com>
2019-10-19 14:39:52 +0200

    Intoduce the classes in order they are referenced

--- src/benchmarks/micro/corefx/System.Threading.Tasks.Dataflow/Perf.Dataflow.cs
+++ src/benchmarks/micro/corefx/System.Threading.Tasks.Dataflow/Perf.Dataflow.cs
@@ -8,6 +8,129 @@ using MicroBenchmarks;
 
 namespace System.Threading.Tasks.Dataflow.Tests
 {
+    #region  Abstract classes
+    [BenchmarkCategory(Categories.CoreFX)]
+    public abstract class PerfTests<T> where T : IDataflowBlock
+    {
+        protected T block;
+
+        public abstract T CreateBlock();
+
+        [GlobalSetup]
+        public void BlockSetup()
+        {
+            block = CreateBlock();
+        }
+
+        [Benchmark]
+        public async Task Completion()
+        {
+            block.Complete();
+            await block.Completion;
+        }
+
+        protected static Task Post(ITargetBlock<int> target, bool retry = false) => Task.Run(() =>
+        {
+            for (int i = 0; i < 100_000; i++)
+            {
+                while (!target.Post(i) && retry) ;
+            }
+        });
+
+        protected static Task Receive<U>(ISourceBlock<U> source, int receiveSize = 1) => Task.Run(() =>
+        {
+            for (int i = 0; i < 100_000 / receiveSize; i++)
+            {
+                source.Receive();
+            }
+        });
+
+        protected static async Task SendAsync(ITargetBlock<int> target)
+        {
+            for (int i = 0; i < 100_000; i++)
+            {
+                await target.SendAsync(i);
+            }
+        }
+
+        protected static async Task ReceiveAsync<U>(ISourceBlock<U> source, int receiveSize = 1)
+        {
+            for (int i = 0; i < 100_000 / receiveSize; i++)
+            {
+                await source.ReceiveAsync();
+            }
+        }
+    }
+
+    [BenchmarkCategory(Categories.CoreFX)]
+    public abstract class SourceBlockPerfTests<T, U> : PerfTests<T> where T : ISourceBlock<U>
+    {
+        protected virtual int ReceiveSize { get; } = 1;
+        protected Task Receive() => Receive(block, ReceiveSize);
+        protected Task ReceiveAsync() => ReceiveAsync(block, ReceiveSize);
+    }
+
+    [BenchmarkCategory(Categories.CoreFX)]
+    public abstract class TargetPerfTests<T> : PerfTests<T> where T : ITargetBlock<int>
+    {
+        [Benchmark(OperationsPerInvoke = 100_000)]
+        public Task Post() => Post(block);
+
+        [Benchmark(OperationsPerInvoke = 100_000)]
+        public Task SendAsync() => SendAsync(block);
+    }
+
+    [BenchmarkCategory(Categories.CoreFX)]
+    public abstract class PropagatorPerfTests<T, U> : TargetPerfTests<IPropagatorBlock<int, U>> where T : IPropagatorBlock<int, U>
+    {
+        protected virtual int ReceiveSize { get; } = 1;
+        protected Task Receive() => Receive(block, ReceiveSize);
+        protected Task ReceiveAsync() => ReceiveAsync(block, ReceiveSize);
+
+        [Benchmark(OperationsPerInvoke = 100_000)]
+        public async Task PostReceiveSequential()
+        {
+            await Post();
+            await Receive();
+        }
+
+        [Benchmark(OperationsPerInvoke = 100_000)]
+        public async Task SendReceiveAsyncSequential()
+        {
+            await SendAsync();
+            await ReceiveAsync();
+        }
+
+        [Benchmark(OperationsPerInvoke = 100_000)]
+        public async Task PostReceiveParallel()
+        {
+            await Task.WhenAll(Post(), Receive());
+        }
+
+        [Benchmark(OperationsPerInvoke = 100_000)]
+        public async Task SendReceiveAsyncParallel()
+        {
+            await Task.WhenAll(SendAsync(), ReceiveAsync());
+        }
+    }
+
+    [BenchmarkCategory(Categories.CoreFX)]
+    public abstract class BoundedPropagatorPerfTests<T, U> : PerfTests<T> where T : IPropagatorBlock<int, U>
+    {
+        [Benchmark(OperationsPerInvoke = 100_000)]
+        public async Task PostReceiveParallel()
+        {
+            await Task.WhenAll(Post(block, retry: true), Receive(block));
+        }
+
+        [Benchmark(OperationsPerInvoke = 100_000)]
+        public async Task SendReceiveAsyncParallel()
+        {
+            await Task.WhenAll(SendAsync(block), ReceiveAsync(block));
+        }
+    }
+    #endregion
+
     #region BufferBlock
     public class UnboundedBufferBlockPerfTests : PropagatorPerfTests<IPropagatorBlock<int, int>, int>
     {
@@ -246,129 +369,6 @@ namespace System.Threading.Tasks.Dataflow.Tests
                 SendAsync(block.Target2),
                 ReceiveAsync()
             );
-        }
-    }
-    #endregion
-
-    #region  Abstract classes
-    [BenchmarkCategory(Categories.CoreFX)]
-    public abstract class PerfTests<T> where T : IDataflowBlock
-    {
-        protected T block;
-
-        public abstract T CreateBlock();
-
-        [GlobalSetup]
-        public void BlockSetup()
-        {
-            block = CreateBlock();
-        }
-
-        [Benchmark]
-        public async Task Completion()
-        {
-            block.Complete();
-            await block.Completion;
-        }
-
-        protected static Task Post(ITargetBlock<int> target, bool retry = false) => Task.Run(() =>
-        {
-            for (int i = 0; i < 100_000; i++)
-            {
-                while (!target.Post(i) && retry) ;
-            }
-        });
-
-        protected static Task Receive<U>(ISourceBlock<U> source, int receiveSize = 1) => Task.Run(() =>
-        {
-            for (int i = 0; i < 100_000 / receiveSize; i++)
-            {
-                source.Receive();
-            }
-        });
-
-        protected static async Task SendAsync(ITargetBlock<int> target)
-        {
-            for (int i = 0; i < 100_000; i++)
-            {
-                await target.SendAsync(i);
-            }
-        }
-
-        protected static async Task ReceiveAsync<U>(ISourceBlock<U> source, int receiveSize = 1)
-        {
-            for (int i = 0; i < 100_000 / receiveSize; i++)
-            {
-                await source.ReceiveAsync();
-            }
-        }
-    }
-
-    [BenchmarkCategory(Categories.CoreFX)]
-    public abstract class SourceBlockPerfTests<T, U> : PerfTests<T> where T : ISourceBlock<U>
-    {
-        protected virtual int ReceiveSize { get; } = 1;
-        protected Task Receive() => Receive(block, ReceiveSize);
-        protected Task ReceiveAsync() => ReceiveAsync(block, ReceiveSize);
-    }
-
-    [BenchmarkCategory(Categories.CoreFX)]
-    public abstract class TargetPerfTests<T> : PerfTests<T> where T : ITargetBlock<int>
-    {
-        [Benchmark(OperationsPerInvoke = 100_000)]
-        public Task Post() => Post(block);
-
-        [Benchmark(OperationsPerInvoke = 100_000)]
-        public Task SendAsync() => SendAsync(block);
-    }
-
-    [BenchmarkCategory(Categories.CoreFX)]
-    public abstract class PropagatorPerfTests<T, U> : TargetPerfTests<IPropagatorBlock<int, U>> where T : IPropagatorBlock<int, U>
-    {
-        protected virtual int ReceiveSize { get; } = 1;
-        protected Task Receive() => Receive(block, ReceiveSize);
-        protected Task ReceiveAsync() => ReceiveAsync(block, ReceiveSize);
-
-        [Benchmark(OperationsPerInvoke = 100_000)]
-        public async Task PostReceiveSequential()
-        {
-            await Post();
-            await Receive();
-        }
-
-        [Benchmark(OperationsPerInvoke = 100_000)]
-        public async Task SendReceiveAsyncSequential()
-        {
-            await SendAsync();
-            await ReceiveAsync();
-        }
-
-        [Benchmark(OperationsPerInvoke = 100_000)]
-        public async Task PostReceiveParallel()
-        {
-            await Task.WhenAll(Post(), Receive());
-        }
-
-        [Benchmark(OperationsPerInvoke = 100_000)]
-        public async Task SendReceiveAsyncParallel()
-        {
-            await Task.WhenAll(SendAsync(), ReceiveAsync());
-        }
-    }
-
-    [BenchmarkCategory(Categories.CoreFX)]
-    public abstract class BoundedPropagatorPerfTests<T, U> : PerfTests<T> where T : IPropagatorBlock<int, U>
-    {
-        [Benchmark(OperationsPerInvoke = 100_000)]
-        public async Task PostReceiveParallel()
-        {
-            await Task.WhenAll(Post(block, retry: true), Receive(block));
-        }
-
-        [Benchmark(OperationsPerInvoke = 100_000)]
-        public async Task SendReceiveAsyncParallel()
-        {
-            await Task.WhenAll(SendAsync(block), ReceiveAsync(block));
         }
     }
     #endregion
