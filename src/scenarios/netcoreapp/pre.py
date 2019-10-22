e53d9e667f15fd15c4f5ed46ee0b0cc95445278f
Olivia Chen <bache@microsoft.com>
2019-10-22 15:43:01 -0700

    change netcoreapp pre and test

--- src/scenarios/netcoreapp/pre.py
+++ src/scenarios/netcoreapp/pre.py
@@ -3,7 +3,6 @@ pre-command
 '''
 import sys
 from performance.logger import setup_loggers
-from shared import const
 from shared.precommands import PreCommands
 
 setup_loggers(True)
--- src/scenarios/netcoreapp/test.py
+++ src/scenarios/netcoreapp/test.py
@@ -2,20 +2,15 @@ import os
 from shared.runner import TestTraits, Runner
 from shared import const
 
-SCENARIO_NAME = 'NetCoreApp'
-PROJECT_FILE = os.path.join(SCENARIO_NAME, SCENARIO_NAME+'.csproj')
-
+SCENARIO_NAME = '.NET Core 2.0 Console Template'
+EXE_NAME = 'NetCoreApp'
 
 def main():
-    traits = TestTraits(scenarioname='NetCoreApp',
-                        exename=const.DOTNET,
-                        startupmetric='ProcessTime',
-                        appargs='build %s' % PROJECT_FILE,
+    traits = TestTraits(scenarioname=SCENARIO_NAME,
+                        exename=EXE_NAME,
                         guiapp='false',  # string passed through to tool
                         sdk=True,
                         startup=True,
-                        # iterationsetup='dotnet',
-                        # setupargs='clean %s' % PROJECT_FILE
                         )
     runner = Runner(traits)
     runner.parseargs()
