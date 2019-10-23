20c02a4737147500ba9b230371da7b1e7eea88ea
Olivia Chen <bache@microsoft.com>
2019-10-23 12:13:33 -0700

    add setup logger

--- src/scenarios/shared/sdk_iteration_setup.py
+++ src/scenarios/shared/sdk_iteration_setup.py
@@ -1,13 +1,16 @@
 from performance.common import remove_directory, copy_directory
 from shared import const
 from dotnet import shutdown_server
+from performance.logger import setup_loggers
+from logging import getLogger
 
 
 def main():
+    setup_loggers(True)
     shutdown_server(verbose=True)
-    print("Removing project directory...") # set up logger would create new log files, which is not necessary
+    getLogger().info("Removing project directory...") # set up logger would create new log files, which is not necessary
     remove_directory(const.APPDIR)
-    print("Copying clean project directory...")
+    getLogger().info("Copying clean project directory...")
     copy_directory(const.TMPDIR, const.APPDIR)
 
 
