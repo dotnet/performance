f59ee9084b9e29ff427c960108d7a0056181e933
Olivia Chen <bache@microsoft.com>
2019-10-24 14:21:01 -0700

    temp change for testing in CI: publish etl file to artifacts; disable other microbenchmarks

--- src/scenarios/netcoreapp/post.py
+++ src/scenarios/netcoreapp/post.py
@@ -4,8 +4,11 @@ post cleanup script
 
 from performance.common import remove_directory
 from shared.const import BINDIR, PUBDIR, APPDIR, TRACEDIR
- 
+from shutil import copytree
+
+copytree(TRACEDIR, "D:\\a\\1\\s\\artifacts\\log\\traces")
 remove_directory(BINDIR)
 remove_directory(PUBDIR)
 remove_directory(APPDIR)
 remove_directory(TRACEDIR)
+
