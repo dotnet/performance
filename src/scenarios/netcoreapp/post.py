491e25326dcbd6bf911d8a26a670ae12cf8f3447
Olivia Chen <bache@microsoft.com>
2019-10-24 16:08:50 -0700

    update artifacts folder path

--- src/scenarios/netcoreapp/post.py
+++ src/scenarios/netcoreapp/post.py
@@ -1,12 +1,14 @@
 '''
 post cleanup script
 '''
-
+import os
 from performance.common import remove_directory
 from shared.const import BINDIR, PUBDIR, APPDIR, TRACEDIR
 from shutil import copytree
 
-copytree(TRACEDIR, "D:\\a\\1\\s\\artifacts\\log\\traces")
+copytree(TRACEDIR, "D:\\a\\1\\s\\artifacts\\log\\x64_netcoreapp5.0_scenarios\\traces")
+files = os.listdir("D:\\a\\1\\s\\artifacts\\log\\x64_netcoreapp5.0_scenarios")
+print(files)
 remove_directory(BINDIR)
 remove_directory(PUBDIR)
 remove_directory(APPDIR)
