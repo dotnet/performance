18c129c273a5a2fe6d99bd58958185307b060aa3
Olivia Chen <bache@microsoft.com>
2019-10-25 11:54:35 -0700

    update artifacts folder

--- src/scenarios/netcoreapp/post.py
+++ src/scenarios/netcoreapp/post.py
@@ -6,9 +6,16 @@ from performance.common import remove_directory
 from shared.const import BINDIR, PUBDIR, APPDIR, TRACEDIR
 import shutil
 
-artifacts_dir = 'D:\\a\\1\\s\\artifacts'
+# artifacts_dir = 'E:\\performance-scenarios\\artifacts'
+# dest_dir = os.path.join(artifacts_dir, TRACEDIR)
+# shutil.copytree(TRACEDIR, dest_dir)
+
+
+artifacts_dir = 'E:\\performance-scenarios\\artifacts\\log'
 dest_dir = os.path.join(artifacts_dir, TRACEDIR)
 shutil.copytree(TRACEDIR, dest_dir)
+
+print(dest_dir)
 print(os.listdir(dest_dir))
 
 remove_directory(BINDIR)
