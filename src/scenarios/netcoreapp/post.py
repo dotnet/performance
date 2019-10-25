8c89e980a1c9d1f4224918dccfb8395bced3d7a2
Olivia Chen <bache@microsoft.com>
2019-10-25 11:30:45 -0700

    TEST ONLY: add publish trace files to job.yml

--- src/scenarios/netcoreapp/post.py
+++ src/scenarios/netcoreapp/post.py
@@ -6,11 +6,11 @@ from performance.common import remove_directory
 from shared.const import BINDIR, PUBDIR, APPDIR, TRACEDIR
 import shutil
 
-files = os.listdir(TRACEDIR)
-for file in files:
-    shutil.copyfile(os.path.join(TRACEDIR, file),
-                    os.path.join('D:\\a\\1\\s\\artifacts\\log\\x64_netcoreapp5.0_scenarios', file))
-print(os.listdir('D:\\a\\1\\s\\artifacts\\log\\x64_netcoreapp5.0_scenarios'))
+artifacts_dir = 'D:\\a\\1\\s\\artifacts'
+dest_dir = os.path.join(artifacts_dir, TRACEDIR)
+shutil.copytree(TRACEDIR, dest_dir)
+print(os.listdir(dest_dir))
+
 remove_directory(BINDIR)
 remove_directory(PUBDIR)
 remove_directory(APPDIR)
