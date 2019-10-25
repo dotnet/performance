0850040fc3e88c6397e7f37b699b1a188eac10e6
Olivia Chen <bache@microsoft.com>
2019-10-25 10:10:10 -0700

    update individual files to the artifacts folder

--- src/scenarios/netcoreapp/post.py
+++ src/scenarios/netcoreapp/post.py
@@ -4,11 +4,13 @@ post cleanup script
 import os
 from performance.common import remove_directory
 from shared.const import BINDIR, PUBDIR, APPDIR, TRACEDIR
-from shutil import copytree
+import shutil
 
-copytree(TRACEDIR, "D:\\a\\1\\s\\artifacts\\log\\x64_netcoreapp5.0_scenarios\\traces")
-files = os.listdir("D:\\a\\1\\s\\artifacts\\log\\x64_netcoreapp5.0_scenarios")
-print(files)
+files = os.listdir(TRACEDIR)
+for file in files:
+    shutil.copyfile(os.path.join(TRACEDIR, file),
+                    os.path.join('D:\\a\\1\\s\\artifacts\\log\\x64_netcoreapp5.0_scenarios', file))
+print(os.listdir('D:\\a\\1\\s\\artifacts\\log\\x64_netcoreapp5.0_scenarios'))
 remove_directory(BINDIR)
 remove_directory(PUBDIR)
 remove_directory(APPDIR)
