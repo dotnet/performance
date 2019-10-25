52d0cdb53394043e209aa119af6b09a7784c15ed
Olivia Chen <bache@microsoft.com>
2019-10-25 13:42:57 -0700

    add traversing root directory

--- src/scenarios/netcoreapp/post.py
+++ src/scenarios/netcoreapp/post.py
@@ -6,17 +6,20 @@ from performance.common import remove_directory
 from shared.const import BINDIR, PUBDIR, APPDIR, TRACEDIR
 import shutil
 
-# artifacts_dir = 'E:\\performance-scenarios\\artifacts'
-# dest_dir = os.path.join(artifacts_dir, TRACEDIR)
-# shutil.copytree(TRACEDIR, dest_dir)
 
+def traverse_dirs(root_dir):
+    # traverse root directory, and list directories as dirs and files as files
+    for root, dirs, files in os.walk(root_dir):
+        path = root.split(os.sep)
+        print((len(path) - 1) * '---', os.path.basename(root))
+        for file in files:
+            print(len(path) * '---', file)
 
-artifacts_dir = 'E:\\performance-scenarios\\artifacts\\log'
+artifacts_dir = 'D:\\a\\1\\s\\artifacts'
 dest_dir = os.path.join(artifacts_dir, TRACEDIR)
 shutil.copytree(TRACEDIR, dest_dir)
 
-print(dest_dir)
-print(os.listdir(dest_dir))
+traverse_dirs('D:\\a\\1\\s')
 
 remove_directory(BINDIR)
 remove_directory(PUBDIR)
