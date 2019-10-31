25605a71d8f9d94f3e10043c6c4597d9c644ecac
Bill Wert <billwert@microsoft.com>
2019-10-31 10:29:32 -0700

    add vb template

---
+++ src/scenarios/emptyvbconsoletemplate/test.py
@@ -0,0 +1,18 @@
+'''
+C# Console app
+'''
+import sys
+import os
+from shared.runner import TestTraits, Runner
+
+SCENARIONAME = 'Empty VB Console Template'
+EXENAME = 'emptycsconsoletemplate'
+
+if __name__ == "__main__":
+    traits = TestTraits(scenarioname=SCENARIONAME, 
+                        exename=EXENAME, 
+                        startupmetric='TimeToMain',
+                        startup=True,
+                        guiapp='false', # string passed through to tool
+                        )
+    Runner(traits).run()
