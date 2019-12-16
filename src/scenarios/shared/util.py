f883e0cc483a59d724f20f358eeb7a45d989551d
Olivia Chen <51934529+ooooolivia@users.noreply.github.com>
2019-12-16 15:19:01 -0800

    publish startup as self-contained app (#1089)
    * publish startup as self-contained app

--- src/scenarios/shared/util.py
+++ src/scenarios/shared/util.py
@@ -3,6 +3,7 @@ Utility routines
 '''
 import sys
 import os
+import platform
 from os import environ
 from shared import const
 from performance.constants import UPLOAD_TOKEN_VAR
@@ -36,3 +37,25 @@ def uploadtokenpresent():
 
 def runninginlab():
     return environ.get('PERFLAB_INLAB') is not None
+
+def getruntimeidentifier():
+    rid = None
+    if sys.platform == 'win32':
+        rid = 'win-'
+    elif sys.platform == 'linux' or sys.platform == 'linux2':
+        rid = 'linux-'
+    else:
+        raise Exception('Platform %s not supported.' % sys.platform)
+
+    if 'aarch64' in platform.machine() or os.environ.get('PERFLAB_BUILDARCH') == 'arm64':
+        rid += 'arm64'
+    elif platform.machine().endswith('64'):
+        rid += 'x64'
+    elif platform.machine().endswith('86'):
+        rid += 'x86'
+    else:
+        raise Exception('Machine %s not supported.' % platform.machine())
+
+    return rid
+
+
