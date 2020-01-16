6884e2f9624074ad8f5b3bac0a064f73a27b1354
David Cantu <dacantu@microsoft.com>
2020-01-15 19:35:56 -0800

    Add benchmarks for (de)serialization using ReferenceHandling.Preserve

---
+++ src/benchmarks/micro/corefx/System.Text.Json/Serializer/ReadPreservedReferences.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+using MicroBenchmarks.Serializers;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    [GenericTypeArguments(typeof(LoginViewModel))]
+    [GenericTypeArguments(typeof(Location))]
+    [GenericTypeArguments(typeof(IndexViewModel))]
+    [GenericTypeArguments(typeof(MyEventsListerViewModel))]
+    [GenericTypeArguments(typeof(SimpleListOfInt))]
+    [GenericTypeArguments(typeof(SimpleStructWithProperties))]
+    public class ReadPreservedReferences<T>
+    {
+        [Params(false, true)]
+        public bool IsDataPreserved;
+
+        private string _serialized;
+        private JsonSerializerOptions _options;
+        private JsonSerializerSettings _settings;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _options = new JsonSerializerOptions();
+
+            // Once the API is merged, replace this with _options.ReferenceHandling = ReferenceHandling.Preserve;
+            PropertyInfo referenceHandlingOption = _options.GetType().GetProperty("ReferenceHandling");
+            Type refHandlingType = referenceHandlingOption.PropertyType;
+            PropertyInfo preserve = refHandlingType.GetProperty("Preserve", BindingFlags.Public | BindingFlags.Static);
+            referenceHandlingOption.SetValue(_options, preserve.GetValue(null, null));
+
+            _settings = new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.All };
+
+            T value = DataGenerator.Generate<T>();
+
+            if (IsDataPreserved)
+            {
+                _serialized = JsonConvert.SerializeObject(value, _settings);
+            }
+            else
+            {
+                // Use payload that does not contain metadata in order to see what is the penalty of having ReferenceHandling.Preserve set.
+                _serialized = JsonConvert.SerializeObject(value);
+            }
+        }
+
+
+        [BenchmarkCategory(Categories.CoreFX, Categories.JSON)]
+        [Benchmark]
+        public T DeserializePreserved() => JsonSerializer.Deserialize<T>(_serialized, _options);
+
+        [BenchmarkCategory(Categories.ThirdParty, Categories.JSON)]
+        [Benchmark]
+        public T NewtonsoftDeserializePreserved() => JsonConvert.DeserializeObject<T>(_serialized, _settings);
+    }    
+}
---
+++ src/benchmarks/micro/corefx/System.Text.Json/Serializer/WritePreservedReferences.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+using MicroBenchmarks.Serializers;
+using Newtonsoft.Json;
+using System.Reflection;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    [GenericTypeArguments(typeof(LoginViewModel))]
+    [GenericTypeArguments(typeof(Location))]
+    [GenericTypeArguments(typeof(IndexViewModel))]
+    [GenericTypeArguments(typeof(MyEventsListerViewModel))]
+    [GenericTypeArguments(typeof(SimpleListOfInt))]
+    [GenericTypeArguments(typeof(SimpleStructWithProperties))]
+    public class WritePreservedReferences<T>
+    {
+        private T _value;
+        private JsonSerializerOptions _options;
+        private JsonSerializerSettings _settings;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _value = DataGenerator.Generate<T>();
+
+            _options = new JsonSerializerOptions();
+
+            PropertyInfo referenceHandlingOption = _options.GetType().GetProperty("ReferenceHandling");
+            Type refHandlingType = referenceHandlingOption.PropertyType;
+            PropertyInfo preserve = refHandlingType.GetProperty("Preserve", BindingFlags.Public | BindingFlags.Static);
+            referenceHandlingOption.SetValue(_options, preserve.GetValue(null, null));
+
+            _settings = new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.All };
+        }
+
+        [BenchmarkCategory(Categories.CoreFX, Categories.JSON)]
+        [Benchmark]
+        public string SerializePreserved() => JsonSerializer.Serialize(_value, _options);
+
+        [BenchmarkCategory(Categories.ThirdParty, Categories.JSON)]
+        [Benchmark]
+        public string NewtonsoftSerializePreserved() => JsonConvert.SerializeObject(_value, _settings);
+    }
+}
