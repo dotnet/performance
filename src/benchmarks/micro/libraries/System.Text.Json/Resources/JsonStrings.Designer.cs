2811e121b1852a068d9495892837fc2a6439a71c
Adam Sitnik <adam.sitnik@gmail.com>
2020-01-22 20:43:20 +0100

    fix ReadSingleSpanSequenceEmptyLoop benchmark

--- src/benchmarks/micro/libraries/System.Text.Json/Resources/JsonStrings.Designer.cs
+++ src/benchmarks/micro/libraries/System.Text.Json/Resources/JsonStrings.Designer.cs
@@ -40,7 +40,7 @@ namespace System.Text.Json.Tests
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MicroBenchmarks.corefx.System.Text.Json.Resources.JsonStrings", typeof(JsonStrings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MicroBenchmarks.libraries.System.Text.Json.Resources.JsonStrings", typeof(JsonStrings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
