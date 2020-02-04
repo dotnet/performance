506002b643e4589ef1f39da7cdd1a467ae7958ac
Olivia Chen <bache@microsoft.com>
2020-02-04 09:26:34 -0800

    set up client environment

--- src/scenarios/init.ps1
+++ src/scenarios/init.ps1
@@ -1,2 +1,31 @@
+Param(
+    $Channel = 'master' # default is master
+)
+
 $scripts = Join-Path $PSScriptRoot '..\..\scripts' -Resolve
 $env:PYTHONPATH="$scripts;$PSScriptRoot"
+$dotnetScript= Join-Path "$scripts" 'dotnet.py' -Resolve
+$dotnetDirectory= Join-Path $PSScriptRoot 'dotnet'
+
+if (Test-Path $dotnetDirectory){
+    Write-Host "Removing $dotnetDirectory ..."
+    Remove-Item  $dotnetDirectory -Recurse
+}
+
+Write-Host "Downloading dotnet from channel $Channel"
+python $dotnetScript install --channels $Channel --install-dir $dotnetDirectory
+
+if (!$?){
+    Write-Host "Dotnet installation failed."
+    Exit 1
+}
+
+$env:DOTNET_CLI_TELEMETRY_OPTOUT='1'
+$env:DOTNET_MULTILEVEL_LOOKUP='0'
+$env:UseSharedCompilation='false'
+$env:DOTNET_ROOT=$dotnetDirectory
+$env:Path="$dotnetDirectory;$env:Path"
+
+dotnet --info
+
+
--- src/scenarios/init.sh
+++ src/scenarios/init.sh
@@ -1,2 +1,26 @@
+if [[ -z $1 ]]; then
+    echo "Please specify a channel to download dotnet from; example: ./init.sh <channel>"
+    exit 1
+fi
+
 ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export PYTHONPATH=$PYTHONPATH:$ABSOLUTE_PATH/../../scripts:$ABSOLUTE_PATH
+SCRIPT_PATH="$ABSOLUTE_PATH/../../scripts"
+export PYTHONPATH=$PYTHONPATH:$SCRIPT_PATH:$ABSOLUTE_PATH
+DOTNET_SCRIPT="$SCRIPT_PATH/dotnet.py"
+DOTNET_DIRECTORY="$ABSOLUTE_PATH/dotnet"
+
+if [[ -d $DOTNET_DIRECTORY ]]; then
+    echo "Removing $DOTNET_DIRECTORY"
+    rm -r $DOTNET_DIRECTORY
+fi
+
+echo "Downloading dotnet from channel $1"
+python $DOTNET_SCRIPT install --channels $1 --install-dir $DOTNET_DIRECTORY
+
+export DOTNET_CLI_TELEMETRY_OPTOUT='1'
+export DOTNET_MULTILEVEL_LOOKUP='0'
+export UseSharedCompilation='false'
+export DOTNET_ROOT=$DOTNET_DIRECTORY
+export PATH="$DOTNET_DIRECTORY:$PATH"
+
+dotnet --info
