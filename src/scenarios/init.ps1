55af7ea05497ef5b7f3a3206a508ec7454e0bda2
Olivia Chen <bache@microsoft.com>
2020-02-25 15:45:24 -0800

    change default install location

--- src/scenarios/init.ps1
+++ src/scenarios/init.ps1
@@ -3,43 +3,32 @@ Param(
     [string] $DotnetDirectory
 )
 
+If ($DotnetDirectory -eq ""){
+    $DotnetDirectory = Join-Path $PSScriptRoot '..\..\tools\dotnet\x64'
+}
+
 # Add scripts and current directory to PYTHONPATH
 $scripts = Join-Path $PSScriptRoot '..\..\scripts' -Resolve
 $env:PYTHONPATH="$scripts;$PSScriptRoot"
 
-If (($InstallDotnetFromChannel -ne "") -and ($DotnetDirectory -eq "")){
+$channel = $InstallDotnetFromChannel
+
+If (($channel -ne "")){
     # Download dotnet from the specified channel
-    
-    $installDirectory= Join-Path $PSScriptRoot 'dotnet'
-    # Remove existing dotnet directory to make sure we only have one version of dotnet
-    If (Test-Path $installDirectory){
-        Write-Host "Removing $installDirectory ..."
-        Remove-Item  $installDirectory -Recurse
-    }
-
-    Write-Host "Downloading dotnet from channel $InstallDotnetFromChannel"
+    Write-Host "Downloading dotnet from channel $channel"
     $dotnetScript= Join-Path "$scripts" 'dotnet.py' -Resolve
-    python $dotnetScript install --channels $InstallDotnetFromChannel --install-dir $installDirectory
-
+    python $dotnetScript install --channels $channel
     If (!$?){
         Write-Host "Dotnet installation failed."
         Exit 1
     }
 }
 
-If ($DotnetDirectory -ne ""){
-    $env:Path="$DotnetDirectory;$env:Path"
-    $env:DOTNET_ROOT=$DotnetDirectory
-}
-Elseif ($installDirectory -ne ""){
-    $env:Path="$installDirectory;$env:Path"
-    $env:DOTNET_ROOT=$installDirectory
-}
-
+$env:Path="$DotnetDirectory;$env:Path"
+$env:DOTNET_ROOT=$DotnetDirectory
 $env:DOTNET_CLI_TELEMETRY_OPTOUT='1'
 $env:DOTNET_MULTILEVEL_LOOKUP='0'
 $env:UseSharedCompilation='false'
 
-dotnet --info
 
 
--- src/scenarios/init.sh
+++ src/scenarios/init.sh
@@ -3,7 +3,7 @@ absolutePath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 scriptPath="$absolutePath/../../scripts"
 export PYTHONPATH=$PYTHONPATH:$scriptPath:$absolutePath
 
-dotnetDirectory=""
+dotnetDirectory="$absolutePath/../../tools/dotnet/x64"
 channel=""
 # Parse arguments
 if [ "$1" == "-dotnetdir" ]
@@ -17,31 +17,13 @@ fi
 # Download dotnet from the specified channel
 if [ "$channel" != "" ]
 then
-    installDirectory="$absolutePath/dotnet"
-    # Remove existing dotnet directory to make sure we only have one version of dotnet 
-    if [ -d "$installDirectory" ]
-    then
-        echo "Removing $installDirectory"
-        rm -r $installDirectory
-    fi
     dotnetScript="$scriptPath/dotnet.py"
     echo "Downloading dotnet from channel $channel"
-    echo installDir: $installDirectory
-    python3 $dotnetScript install --channels $channel --install-dir $installDirectory
+    python3 $dotnetScript install --channels $channel -v
 fi
 
-if [ "$dotnetDirectory" != "" ]
-then
-    export DOTNET_ROOT=$dotnetDirectory
-    export PATH="$dotnetDirectory:$PATH"
-elif [ "$installDirectory" != "" ]
-then
-    export DOTNET_ROOT=$installDirectory
-    export PATH="$installDirectory:$PATH"
-fi
-
+export DOTNET_ROOT=$dotnetDirectory
+export PATH="$dotnetDirectory:$PATH"
 export DOTNET_CLI_TELEMETRY_OPTOUT='1'
 export DOTNET_MULTILEVEL_LOOKUP='0'
 export UseSharedCompilation='false'
-
-dotnet --info
