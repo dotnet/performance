916ebb3da9261cfdc886c9d5b28bef4380646f8f
Günther Foidl <gue@korporal.at>
2020-03-03 14:07:43 +0100

    Benchmarks for Math.Min/Max double/float overloads

---
+++ src/benchmarks/micro/runtime/Math/Functions/Double/MinDouble.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using BenchmarkDotNet.Attributes;
+
+namespace System.MathBenchmarks
+{
+    public partial class Double
+    {
+        // Tests Math.Min(double) over 5000 iterations for the domain -1, +1
+
+        private const double minDelta = 0.0004;
+        private const double minExpectedResult = 1.0;
+
+        [Benchmark]
+        public void Min() => MinTest();
+
+        public static void MinTest()
+        {
+            var result = 0.0; var val1 = 1.0; var val2 = 1.0 + minDelta;
+
+            for (var iteration = 0; iteration < MathTests.Iterations; iteration++)
+            {
+                val2 -= minDelta;
+                result += Math.Min(val1, val2);
+            }
+
+            var diff = Math.Abs(minExpectedResult - result);
+
+            if (diff > MathTests.DoubleEpsilon)
+            {
+                throw new Exception($"Expected Result {minExpectedResult,20:g17}; Actual Result {result,20:g17}");
+            }
+        }
+    }
+}
---
+++ src/benchmarks/micro/runtime/Math/Functions/Single/MinSingle.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using BenchmarkDotNet.Attributes;
+
+namespace System.MathBenchmarks
+{
+    public partial class Single
+    {
+        // Tests Math.Min(double) over 5000 iterations for the domain -1, +1
+
+        private const float minDelta = 0.0004f;
+        private const float minExpectedResult = 0.9267354f;
+
+        [Benchmark]
+        public void Min() => MinTest();
+
+        public static void MinTest()
+        {
+            var result = 0.0f; var val1 = 1.0f; var val2 = 1.0f + minDelta;
+
+            for (var iteration = 0; iteration < MathTests.Iterations; iteration++)
+            {
+                val2 -= minDelta;
+                result += Math.Min(val1, val2);
+            }
+
+            var diff = Math.Abs(minExpectedResult - result);
+
+            if (diff > MathTests.SingleEpsilon)
+            {
+                throw new Exception($"Expected Result {minExpectedResult,10:g9}; Actual Result {result,10:g9}");
+            }
+        }
+    }
+}
