6960077e16debd6ed4f0c0a3e02b330794630f53
Drew Scoggins <anscoggi@microsoft.com>
2020-04-01 16:42:32 -0700

    Add error detection

--- scripts/parse_props.py
+++ scripts/parse_props.py
@@ -1,4 +1,10 @@
 import xml.etree.ElementTree as ET
+import os
+
+if not os.path.exists('eng/Versions.props'):
+    raise  ValueError("Versions.props does not exist")
 tree = ET.parse('eng/Versions.props')
 root = tree.getroot().find("PropertyGroup/MicrosoftDotnetSdkInternalPackageVersion")
+if root == None:
+    raise ValueError("Structure of Versions.props has changed")
 print("##vso[task.setvariable variable=DotnetVersion;isSecret=false;isOutput=false]--dotnet-versions {}".format(root.text))
