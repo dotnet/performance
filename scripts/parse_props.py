1bc6b55d4a393b5ae9bb7f447b18ae087db67542
Drew Scoggins <anscoggi@microsoft.com>
2020-04-06 13:30:43 -0700

    Return empty string for vso variable

--- scripts/parse_props.py
+++ scripts/parse_props.py
@@ -4,16 +4,17 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Get the branch name.')
 parser.add_argument('--branch-name', type=str, dest='branch',
-                   help='an integer for the accumulator')
+                   help='Name of the branch we are running')
 
 args = parser.parse_args()
 
 if not args.branch == "master":
-    exit(0)
-if not os.path.exists('eng/Versions.props'):
-    raise  ValueError("Versions.props does not exist")
-tree = ET.parse('eng/Versions.props')
-root = tree.getroot().find("PropertyGroup/MicrosoftDotnetSdkInternalPackageVersion")
-if root == None:
-    raise ValueError("Structure of Versions.props has changed")
-print("##vso[task.setvariable variable=DotnetVersion;isSecret=false;isOutput=false]--dotnet-versions {}".format(root.text))
+    print("##vso[task.setvariable variable=DotnetVersion;isSecret=false;isOutput=false]")
+else:
+    if not os.path.exists('eng/Versions.props'):
+        raise  ValueError("Versions.props does not exist")
+    tree = ET.parse('eng/Versions.props')
+    root = tree.getroot().find("PropertyGroup/MicrosoftDotnetSdkInternalPackageVersion")
+    if root == None:
+        raise ValueError("Structure of Versions.props has changed")
+    print("##vso[task.setvariable variable=DotnetVersion;isSecret=false;isOutput=false]--dotnet-versions {}".format(root.text))
