69bebaddf0350e8a00e4f7727b9dddef0aa36708
Clinton Ingram <clinton.ingram@outlook.com>
2020-04-08 03:44:18 -0700

    Add Vector<T> conversion benchmarks (#1267)
    * add Vector<T> conversion benchmarks
    
    * address feedback

--- src/harness/BenchmarkDotNet.Extensions/ValuesGenerator.cs
+++ src/harness/BenchmarkDotNet.Extensions/ValuesGenerator.cs
@@ -48,9 +48,9 @@ namespace BenchmarkDotNet.Extensions
 
             var result = new T[count];
 
-            if (typeof(T) == typeof(byte))
+            if (typeof(T) == typeof(byte) || typeof(T) == typeof(sbyte))
             {
-                random.NextBytes((byte[])(object)result);
+                random.NextBytes(Unsafe.As<byte[]>(result));
             }
             else
             {
@@ -84,19 +84,26 @@ namespace BenchmarkDotNet.Extensions
         {
             if (typeof(T) == typeof(char))
                 return (T)(object)(char)random.Next(char.MinValue, char.MaxValue);
+            if (typeof(T) == typeof(short))
+                return (T)(object)(short)random.Next(short.MaxValue);
+            if (typeof(T) == typeof(ushort))
+                return (T)(object)(ushort)random.Next(short.MaxValue);
             if (typeof(T) == typeof(int))
                 return (T)(object)random.Next();
+            if (typeof(T) == typeof(uint))
+                return (T)(object)(uint)random.Next();
+            if (typeof(T) == typeof(long))
+                return (T)(object)(long)random.Next();
+            if (typeof(T) == typeof(ulong))
+                return (T)(object)(ulong)random.Next();
+            if (typeof(T) == typeof(float))
+                return (T)(object)(float)random.NextDouble();
             if (typeof(T) == typeof(double))
                 return (T)(object)random.NextDouble();
             if (typeof(T) == typeof(bool))
                 return (T)(object)(random.NextDouble() > 0.5);
             if (typeof(T) == typeof(string))
-                return (T) (object) GenerateRandomString(random, 1, 50);
-            if (typeof(T) == typeof(uint) || typeof(T) == typeof(ulong))
-            {
-                int randomValue = random.Next(0, int.MaxValue);
-                return Unsafe.As<int, T>(ref randomValue);
-            }
+                return (T)(object)GenerateRandomString(random, 1, 50);
 
             throw new NotImplementedException($"{typeof(T).Name} is not implemented");
         }
