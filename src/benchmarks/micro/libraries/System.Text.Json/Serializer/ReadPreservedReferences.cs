25ee7abc3cb8a2dbb678758e3affe61dcc8e51aa
Drew Scoggins <anscoggi@microsoft.com>
2020-06-02 18:20:29 -0700

    Update json tests (#1345)
    * Update JSON tests to use new property name
    
    * Remove JSON tests
    
    Because of codeflow issues these tests having a breaking change have shutdown the lab.
    I am commenting these test out until code has flowed around and we can build the tests
    again.

--- src/benchmarks/micro/libraries/System.Text.Json/Serializer/ReadPreservedReferences.cs
+++ src/benchmarks/micro/libraries/System.Text.Json/Serializer/ReadPreservedReferences.cs
@@ -1,7 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
+/*
 using BenchmarkDotNet.Attributes;
 using MicroBenchmarks;
 using MicroBenchmarks.Serializers;
@@ -53,3 +53,4 @@ namespace System.Text.Json.Serialization.Tests
         public T NewtonsoftDeserializePreserved() => JsonConvert.DeserializeObject<T>(_serialized, _settings);
     }    
 }
+*/
