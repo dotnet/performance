1277c93e16dd7b06d34688d70c02a2f8a3710717
Bill Wert <billwert@microsoft.com>
2020-06-18 13:26:51 -0700

    add aggregates for file types

--- src/tools/ScenarioMeasurement/SizeOnDisk/SizeOnDisk.cs
+++ src/tools/ScenarioMeasurement/SizeOnDisk/SizeOnDisk.cs
@@ -42,6 +42,7 @@ namespace ScenarioMeasurement
             long totalSize = 0;
             int totalCount = 0;
             bool directoryIsTop = dirs.Length > 1; // if we were asked to log more than one directory, include the summary info as top counters.
+            var extensionBuckets = new Dictionary<string, (long size, int count)>();
             foreach (var directory in directories)
             {
                 var resultSize = directory.Value.Values.Sum();
@@ -53,8 +54,23 @@ namespace ScenarioMeasurement
                 counters.Add(new Counter { MetricName = "count", TopCounter = directoryIsTop, Name = $"{RemoveVersions(directory.Key)} - Count", Results = new[] { (double)resultCount } });
                 foreach (var file in directory.Value)
                 {
-                    counters.Add(new Counter { MetricName = "bytes", Name = $"{Path.Join(RemoveVersions(directory.Key), RemoveVersions(file.Key))}", Results = new[] { (double)file.Value } });
+                    var fileName = RemoveVersions(file.Key);
+                    var extension =  $"Aggregate - {Path.GetExtension(fileName).Substring(1)}";
+                    if(!extensionBuckets.ContainsKey(extension)) 
+                    {
+                        extensionBuckets.Add(extension, (0,0));
+                    }
+                    var bucket = extensionBuckets[extension];
+                    bucket.size += file.Value;
+                    bucket.count++;
+                    extensionBuckets[extension] = bucket;
+                    counters.Add(new Counter { MetricName = "bytes", Name = $"{Path.Join(RemoveVersions(directory.Key), fileName)}", Results = new[] { (double)file.Value } });
                 }
+            }
+            foreach(var bucket in extensionBuckets)
+            {
+                    counters.Add(new Counter { MetricName = "bytes", Name = bucket.Key, Results = new[] { (double)bucket.Value.size } });
+                    counters.Add(new Counter { MetricName = "count", Name = $"{bucket.Key} - Count", Results = new[] { (double)bucket.Value.count } });
             }
             counters.Add(new Counter { MetricName = "bytes", Name = scenarioName, DefaultCounter = true, TopCounter = true, Results = new[] { (double)totalSize } });
             counters.Add(new Counter { MetricName = "count", Name = $"{scenarioName} - Count", TopCounter = true, Results = new[] { (double)totalCount } });
