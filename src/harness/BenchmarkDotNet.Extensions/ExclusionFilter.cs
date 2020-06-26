43eb449ff99e1d446f261fe38671ad89dabf2f93
Drew Scoggins <anscoggi@microsoft.com>
2020-06-26 10:12:42 -0700

    Remove extra using statements

--- src/harness/BenchmarkDotNet.Extensions/ExclusionFilter.cs
+++ src/harness/BenchmarkDotNet.Extensions/ExclusionFilter.cs
@@ -2,9 +2,7 @@
 using BenchmarkDotNet.Running;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace BenchmarkDotNet.Extensions
 {
