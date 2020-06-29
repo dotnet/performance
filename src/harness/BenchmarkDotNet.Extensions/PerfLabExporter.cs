d97335e807034ddf4cb8d932bd9d0782677acac5
Drew Scoggins <anscoggi@microsoft.com>
2020-06-28 22:30:43 -0700

    Disasm diff (#1379)
    * add --disasm-diff argument
    
    * Add output to perflab json
    
    * Update src/harness/BenchmarkDotNet.Extensions/PerfLabExporter.cs
    
    Remove extra new lines
    
    Co-authored-by: Adam Sitnik <adam.sitnik@gmail.com>
    
    Co-authored-by: Adam Sitnik <adam.sitnik@gmail.com>

--- src/harness/BenchmarkDotNet.Extensions/PerfLabExporter.cs
+++ src/harness/BenchmarkDotNet.Extensions/PerfLabExporter.cs
@@ -2,18 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Loggers;
-using BenchmarkDotNet.Parameters;
 using BenchmarkDotNet.Reports;
-using BenchmarkDotNet.Running;
 using Reporting;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 
 namespace BenchmarkDotNet.Extensions
 {
@@ -21,14 +15,20 @@ namespace BenchmarkDotNet.Extensions
     {
         protected override string FileExtension => "json";
         protected override string FileCaption => "perf-lab-report";
+
         public PerfLabExporter()
         {
         }
+
         public override void ExportToLog(Summary summary, ILogger logger)
         {
             var reporter = Reporter.CreateReporter();
             if (!reporter.InLab) // not running in the perf lab
                 return;
+
+            DisassemblyDiagnoser disassemblyDiagnoser = summary.Reports
+                .FirstOrDefault()? // dissasembler was either enabled for all or none of them (so we use the first one)
+                .BenchmarkCase.Config.GetDiagnosers().OfType<DisassemblyDiagnoser>().FirstOrDefault();
 
             foreach (var report in summary.Reports)
             {
@@ -84,6 +84,13 @@ namespace BenchmarkDotNet.Extensions
                         MetricName = m.Descriptor.Unit,
                         Results = new[] { m.Value }
                     });
+                }
+
+                if (disassemblyDiagnoser != null && disassemblyDiagnoser.Results.TryGetValue(report.BenchmarkCase, out var disassemblyResult))
+                {
+                    string disassembly = DiffableDisassemblyExporter.BuildDisassemblyString(disassemblyResult, disassemblyDiagnoser.Config);
+
+                    test.AddData("disasm", disassembly);
                 }
 
                 reporter.AddTest(test);
