0f51d0d7fd4208e6b35ff82f1abe95146eed1fda
Olivia Chen <51934529+ooooolivia@users.noreply.github.com>
2020-07-12 13:13:17 -0700

    Modify linker parameter (#1393)
    * add publish param
    
    * change linker dump
    
    * nit fix
    
    * add back old linker param
    
    * revert dotnet.py and use positional args in precommands.py
    
    * add check null

--- src/scenarios/blazor/test.py
+++ src/scenarios/blazor/test.py
@@ -8,8 +8,16 @@ from shared.const import APPDIR
 EXENAME = 'emptyblazorwasmtemplate'
 
 if __name__ == "__main__":
+    net5_linker_dump = os.path.join(APPDIR, 'obj', 'Release', 'net5.0', 'browser-wasm', 'linked', 'linker-dependencies.xml.gz')
+    netstandard21_linker_dump = os.path.join(APPDIR, 'obj', 'Release', 'netstandard2.1', 'blazor', 'linker', 'linker-dependencies.xml.gz')
+    artifact=None
+    if os.path.exists(net5_linker_dump):
+        artifact = net5_linker_dump
+    elif os.path.exists(netstandard21_linker_dump):
+        artifact = netstandard21_linker_dump
+
     traits = TestTraits(exename=EXENAME, 
                         guiapp='false',
-                        artifact=os.path.join(APPDIR, 'obj', 'Release', 'netstandard2.1', 'blazor', 'linker', 'linker-dependencies.xml.gz')
+                        artifact=artifact
                         )
     Runner(traits).run()
