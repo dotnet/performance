9b50a223a8c29e8aa42708a919d06ecb02a77295
Adam Sitnik <adam.sitnik@gmail.com>
2020-08-18 15:04:13 +0200

    use more descriptive names

--- src/benchmarks/micro/libraries/System.Utf8String.Experimental/Perf.AsciiUtility.cs
+++ src/benchmarks/micro/libraries/System.Utf8String.Experimental/Perf.AsciiUtility.cs
@@ -11,38 +11,50 @@ namespace System.Text.Experimental
     [BenchmarkCategory(Categories.Libraries, Categories.Runtime)]
     public class Perf_AsciiUtility
     {
-        protected string ascii_11;
-        protected Utf8String ascii_11_ustring;
-        protected string nonascii_110;
-        protected Utf8String nonascii_110_ustring;
-        protected string nonascii_chinese;
-        protected Utf8String nonascii_chinese_ustring;
-        protected string nonascii_cyrillic;
-        protected Utf8String nonascii_cyrillic_ustring;
-        protected string nonascii_greek;
-        protected Utf8String nonascii_greek_ustring;
+        // the benchmark uses text files from Project Gutenberg
+        public enum InputFile
+        {
+            EnglishAllAscii, // English, all-ASCII, should stay entirely within fast paths
+            EnglishMostlyAscii, // English, mostly ASCII with some rare non-ASCII chars, exercises that the occasional non-ASCII char doesn't kill our fast paths
+            Chinese, // Chinese, exercises 3-byte scalar processing paths typical of East Asian languages
+            Cyrillic, // Cyrillic, exercises a combination of ASCII and 2-byte scalar processing paths
+            Greek, // Greek, similar to the Cyrillic case but with a different distribution of ASCII and non-ASCII chars
+        }
+
+        private string _englishAllAsciiUnicode;
+        private Utf8String _englishAllAsciiUtf8;
+        private string _englishMostlyAsciiUnicode;
+        private Utf8String _englishMostlyAsciiUtf8;
+        private string _chineseUnicode;
+        private Utf8String _chineseUtf8;
+        private string _cyrillicUnicode;
+        private Utf8String _cyrillicUtf8;
+        private string _greekUnicode;
+        private Utf8String _greekUtf8;
 
         [GlobalSetup]
         public void Setup()
         {
-            string path = Path.Combine(Environment.CurrentDirectory, "libraries", "System.Utf8String.Experimental");
-            ascii_11 = File.ReadAllText(Path.Combine(path, "11.txt"));
-            ascii_11_ustring = new Utf8String(ascii_11);
-            nonascii_110 = File.ReadAllText(Path.Combine(path, "11-0.txt"));
-            nonascii_110_ustring = new Utf8String(nonascii_110);
-            nonascii_chinese = File.ReadAllText(Path.Combine(path, "25249-0.txt"));
-            nonascii_chinese_ustring = new Utf8String(nonascii_chinese);
-            nonascii_cyrillic = File.ReadAllText(Path.Combine(path, "30774-0.txt"));
-            nonascii_cyrillic_ustring = new Utf8String(nonascii_cyrillic);
-            nonascii_greek = File.ReadAllText(Path.Combine(path, "39251-0.txt"));
-            nonascii_greek_ustring = new Utf8String(nonascii_greek);
+            _englishAllAsciiUnicode = LoadFile(InputFile.EnglishAllAscii);
+            _englishAllAsciiUtf8 = new Utf8String(_englishAllAsciiUnicode);
+            _englishMostlyAsciiUnicode = LoadFile(InputFile.EnglishMostlyAscii);
+            _englishMostlyAsciiUtf8 = new Utf8String(_englishMostlyAsciiUnicode);
+            _chineseUnicode = LoadFile(InputFile.Chinese);
+            _chineseUtf8 = new Utf8String(_chineseUnicode);
+            _cyrillicUnicode = LoadFile(InputFile.Cyrillic);
+            _cyrillicUtf8 = new Utf8String(_cyrillicUnicode);
+            _greekUnicode = LoadFile(InputFile.Greek);
+            _greekUtf8 = new Utf8String(_greekUnicode);
         }
+
+        private string LoadFile(InputFile inputFile) => File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "libraries", "System.Utf8String.Experimental", $"{inputFile}.txt"));
+
 
         [Benchmark]
         public int ToUtf16_nonascii_110()
         {
-            string str = nonascii_110;
-            Utf8Span span = new Utf8Span(nonascii_110_ustring);
+            string str = _englishMostlyAsciiUnicode;
+            Utf8Span span = new Utf8Span(_englishMostlyAsciiUtf8);
             Memory<char> memory = new char[str.Length];
             Span<char> destination = memory.Span;
             return span.ToChars(destination);
@@ -51,8 +63,8 @@ namespace System.Text.Experimental
         [Benchmark]
         public int ToUtf16_nonascii_chinese()
         {
-            string str = nonascii_chinese;
-            Utf8Span span = new Utf8Span(nonascii_chinese_ustring);
+            string str = _chineseUnicode;
+            Utf8Span span = new Utf8Span(_chineseUtf8);
             Memory<char> memory = new char[str.Length];
             Span<char> destination = memory.Span;
             return span.ToChars(destination);
@@ -61,8 +73,8 @@ namespace System.Text.Experimental
         [Benchmark]
         public int ToUtf16_nonascii_cyrillic()
         {
-            string str = nonascii_cyrillic;
-            Utf8Span span = new Utf8Span(nonascii_cyrillic_ustring);
+            string str = _cyrillicUnicode;
+            Utf8Span span = new Utf8Span(_cyrillicUtf8);
             Memory<char> memory = new char[str.Length];
             Span<char> destination = memory.Span;
             return span.ToChars(destination);
@@ -71,8 +83,8 @@ namespace System.Text.Experimental
         [Benchmark]
         public int ToUtf16_nonascii_greek()
         {
-            string str = nonascii_greek;
-            Utf8Span span = new Utf8Span(nonascii_greek_ustring);
+            string str = _greekUnicode;
+            Utf8Span span = new Utf8Span(_greekUtf8);
             Memory<char> memory = new char[str.Length];
             Span<char> destination = memory.Span;
             return span.ToChars(destination);
@@ -81,22 +93,22 @@ namespace System.Text.Experimental
         [Benchmark]
         public bool IsAscii_GetIndexOfFirstNonAsciiByte()
         {
-            Utf8Span ascii_11_span = new Utf8Span(ascii_11_ustring);
+            Utf8Span ascii_11_span = new Utf8Span(_englishAllAsciiUtf8);
             return ascii_11_span.IsAscii();
         }
 
         [Benchmark]
         public bool IsNormalized_GetIndexOfFirstNonAsciiChar()
         {
-            bool b1 = ascii_11.IsNormalized();
-            bool b2 = ascii_11.IsNormalized();
+            bool b1 = _englishAllAsciiUnicode.IsNormalized();
+            bool b2 = _englishAllAsciiUnicode.IsNormalized();
             return b1 & b2;
         }
 
         [Benchmark]
         public void IsNormalized_WidenAsciiToUtf16()
         {
-            Utf8Span ascii_11_span = new Utf8Span(ascii_11_ustring);
+            Utf8Span ascii_11_span = new Utf8Span(_englishAllAsciiUtf8);
             char[] returned = ascii_11_span.ToCharArray();
             char[] second = ascii_11_span.ToCharArray();
             if (returned[0] != second[0])
