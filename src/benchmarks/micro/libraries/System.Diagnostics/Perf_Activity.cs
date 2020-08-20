d1d625488dc830aee840f3ef471ceeb5163439e4
Mikel Blanchard <mblanchard@macrosssoftware.com>
2020-08-19 21:25:18 -0700

    Code review.

--- src/benchmarks/micro/libraries/System.Diagnostics/Perf_Activity.cs
+++ src/benchmarks/micro/libraries/System.Diagnostics/Perf_Activity.cs
@@ -4,11 +4,7 @@
 
 using BenchmarkDotNet.Attributes;
 using MicroBenchmarks;
-using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
 
 namespace System.Diagnostics
 {
@@ -18,8 +14,10 @@ namespace System.Diagnostics
     {
         private static readonly ActivitySource s_ActivitySource;
         private static readonly ActivityListener s_ActivityListener;
-        private static readonly Activity s_Activity;
-        private static readonly ActivityLink s_ActivityLink;
+        private static readonly Activity s_ActivitySmall;
+        private static readonly Activity s_ActivityLarge;
+        private static readonly ActivityLink s_ActivityLinkSmall;
+        private static readonly ActivityLink s_ActivityLinkLarge;
 
         static Perf_Activity()
         {
@@ -32,9 +30,18 @@ namespace System.Diagnostics
                 GetRequestedDataUsingContext = (ref ActivityCreationOptions<ActivityContext> o) => ActivityDataRequest.AllDataAndRecorded
             };
 
+            Dictionary<string, object> LargeTagSet = new Dictionary<string, object>();
+            for (int i = 0; i < 1024; i++)
+            {
+                if (i % 2 == 0)
+                    LargeTagSet.Add($"Key{i}", i);
+                else
+                    LargeTagSet.Add($"Key{i}", i.ToString());
+            }
+
             ActivitySource.AddActivityListener(s_ActivityListener);
 
-            s_Activity = s_ActivitySource.StartActivity(
+            s_ActivitySmall = s_ActivitySource.StartActivity(
                 "TestActivity",
                 ActivityKind.Internal,
                 parentContext: default,
@@ -53,14 +60,34 @@ namespace System.Diagnostics
                     new ActivityLink(default),
                 });
 
-            s_Activity.AddEvent(new ActivityEvent("TestEvent1"));
-            s_Activity.AddEvent(new ActivityEvent("TestEvent2"));
-            s_Activity.AddEvent(new ActivityEvent("TestEvent3"));
-            s_Activity.AddEvent(new ActivityEvent("TestEvent4"));
+            s_ActivitySmall.AddEvent(new ActivityEvent("TestEvent1"));
+            s_ActivitySmall.AddEvent(new ActivityEvent("TestEvent2"));
+            s_ActivitySmall.AddEvent(new ActivityEvent("TestEvent3"));
+            s_ActivitySmall.AddEvent(new ActivityEvent("TestEvent4"));
 
-            s_Activity.Stop();
+            s_ActivitySmall.Stop();
 
-            s_ActivityLink = new ActivityLink(
+            ActivityLink[] LargeLinkSet = new ActivityLink[1024];
+            for (int i = 0; i < 1024; i++)
+            {
+                LargeLinkSet[i] = new ActivityLink(default);
+            }
+
+            s_ActivityLarge = s_ActivitySource.StartActivity(
+                "TestActivity",
+                ActivityKind.Internal,
+                parentContext: default,
+                tags: LargeTagSet,
+                links: LargeLinkSet);
+
+            for (int i = 0; i < 1024; i++)
+            {
+                s_ActivityLarge.AddEvent(new ActivityEvent($"TestEvent{i}"));
+            }
+
+            s_ActivityLarge.Stop();
+
+            s_ActivityLinkSmall = new ActivityLink(
                 default,
                 new ActivityTagsCollection(
                     new Dictionary<string, object>
@@ -70,303 +97,89 @@ namespace System.Diagnostics
                         ["tag3"] = "string2",
                         ["tag4"] = false,
                     }));
+
+            s_ActivityLinkLarge = new ActivityLink(
+                default,
+                new ActivityTagsCollection(LargeTagSet));
         }
 
-        [Params(5000)]
-        public int NumberOfActivities { get; set; }
-
         [Benchmark]
-        public void EnumerateActivityTags()
+        public void EnumerateActivityTagsSmall()
         {
-            int total = 0;
-
-            for (int i = 0; i < NumberOfActivities; i++)
+            foreach (var _ in s_ActivitySmall.Tags)
             {
-                foreach (var tagString in s_Activity.Tags)
-                {
-                    total++;
-                }
             }
         }
 
         [Benchmark]
-        public void EnumerateActivityTagObjects()
+        public void EnumerateActivityTagsLarge()
         {
-            int total = 0;
-
-            for (int i = 0; i < NumberOfActivities; i++)
+            foreach (var _ in s_ActivityLarge.Tags)
             {
-                foreach (var tagObject in s_Activity.TagObjects)
-                {
-                    total++;
-                }
             }
         }
 
         [Benchmark]
-        public void OTelHelperActivityTagObjects()
+        public void EnumerateActivityTagObjectsSmall()
         {
-            int total = 0;
-
-            Enumerator<IEnumerable<KeyValuePair<string, object>>, KeyValuePair<string, object>, int>.AllocationFreeForEachDelegate foreachDelegate
-                = Enumerator<IEnumerable<KeyValuePair<string, object>>, KeyValuePair<string, object>, int>.FindAllocationFreeForEach(s_Activity.TagObjects);
-
-            for (int i = 0; i < NumberOfActivities; i++)
+            foreach (var _ in s_ActivitySmall.TagObjects)
             {
-                foreachDelegate(s_Activity.TagObjects, ref total, ForeachTagObjectRef);
-            }
-        }
-
-        private static readonly Enumerator<IEnumerable<KeyValuePair<string, object>>, KeyValuePair<string, object>, int>.ForEachDelegate ForeachTagObjectRef = ForeachTagObject;
-
-        private static bool ForeachTagObject(ref int total, KeyValuePair<string, object> tag)
-        {
-            total++;
-            return true;
-        }
-
-        [Benchmark]
-        public void EnumerateActivityLinks()
-        {
-            int total = 0;
-
-            for (int i = 0; i < NumberOfActivities; i++)
-            {
-                foreach (var link in s_Activity.Links)
-                {
-                    total++;
-                }
             }
         }
 
         [Benchmark]
-        public void OTelHelperActivityLinks()
+        public void EnumerateActivityTagObjectsLarge()
         {
-            int total = 0;
-
-            Enumerator<IEnumerable<ActivityLink>, ActivityLink, int>.AllocationFreeForEachDelegate foreachDelegate
-                = Enumerator<IEnumerable<ActivityLink>, ActivityLink, int>.FindAllocationFreeForEach(s_Activity.Links);
-
-            for (int i = 0; i < NumberOfActivities; i++)
+            foreach (var _ in s_ActivityLarge.TagObjects)
             {
-                foreachDelegate(s_Activity.Links, ref total, ForeachLinkRef);
-            }
-        }
-
-        private static readonly Enumerator<IEnumerable<ActivityLink>, ActivityLink, int>.ForEachDelegate ForeachLinkRef = ForeachLink;
-
-        private static bool ForeachLink(ref int total, ActivityLink link)
-        {
-            total++;
-            return true;
-        }
-
-        [Benchmark]
-        public void EnumerateActivityEvents()
-        {
-            int total = 0;
-
-            for (int i = 0; i < NumberOfActivities; i++)
-            {
-                foreach (var @event in s_Activity.Events)
-                {
-                    total++;
-                }
             }
         }
 
         [Benchmark]
-        public void OTelHelperActivityEvents()
+        public void EnumerateActivityLinksSmall()
         {
-            int total = 0;
-
-            Enumerator<IEnumerable<ActivityEvent>, ActivityEvent, int>.AllocationFreeForEachDelegate foreachDelegate
-                = Enumerator<IEnumerable<ActivityEvent>, ActivityEvent, int>.FindAllocationFreeForEach(s_Activity.Events);
-
-            for (int i = 0; i < NumberOfActivities; i++)
+            foreach (var _ in s_ActivitySmall.Links)
             {
-                foreachDelegate(s_Activity.Events, ref total, ForeachEventRef);
-            }
-        }
-
-        private static readonly Enumerator<IEnumerable<ActivityEvent>, ActivityEvent, int>.ForEachDelegate ForeachEventRef = ForeachEvent;
-
-        private static bool ForeachEvent(ref int total, ActivityEvent @event)
-        {
-            total++;
-            return true;
-        }
-
-        [Benchmark]
-        public void EnumerateActivityLinkTags()
-        {
-            int total = 0;
-
-            for (int i = 0; i < NumberOfActivities; i++)
-            {
-                foreach (var tag in s_ActivityLink.Tags)
-                {
-                    total++;
-                }
             }
         }
 
         [Benchmark]
-        public void OTelHelperActivityLinkTags()
+        public void EnumerateActivityLinksLarge()
         {
-            int total = 0;
-
-            Enumerator<IEnumerable<KeyValuePair<string, object>>, KeyValuePair<string, object>, int>.AllocationFreeForEachDelegate foreachDelegate
-                = Enumerator<IEnumerable<KeyValuePair<string, object>>, KeyValuePair<string, object>, int>.FindAllocationFreeForEach(s_ActivityLink.Tags);
-
-            for (int i = 0; i < NumberOfActivities; i++)
+            foreach (var _ in s_ActivityLarge.Links)
             {
-                foreachDelegate(s_ActivityLink.Tags, ref total, ForeachTagObjectRef);
             }
         }
 
-        // A helper class for enumerating over IEnumerable<TItem> without allocation if a struct enumerator is available.
-        private class Enumerator<TEnumerable, TItem, TState>
-            where TEnumerable : IEnumerable<TItem>
-            where TState : struct
+        [Benchmark]
+        public void EnumerateActivityEventsSmall()
         {
-            private static readonly MethodInfo GenericGetEnumeratorMethod = typeof(IEnumerable<TItem>).GetMethod("GetEnumerator");
-            private static readonly MethodInfo GeneircCurrentGetMethod = typeof(IEnumerator<TItem>).GetProperty("Current").GetMethod;
-            private static readonly MethodInfo MoveNextMethod = typeof(IEnumerator).GetMethod("MoveNext");
-            private static readonly MethodInfo DisposeMethod = typeof(IDisposable).GetMethod("Dispose");
-            private static readonly ConcurrentDictionary<Type, AllocationFreeForEachDelegate> AllocationFreeForEachDelegates = new ConcurrentDictionary<Type, AllocationFreeForEachDelegate>();
-            private static readonly Func<Type, AllocationFreeForEachDelegate> BuildAllocationFreeForEachDelegateRef = BuildAllocationFreeForEachDelegate;
-
-            public delegate void AllocationFreeForEachDelegate(TEnumerable instance, ref TState state, ForEachDelegate itemCallback);
-
-            public delegate bool ForEachDelegate(ref TState state, TItem item);
-
-            protected Enumerator()
+            foreach (var _ in s_ActivitySmall.Events)
             {
             }
+        }
 
-            public static AllocationFreeForEachDelegate FindAllocationFreeForEach(TEnumerable instance)
+        [Benchmark]
+        public void EnumerateActivityEventsLarge()
+        {
+            foreach (var _ in s_ActivityLarge.Events)
             {
-                var type = instance.GetType();
-
-                return AllocationFreeForEachDelegates.GetOrAdd(
-                    type,
-                    BuildAllocationFreeForEachDelegateRef);
             }
+        }
 
-            /* We want to do this type of logic...
-                public static void AllocationFreeForEach(Dictionary<string, int> dictionary, ref TState state, ForEachDelegate itemCallback)
-                {
-                    using (Dictionary<string, int>.Enumerator enumerator = dictionary.GetEnumerator())
-                    {
-                        while (enumerator.MoveNext())
-                        {
-                            if (!itemCallback(ref state, enumerator.Current))
-                                break;
-                        }
-                    }
-                }
-                ...because it takes advantage of the struct Enumerator on the built-in types which give an allocation-free way to enumerate.
-            */
-            private static AllocationFreeForEachDelegate BuildAllocationFreeForEachDelegate(Type enumerableType)
+        [Benchmark]
+        public void EnumerateActivityLinkTagsSmall()
+        {
+            foreach (var _ in s_ActivityLinkSmall.Tags)
             {
-                var itemCallbackType = typeof(ForEachDelegate);
-
-                var getEnumeratorMethod = ResolveGetEnumeratorMethodForType(enumerableType);
-                if (getEnumeratorMethod == null)
-                {
-                    // Fallback to allocation mode and use IEnumerable<TItem>.GetEnumerator.
-                    // Primarily for Array.Empty and Enumerable.Empty case, but also for user types.
-                    getEnumeratorMethod = GenericGetEnumeratorMethod;
-                }
-
-                var enumeratorType = getEnumeratorMethod.ReturnType;
-
-                var dynamicMethod = new DynamicMethod(
-                    nameof(FindAllocationFreeForEach),
-                    null,
-                    new[] { typeof(TEnumerable), typeof(TState).MakeByRefType(), itemCallbackType },
-                    typeof(AllocationFreeForEachDelegate).Module,
-                    skipVisibility: true);
-
-                var generator = dynamicMethod.GetILGenerator();
-
-                generator.DeclareLocal(enumeratorType);
-
-                var beginLoopLabel = generator.DefineLabel();
-                var processCurrentLabel = generator.DefineLabel();
-                var returnLabel = generator.DefineLabel();
-                var breakLoopLabel = generator.DefineLabel();
-
-                generator.Emit(OpCodes.Ldarg_0);
-                generator.Emit(OpCodes.Callvirt, getEnumeratorMethod);
-                generator.Emit(OpCodes.Stloc_0);
-
-                // try
-                generator.BeginExceptionBlock();
-                {
-                    generator.Emit(OpCodes.Br_S, beginLoopLabel);
-
-                    generator.MarkLabel(processCurrentLabel);
-
-                    generator.Emit(OpCodes.Ldarg_2);
-                    generator.Emit(OpCodes.Ldarg_1);
-                    generator.Emit(OpCodes.Ldloca_S, 0);
-                    generator.Emit(OpCodes.Constrained, enumeratorType);
-                    generator.Emit(OpCodes.Callvirt, GeneircCurrentGetMethod);
-
-                    generator.Emit(OpCodes.Callvirt, itemCallbackType.GetMethod("Invoke"));
-
-                    generator.Emit(OpCodes.Brtrue_S, beginLoopLabel);
-
-                    generator.Emit(OpCodes.Leave_S, returnLabel);
-
-                    generator.MarkLabel(beginLoopLabel);
-
-                    generator.Emit(OpCodes.Ldloca_S, 0);
-                    generator.Emit(OpCodes.Constrained, enumeratorType);
-                    generator.Emit(OpCodes.Callvirt, MoveNextMethod);
-
-                    generator.Emit(OpCodes.Brtrue_S, processCurrentLabel);
-
-                    generator.MarkLabel(breakLoopLabel);
-
-                    generator.Emit(OpCodes.Leave_S, returnLabel);
-                }
-
-                // finally
-                generator.BeginFinallyBlock();
-                {
-                    if (typeof(IDisposable).IsAssignableFrom(enumeratorType))
-                    {
-                        generator.Emit(OpCodes.Ldloca_S, 0);
-                        generator.Emit(OpCodes.Constrained, enumeratorType);
-                        generator.Emit(OpCodes.Callvirt, DisposeMethod);
-                    }
-                }
-
-                generator.EndExceptionBlock();
-
-                generator.MarkLabel(returnLabel);
-
-                generator.Emit(OpCodes.Ret);
-
-                return (AllocationFreeForEachDelegate)dynamicMethod.CreateDelegate(typeof(AllocationFreeForEachDelegate));
             }
+        }
 
-            private static MethodInfo ResolveGetEnumeratorMethodForType(Type type)
+        [Benchmark]
+        public void EnumerateActivityLinkTagsLarge()
+        {
+            foreach (var _ in s_ActivityLinkLarge.Tags)
             {
-                var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
-                foreach (var method in methods)
-                {
-                    if (method.Name == "GetEnumerator" && !method.ReturnType.IsInterface)
-                    {
-                        return method;
-                    }
-                }
-
-                return null;
             }
         }
     }
