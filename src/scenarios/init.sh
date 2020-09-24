b581aa3de34b334efb6b3d2ec71e9e46ed636a22
Olivia Chen <bache@microsoft.com>
2020-09-24 10:15:09 -0700

    update help messages for init scripts

--- src/scenarios/init.sh
+++ src/scenarios/init.sh
@@ -1,9 +1,10 @@
 print_usage() {
-    echo "Invalid argument. Usage:"
-    echo "./init.sh"
-    echo "./init.sh -dotnetdir <custom dotnet directory>"
-    echo "./init.sh -channel <channel to download new dotnet>"
-    exit 1
+    echo "***Please run this script as SOURCE. ex: . ./init.sh***"
+    echo "This script sets up PYTHONPATH and determines which dotnet to use."
+    echo "Choose ONE of the following commands:"
+    echo ". ./init.sh                                             # sets up PYTHONPATH only; uses default dotnet in PATH"
+    echo ". ./init.sh -dotnetdir <custom dotnet directory>        # sets up PYTHONPATH; uses the specified dotnet"
+    echo ". ./init.sh -channel <channel to download new dotnet>   # sets up PYTHONPATH; downloads dotnet from the specified channel or branch to <repo root>/tools/ and uses it; for a list of channels, check <repo root>/scripts/channel_map.py"
 }
 
 # $1 is dotnet directory
@@ -13,6 +14,8 @@ setup_env() {
     export DOTNET_CLI_TELEMETRY_OPTOUT='1'
     export DOTNET_MULTILEVEL_LOOKUP='0'
     export UseSharedCompilation='false'
+    echo "Current dotnet directory: $DOTNET_ROOT"
+    echo "If more than one version exist in this directory, usually the latest runtime and sdk will be used."
 }
 
 # $1 is channel
@@ -26,9 +29,13 @@ download_dotnet() {
 absolutePath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 scriptPath="$absolutePath/../../scripts"
 export PYTHONPATH=$PYTHONPATH:$scriptPath:$absolutePath
+echo "PYTHONPATH=$PYTHONPATH"
 
 # Parse arguments
-if [ "$#" -gt 2 ]
+if [ "$1" == "-help" ] || [ "$1" == "-h" ]
+then
+    print_usage
+elif [ "$#" -gt 2 ]
 then 
     print_usage
 elif [ "$1" == "-dotnetdir" ] && [ "$2" != "" ]
