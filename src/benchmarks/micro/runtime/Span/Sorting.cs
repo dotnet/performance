9004955e96291e35163887e611e01a9d26abe727
Adam Sitnik <adam.sitnik@gmail.com>
2020-09-25 10:42:53 +0200

    when InvocationCount is set, BDN does not run Pilot Stage, so to get the code promoted to Tier 1 before Actual Workload, we enforce more Warmups

--- src/benchmarks/micro/runtime/Span/Sorting.cs
+++ src/benchmarks/micro/runtime/Span/Sorting.cs
@@ -13,6 +13,7 @@ namespace Span
 {
     [BenchmarkCategory(Categories.Runtime)]
     [InvocationCount(InvocationsPerIteration)]
+    [MinWarmupCount(6, forceAutoWarmup: true)] // when InvocationCount is set, BDN does not run Pilot Stage, so to get the code promoted to Tier 1 before Actual Workload, we enforce more Warmups
     public class Sorting
     {
         private const int InvocationsPerIteration = 1000;
