5b96cd029f91330875bc12e684f0821c6b35ac12
Will Smith <lol.tihan@gmail.com>
2020-10-08 16:15:48 -0700

    Added basic FSharp benchmarks

---
+++ src/benchmarks/real-world/FSharp/FSharpCompilerServiceBenchmarks.fs
@@ -0,0 +1,82 @@
+﻿namespace FSharpBenchmarks
+
+open System
+open System.Collections.Immutable
+open System.Diagnostics
+open System.IO
+open System.Threading.Tasks
+open BenchmarkDotNet.Attributes
+open FSharp.Compiler.SourceCodeServices
+open FSharp.Compiler.AbstractIL
+
+[<RequireQualifiedAccess>]
+module Sources =
+
+    let helloWorld =
+        """
+[<EntryPoint>]
+let main _argv =
+    printfn "Hello World!"
+    0
+        """
+
+/// FSharp compiler service benchmarks
+[<BenchmarkCategory("FSharp");MemoryDiagnoser>]
+type FSharpCompilerServiceBenchmarks () =
+
+    let sourceDir = Path.Combine(Environment.CurrentDirectory, "fsharpSource");
+    let tempFsFile = Path.Combine(sourceDir, "temp.fs")
+    let tempOutputFile = Path.Combine(sourceDir, "temp.dll")
+
+    let defaultReferenceArgs =
+        Helpers.getNet5References ()
+        |> Array.map (fun r -> "-r:" + r)
+
+    let compileArgs = 
+        Array.append
+            [|
+                "--preferreduilang:en-US"
+                "--optimize+"
+                "--langversion:preview"
+                "-o:" + tempOutputFile
+                "--nowin32manifest"
+                "--noframework"
+                "--simpleresolution"
+                "--targetprofile:netcore"
+                "--warn:5"
+                tempFsFile
+            |]
+            defaultReferenceArgs
+
+    let mutable checker = Unchecked.defaultof<FSharpChecker>
+
+    [<GlobalSetup>]
+    member _.Setup() =
+        Directory.CreateDirectory(sourceDir) |> ignore
+
+        // Benchmark.NET creates a new process to run the benchmark, so the easiest way
+        // to communicate information is pass by environment variable
+        Environment.SetEnvironmentVariable(Helpers.testProjectEnvVarName, sourceDir)
+
+        match box checker with
+        | null ->
+            checker <- FSharpChecker.Create(projectCacheSize = 200)
+        | _ ->
+            ()
+
+    [<IterationSetup(Target = "CompileHelloWorld")>]
+    member _.CompileHelloWorldSetup() =
+        File.WriteAllText(tempFsFile, Sources.helloWorld)
+
+    [<Benchmark>]
+    member _.CompileHelloWorld() =
+        let errors, _ = checker.Compile(compileArgs) |> Async.RunSynchronously
+        if errors.Length > 0 then
+            failwithf "%A" errors
+
+    [<IterationCleanup(Target = "CompileHelloWorld")>]
+    member _.CompileHelloWorldCleanup() =
+        checker.InvalidateAll()
+        checker.ClearLanguageServiceRootCachesAndCollectAndFinalizeAllTransients()
+        try File.Delete(tempOutputFile) with | _ -> ()
+        try File.Delete(tempFsFile) with | _ -> ()
---
+++ src/benchmarks/real-world/FSharp/Helpers.fs
@@ -0,0 +1,75 @@
+﻿module FSharpBenchmarks.Helpers
+
+open System
+open System.IO
+open System.Diagnostics
+
+let testProjectEnvVarName = "FSHARP_TEST_PROJECT_DIR"
+
+let getNet5References () =
+
+    let programFs = """
+open System
+    
+[<EntryPoint>]
+let main argv = 0"""
+    
+    let projectFile = """
+<Project Sdk="Microsoft.NET.Sdk">
+    
+    <PropertyGroup>
+        <OutputType>Exe</OutputType>
+        <TargetFramework>netcoreapp5.0</TargetFramework>
+        <UseFSharpPreview>true</UseFSharpPreview>
+    </PropertyGroup>
+    
+    <ItemGroup><Compile Include="Program.fs" /></ItemGroup>
+    
+    <Target Name="WriteFrameworkReferences" AfterTargets="AfterBuild">
+        <WriteLinesToFile File="FrameworkReferences.txt" Lines="@(ReferencePath)" Overwrite="true" WriteOnlyWhenDifferent="true" />
+    </Target>
+    
+</Project>"""
+
+    let mutable output = ""
+    let mutable errors = ""
+    let mutable cleanUp = true
+    let projectDirectory = Path.Combine(Path.GetTempPath(), "FSharpBenchmarks", Path.GetRandomFileName())
+    try
+        try
+            Directory.CreateDirectory(projectDirectory) |> ignore
+            let projectFileName = Path.Combine(projectDirectory, "ProjectFile.fsproj")
+            let programFsFileName = Path.Combine(projectDirectory, "Program.fs")
+            let frameworkReferencesFileName = Path.Combine(projectDirectory, "FrameworkReferences.txt")
+            File.WriteAllText(projectFileName, projectFile)
+            File.WriteAllText(programFsFileName, programFs)
+
+            let pInfo = ProcessStartInfo ()
+            pInfo.FileName <- "dotnet"
+            pInfo.Arguments <- "build"
+            pInfo.WorkingDirectory <- projectDirectory
+            pInfo.RedirectStandardOutput <- true
+            pInfo.RedirectStandardError <- true
+            pInfo.UseShellExecute <- false
+
+            let p = Process.Start(pInfo)
+            let timeout = 30000
+            let succeeded = p.WaitForExit(timeout)
+
+            output <- p.StandardOutput.ReadToEnd ()
+            errors <- p.StandardError.ReadToEnd ()
+
+            if not (String.IsNullOrWhiteSpace errors) then failwithf "%A" errors
+            if p.ExitCode <> 0 then failwithf "Program exited with exit code %d" p.ExitCode
+            if not succeeded then failwithf "Program timed out after %d ms" timeout
+
+            File.ReadLines(frameworkReferencesFileName) |> Seq.toArray
+        with | e ->
+            cleanUp <- false
+            printfn "Project directory: %s" projectDirectory
+            printfn "STDOUT: %s" output
+            printfn "STDERR: %s" errors
+            raise (new Exception (sprintf "An error occurred getting netcoreapp references: %A" e))
+    finally
+        if cleanUp then
+            try Directory.Delete(projectDirectory) with | _ -> ()
