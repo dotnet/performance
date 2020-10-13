159ca50023ed897cc32db0c9c1f9bc0d73677cee
Ivan Diaz Sanchez <ivdiazsa@microsoft.com>
2020-10-13 13:58:51 -0700

    Add base template files for GC Infra CI tests in Azure Pipelines (#1547)
    * Add new yml files for GC CI runs
    
    * Initial template for GC Infra CI tests in Azure Pipelines.
    
    * Added "include" paths to azure-pipelines-gc
    
    Co-authored-by: Drew Scoggins <anscoggi@microsoft.com>

---
+++ scripts/gcinfra_setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from performance.common import get_repo_root_path
+from performance.common import push_dir
+from performance.common import RunCommand
+
+def _get_gcinfra_path() -> str:
+    return os.path.join(get_repo_root_path(), "src", "benchmarks", "gc")
+
+def __main(args: list) -> int:
+    infra_base_path = _get_gcinfra_path()
+    with push_dir(infra_base_path):
+        gcperfsim_path = os.path.join(infra_base_path, "src", "exec", "GCPerfSim")
+        gcperf_path = os.path.join(infra_base_path, "src", "analysis", "managed-lib")
+        ctools_path = os.path.join(infra_base_path, "src", "exec", "env")
+
+        with push_dir(gcperfsim_path):
+            cmdline = ['dotnet', 'build', '-c', 'release']
+            RunCommand(cmdline, verbose=True).run()
+
+        with push_dir(gcperf_path):
+            cmdline = ['dotnet', 'publish']
+            RunCommand(cmdline, verbose=True).run()
+
+        with push_dir(ctools_path):
+            cmdline = ['build.cmd']
+            RunCommand(cmdline, verbose=True).run()
+    return 0
+
+if __name__ == "__main__":
+    __main(sys.argv[1:])
