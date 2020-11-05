84681752ab38aaffa48e6221006842f016b43448
Drew Scoggins <anscoggi@microsoft.com>
2020-11-04 16:06:44 -0800

    Add NoInterpreter label to Perf_Image tests (#1581)

--- src/benchmarks/micro/libraries/System.Drawing/Perf_Graphics_DrawBeziers.cs
+++ src/benchmarks/micro/libraries/System.Drawing/Perf_Graphics_DrawBeziers.cs
@@ -8,7 +8,7 @@ using MicroBenchmarks;
 
 namespace System.Drawing.Tests
 {
-    [BenchmarkCategory(Categories.Libraries, Categories.NoWASM)]
+    [BenchmarkCategory(Categories.Libraries, Categories.NoWASM, Categories.NoInterpreter)]
     public class Perf_Graphics_DrawBeziers
     {
         private Bitmap _image;
