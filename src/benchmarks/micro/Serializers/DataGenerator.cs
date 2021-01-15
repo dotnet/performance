e30d54ea81a94dc7cc83d6e2cd406dd657524f55
Mikel Blanchard <mblanchard@macrosssoftware.com>
2021-01-15 10:29:24 -0800

    Formatting.

--- src/benchmarks/micro/Serializers/DataGenerator.cs
+++ src/benchmarks/micro/Serializers/DataGenerator.cs
@@ -58,7 +58,17 @@ namespace MicroBenchmarks.Serializers
             if (typeof(T) == typeof(DateTime?))
                 return (T)(object)DateTime.UtcNow;
             if (typeof(T) == typeof(ClassWithValueTypes))
-                return (T)(object)new ClassWithValueTypes { Value = 0, NullableValueSet = 1, NullableValueNull = null, LargeStruct = CreateLargeStructWithProperties(), NullableLargeStructSet = CreateLargeStructWithProperties(), NullableLargeStructNull = null };
+            {
+                return (T)(object)new ClassWithValueTypes
+                {
+                    Value = 0,
+                    NullableValueSet = 1,
+                    NullableValueNull = null,
+                    LargeStruct = CreateLargeStructWithProperties(),
+                    NullableLargeStructSet = CreateLargeStructWithProperties(),
+                    NullableLargeStructNull = null
+                };
+            }
 
             throw new NotImplementedException();
         }
