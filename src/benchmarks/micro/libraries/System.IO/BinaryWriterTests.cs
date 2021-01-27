96ccc5c23c185f484368e42611fa8b601461369a
Levi Broderick <levib@microsoft.com>
2021-01-27 14:40:31 -0800

    Add BinaryWriter perf tests

---
+++ src/benchmarks/micro/libraries/System.IO/BinaryWriterTests.cs
@@ -0,0 +1,72 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+
+namespace System.IO.Tests
+{
+    [BenchmarkCategory(Categories.Libraries)]
+    public class BinaryWriterTests
+    {
+        private BinaryWriter _bw;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _bw = new BinaryWriter(new NullWriteStream());
+        }
+
+        [Benchmark]
+        public BinaryWriter DefaultCtor() => new BinaryWriter(Stream.Null);
+
+        [Benchmark]
+        public void WriteBool()
+        {
+            _bw.Write(true);
+        }
+
+        [Benchmark]
+        public void WriteAsciiChar()
+        {
+            _bw.Write('a');
+        }
+
+        [Benchmark]
+        public void WriteNonAsciiChar()
+        {
+            _bw.Write('\u00E0');
+        }
+
+        [Benchmark]
+        public void WriteUInt16()
+        {
+            _bw.Write((ushort)0xabcd);
+        }
+
+        [Benchmark]
+        public void WriteUInt32()
+        {
+            _bw.Write((uint)0xdeadbeef);
+        }
+
+        [Benchmark]
+        public void WriteUInt64()
+        {
+            _bw.Write((ulong)0xdeadbeef_aabbccdd);
+        }
+
+        [Benchmark]
+        public void WriteSingle()
+        {
+            _bw.Write((float)Math.PI);
+        }
+
+        [Benchmark]
+        public void WriteDouble()
+        {
+            _bw.Write((double)Math.PI);
+        }
+    }
+}
