e0d77ab7bd02c5e7bca6ea8cf4d3138a97073dea
Levi Broderick <GrabYourPitchforks@users.noreply.github.com>
2021-01-29 01:50:21 -0800

    Add BinaryWriter perf tests (#1639)
    * Add BinaryWriter perf tests
    
    * PR feedback

---
+++ src/benchmarks/micro/libraries/System.IO/BinaryWriterExtendedTests.cs
@@ -0,0 +1,41 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+
+namespace System.IO.Tests
+{
+    [BenchmarkCategory(Categories.Libraries)]
+    public class BinaryWriterExtendedTests
+    {
+        private string _input;
+        private char[] _inputAsChars;
+        private BinaryWriter _bw;
+
+        [Params(32, 8_000, 2_000_000)]
+        public int StringLengthInChars;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _bw = new BinaryWriter(new NullWriteStream());
+
+            _input = new string('x', StringLengthInChars);
+            _inputAsChars = _input.ToCharArray();
+        }
+
+        [Benchmark]
+        public void WriteAsciiCharArray()
+        {
+            _bw.Write(_inputAsChars);
+        }
+
+        [Benchmark]
+        public void WriteAsciiString()
+        {
+            _bw.Write(_input);
+        }
+    }
+}
