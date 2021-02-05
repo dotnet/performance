a06a46ac81f154320b70e23faa650a22b41243e4
Drew Scoggins <anscoggi@microsoft.com>
2021-02-05 15:55:08 -0800

    Add MVC inner loop test (#1650)

---
+++ src/scenarios/mvcinnerloop/test.py
@@ -0,0 +1,16 @@
+'''
+C# Console app
+'''
+from shared.runner import TestTraits, Runner
+import os
+
+EXENAME = 'mvcinnerloop'
+
+if __name__ == "__main__":
+    traits = TestTraits(exename=EXENAME, 
+                        startupmetric='InnerLoop',
+                        guiapp='false',
+                        innerloopcommandargs='-c "from shutil import copyfile; copyfile(\'src%sStartup.cs\', \'app%sStartup.cs\')"' % (os.sep, os.sep) ,
+                        projext = '.csproj'
+                        )
+    Runner(traits).run()
