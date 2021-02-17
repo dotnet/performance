8f2cc0f0720d85178c4b2f7f3b8ee7a6834ea2d3
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2021-02-17 12:24:36 +0000

    add heapify and enumeration benchmarks

--- src/benchmarks/micro/libraries/System.Collections/PriorityQueue/Perf_PriorityQueue.cs
+++ src/benchmarks/micro/libraries/System.Collections/PriorityQueue/Perf_PriorityQueue.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Linq;
 using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
 using MicroBenchmarks;
@@ -15,7 +16,9 @@ namespace System.Collections.Tests
         public int Size;
 
         private int[] _priorities;
+        private (int Element, int Priority)[] _elements;
         private PriorityQueue<int, int> _priorityQueue;
+        private PriorityQueue<int, int> _populatedPriorityQueue;
 
         [GlobalSetup]
         public void Setup()
@@ -27,20 +30,55 @@ namespace System.Collections.Tests
                 _priorities[i] = random.Next();
             }
 
+            _elements = _priorities.Select((i, x) => (i, x)).ToArray();
             _priorityQueue = new PriorityQueue<int, int>(initialCapacity: Size);
+            _populatedPriorityQueue = new PriorityQueue<int, int>(_elements);
         }
 
         [Benchmark]
         public void HeapSort()
         {
             var queue = _priorityQueue;
+            queue.EnqueueRange(_elements);
+
+            while (queue.Count > 0)
+            {
+                queue.Dequeue();
+            }
+        }
+
+        [Benchmark]
+        public void Enumerate()
+        {
+            foreach (var _ in _populatedPriorityQueue.UnorderedItems)
+            {
+
+            }
+        }
+
+        [Benchmark]
+        public void Dequeue_And_Enqueue()
+        {
+            // benchmarks dequeue and enqueue operations
+            // for heaps of fixed size.
+
+            var queue = _priorityQueue;
             var priorities = _priorities;
 
+            // populate the heap: incorporated in the 
+            // benchmark to achieve determinism.
             for (int i = 0; i < Size; i++)
             {
                 queue.Enqueue(i, priorities[i]);
             }
 
+            for (int i = 0; i < Size; i++)
+            {
+                queue.Dequeue();
+                queue.Enqueue(i, priorities[i]);
+            }
+
+            // Drain the heap
             while (queue.Count > 0)
             {
                 queue.Dequeue();
