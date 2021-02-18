c1f3492f5880502324da3445b0ea188d237fae5f
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2021-02-18 13:50:27 +0000

    extend benchmarks to reference types

--- src/benchmarks/micro/libraries/System.Collections/PriorityQueue/Perf_PriorityQueue.cs
+++ src/benchmarks/micro/libraries/System.Collections/PriorityQueue/Perf_PriorityQueue.cs
@@ -10,36 +10,38 @@ using MicroBenchmarks;
 namespace System.Collections.Tests
 {
     [BenchmarkCategory(Categories.Libraries, Categories.Collections, Categories.GenericCollections)]
-    public class Perf_PriorityQueue
+    public abstract class Perf_PriorityQueue<TElement, TPriority>
     {
         [Params(10, 100, 1000, 10_000, 100_000)]
         public int Size;
 
-        private int[] _priorities;
-        private (int Element, int Priority)[] _elements;
-        private PriorityQueue<int, int> _priorityQueue;
-        private PriorityQueue<int, int> _populatedPriorityQueue;
+        private (TElement Element, TPriority Priority)[] _items;
+        private PriorityQueue<TElement, TPriority> _priorityQueue;
+        private PriorityQueue<TElement, TPriority> _prePopulatedPriorityQueue;
+
+        public abstract IEnumerable<(TElement Element, TPriority Priority)> GenerateItems(int count);
 
         [GlobalSetup]
         public void Setup()
         {
-            var random = new Random(42);
-            _priorities = new int[Size];
-            for (int i = 0; i < Size; i++)
-            {
-                _priorities[i] = random.Next();
-            }
+            _items = GenerateItems(Size).ToArray();
+            //var random = new Random(42);
+            //_elements = new int[Size];
+            //for (int i = 0; i < Size; i++)
+            //{
+            //    _elements[i] = random.Next();
+            //}
 
-            _elements = _priorities.Select((i, x) => (i, x)).ToArray();
-            _priorityQueue = new PriorityQueue<int, int>(initialCapacity: Size);
-            _populatedPriorityQueue = new PriorityQueue<int, int>(_elements);
+            //_items = _elements.Select((i, x) => (i, x)).ToArray();
+            _priorityQueue = new PriorityQueue<TElement, TPriority>(initialCapacity: Size);
+            _prePopulatedPriorityQueue = new PriorityQueue<TElement, TPriority>(_items);
         }
 
         [Benchmark]
         public void HeapSort()
         {
             var queue = _priorityQueue;
-            queue.EnqueueRange(_elements);
+            queue.EnqueueRange(_items);
 
             while (queue.Count > 0)
             {
@@ -50,7 +52,7 @@ namespace System.Collections.Tests
         [Benchmark]
         public void Enumerate()
         {
-            foreach (var _ in _populatedPriorityQueue.UnorderedItems)
+            foreach (var _ in _prePopulatedPriorityQueue.UnorderedItems)
             {
 
             }
@@ -63,19 +65,19 @@ namespace System.Collections.Tests
             // for heaps of fixed size.
 
             var queue = _priorityQueue;
-            var priorities = _priorities;
+            var items = _items;
 
             // populate the heap: incorporated in the 
             // benchmark to achieve determinism.
-            for (int i = 0; i < Size; i++)
+            foreach ((TElement element, TPriority priority) in items)
             {
-                queue.Enqueue(i, priorities[i]);
+                queue.Enqueue(element, priority);
             }
 
-            for (int i = 0; i < Size; i++)
+            foreach ((TElement element, TPriority priority) in items)
             {
                 queue.Dequeue();
-                queue.Enqueue(i, priorities[i]);
+                queue.Enqueue(element, priority);
             }
 
             // Drain the heap
@@ -86,25 +88,62 @@ namespace System.Collections.Tests
         }
 
         [Benchmark]
-        public void K_Min_Elements()
+        public void K_Max_Elements()
         {
             const int k = 5;
             var queue = _priorityQueue;
-            var priorities = _priorities;
+            var items = _items;
 
             for (int i = 0; i < k; i++)
             {
-                queue.Enqueue(i, _priorities[i]);
+                (TElement element, TPriority priority) = items[i];
+                queue.Enqueue(element, priority);
             }
 
             for (int i = k; i < Size; i++)
             {
-                queue.EnqueueDequeue(i, _priorities[i]);
+                (TElement element, TPriority priority) = items[i];
+                queue.EnqueueDequeue(element, priority);
             }
 
             for (int i = 0; i < k; i++)
             {
                 queue.Dequeue();
+            }
+        }
+    }
+
+    public class Perf_PriorityQueue_String_String : Perf_PriorityQueue<string, string>
+    {
+        public override IEnumerable<(string Element, string Priority)> GenerateItems(int count)
+        {
+            var random = new Random(42);
+            const int MaxSize = 30;
+            byte[] buffer = new byte[MaxSize];
+
+            for (int i = 0; i < count; i++)
+            {
+                yield return (GenerateString(), GenerateString());
+            }
+
+            string GenerateString()
+            {
+                int size = random.Next(MaxSize);
+                Span<byte> slice = buffer.AsSpan().Slice(size);
+                random.NextBytes(slice);
+                return Convert.ToBase64String(slice);
+            }
+        }
+    }
+
+    public class Perf_PriorityQueue_Int_Int : Perf_PriorityQueue<int, int>
+    {
+        public override IEnumerable<(int Element, int Priority)> GenerateItems(int count)
+        {
+            var random = new Random(42);
+            for (int i = 0; i < count; i++)
+            {
+                yield return (random.Next(), random.Next());
             }
         }
     }
