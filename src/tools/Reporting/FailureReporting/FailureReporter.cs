e71990baa27aa23b0eb0b34469c7e2a408305520
Aaron <tohron@hotmail.com>
2021-03-10 16:56:08 -0800

    Cleanup

--- src/tools/Reporting/FailureReporting/FailureReporter.cs
+++ src/tools/Reporting/FailureReporting/FailureReporter.cs
@@ -1,46 +1,17 @@
 ﻿using Reporting;
 using System;
-using System.Collections.Generic;
 using System.IO;
 
 namespace FailureReporting
 {
     public class FailureReporter
     {
-        public static void CreateFailureReport(string reportJsonPath, Logger logger)
+        public static void CreateFailureReport(string reportJsonPath)
         {
             var reporter = Reporter.CreateReporter();
             if (reporter.InLab && !String.IsNullOrEmpty(reportJsonPath))
             {
                 File.WriteAllText(reportJsonPath, reporter.GetJson());
-            }
-            logger.Log(reporter.WriteResultTable());
-        }
-
-        public class Logger
-        {
-            public Logger(string fileName)
-            {
-
-            }
-            public void Log(string message)
-            {
-                Console.WriteLine(message);
-            }
-
-            public void LogIterationHeader(string message)
-            {
-                Console.WriteLine($"=============== {message} ================ ");
-            }
-
-            public void LogStepHeader(string message)
-            {
-                Console.WriteLine($"***{message}***");
-            }
-
-            public void LogVerbose(string message)
-            {
-                Console.WriteLine(message);
             }
         }
     }
