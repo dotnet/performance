94f493d346cea2fc075940a034e008a95d01612a
Drew Scoggins <anscoggi@microsoft.com>
2021-03-26 17:16:32 -0700

    Update checked in Startup.cs

--- src/scenarios/blazorserverinnerloop/src/Startup.cs
+++ src/scenarios/blazorserverinnerloop/src/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using test2.Data;
+using blazorserverinnerloop.Data;
 
 namespace blazorserverinnerloop
 {
@@ -40,8 +40,10 @@ namespace blazorserverinnerloop
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                string s = "/Error";
+                app.UseExceptionHandler(s);
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
