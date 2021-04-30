fe558e49591db901ff29058275f71f590a142faf
Drew Scoggins <anscoggi@microsoft.com>
2021-04-30 10:23:01 -0700

    Add WASM AOT Size on Disk (#1783)
    * Add WASM AOT Size on Disk scenarios
    
    * Fix scenario name and close file handle

--- src/scenarios/shared/postcommands.py
+++ src/scenarios/shared/postcommands.py
@@ -2,7 +2,7 @@ from shared.const import APPDIR, TMPDIR, TRACEDIR, PUBDIR, BINDIR, CROSSGENDIR
 from performance.common import remove_directory
 
 def clean_directories():
-    to_remove = (APPDIR, TMPDIR, TRACEDIR, PUBDIR, BINDIR, CROSSGENDIR)
+    to_remove = (APPDIR, TMPDIR, TRACEDIR, PUBDIR, BINDIR, CROSSGENDIR, "emsdk")
     print(f"Removing {','.join(to_remove)} if exist ...")
     for dir in to_remove:
         remove_directory(dir)
