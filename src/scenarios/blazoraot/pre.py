93b43729f90953d21ed1d94d6c30c06e2555e207
Drew Scoggins <anscoggi@microsoft.com>
2021-05-12 17:27:12 -0700

    Update AOT tool installation
    This gets rid of the use of the old aot install tool.

--- src/scenarios/blazoraot/pre.py
+++ src/scenarios/blazoraot/pre.py
@@ -16,9 +16,8 @@ precommands.new(template='blazorwasm',
                 bin_dir=const.BINDIR,
                 exename=EXENAME,
                 working_directory=sys.path[0])
-subprocess.run(["dotnet", "new", "tool-manifest", "--force"])
-subprocess.run(["dotnet", "tool", "install", "dotnet-install-blazoraot", "--version", "6.0.0-preview*"])
-subprocess.run(["dotnet", "install-blazoraot"])
+subprocess.run(["dotnet", "workload", "install", "microsoft-net-sdk-blazorwebassembly-aot", "--skip-manifest-update"])
+#subprocess.run(["dotnet", "install-blazoraot"])
 f = open(os.path.join(os.getcwd(), "app", "emptyblazorwasmtemplate.csproj"), 'r')
 outFileText = ""
 for line in f.readlines():
