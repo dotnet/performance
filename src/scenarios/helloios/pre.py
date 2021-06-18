05682b7442bc5cb09ad5cad1c145b6c4a231e786
Bill Wert <billwert@microsoft.com>
2021-06-18 10:18:16 -0700

    remove unused stuff

--- src/scenarios/helloios/pre.py
+++ src/scenarios/helloios/pre.py
@@ -1,14 +1,9 @@
 '''
 pre-command
 '''
-import sys
-import os
-from zipfile import ZipFile
 from performance.logger import setup_loggers
 from shutil import copytree
-from shared.precommands import PreCommands
 from shared.const import PUBDIR
-from argparse import ArgumentParser
 
 setup_loggers(True)
 
