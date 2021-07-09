1f75fff78ab67c5837ef27b9d1371ca896bd13b8
Drew Scoggins <anscoggi@microsoft.com>
2021-07-09 09:41:30 -0700

    Add removal step for non AOT tests

--- src/scenarios/blazor/pre.py
+++ src/scenarios/blazor/pre.py
@@ -1,7 +1,7 @@
 '''
 pre-command
 '''
-import sys, os
+import sys, os, subprocess
 from performance.logger import setup_loggers
 from shared import const
 from shared.precommands import PreCommands
@@ -9,6 +9,7 @@ from test import EXENAME
 
 # For blazor3.2 the linker argument '--dump-dependencies' should be added statically to enable linker dump
 # For blazor5.0 the linker argument can be added to the command line as an msbuild property
+subprocess.run(["dotnet", "workload", "uninstall", "microsoft-net-sdk-blazorwebassembly-aot"])
 setup_loggers(True)
 precommands = PreCommands()
 precommands.new(template='blazorwasm',
