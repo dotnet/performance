c5f233785f18c2f160f45b50f8909a04164812bc
Stephen Toub <stoub@microsoft.com>
2021-07-27 16:53:08 -0400

    Revise the PipeStream perf tests

--- src/benchmarks/micro/libraries/System.IO.Pipes/Perf.PipeTest.cs
+++ src/benchmarks/micro/libraries/System.IO.Pipes/Perf.PipeTest.cs
@@ -11,6 +11,8 @@ namespace System.IO.Pipes.Tests
     [BenchmarkCategory(Categories.Libraries, Categories.NoWASM)]
     public abstract class Perf_PipeTest : PipeTestBase
     {
+        private const int Iterations = 1000;
+
         [Params(1000000)]
         public int size; 
 
@@ -32,15 +34,56 @@ namespace System.IO.Pipes.Tests
         [GlobalCleanup]
         public void Cleanup() => _serverClientPair.Dispose();
         
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = Iterations)]
         public async Task ReadWrite()
         {
-            Task write = Task.Run(() => _serverClientPair.writeablePipe.Write(_sent, 0, _sent.Length));
-            int totalReadLength = 0;
-            while (totalReadLength < _sent.Length)
+            Task write = Task.Run(delegate
             {
-                int readLength = _serverClientPair.readablePipe.Read(_received, totalReadLength, size - totalReadLength);
-                totalReadLength += readLength;
+                for (int i = 0; i < Iterations; i++)
+                {
+                    _serverClientPair.writeablePipe.Write(_sent, 0, _sent.Length);
+                }
+            });
+
+            for (int i = 0; i < Iterations; i++)
+            {
+                int totalReadLength = 0;
+                while (totalReadLength < _sent.Length)
+                {
+                    totalReadLength += _serverClientPair.readablePipe.Read(_received, totalReadLength, size - totalReadLength);
+                }
+            }
+
+            await write;
+        }
+
+        [Benchmark(OperationsPerInvoke = Iterations)]
+        public async Task ReadWriteAsync()
+        {
+            Task write = Task.Run(async delegate
+            {
+                for (int i = 0; i < Iterations; i++)
+                {
+#if !NETFRAMEWORK
+                    await _serverClientPair.writeablePipe.WriteAsync(_sent);
+#else
+                    await _serverClientPair.writeablePipe.WriteAsync(_sent, 0, _sent.Length);
+#endif
+                }
+            });
+
+            for (int i = 0; i < Iterations; i++)
+            {
+                int totalReadLength = 0;
+                while (totalReadLength < _sent.Length)
+                {
+                    totalReadLength += await
+#if !NETFRAMEWORK
+                        _serverClientPair.readablePipe.ReadAsync(_received.AsMemory(totalReadLength));
+#else
+                        _serverClientPair.readablePipe.ReadAsync(_received, totalReadLength, size - totalReadLength);
+#endif
+                }
             }
 
             await write;
