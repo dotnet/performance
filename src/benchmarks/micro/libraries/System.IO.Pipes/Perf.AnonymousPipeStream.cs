f7ec27a4da2d61b97184f07989e5d9c6e0c21988
Stephen Toub <stoub@microsoft.com>
2021-08-05 03:25:27 -0400

    Revise the PipeStream perf tests (#1897)

--- src/benchmarks/micro/libraries/System.IO.Pipes/Perf.AnonymousPipeStream.cs
+++ src/benchmarks/micro/libraries/System.IO.Pipes/Perf.AnonymousPipeStream.cs
@@ -4,24 +4,13 @@
 
 namespace System.IO.Pipes.Tests
 {
-    public class Perf_AnonymousPipeStream_ServerIn_ClientOut : Perf_PipeTest
+    public class Perf_AnonymousPipeStream : Perf_PipeTest
     {
         protected override ServerClientPair CreateServerClientPair()
         {
             ServerClientPair ret = new ServerClientPair();
             ret.readablePipe = new AnonymousPipeServerStream(PipeDirection.In);
             ret.writeablePipe = new AnonymousPipeClientStream(PipeDirection.Out, ((AnonymousPipeServerStream)ret.readablePipe).ClientSafePipeHandle);
-            return ret;
-        }
-    }
-
-    public class Perf_AnonymousPipeStream_ServerOut_ClientIn : Perf_PipeTest
-    {
-        protected override ServerClientPair CreateServerClientPair()
-        {
-            ServerClientPair ret = new ServerClientPair();
-            ret.writeablePipe = new AnonymousPipeServerStream(PipeDirection.Out);
-            ret.readablePipe = new AnonymousPipeClientStream(PipeDirection.In, ((AnonymousPipeServerStream)ret.writeablePipe).ClientSafePipeHandle);
             return ret;
         }
     }
