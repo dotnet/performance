fe4493bb410e01c2b5470e2a90b55b356dd2fa34
Parker Bibus <parkerbibus@microsoft.com>
2021-09-03 09:21:44 -0700

    test adding timing for just the blazor wasm HR scenario to lower down the potential problem scope.

--- src/scenarios/blazorwasmdotnetwatch/test.py
+++ src/scenarios/blazorwasmdotnetwatch/test.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     traits = TestTraits(exename=EXENAME, 
                         startupmetric='DotnetWatch',
                         guiapp='false',
-                        innerloopcommandargs='-c "from shutil import copyfile, move; copyfile(\'app%sPages%sCounter.razor\', \'src%sPages%sCounter2.razor\');copyfile(\'src%sPages%sCounter.razor\', \'app%sPages%sCounter.razor\'); move(\'src%sPages%sCounter2.razor\', \'src%sPages%sCounter.razor\')"' % (os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep) ,
+                        innerloopcommandargs='-c "from shutil import copyfile, move; import time; time.sleep(2.0); copyfile(\'app%sPages%sCounter.razor\', \'src%sPages%sCounter2.razor\');copyfile(\'src%sPages%sCounter.razor\', \'app%sPages%sCounter.razor\'); move(\'src%sPages%sCounter2.razor\', \'src%sPages%sCounter.razor\')"' % (os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, os.sep) ,
                         projext = '.csproj',
                         processwillexit='false',
                         measurementdelay='20',
