fe1dcd35afd9719a4cc7fede2e3cdea54cc7b3c8
ntr <ntr@ihfood.dk>
2021-10-17 13:05:32 +0200

    switch to target length approach

--- src/benchmarks/micro/libraries/System.IO/StringReaderReadLineTests.cs
+++ src/benchmarks/micro/libraries/System.IO/StringReaderReadLineTests.cs
@@ -13,8 +13,20 @@ namespace System.IO.Tests
     [BenchmarkCategory(Categories.Libraries)]
     public class StringReaderReadLineTests
     {
-        public const int LineCount = 256_000;
-        public const int ReaderCount = 10000;
+        // Benchmark is based on measuring individual `ReadLine` calls
+        // of lines of different lengths within a range defined as
+        // benchmark parameters.
+        // To fulfill BDNs requirement for an iteration time of 250ms,
+        // this means a lot of invocations, more than a single string in
+        // a `StringReader` can accommodate.
+        // Therefore, the benchmark generates of string with a certain
+        // target length and then a number of readers for the same
+        // string, since `StringReader` does not support being "reset".
+        // The trade off is we need to switch reader during the `ReadLine`
+        // call when current reader ends, which adds a bit of overhead. 
+        // This is deemed negligible and amortized constant.
+        private const int StringReaderTargetLength = 16 * 1024 * 1024;
+        private const int ReaderCount = 1000;
 
         private string _text;
         private StringReader[] _readers;
@@ -23,32 +35,33 @@ namespace System.IO.Tests
 
         public StringReaderReadLineTests()
         {
-            LineConfigs = new Config[]
+            LineLengthRanges = new Range[]
             {
-                new (){ LineLengthMin = 0, LineLengthMax = 0 },
-                new (){ LineLengthMin = 0, LineLengthMax = 64 },
-                new (){ LineLengthMin = 128, LineLengthMax = 512 },
+                new (){ Min =   0, Max =    0 },
+                new (){ Min =   1, Max =    8 },
+                new (){ Min =   9, Max =   32 },
+                new (){ Min =  33, Max =  128 },
+                new (){ Min = 129, Max = 1024 },
             };
         }
 
-        [ParamsSource(nameof(LineConfigs))]
-        public Config LineConfig { get; set; }
+        [ParamsSource(nameof(LineLengthRanges))]
+        public Range LineLengthRange { get; set; }
 
-        public IEnumerable<Config> LineConfigs { get; }
+        public IEnumerable<Range> LineLengthRanges { get; }
 
-        public class Config
+        public class Range
         {
-            public int LineLengthMin { get; set; }
-            public int LineLengthMax { get; set; }
+            public int Min { get; set; }
+            public int Max { get; set; }
 
-            public override string ToString() =>
-                $"LineLength [{LineLengthMin,4},{LineLengthMax,4}]";
+            public override string ToString() => $"[{Min,4},{Max,4}]";
         }
 
         [GlobalSetup]
         public void GlobalSetup()
         {
-            _text = GenerateLinesText(LineConfig);
+            _text = GenerateLinesText(LineLengthRange, StringReaderTargetLength);
             _readers = Enumerable.Range(0, ReaderCount)
                 .Select(i => new StringReader(_text)).ToArray();
             _readerIndex = 0;
@@ -64,21 +77,23 @@ namespace System.IO.Tests
             return line;
         }
 
-        private static string GenerateLinesText(Config config)
+        private static string GenerateLinesText(Range lineLengthRange, int textTargetLength)
         {
-            var min = config.LineLengthMin;
-            var max = config.LineLengthMax;
-            var count = LineCount;
+            var min = lineLengthRange.Min;
+            var max = lineLengthRange.Max;
 
-            var newLines = Math.Min(min, max) > 0 
-                ? new[] { "\n", "\r", "\r\n" } 
-                : new[] { "\r\n" };
+            // Just to make things interesting 
+            // new line characters are randomized.
+            // Note if lines are empty not \r and \n
+            // might be "combined".
+            var newLines = new[] { "\n", "\r", "\r\n" };
 
-            var capacity = (2 + max / 2 + min / 2) * count;
+            var capacity = textTargetLength + max + 2;
             var sb = new StringBuilder(capacity);
 
             var random = new Random(42);
-            for (int l = 0; l < count; l++)
+            int lineCount = 0;
+            while(sb.Length < textTargetLength)
             {
                 var charsCount = random.Next(min, max);
                 for (int c = 0; c < charsCount; c++)
@@ -88,8 +103,13 @@ namespace System.IO.Tests
                 }
                 var newLine = newLines[random.Next(newLines.Length)];
                 sb.Append(newLine);
+                ++lineCount;
             }
             var text = sb.ToString();
+
+            Console.WriteLine($"// Generated lines {lineCount} and " +
+                $"text length {text.Length} out of capacity {capacity}");
+
             return text;
         }
     }
