22005a2f71420ffa0b8398351461a4a4b32fab28
Parker Bibus <parkerbibus@microsoft.com>
2021-10-25 13:18:43 -0700

    Force set the TFM for these projects to net7.0 until net7.0 becomes the default.

--- src/scenarios/emptyconsoletemplateinnerloopmsbuild/pre.py
+++ src/scenarios/emptyconsoletemplateinnerloopmsbuild/pre.py
@@ -2,6 +2,7 @@
 pre-command
 '''
 import sys
+import os.path
 from performance.logger import setup_loggers
 from shared import const
 from shared.precommands import PreCommands
@@ -15,3 +16,15 @@ precommands.new(template='console',
                 exename=EXENAME,
                 working_directory=sys.path[0])
 precommands.execute()
+
+# Update TFM to net7.0 while we wait for the default to be updated
+f = open(os.path.join(os.getcwd(), "app", "emptyconsoletemplateinnerloop.csproj"), 'r')
+outFileText = ""
+for line in f.readlines():
+    line = line.replace('net6.0', 'net7.0')
+    outFileText += line
+f.close()
+os.remove(os.path.join(os.getcwd(), "app", "emptyconsoletemplateinnerloop.csproj"))
+f = open(os.path.join(os.getcwd(), "app", "emptyconsoletemplateinnerloop.csproj"), 'w')
+f.write(outFileText)
+f.close()
