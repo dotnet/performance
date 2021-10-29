872da968cc155e8c3d3e3401444f0c16ef75df2c
Parker Bibus <parkerbibus@microsoft.com>
2021-10-29 16:56:29 -0700

    Update the ios exe name and make a macatalyst specific test for the exe.

--- src/scenarios/mauimaccatalyst/test.py
+++ src/scenarios/mauimaccatalyst/test.py
@@ -3,7 +3,7 @@ C# Console app
 '''
 from shared.runner import TestTraits, Runner
 
-EXENAME = 'MauiiOSDefault'
+EXENAME = 'MauiMacCatalystDefault'
 
 if __name__ == "__main__":
     traits = TestTraits(exename=EXENAME, 
