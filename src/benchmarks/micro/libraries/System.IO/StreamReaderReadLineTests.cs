529f33c1955ae3360d794d1fc80dfb978bb2f222
nietras <nietras@users.noreply.github.com>
2021-12-08 09:03:44 +0100

    Add StreamReaderReadLineTests and base TextReaderReadLineTests (#2170)
    * Add StreamReaderReadLineTests and base TextReaderReadLineTests
    
    * change to create memorystream on each invocation instead

--- src/benchmarks/micro/libraries/System.IO/StreamReaderReadLineTests.cs
+++ src/benchmarks/micro/libraries/System.IO/StreamReaderReadLineTests.cs
@@ -2,25 +2,29 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using BenchmarkDotNet.Attributes;
-using MicroBenchmarks;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
 
 namespace System.IO.Tests
 {
     [BenchmarkCategory(Categories.Libraries)]
-    public class StringReaderReadLineTests : TextReaderReadLineTests
+    public class StreamReaderReadLineTests : TextReaderReadLineTests
     {
+        private byte[] _bytes;
+
         [GlobalSetup]
-        public void GlobalSetup() => _text = GenerateLinesText(LineLengthRange, 16 * 1024);
+        public void GlobalSetup()
+        {
+            _text = GenerateLinesText(LineLengthRange, 16 * 1024);
+            _bytes = Encoding.UTF8.GetBytes(_text);
+        }
 
         [Benchmark]
         public void ReadLine()
         {
-            using StringReader reader = new (_text);
+            using StreamReader reader = new (new MemoryStream(_bytes));
             while (reader.ReadLine() != null) ;
         }
 
@@ -28,7 +32,7 @@ namespace System.IO.Tests
         [BenchmarkCategory(Categories.NoWASM)]
         public async Task ReadLineAsync()
         {
-            using StringReader reader = new(_text);
+            using StreamReader reader = new(new MemoryStream(_bytes));
             while (await reader.ReadLineAsync() != null) ;
         }
     }
