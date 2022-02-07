e5c4d12eba0d36c1a9fd6d0fd6d474ed51dcb346
Adam Sitnik <adam.sitnik@gmail.com>
2022-02-07 18:03:59 +0100

    parametrize the benchmark using [Params], don't use [IterationSetup]

--- src/benchmarks/micro/libraries/Microsoft.Extensions.Configuration.Xml/XmlConfigurationProviderBenchmarks.cs
+++ src/benchmarks/micro/libraries/Microsoft.Extensions.Configuration.Xml/XmlConfigurationProviderBenchmarks.cs
@@ -7,75 +7,31 @@ namespace MicroBenchmarks.libraries.Microsoft.Extensions.Configuration
     [BenchmarkCategory(Categories.Libraries)]
     public class XmlConfigurationProviderBenchmarks
     {
-        private MemoryStream _simpleXml;
-        private MemoryStream _deepXml;
-        private MemoryStream _namesXml;
-        private MemoryStream _repeatedXml;
+        private MemoryStream _memoryStream;
         private XmlConfigurationProvider _provider;
 
-        private MemoryStream ReadTestFile(string fileName)
-        {
-            using (FileStream fileStream = File.OpenRead(Path.Combine("./libraries/Microsoft.Extensions.Configuration.Xml/TestFiles", fileName)))
-            {
-                var memoryStream = new MemoryStream();
-
-                fileStream.CopyTo(memoryStream);
-
-                return memoryStream;
-            }
-        }
+        [Params("simple.xml", "deep.xml", "names.xml", "repeated.xml")]
+        public string FileName { get; set; }
 
         [GlobalSetup]
         public void GlobalSetup()
         {
-            _simpleXml = ReadTestFile("simple.xml");
-            _deepXml = ReadTestFile("deep.xml");
-            _namesXml = ReadTestFile("names.xml");
-            _repeatedXml = ReadTestFile("repeated.xml");
-
             _provider = new XmlConfigurationProvider(new XmlConfigurationSource());
-        }
 
-        [IterationSetup]
-        public void IterationSetup()
-        {
-            _simpleXml.Position = 0;
-            _deepXml.Position = 0;
-            _namesXml.Position = 0;
-            _repeatedXml.Position = 0;
-        }
+            using (FileStream fileStream = File.OpenRead(Path.Combine("./libraries/Microsoft.Extensions.Configuration.Xml/TestFiles", FileName)))
+            {
+                _memoryStream = new MemoryStream();
 
-        [GlobalCleanup]
-        public void GlobalCleanup()
-        {
-            _simpleXml.Dispose();
-            _deepXml.Dispose();
-            _namesXml.Dispose();
-            _repeatedXml.Dispose();
+                fileStream.CopyTo(_memoryStream);
+            }
         }
 
         [Benchmark]
-        public void Simple()
+        public void Load()
         {
-            _provider.Load(_simpleXml);
-        }
+            _memoryStream.Position = 0;
 
-        [Benchmark]
-        public void Deep()
-        {
-            _provider.Load(_deepXml);
-        }
-
-        [Benchmark]
-        public void Names()
-        {
-            _provider.Load(_namesXml);
-        }
-
-        [Benchmark]
-        public void Repeated()
-        {
-            _provider.Load(_repeatedXml);
+            _provider.Load(_memoryStream);
         }
     }
 }
