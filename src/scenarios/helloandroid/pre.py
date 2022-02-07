204403097a9d84948257e6cbad1e9f1211a3183c
Parker Bibus <parkerbibus@microsoft.com>
2022-02-07 11:26:28 -0800

    Update pre.py's to make adding files easier down the line.

--- src/scenarios/helloandroid/pre.py
+++ src/scenarios/helloandroid/pre.py
@@ -14,31 +14,36 @@ setup_loggers(True)
 
 parser = ArgumentParser()
 parser.add_argument('--unzip', help='Unzip APK and report extracted tree', action='store_true', default=False)
+parser.add_argument(
+        '--apk-name',
+        dest='apk',
+        required=True,
+        type=str,
+        help='Name of the APK to setup (with .apk)')
 args = parser.parse_args()
 
 if not os.path.exists(PUBDIR):
     os.mkdir(PUBDIR)
-apknames = ['HelloAndroid.apk', 'HelloAndroidWithDiag.apk']
-for apkname in apknames:
-    apknamezip = '%s.zip' % (apkname)
-    if not os.path.exists(apkname):
-        getLogger().error('Cannot find %s' % (apkname))
-        exit(-1)
-    if args.unzip:
-        if not os.path.exists(apknamezip):
-            copyfile(apkname, apknamezip)
+apkname = args.apk
+apknamezip = '%s.zip' % (apkname)
+if not os.path.exists(apkname):
+    getLogger().error('Cannot find %s' % (apkname))
+    exit(-1)
+if args.unzip:
+    if not os.path.exists(apknamezip):
+        copyfile(apkname, apknamezip)
 
-        with ZipFile(apknamezip) as zip:
-            zip.extractall(os.path.join('.', PUBDIR))
+    with ZipFile(apknamezip) as zip:
+        zip.extractall(os.path.join('.', PUBDIR))
 
-        assets_dir = os.path.join(PUBDIR, 'assets')
-        assets_zip = os.path.join(assets_dir, 'assets.zip')
-        with ZipFile(assets_zip) as zip:
-            zip.extractall(assets_dir)
+    assets_dir = os.path.join(PUBDIR, 'assets')
+    assets_zip = os.path.join(assets_dir, 'assets.zip')
+    with ZipFile(assets_zip) as zip:
+        zip.extractall(assets_dir)
 
-        os.remove(assets_zip)
-    else:
-        copyfile(apkname, os.path.join(PUBDIR, apkname))
+    os.remove(assets_zip)
+else:
+    copyfile(apkname, os.path.join(PUBDIR, apkname))
 
 
 
