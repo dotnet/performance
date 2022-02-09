a74ff5e2291dbcff4978db83a4ee6347bf01eddb
Trayan Zapryanov <lrt80@abv.bg>
2022-02-09 12:43:28 +0200

    Add initial performance tests for XmlConvert. (#2243)
    Co-authored-by: Traian Zaprianov <Traian.Zaprianov@docuware.com>
    Co-authored-by: Dan Moseley <danmose@microsoft.com>

---
+++ src/benchmarks/micro/libraries/System.Private.Xml/Perf.XmlConvert.cs
@@ -0,0 +1,31 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+
+namespace System.Xml.Tests
+{
+    [BenchmarkCategory(Categories.Libraries)]
+    public class Perf_XmlConvert
+    {
+        private DateTime _testDateTime = new DateTime(1996, 6, 3, 22, 15, 0);
+        private TimeSpan _testTimeSpan = new TimeSpan(1, 2, 3, 4, 56);
+
+        [Benchmark]
+        public string DateTime_ToString() => XmlConvert.ToString(_testDateTime, XmlDateTimeSerializationMode.Utc);
+
+        [Benchmark]
+        public string DateTime_ToString_Local() => XmlConvert.ToString(_testDateTime, XmlDateTimeSerializationMode.Local);
+
+        [Benchmark]
+        public string DateTime_ToString_Unspecified() => XmlConvert.ToString(_testDateTime, XmlDateTimeSerializationMode.Unspecified);
+
+        [Benchmark]
+        public string DateTime_ToString_RoundtripKind() => XmlConvert.ToString(_testDateTime, XmlDateTimeSerializationMode.RoundtripKind);
+
+        [Benchmark]
+        public string TimeSpan_ToString() => XmlConvert.ToString(_testTimeSpan);
+    }
+}
