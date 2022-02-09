bbd50ff60a9b570846d1afbd4353c38c64af914c
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2022-02-09 16:37:25 +0000

    Add cold start Json serialization benchmarks (#2236)
    * Add cold start Json serialization benchmarks
    
    * Apply suggestions from code review
    
    Co-authored-by: Adam Sitnik <adam.sitnik@gmail.com>

---
+++ src/benchmarks/micro/libraries/System.Text.Json/Serializer/ColdStartSerialization.cs
@@ -0,0 +1,69 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+using MicroBenchmarks.Serializers;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    [BenchmarkCategory(Categories.Libraries, Categories.JSON)]
+    [GenericTypeArguments(typeof(SimpleStructWithProperties))]
+    public class ColdStartSerialization<T>
+    {
+        // Measures cold start performance for the serializer
+        // using a fresh JsonSerializerOptions instance per run.
+
+        private T _value;
+        private readonly JsonSerializerOptions _defaultOptions = new JsonSerializerOptions();
+        private readonly JsonStringEnumConverter _converter = new JsonStringEnumConverter();
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _value = DataGenerator.Generate<T>();
+            RoundtripSerialization(_defaultOptions);
+        }
+
+        private T RoundtripSerialization(JsonSerializerOptions options)
+        {
+            string json = JsonSerializer.Serialize(_value, options);
+            return JsonSerializer.Deserialize<T>(json, options);
+        }
+
+        [Benchmark]
+        public T CachedDefaultOptions() => RoundtripSerialization(_defaultOptions);
+
+        [Benchmark]
+        public T NewDefaultOptions() => RoundtripSerialization(new JsonSerializerOptions());
+
+        [Benchmark]
+        public T NewCustomizedOptions() => 
+            RoundtripSerialization(
+                new JsonSerializerOptions {
+                    AllowTrailingCommas = true,
+                    WriteIndented = true,
+                    MaxDepth = 1000,
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                });
+
+        [Benchmark]
+        public T NewCustomConverter() =>
+            RoundtripSerialization(
+                new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Converters = { new JsonStringEnumConverter() }
+                });
+
+        [Benchmark]
+        public T NewCachedCustomConverter() =>
+            RoundtripSerialization(
+                new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Converters = { _converter }
+                });
+    }
+}
