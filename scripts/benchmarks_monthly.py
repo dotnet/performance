46ae9575ea9509fcab0a03f43ad1c20317e5c252
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-02-25 18:40:27 -0800

    Collect the monthly run results into a tar.gz file

--- scripts/benchmarks_monthly.py
+++ scripts/benchmarks_monthly.py
@@ -10,12 +10,15 @@ from argparse import ArgumentParser, ArgumentTypeError
 from logging import getLogger
 
 import benchmarks_ci
+import datetime
+import tarfile
 import sys
+import os
 
 VERSIONS = {
-    '7.0-preview2': { 'tfm': 'net7.0', 'build': '7.0.100-preview.2.22124.4' },
-    '7.0-preview1': { 'tfm': 'net7.0', 'build': '7.0.100-preview.1.22077.12' },
-    '6.0': { 'tfm': 'net6.0' }
+    'net7.0-preview2': { 'tfm': 'net7.0', 'build': '7.0.100-preview.2.22124.4' },
+    'net7.0-preview1': { 'tfm': 'net7.0', 'build': '7.0.100-preview.1.22077.12' },
+    'net6.0': { 'tfm': 'net6.0' }
 }
 
 def get_version_from_name(name: str) -> str:
@@ -36,6 +39,12 @@ def add_arguments(parser: ArgumentParser) -> ArgumentParser:
         choices=VERSIONS,
         help='Specifies the .NET release for the benchmarks run')
 
+    parser.add_argument(
+        '--filter',
+        dest='filter',
+        default='*',
+        help='Specifies the benchmark filter to pass to BenchmarkDotNet')
+
     return parser
 
 def __process_arguments(args: list):
@@ -43,6 +52,7 @@ def __process_arguments(args: list):
         description='Tool to execute the monthly manual micro benchmark performance runs',
         allow_abbrev=False
     )
+
     add_arguments(parser)
     return parser.parse_args(args)
 
@@ -53,13 +63,29 @@ def __main(args: list) -> int:
         raise Exception('Version must be specified.')
 
     version = get_version_from_name(args.version)
-    benchmarkArgs = ['--filter', '*', '-f', version['tfm']]
+    benchmarkArgs = ['--filter', args.filter, '-f', version['tfm']]
 
     if 'build' in version:
         benchmarkArgs += ['--dotnet-versions', version['build']]
 
-    getLogger().critical('\nExecuting: benchmarks_ci.py ' + str.join(' ', benchmarkArgs) + '\n')
+    getLogger().log(getLogger().getEffectiveLevel(), '\nExecuting: benchmarks_ci.py ' + str.join(' ', benchmarkArgs) + '\n')
     benchmarks_ci.__main(benchmarkArgs)
+
+    rootPath = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+    resultsPath = os.path.join(rootPath, 'artifacts', 'bin', 'MicroBenchmarks', 'Release', version['tfm'], 'BenchmarkDotNet.Artifacts', 'results')
+
+    getLogger().log(getLogger().getEffectiveLevel(), 'Results were created in the following folder:')
+    getLogger().log(getLogger().getEffectiveLevel(), '  ' + resultsPath)
+
+    today = str(datetime.date.today())
+    resultsName = today + '-' + args.version
+    resultsTarPath = os.path.join(rootPath, 'artifacts', resultsName + '.tar.gz')
+    resultsTar = tarfile.open(resultsTarPath, 'w:gz')
+    resultsTar.add(resultsPath, arcname=resultsName)
+    resultsTar.close()
+
+    getLogger().log(getLogger().getEffectiveLevel(), 'Results were collected into the following tar archive:')
+    getLogger().log(getLogger().getEffectiveLevel(), '  ' + resultsTarPath)
 
 if __name__ == '__main__':
     __main(sys.argv[1:])
