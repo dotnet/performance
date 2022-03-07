1b77ec292cc7b6d6f9278068450eded158958826
Parker Bibus <parkerbibus@microsoft.com>
2022-03-07 13:02:06 -0800

    Missed updates for the mauiios run.

--- src/scenarios/mauiios/pre.py
+++ src/scenarios/mauiios/pre.py
@@ -48,6 +48,6 @@ if args.unzip:
     
 else:
     if(os.path.isdir(name)):
-        copytree(name, f"{PUBDIR}/{name}")
+        copytree(name, os.path.join(PUBDIR, name))
     else:
-        copyfile(name, f"{PUBDIR}/")
+        copyfile(name, os.path.join(PUBDIR, name))
