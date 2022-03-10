64770d72aa28e02c6e6e0ef257133d65b11536e8
Michael Adelson <mike.adelson314@gmail.com>
2022-03-10 13:30:33 -0500

    Added Perf_ImmutableSortedSet benchmark.
    See https://github.com/dotnet/performance/pull/2268#issuecomment-1062799822

---
+++ src/benchmarks/micro/libraries/System.Collections/ImmutableSortedSet/Perf_ImmutableSortedSet.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+
+namespace System.Collections.Tests
+{
+    [BenchmarkCategory(Categories.Libraries, Categories.Collections, Categories.GenericCollections)]
+    public class Perf_ImmutableSortedSet
+    {
+        private ImmutableSortedSet<int> _set;
+
+        [GlobalSetup]
+        public void Setup() => _set = ImmutableSortedSet.CreateRange(Enumerable.Range(0, 100_000));
+
+        [Benchmark]
+        public int Min() => _set.Min;
+
+        [Benchmark]
+        public int Max() => _set.Max;
+    }
+}
