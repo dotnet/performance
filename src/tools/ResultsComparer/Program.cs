c34830c9737f55c18b6ee7f3fec51d19ae55b874
Adam Sitnik <adam.sitnik@gmail.com>
2022-04-12 08:17:59 +0200

    move common logic to Helper type

--- src/tools/ResultsComparer/Program.cs
+++ src/tools/ResultsComparer/Program.cs
@@ -22,8 +22,6 @@ namespace ResultsComparer
 {
     public class Program
     {
-        private const string FullBdnJsonFileExtension = "full.json";
-
         public static int Main(string[] args)
         {
             // we print a lot of numbers here and we want to make it always in invariant way
