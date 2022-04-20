8373c892f5198b33c8246916d60e5a5425b0dda2
Drew Scoggins <anscoggi@microsoft.com>
2022-04-20 16:22:55 -0700

    Remove unused variable

--- src/tools/ScenarioMeasurement/Startup/WPFParser.cs
+++ src/tools/ScenarioMeasurement/Startup/WPFParser.cs
@@ -24,7 +24,6 @@ namespace ScenarioMeasurement
             double threadTime = 0;
             var ins = new Dictionary<int, double>();
             double start = -1;
-            double stopTracker = 0;
             int? pid = null;
             bool doneParsingXaml = false;
             double lastKnownTime = 0.0;
