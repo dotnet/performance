17177154056b53fa0f373ce4a447562ff9691ab3
Drew Scoggins <anscoggi@microsoft.com>
2022-04-25 14:33:57 -0700

    Add check for WPF ETW manifest and install if not present

--- src/scenarios/wpf/pre.py
+++ src/scenarios/wpf/pre.py
@@ -1,13 +1,22 @@
 '''
 pre-command
 '''
-import sys
+import sys, subprocess
 from performance.logger import setup_loggers
 from shared.precommands import PreCommands
 from shared import const
 from test import EXENAME
+from logging import getLogger
 
 setup_loggers(True)
+
+getLogger().info("Checking status of Microsoft-Windows-WPF Event manifest")
+exitcode = subprocess.call(['wevtutil','gp','Microsoft-Windows-WPF'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+if(exitcode != 0):
+    getLogger().info("Microsoft-Windows-WPF Event manifest not installed, installing...")
+    subprocess.call(['wevtutil', 'im', 'C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\WPF\\wpf-etw.man'])
+getLogger().info("Microsoft-Windows-WPF installed, continuing")
+
 precommands = PreCommands()
 precommands.new(template='wpf',
                 output_dir=const.APPDIR,
--- src/scenarios/wpfsfc/pre.py
+++ src/scenarios/wpfsfc/pre.py
@@ -2,12 +2,21 @@
 pre-command
 '''
 import sys
-import os
+import os, subprocess
 from performance.logger import setup_loggers
 from shared.precommands import PreCommands
 from shared import const
+from logging import getLogger
 
 setup_loggers(True)
+
+getLogger().info("Checking status of Microsoft-Windows-WPF Event manifest")
+exitcode = subprocess.call(['wevtutil','gp','Microsoft-Windows-WPF'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+if(exitcode != 0):
+    getLogger().info("Microsoft-Windows-WPF Event manifest not installed, installing...")
+    subprocess.call(['wevtutil', 'im', 'C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\WPF\\wpf-etw.man'])
+getLogger().info("Microsoft-Windows-WPF installed, continuing")
+
 precommands = PreCommands()
 precommands.existing(os.path.join(sys.path[0], const.SRCDIR), 'wpfsfc.csproj')
 precommands.execute()
