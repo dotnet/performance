dotnet/runtime

commit 0e84780fd2280166cd82374c770177613527f06b

Fixing API compat issues between ref/src for attributes on properties (#68432)
* Fixing API compat issues between ref/src for attributes on properties

* Fix some build breaks due to obsoleted API

* Fix remaining attribute inconsistencies

* Update API compat baselines for the shared framework.

* Temporarily suppress ApiCompat baseline validation

This will allow us to ensure the codebase is ready to ingest the arcade
fix to ApiCompat.

* Fix usage of obsolete API in Data.Common tests

src/coreclr/System.Private.CoreLib/src/System/Reflection/RuntimeAssembly.cs
@@ -84,6 +84,7 @@ namespace System.Reflection
             return null;
         }
 
+        [Obsolete("Assembly.CodeBase and Assembly.EscapedCodeBase are only included for .NET Framework compatibility. Use Assembly.Location.", DiagnosticId = "SYSLIB0012", UrlFormat = "https://aka.ms/dotnet-warnings/{0}")]
         [RequiresAssemblyFiles(ThrowingMessageInRAF)]
         public override string? CodeBase
         {
@@ -103,7 +104,9 @@ namespace System.Reflection
                 if (codeBase.Length == 0)
                 {
                     // For backward compatibility, return CoreLib codebase for assemblies loaded from memory.
+#pragma warning disable SYSLIB0012
                     codeBase = typeof(object).Assembly.CodeBase;
+#pragma warning restore SYSLIB0012
                 }
                 return codeBase;
             }

src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeHelpers.CoreCLR.cs
@@ -115,6 +115,7 @@ namespace System.Runtime.CompilerServices
         [MethodImpl(MethodImplOptions.InternalCall)]
         public static extern new bool Equals(object? o1, object? o2);
 
+        [Obsolete("OffsetToStringData has been deprecated. Use string.GetPinnableReference() instead.")]
         public static int OffsetToStringData
         {
             // This offset is baked in by string indexer intrinsic, so there is no harm

src/coreclr/System.Private.CoreLib/src/System/Runtime/InteropServices/Marshal.CoreCLR.cs
@@ -79,7 +79,9 @@ namespace System.Runtime.InteropServices
         [EditorBrowsable(EditorBrowsableState.Never)]
         [Obsolete("ReadInt64(Object, Int32) may be unavailable in future releases.")]
         [RequiresDynamicCode("Marshalling code for the object might not be available")]
+#pragma warning disable CS0618 // Type or member is obsolete
         public static long ReadInt64([MarshalAs(UnmanagedType.AsAny), In] object ptr, int ofs)
+#pragma warning restore CS0618 // Type or member is obsolete
         {
             return ReadValueSlow(ptr, ofs, (IntPtr nativeHome, int offset) => ReadInt64(nativeHome, offset));
         }

src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeHelpers.CoreRT.cs
@@ -110,6 +110,7 @@ namespace System.Runtime.CompilerServices
             return ObjectHeader.GetHashCode(o);
         }
 
+        [Obsolete("OffsetToStringData has been deprecated. Use string.GetPinnableReference() instead.")]
         public static int OffsetToStringData
         {
             // This offset is baked in by string indexer intrinsic, so there is no harm

src/coreclr/nativeaot/System.Private.CoreLib/src/System/Threading/Overlapped.cs
@@ -207,7 +207,7 @@ namespace System.Threading
             set { _overlappedData.OffsetHigh = value; }
         }
 
-        [Obsolete("This property is not 64-bit compatible.  Use EventHandleIntPtr instead.  http://go.microsoft.com/fwlink/?linkid=14202")]
+        [Obsolete("Overlapped.EventHandle is not 64-bit compatible and has been deprecated. Use EventHandleIntPtr instead.")]
         public int EventHandle
         {
             get { return EventHandleIntPtr.ToInt32(); }

src/coreclr/nativeaot/System.Private.Reflection.Core/src/System/Reflection/Runtime/General/NonOverriddenApis.cs
@@ -41,7 +41,9 @@ namespace System.Reflection.Runtime.Assemblies
         public sealed override bool IsDynamic => base.IsDynamic;
         public sealed override string ToString() => base.ToString();
         [RequiresAssemblyFilesAttribute("The code will return an empty string for assemblies embedded in a single-file app")]
+#pragma warning disable SYSLIB0012
         public sealed override string EscapedCodeBase => base.EscapedCodeBase;
+#pragma warning restore SYSLIB0012
         [RequiresAssemblyFiles("The code will throw for assemblies embedded in a single-file app")]
         public sealed override FileStream[] GetFiles() => base.GetFiles();
 #endif //DEBUG

src/libraries/Microsoft.VisualBasic.Core/ref/Microsoft.VisualBasic.Core.cs
@@ -1341,6 +1341,7 @@ namespace Microsoft.VisualBasic.FileIO
         public TextFieldParser(string path) { }
         public TextFieldParser(string path, System.Text.Encoding defaultEncoding) { }
         public TextFieldParser(string path, System.Text.Encoding defaultEncoding, bool detectEncoding) { }
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         public string[]? CommentTokens { get { throw null; } set { } }
         public string[]? Delimiters { get { throw null; } set { } }
         public bool EndOfData { get { throw null; } }

src/libraries/Microsoft.VisualBasic.Core/src/Microsoft/VisualBasic/Constants.vb
@@ -8,7 +8,7 @@ Namespace Global.Microsoft.VisualBasic
 
         Public Const vbCrLf As String = ChrW(13) & ChrW(10)
         Public Const vbObjectError As Integer = &H80040000I
-        <Global.System.Obsolete("For a carriage return and line feed, use vbCrLf.  For the current platform's newline, use System.Environment.NewLine.")>
+        <Global.System.Obsolete("vbNewLine has been deprecated. For a carriage return and line feed, use vbCrLf. For the current platform's newline, use System.Environment.NewLine.")>
         Public Const vbNewLine As String = ChrW(13) & ChrW(10)
         Public Const vbCr As String = ChrW(13)
         Public Const vbLf As String = ChrW(10)

dotnet/performance

commit b263096c7a2997c92ff0eba44a5ecebb94a25009

Fix UnsupportedOS issue
This was introduced by https://github.com/dotnet/runtime/pull/68432. We need to mark the
test as not runnable on the platforms where we are missing this property

src/benchmarks/micro/libraries/System.Net.NetworkInformation/NetworkInterfaceTests.cs
@@ -15,6 +15,12 @@ namespace System.Net.NetworkInformation.Tests
         public NetworkInterface[] GetAllNetworkInterfaces() => NetworkInterface.GetAllNetworkInterfaces();
 
         [Benchmark]
+    #if NET5_0_OR_GREATER
+        [System.Runtime.Versioning.UnsupportedOSPlatform("osx")]
+        [System.Runtime.Versioning.UnsupportedOSPlatform("ios")]
+        [System.Runtime.Versioning.UnsupportedOSPlatform("tvos")]
+        [System.Runtime.Versioning.UnsupportedOSPlatform("freebsd")]
+    #endif
         public void GetAllNetworkInterfacesProperties()
         {
             foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())