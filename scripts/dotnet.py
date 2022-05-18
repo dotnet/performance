69d25843ad15a13ec49c6b5bac6370ef6a4bba7c
Parker Bibus <parkerbibus@microsoft.com>
2022-05-18 13:38:28 -0700

    Update retry, but it appears that this may not be the issue, so also print the http response to see if there is a different issue not being bubbled up.

--- scripts/dotnet.py
+++ scripts/dotnet.py
@@ -621,6 +621,7 @@ def get_commit_date(
         url = urlformat % (owner, repo, commit_sha)
 
     build_timestamp = None
+    item = None
     retrycount = 0
     success = 0
     while success == 0 and retrycount <= 3:
@@ -632,10 +633,16 @@ def get_commit_date(
                 success = 1
         except URLError:
             retrycount += 1
+            getLogger().warning(f"URL Error trying to get commit date from {url}, Attempt {retrycount}")
+            sleep(2)
 
     if not build_timestamp:
-        raise RuntimeError(
-            'Could not get timestamp for commit %s' % commit_sha)
+        if not item:
+            raise RuntimeError(
+                'Could not get timestamp for commit %s' % commit_sha)
+        else:
+            raise RuntimeError(
+                'Could not get timestamp for commit %s, data retrieved:\n %s' % (commit_sha, item))
     return build_timestamp
 
 def get_project_name(csproj_file: str) -> str:
