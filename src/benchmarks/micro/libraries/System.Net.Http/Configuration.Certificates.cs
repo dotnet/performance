a7e0a78cc3e79be352fb2beb4b6eb713360254d5
Kunal Pathak <Kunal.Pathak@microsoft.com>
2022-06-07 09:07:31 -0700

    Wrap certification initialization with try-catch (#2469)
    * Wrap certification initialization with try-catch
    
    * nit comment

--- src/benchmarks/micro/libraries/System.Net.Http/Configuration.Certificates.cs
+++ src/benchmarks/micro/libraries/System.Net.Http/Configuration.Certificates.cs
@@ -24,15 +24,24 @@ namespace System.Net.Test.Common
             public static X509Certificate2 GetRSA4096Certificate() => GetCertificate("rsa4096.pfx");
 
             private static X509Certificate2 GetCertificate(string certificateFileName)
-                => new X509Certificate2(
-                    File.ReadAllBytes(
-                        Path.Combine(
-                            AppContext.BaseDirectory, 
-                            "libraries", 
-                            "System.Net.Http", 
-                            certificateFileName)),
-                    CertificatePassword,
-                    X509KeyStorageFlags.DefaultKeySet);
+            {
+                try
+                {
+                    return new X509Certificate2(
+                         File.ReadAllBytes(
+                             Path.Combine(
+                                 AppContext.BaseDirectory,
+                                 "libraries",
+                                 "System.Net.Http",
+                                 certificateFileName)),
+                         CertificatePassword,
+                         X509KeyStorageFlags.DefaultKeySet);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
         }
     }
 }
