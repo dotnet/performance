d80b7c67eb6af553127e0a33d27d225a438bc011
Parker Bibus <parkerbibus@microsoft.com>
2022-07-22 07:24:44 -0700

    Remove unneeded testing/logging things since we are using PerfLabGenericSource, and add in a first render if statement for the source adder to add the event after.

--- src/scenarios/mauiblazordesktop/src/Replacement.Index.razor.cs
+++ src/scenarios/mauiblazordesktop/src/Replacement.Index.razor.cs
@@ -4,19 +4,11 @@ namespace MauiBlazorDesktopTesting.Pages
 {
     public partial class Index
     {
-        private static EventSource log = new EventSource(
-            "Perf-Custom-Event",
-            EventSourceSettings.EtwSelfDescribingEventFormat);
-
         protected override void OnAfterRender(bool firstRender)
         {
-            //using (StreamWriter sw = File.AppendText(@".\traces\testblazorhybridfile.txt"))
-            //{
-            //    sw.WriteLine("This is the new text");
-            //}
-            //System.Console.WriteLine("First Render!!!!");
-            log.Write("FirstRender", new EventSourceOptions {Level=EventLevel.LogAlways, Opcode=EventOpcode.Info });
-            //Environment.Exit(97);
+            if (firstRender) {
+
+            }
         }
     }
 }
