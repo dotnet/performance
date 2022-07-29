5671a1028c19cfcfdf1ed8a01cff7169d18698fa
Tanner Gooding <tagoo@outlook.com>
2022-07-29 06:54:16 -0700

    Adding benchmarks covering the new logarithmic functions for Single/Double

--- src/benchmarks/micro/runtime/Math/Functions/Double/LogP1.cs
+++ src/benchmarks/micro/runtime/Math/Functions/Double/LogP1.cs
@@ -9,29 +9,29 @@ namespace System.MathBenchmarks
 {
     public partial class Double
     {
-        // Tests Math.Log(double) over 5000 iterations for the domain 0, +2
+        // Tests double.LogP1(double) over 5000 iterations for the domain 0, +2
 
-        private const double logDelta = 0.0004;
-        private const double logExpectedResult = -1529.0865454048721;
+        private const double logP1Delta = 0.0004;
+        private const double logP1ExpectedResult = 3240.1414489328322;
 
         [Benchmark]
-        public void Log() => LogTest();
+        public void LogP1() => LogP1Test();
 
-        public static void LogTest()
+        public static void LogP1Test()
         {
             double result = 0.0, value = 0.0;
 
             for (int iteration = 0; iteration < MathTests.Iterations; iteration++)
             {
-                value += logDelta;
-                result += Math.Log(value);
+                value += logP1Delta;
+                result += double.LogP1(value);
             }
 
-            double diff = Math.Abs(logExpectedResult - result);
+            double diff = Math.Abs(logP1ExpectedResult - result);
 
             if (diff > MathTests.DoubleEpsilon)
             {
-                throw new Exception($"Expected Result {logExpectedResult,20:g17}; Actual Result {result,20:g17}");
+                throw new Exception($"Expected Result {logP1ExpectedResult,20:g17}; Actual Result {result,20:g17}");
             }
         }
     }
