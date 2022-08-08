a43cbf2d08deb04005d9b3bf756313a614b8d7f2
Parker Bibus <parkerbibus@microsoft.com>
2022-08-08 09:30:59 -0500

    Add Maui Blazor Desktop (#2526)
    * Add `mauiblazordesktop` scenario
    * Add WinUIBlazor startup parser
    * Update release/6.0 to use dotnet version 6.0.4xx

---
+++ src/scenarios/mauiblazordesktop/test.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+from shared.runner import TestTraits, Runner
+
+EXENAME = 'MauiBlazorDesktopTesting'
+
+def main():
+    traits = TestTraits(exename=EXENAME,
+                        guiapp='true',
+                        startupmetric='WinUIBlazor',
+                        timeout=30,
+                        measurementdelay='6',
+                        runwithoutexit='false',
+                        processwillexit="false",
+                        )
+    runner = Runner(traits)
+    runner.run()
+
+
+if __name__ == "__main__":
+    result = subprocess.run(['powershell', '-Command', r'Get-ChildItem .\pub\Microsoft.Maui.dll | Select-Object -ExpandProperty VersionInfo | Select-Object ProductVersion | Select-Object -ExpandProperty ProductVersion'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    os.environ["MAUI_VERSION"] = result.stdout.decode('utf-8').strip()
+    print(f'Env: MAUI_VERSION: {os.environ["MAUI_VERSION"]}')
+    if("sha" not in os.environ["MAUI_VERSION"] and "azdo" not in os.environ["MAUI_VERSION"]):
+        raise ValueError(f"MAUI_VERSION does not contain sha and azdo indicating failure to retrieve or set the value. MAUI_VERSION: {os.environ['MAUI_VERSION']}")
+    main()
