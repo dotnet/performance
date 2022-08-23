6a09b8636bc811fa597c495b1ebaa1a0050f7833
Petr Pokorny <petrpokorny@microsoft.com>
2022-08-23 21:28:59 +0200

    Add F# Compiler Service to scenarios project

--- src/scenarios/fsharpcompilerservice/pre.py
+++ src/scenarios/fsharpcompilerservice/pre.py
@@ -9,15 +9,6 @@ from shared.precommands import PreCommands
 from test import EXENAME
 
 setup_loggers(True)
-# precommands = PreCommands()
-# precommands.new(template='console',
-#                 output_dir=const.APPDIR,
-#                 bin_dir=const.BINDIR,
-#                 exename=EXENAME,
-#                 working_directory=sys.path[0],
-#                 language='F#')
-# precommands.execute()
-
 precommands = PreCommands()
 precommands.existing(os.path.join(sys.path[0], const.SRCDIR), 'FSharp.Compiler.Service.fsproj')
 precommands.execute()
