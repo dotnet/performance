bdb53f2e0ae68222ec43350cc3c793161de9a90f
Kunal Pathak <Kunal.Pathak@microsoft.com>
2022-10-28 07:37:25 -0700

    Add Benchmarks from bepuphysics2
    Latest code taken from https://github.com/bepu/bepuphysics2/commit/2373f40e6e6ef9bb5c84aa2d7295c46b59b9cc0f.

---
+++ src/benchmarks/real-world/bepuphysics2/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using DemoBenchmarks;
+
+BenchmarkRunner.Run(typeof(OneBodyConstraintBenchmarks));
+BenchmarkRunner.Run(typeof(TwoBodyConstraintBenchmarks));
+BenchmarkRunner.Run(typeof(ThreeBodyConstraintBenchmarks));
+BenchmarkRunner.Run(typeof(FourBodyConstraintBenchmarks));
+BenchmarkRunner.Run(typeof(ConvexCollisionTesters));
+BenchmarkRunner.Run(typeof(CollisionBatcherTasks));
+BenchmarkRunner.Run(typeof(Sweeps));
+BenchmarkRunner.Run(typeof(ShapeRayTests));
+BenchmarkRunner.Run(typeof(GatherScatter));
+BenchmarkRunner.Run(typeof(RagdollTubeBenchmark));
+BenchmarkRunner.Run(typeof(ShapePileBenchmark));
+
