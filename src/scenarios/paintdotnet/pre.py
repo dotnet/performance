3a54a4d94c675bed3e39c0e0d3add04d2b68cf7c
Jiri Cincura ↹ <jiri@cincura.net>
2022-11-15 19:14:40 +0100

    Fix PDN pre-command (#2724) (#2728)

--- src/scenarios/paintdotnet/pre.py
+++ src/scenarios/paintdotnet/pre.py
@@ -5,21 +5,21 @@ import sys
 import os, subprocess
 import zipfile
 from performance.logger import setup_loggers
-from os.path import join
+from os.path import join, normpath
 from shared.util import helixcorrelationpayload
 from performance.common import runninginlab
 from shared.precommands import PreCommands
 from shared import const
 from logging import getLogger
-from shutil import copytree
+from shutil import copytree, rmtree
 
 setup_loggers(True)
 
 precommands = PreCommands()
 
-if runninginlab():   
-    with zipfile.ZipFile(join(helixcorrelationpayload(), "PDN\PDN.zip"), 'r') as publish:
-        publish.extractall(const.PUBDIR)
-elif precommands.pathtozip:
-    with zipfile.ZipFile(precommands.pathtozip, 'r') as publish:
-        publish.extractall(const.PUBDIR)
+output = precommands.output or const.PUBDIR
+with zipfile.ZipFile(precommands.pathtozip, 'r') as publish:
+    publish.extractall(output)
+copytree(join(output, 'PDN8', 'Release'), output, dirs_exist_ok=True)
+rmtree(join(output, 'PDN8'))
+getLogger().info(f"Unpacked {precommands.pathtozip} into {output}.")
