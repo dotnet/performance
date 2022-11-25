9fdf1fa6437e2a2fd0ed87330f93724187997bd2
Jiri Cincura <jiri@cincura.net>
2022-11-25 16:13:41 +0100

    Fix SDK scenarios

--- src/scenarios/mvcinnerloop/pre.py
+++ src/scenarios/mvcinnerloop/pre.py
@@ -18,15 +18,3 @@ precommands.new(template='mvc',
                 working_directory=sys.path[0])
 precommands.execute()
 shutil.copy(os.path.join('src', 'Program.cs'), os.path.join(const.APPDIR, 'Program.cs'))
-
-# Update TFM to net7.0 while we wait for the default to be updated
-f = open(os.path.join(os.getcwd(), "app", "mvcinnerloop.csproj"), 'r')
-outFileText = ""
-for line in f.readlines():
-    line = line.replace('net6.0', 'net7.0')
-    outFileText += line
-f.close()
-os.remove(os.path.join(os.getcwd(), "app", "mvcinnerloop.csproj"))
-f = open(os.path.join(os.getcwd(), "app", "mvcinnerloop.csproj"), 'w')
-f.write(outFileText)
-f.close()
