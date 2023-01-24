45e43356a598a1eed1f41105b0c34b0130025034
Parker Bibus <parkerbibus@microsoft.com>
2023-01-24 14:00:44 -0800

    Fix maui desktop scenario runs and make it so that they do not uninstall maui since we prepare the runs in the cloud.

--- src/scenarios/mauiblazordesktop/pre.py
+++ src/scenarios/mauiblazordesktop/pre.py
@@ -15,61 +15,8 @@ import winreg
 
 setup_loggers(True)
 NugetURL = 'https://raw.githubusercontent.com/dotnet/maui/net6.0/NuGet.config'
-WebViewURL = 'https://go.microsoft.com/fwlink/p/?LinkId=2124703' # Obtained from https://developer.microsoft.com/en-us/microsoft-edge/webview2/#download-section
-WebViewInstalled = False
 NugetFile = requests.get(NugetURL)
 open('./Nuget.config', 'wb').write(NugetFile.content)
-
-lmkey = r"SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate\Clients\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}"
-cukey = r"Software\Microsoft\EdgeUpdate\Clients\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}"
-with winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE) as hklm_hive:
-    try:
-        with winreg.OpenKey(hklm_hive, lmkey) as openkey:
-            pvvalue = winreg.QueryValueEx(openkey, 'pv')[0]
-            if pvvalue and pvvalue != '' and pvvalue != '0.0.0.0':
-                WebViewInstalled = True
-                getLogger().info(f"WebView Found; pvvalue(version) {pvvalue}")
-    except:
-        getLogger().warning("WebView not verified in Local_Machine Registry")
-if not WebViewInstalled:
-    try:
-        with winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER) as hkcu_hive:
-            with winreg.OpenKey(hkcu_hive, cukey) as openkey:
-                pvvalue = winreg.QueryValueEx(openkey, 'pv')[0]
-                if pvvalue and pvvalue != '' and pvvalue != '0.0.0.0':
-                    WebViewInstalled = True
-                    getLogger().info(f"WebView Found; pvvalue(version) {pvvalue}")
-    except:
-        getLogger().warning("WebView not verified in Current_Machine Registry")
-if not WebViewInstalled:
-    getLogger().info("Installing WebView2")
-    WebViewInstallFile = requests.get(WebViewURL)
-    open('./MicrosoftEdgeWebview2Setup.exe', 'wb').write(WebViewInstallFile.content)
-    subprocess.run(['powershell', '-Command', r'Start-Process "./MicrosoftEdgeWebview2Setup.exe" -Wait'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-    with winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE) as hklm_hive:
-        try:
-            with winreg.OpenKey(hklm_hive, lmkey) as openkey:
-                pvvalue = winreg.QueryValueEx(openkey, 'pv')[0]
-                if pvvalue and pvvalue != '' and pvvalue != '0.0.0.0':
-                    WebViewInstalled = True
-                    getLogger().info(f"WebView Found; pvvalue(version) {pvvalue}")
-        except:
-            getLogger().warning("WebView not verified in Local_Machine Registry")
-    if not WebViewInstalled:
-        try:
-            with winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER) as hkcu_hive:
-                with winreg.OpenKey(hkcu_hive, cukey) as openkey:
-                    pvvalue = winreg.QueryValueEx(openkey, 'pv')[0]
-                    if pvvalue and pvvalue != '' and pvvalue != '0.0.0.0':
-                        WebViewInstalled = True
-                        getLogger().info(f"WebView Found; pvvalue(version) {pvvalue}")
-        except:
-            getLogger().warning("WebView not verified in Current_Machine Registry.")
-            getLogger().error("Blazor cannot run without WebView installed, exiting execution.")
-            sys.exit(-1)
-else:
-    getLogger().info("WebViewAlreadyInstalled")
-    
 
 precommands = PreCommands()
 precommands.install_workload('maui', ['--from-rollback-file', 'https://aka.ms/dotnet/maui/net6.0.json', '--configfile', './Nuget.config'])
--- src/scenarios/mauidesktop/post.py
+++ src/scenarios/mauidesktop/post.py
@@ -2,8 +2,6 @@
 post cleanup script
 '''
 
-from shared.postcommands import PostCommands, clean_directories
+from shared.postcommands import clean_directories
 
-postcommands = PostCommands()
 clean_directories()
-postcommands.uninstall_workload('maui')
