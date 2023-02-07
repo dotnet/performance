9c30d92da79f744a42189e5f3558e282c6c521a2
Jiri Cincura <jiri@cincura.net>
2023-02-07 11:32:02 +0100

    Handle PDN version (printing in log and storing)

--- src/scenarios/paintdotnet/test.py
+++ src/scenarios/paintdotnet/test.py
@@ -1,16 +1,19 @@
-import os
+import os, subprocess
 import winreg
 from datetime import datetime, timezone
 from shared.runner import TestTraits, Runner
 from shared import const
 from performance.logger import setup_loggers, getLogger
+from os.path import join
+from shared.versionmanager import get_version_from_dll_powershell
 
 EXENAME = 'paintdotnet'
 
 def main():
     setup_loggers(True)
 
-    set_environment()
+    pdn_version = get_pdn_version()
+    set_environment(pdn_version)
     set_registry()
     traits = TestTraits(exename=EXENAME,
                         guiapp='true',
@@ -24,9 +27,10 @@ def main():
     runner = Runner(traits)
     runner.run()
 
-def set_environment():
+def set_environment(pdn_version):
     os.environ['DOTNET_ROLL_FORWARD'] = 'LatestMajor'
     os.environ['DOTNET_ROLL_FORWARD_TO_PRERELEASE'] = '1'
+    os.environ['PDN_VERSION'] = pdn_version
     getLogger().info("Environment variables set.")
 
 def set_registry():
@@ -35,6 +39,11 @@ def set_registry():
         winreg.SetValueEx(key, 'Updates/LastCheckTimeUtc', None, winreg.REG_SZ, str(int(value)))
     getLogger().info("Fixed up Updates/LastCheckTimeUtc.")
 
+def get_pdn_version():
+    file = join(os.environ['HELIX_WORKITEM_ROOT'], const.PUBDIR, EXENAME) + '.dll'
+    version = get_version_from_dll_powershell(file)
+    getLogger().info(f"PDN version is {version}.")
+    return version
 
 if __name__ == "__main__":
     main()
