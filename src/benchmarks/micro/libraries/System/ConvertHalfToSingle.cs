c8dcf8612f242b04b5eb74cec1ee1b491801fe84
ちーず(･8･)けーき <31585494+MineCake147E@users.noreply.github.com>
2023-03-31 17:13:14 +0900

    Added benchmarks for conversion between Half and Single

---
+++ src/benchmarks/micro/libraries/System/ConvertHalfToSingle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using BenchmarkDotNet.Attributes;
+
+using MicroBenchmarks;
+
+namespace System
+{
+    [BenchmarkCategory(Categories.Runtime, Categories.Libraries)]
+    public class ConvertHalfToSingle
+    {
+        private Half[] bufferA;
+        private float[] bufferDst;
+
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var samples = 65536;
+            bufferDst = new float[samples];
+            var bA = bufferA = new Half[samples];
+            var spanA = bA.AsSpan();
+            for (var i = 0; i < spanA.Length; i++)
+            {
+                spanA[i] = BitConverter.UInt16BitsToHalf((ushort)i);
+            }
+            ref var x9 = ref MemoryMarshal.GetReference(spanA);
+            var length = spanA.Length;
+            var olen = length - 2;
+            var rng = new Random(12345);    //ValuesGenerator doesn't support exhaustive permutation
+            for (var i = 0; i < olen; i++)
+            {
+                var x = rng.Next(i, length);
+                (Unsafe.Add(ref x9, x), Unsafe.Add(ref x9, i)) = (Unsafe.Add(ref x9, i), Unsafe.Add(ref x9, x));
+            }
+        }
+
+        [Benchmark]
+        public void SimpleLoop()
+        {
+            var bA = bufferA.AsSpan();
+            var bD = bufferDst.AsSpan();
+            ref var rsi = ref MemoryMarshal.GetReference(bA);
+            ref var rdi = ref MemoryMarshal.GetReference(bD);
+            nint i = 0, length = Math.Min(bA.Length, bD.Length);
+            for (; i < length; i++)
+            {
+                Unsafe.Add(ref rdi, i) = (float)Unsafe.Add(ref rsi, i);
+            }
+        }
+
+        [Benchmark]
+        public void UnrolledLoop()
+        {
+            var bA = bufferA.AsSpan();
+            var bD = bufferDst.AsSpan();
+            ref var rsi = ref MemoryMarshal.GetReference(bA);
+            ref var rdi = ref MemoryMarshal.GetReference(bD);
+            nint i = 0, length = Math.Min(bA.Length, bD.Length);
+            var olen = length - 3;
+            for (; i < olen; i += 4)
+            {
+                Unsafe.Add(ref rdi, i + 0) = (float)Unsafe.Add(ref rsi, i + 0);
+                Unsafe.Add(ref rdi, i + 1) = (float)Unsafe.Add(ref rsi, i + 1);
+                Unsafe.Add(ref rdi, i + 2) = (float)Unsafe.Add(ref rsi, i + 2);
+                Unsafe.Add(ref rdi, i + 3) = (float)Unsafe.Add(ref rsi, i + 3);
+            }
+            for (; i < length; i++)
+            {
+                Unsafe.Add(ref rdi, i) = (float)Unsafe.Add(ref rsi, i);
+            }
+        }
+    }
+}
---
+++ src/benchmarks/micro/libraries/System/ConvertSingleToHalf.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using BenchmarkDotNet.Attributes;
+
+using MicroBenchmarks;
+
+namespace System
+{
+    [BenchmarkCategory(Categories.Runtime, Categories.Libraries)]
+    public class ConvertSingleToHalf
+    {
+        private float[] bufferSrc;
+        private Half[] bufferDst;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var samples = 65536;
+            var vS = bufferSrc = new float[samples];
+            bufferDst = new Half[samples];
+            var vspan = vS.AsSpan();
+            for (var i = 0; i < vspan.Length; i++)
+            {
+                vspan[i] = (float)BitConverter.UInt16BitsToHalf((ushort)i);
+            }
+            //Random Permutation
+            ref var x9 = ref MemoryMarshal.GetReference(vspan);
+            var length = vspan.Length;
+            var olen = length - 2;
+            var rng = new Random(12345);    //ValuesGenerator doesn't support exhaustive permutation
+            for (var i = 0; i < olen; i++)
+            {
+                var x = rng.Next(i, length);
+                (Unsafe.Add(ref x9, x), Unsafe.Add(ref x9, i)) = (Unsafe.Add(ref x9, i), Unsafe.Add(ref x9, x));
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void SimpleLoop()
+        {
+            var bA = bufferSrc.AsSpan();
+            var bD = bufferDst.AsSpan();
+            ref var rsi = ref MemoryMarshal.GetReference(bA);
+            ref var rdi = ref MemoryMarshal.GetReference(bD);
+            nint i = 0, length = Math.Min(bA.Length, bD.Length);
+            for (; i < length; i++)
+            {
+                Unsafe.Add(ref rdi, i) = (Half)Unsafe.Add(ref rsi, i);
+            }
+        }
+
+        [Benchmark]
+        public void UnrolledLoop()
+        {
+            var bA = bufferSrc.AsSpan();
+            var bD = bufferDst.AsSpan();
+            ref var rsi = ref MemoryMarshal.GetReference(bA);
+            ref var rdi = ref MemoryMarshal.GetReference(bD);
+            nint i = 0, length = Math.Min(bA.Length, bD.Length);
+            var olen = length - 3;
+            for (; i < olen; i += 4)
+            {
+                Unsafe.Add(ref rdi, i + 0) = (Half)Unsafe.Add(ref rsi, i + 0);
+                Unsafe.Add(ref rdi, i + 1) = (Half)Unsafe.Add(ref rsi, i + 1);
+                Unsafe.Add(ref rdi, i + 2) = (Half)Unsafe.Add(ref rsi, i + 2);
+                Unsafe.Add(ref rdi, i + 3) = (Half)Unsafe.Add(ref rsi, i + 3);
+            }
+            for (; i < length; i++)
+            {
+                Unsafe.Add(ref rdi, i) = (Half)Unsafe.Add(ref rsi, i);
+            }
+        }
+    }
+}
