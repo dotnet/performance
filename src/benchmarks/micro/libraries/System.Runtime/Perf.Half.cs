f1e9404dc97d8dd85ef954391c75a7b9b8599cb2
ちーず(･8･)けーき <31585494+MineCake147E@users.noreply.github.com>
2023-05-26 22:52:04 +0900

    Removed permuted benchmarks entirely

--- src/benchmarks/micro/libraries/System.Runtime/Perf.Half.cs
+++ src/benchmarks/micro/libraries/System.Runtime/Perf.Half.cs
@@ -17,11 +17,6 @@ namespace System.Tests
     [BenchmarkCategory(Categories.Libraries)]
     public class Perf_Half
     {
-        private float[] bufferSourceSingle;
-        private float[] bufferDestinationSingle;
-        private Half[] bufferDestinationHalf;
-        private Half[] bufferSourceHalf;
-
         public static IEnumerable<Half> Values => new Half[]
         {
             BitConverter.UInt16BitsToHalf(0x03ff),  //Maximum subnormal number in Half
@@ -37,113 +32,12 @@ namespace System.Tests
             float.NaN
         };
 
-        [GlobalSetup]
-        public void Setup()
-        {
-            const int Samples = 65536;
-            bufferDestinationSingle = new float[Samples];
-            Half[] bSH = bufferSourceHalf = new Half[Samples];
-            Span<Half> spanSH = bSH.AsSpan();
-            for (var i = 0; i < spanSH.Length; i++)
-            {
-                spanSH[i] = BitConverter.UInt16BitsToHalf((ushort)i);
-            }
-            ref Half x9 = ref MemoryMarshal.GetReference(spanSH);
-            int length = spanSH.Length;
-            int olen = length - 2;
-            var rng = new Random(12345);    //ValuesGenerator doesn't support exhaustive permutation
-            for (var i = 0; i < olen; i++)
-            {
-                int x = rng.Next(i, length);
-                (Unsafe.Add(ref x9, x), Unsafe.Add(ref x9, i)) = (Unsafe.Add(ref x9, i), Unsafe.Add(ref x9, x));
-            }
-            bufferDestinationHalf = new Half[Samples];
-            bufferSourceSingle = bSH.Select(a => (float)a).ToArray();
-        }
-        #region Permuted
-
-        [Benchmark]
-        public void SingleToHalfPermutedSimple()
-        {
-            Span<float> bA = bufferSourceSingle.AsSpan();
-            Span<Half> bD = bufferDestinationHalf.AsSpan();
-            ref float rsi = ref MemoryMarshal.GetReference(bA);
-            ref Half rdi = ref MemoryMarshal.GetReference(bD);
-            nint i = 0, length = Math.Min(bA.Length, bD.Length);
-            for (; i < length; i++)
-            {
-                Unsafe.Add(ref rdi, i) = (Half)Unsafe.Add(ref rsi, i);
-            }
-        }
-
-        [Benchmark]
-        public void SingleToHalfPermutedUnrolled()
-        {
-            Span<float> bA = bufferSourceSingle.AsSpan();
-            Span<Half> bD = bufferDestinationHalf.AsSpan();
-            ref float rsi = ref MemoryMarshal.GetReference(bA);
-            ref Half rdi = ref MemoryMarshal.GetReference(bD);
-            nint i = 0, length = Math.Min(bA.Length, bD.Length);
-            var olen = length - 3;
-            for (; i < olen; i += 4)
-            {
-                Unsafe.Add(ref rdi, i + 0) = (Half)Unsafe.Add(ref rsi, i + 0);
-                Unsafe.Add(ref rdi, i + 1) = (Half)Unsafe.Add(ref rsi, i + 1);
-                Unsafe.Add(ref rdi, i + 2) = (Half)Unsafe.Add(ref rsi, i + 2);
-                Unsafe.Add(ref rdi, i + 3) = (Half)Unsafe.Add(ref rsi, i + 3);
-            }
-            for (; i < length; i++)
-            {
-                Unsafe.Add(ref rdi, i) = (Half)Unsafe.Add(ref rsi, i);
-            }
-        }
-        [Benchmark]
-        public void HalfToSinglePermutedSimple()
-        {
-            Span<Half> bA = bufferSourceHalf.AsSpan();
-            Span<float> bD = bufferDestinationSingle.AsSpan();
-            ref Half rsi = ref MemoryMarshal.GetReference(bA);
-            ref float rdi = ref MemoryMarshal.GetReference(bD);
-            nint i = 0, length = Math.Min(bA.Length, bD.Length);
-            for (; i < length; i++)
-            {
-                Unsafe.Add(ref rdi, i) = (float)Unsafe.Add(ref rsi, i);
-            }
-        }
-
-        [Benchmark]
-        public void HalfToSinglePermutedUnrolled()
-        {
-            Span<Half> bA = bufferSourceHalf.AsSpan();
-            Span<float> bD = bufferDestinationSingle.AsSpan();
-            ref Half rsi = ref MemoryMarshal.GetReference(bA);
-            ref float rdi = ref MemoryMarshal.GetReference(bD);
-            nint i = 0, length = Math.Min(bA.Length, bD.Length);
-            var olen = length - 3;
-            for (; i < olen; i += 4)
-            {
-                Unsafe.Add(ref rdi, i + 0) = (float)Unsafe.Add(ref rsi, i + 0);
-                Unsafe.Add(ref rdi, i + 1) = (float)Unsafe.Add(ref rsi, i + 1);
-                Unsafe.Add(ref rdi, i + 2) = (float)Unsafe.Add(ref rsi, i + 2);
-                Unsafe.Add(ref rdi, i + 3) = (float)Unsafe.Add(ref rsi, i + 3);
-            }
-            for (; i < length; i++)
-            {
-                Unsafe.Add(ref rdi, i) = (float)Unsafe.Add(ref rsi, i);
-            }
-        }
-
-        #endregion
-
-        #region Simple
         [Benchmark]
         [ArgumentsSource(nameof(SingleValues))]
-        public Half SingleToHalfSimple(float value) => (Half)value;
+        public Half SingleToHalf(float value) => (Half)value;
 
         [Benchmark]
         [ArgumentsSource(nameof(Values))]
-        public float HalfToSingleSimple(Half value) => (float)value;
-
-        #endregion
+        public float HalfToSingle(Half value) => (float)value;
     }
 }
