365da409ab7f2d8a6503edfd71711b2da89466c5
ちーず(･8･)けーき <31585494+MineCake147E@users.noreply.github.com>
2023-05-26 16:24:51 +0900

    Added simpler Half conversion benchmarks

--- src/benchmarks/micro/libraries/System.Runtime/Perf.Half.cs
+++ src/benchmarks/micro/libraries/System.Runtime/Perf.Half.cs
@@ -22,6 +22,21 @@ namespace System.Tests
         private Half[] bufferDestinationHalf;
         private Half[] bufferSourceHalf;
 
+        public static IEnumerable<Half> Values => new Half[]
+        {
+            BitConverter.UInt16BitsToHalf(0x03ff),  //Maximum subnormal number in Half
+            (Half)12345.0f /* same value used by other tests to compare the perf */,
+            BitConverter.UInt16BitsToHalf(0x7dff)   //NaN
+        };
+
+        public static IEnumerable<float> SingleValues => new float[]
+        {
+            6.097555E-05f,
+            12345.0f /* same value used by other tests to compare the perf */,
+            65520.0f,   //Minimum value that is infinity in Half
+            float.NaN
+        };
+
         [GlobalSetup]
         public void Setup()
         {
@@ -47,7 +62,7 @@ namespace System.Tests
         }
         #region Permuted
 
-        [Benchmark(Baseline = true)]
+        [Benchmark]
         public void SingleToHalfPermutedSimple()
         {
             Span<float> bA = bufferSourceSingle.AsSpan();
@@ -122,9 +137,11 @@ namespace System.Tests
 
         #region Simple
         [Benchmark]
+        [ArgumentsSource(nameof(SingleValues))]
         public Half SingleToHalfSimple(float value) => (Half)value;
 
         [Benchmark]
+        [ArgumentsSource(nameof(Values))]
         public float HalfToSingleSimple(Half value) => (float)value;
 
         #endregion
