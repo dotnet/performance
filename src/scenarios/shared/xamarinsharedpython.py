28ad3456a499e7ace1117068c69eaafc46ac9267
Milos Kotlar <kotlarmilos@gmail.com>
2023-07-18 18:45:16 +0200

    Implement install_versioned_xamarin function

---
+++ src/scenarios/shared/xamarinsharedpython.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+import requests
+from shared.precommands import PreCommands
+
+# Remove the aab files as we don't need them, this saves space in the correlation payload
+def remove_aab_files(output_dir="."):
+    file_list = os.listdir(output_dir)
+    for file in file_list:
+        if file.endswith(".aab"):
+            os.remove(os.path.join(output_dir, file))
+
+def install_versioned_xamarin(precommands):
+    target_framework_wo_platform = precommands.framework.split('-')[0]
+
+    # Download what we need
+    with open ("XamarinNuGet.config", "wb") as f:
+        f.write(requests.get(f'https://raw.githubusercontent.com/xamarin/xamarin-macios/{target_framework_wo_platform}/NuGet.config', allow_redirects=True).content)
+
+    workload_install_args = ['--configfile', 'XamarinNuGet.config']
+    # if int(target_framework_wo_platform.split('.')[0][3:]) > 7: # Use the rollback file for versions greater than 7
+    #     workload_install_args += ['--from-rollback-file', f'https://aka.ms/dotnet/maui/{target_framework_wo_platform}.json']
+
+    precommands.install_workload('ios', workload_install_args) 
--- src/scenarios/xamarinios/pre.py
+++ src/scenarios/xamarinios/pre.py
@@ -6,7 +6,7 @@ import sys
 import subprocess
 from performance.logger import setup_loggers, getLogger
 from shared import const
-from shared.mauisharedpython import remove_aab_files
+from shared.xamarinsharedpython import remove_aab_files, install_versioned_xamarin
 from shared.precommands import PreCommands
 from shared.versionmanager import versions_write_json, get_version_from_dll_powershell_ios
 from test import EXENAME
@@ -14,9 +14,9 @@ from test import EXENAME
 setup_loggers(True)
 
 precommands = PreCommands()
-precommands.install_workload('ios')
+install_versioned_xamarin(precommands)
 
-# Setup the Maui folder
+# Setup the Xamarin folder
 precommands.new(template='ios',
                 output_dir=const.APPDIR,
                 bin_dir=const.BINDIR,
@@ -25,6 +25,7 @@ precommands.new(template='ios',
                 no_restore=False)
 
 # Build the APK
+shutil.copy('./XamarinNuGet.config', './app/Nuget.config')
 precommands.execute(['/p:_RequireCodeSigning=false', '/p:ApplicationId=net.dot.xamarintesting'])
 
 # Remove the aab files as we don't need them, this saves space
@@ -32,3 +33,9 @@ output_dir = const.PUBDIR
 if precommands.output:
     output_dir = precommands.output
 remove_aab_files(output_dir)
+
+# Copy the XamarinVersion to a file so we have it on the machine
+xamarin_version = get_version_from_dll_powershell_ios(rf"./{const.APPDIR}/obj/Release/{precommands.framework}/ios-arm64/ipa/Payload/{EXENAME}.app/Microsoft.iOS.dll")
+version_dict = { "xamarinVersion": xamarin_version }
+versions_write_json(version_dict, rf"{output_dir}/versions.json")
+print(f"Versions: {version_dict} from location " + rf"./{const.APPDIR}/obj/Release/{precommands.framework}/ios-arm64/ipa/Payload/{EXENAME}.app/Microsoft.iOS.dll")
