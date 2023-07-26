d137d0a67a2d06b82fd85656dd2ebc53179423c8
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2023-07-26 13:59:28 +0000

    Fix: The value cannot be an empty string.

--- src/tools/ScenarioMeasurement/SizeOnDisk/SizeOnDisk.cs
+++ src/tools/ScenarioMeasurement/SizeOnDisk/SizeOnDisk.cs
@@ -153,7 +153,9 @@ class SizeOnDisk
         {
             var groups = Regex.Match(wasmFile, @"(?<path>\D+)\/(?<dotnet>\D+)(?<major>\d.)(?<minor>\d.\d)-(?<hash>.*)(?<jsExtension>.js)").Groups;
             // e.g. dotnet.native.8.0.0-rc.1.23375.3.trarwnmzt5.js -> rc.1.23375.3.trarwnmzt5
-            versions.Add(groups["hash"].Value);
+            string hash = groups["hash"].Value;
+            if (!string.IsNullOrEmpty(hash))
+                versions.Add(hash);
         }
     }
 
