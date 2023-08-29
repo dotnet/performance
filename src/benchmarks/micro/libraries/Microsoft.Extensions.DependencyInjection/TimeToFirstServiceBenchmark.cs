af5e1669614bd343d5a595b32f4560d93c338250
Steve Harter <steveharter@users.noreply.github.com>
2023-08-29 15:17:56 -0500

    Add sealed modifiers; share cache instance for rare threading case

--- src/benchmarks/micro/libraries/Microsoft.Extensions.DependencyInjection/TimeToFirstServiceBenchmark.cs
+++ src/benchmarks/micro/libraries/Microsoft.Extensions.DependencyInjection/TimeToFirstServiceBenchmark.cs
@@ -3,7 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 // Define INTERNAL_DI on platforms that support emit.
-//#define INTERNAL_DI
+#define INTERNAL_DI
 
 using System;
 using System.Reflection;
