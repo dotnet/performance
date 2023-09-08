281752d0840a46653a39389520b656a709dbc39d
mrsharm <musharm@microsoft.com>
2023-09-08 10:38:58 -0700

    Fixed merge conflicts

--- src/benchmarks/gc/GCPerfSim/GCPerfSim.cs
+++ src/benchmarks/gc/GCPerfSim/GCPerfSim.cs
@@ -118,6 +118,7 @@ meaning every Nth LOH object allocated will survive.
 -pohSurvInterval/-pohsi:
 (.NET 5.0 or later)
 meaning every Nth POH object allocated will survive.
+(.NET 5.0 or later)
 
 Note that -sohSurvInterval/-lohSurvInterval are only applicable for steady state.
 During initialization everything survives.
@@ -133,7 +134,6 @@ meaning every Nth LOH object survived will be pinned.
 
 -lohFinalizableInterval/-lohfi: lohFinalizableInterval
 meaning every Nth LOH object survived will be finalizable.
-
 -pohFinalizableInterval/-pohfi: pohFinalizableInterval
 (.NET 5.0 or later)
 meaning every Nth POH object survived will be finalizable.
@@ -145,7 +145,6 @@ What kind of objects are we allocating? Current supported types:
 
 -verifyLiveSize: 1/0
 Perform some verification that the live object size matches the expected size
-
 -printEveryNthIter: printEveryNthIter
 Display a summary every N iterations
 
