f4dedaaaa0557ec076db994f92cc2677c7fe10b8
Parker Bibus <parkerbibus@microsoft.com>
2023-09-12 11:33:04 -0700

    Fix mono-libclang-path dependency build argument.

--- scripts/benchmarks_local.py
+++ scripts/benchmarks_local.py
@@ -200,7 +200,10 @@ def generate_all_runtype_dependencies(parsed_args: Namespace, repo_path: str, co
     if check_for_runtype_specified(parsed_args, [RunType.MonoAOTLLVM]):
         artifact_mono_aot_llvm = os.path.join(get_run_artifact_path(parsed_args, RunType.MonoAOTLLVM, commit), "monoaot")
         if force_regenerate or not os.path.exists(artifact_mono_aot_llvm):
-            build_runtime_dependency(parsed_args, repo_path, "mono+libs+host+packs", additional_args=['/p:BuildMonoAOTCrossCompiler=true', f'/p:MonoLibClang={parsed_args.mono_libclang_path}' if parsed_args.mono_libclang_path else None, f'/p:AotHostArchitecture={parsed_args.architecture}', f'/p:AotHostOS={parsed_args.os}'])
+            build_args = ['/p:BuildMonoAOTCrossCompiler=true', f'/p:AotHostArchitecture={parsed_args.architecture}', f'/p:AotHostOS={parsed_args.os}']
+            if parsed_args.mono_libclang_path:
+                build_args.append(f'/p:MonoLibClang={parsed_args.mono_libclang_path}') 
+            build_runtime_dependency(parsed_args, repo_path, "mono+libs+host+packs", additional_args=build_args)
             
             # Move to the bin/aot location
             src_dir_aot = os.path.join(repo_path, "artifacts", "bin", "mono", f"{parsed_args.os}.{parsed_args.architecture}.Release", "cross", f"{parsed_args.os}-{parsed_args.architecture}")
