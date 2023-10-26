e409af98706ed3cce45fd9a48a4366be666af61a
Milos Kotlar <kotlarmilos@gmail.com>
2023-10-26 17:55:38 +0200

    Use branch var instead of hardcoded value

--- src/scenarios/mauiiospodcast/pre.py
+++ src/scenarios/mauiiospodcast/pre.py
@@ -14,7 +14,7 @@ precommands = PreCommands()
 install_versioned_maui(precommands)
 
 branch = f'{precommands.framework[:6]}'
-subprocess.run(['git', 'clone', 'https://github.com/microsoft/dotnet-podcasts.git', '-b', 'net8.0', '--single-branch', '--depth', '1'])
+subprocess.run(['git', 'clone', 'https://github.com/microsoft/dotnet-podcasts.git', '-b', branch, '--single-branch', '--depth', '1'])
 subprocess.run(['powershell', '-Command', r'Remove-Item -Path .\\dotnet-podcasts\\.git -Recurse -Force']) # Git files have permission issues, do their deletion separately
 
 precommands.existing(projectdir='./dotnet-podcasts', projectfile='./src/Mobile/Microsoft.NetConf2021.Maui.csproj')
