5d27850648e18f88af96758be0fc4f747a4145c8
Parker Bibus <parkerbibus@microsoft.com>
2023-11-08 17:01:28 -0800

    Fix attempt 2.

--- src/scenarios/mauiblazorios/pre.py
+++ src/scenarios/mauiblazorios/pre.py
@@ -38,9 +38,8 @@ with open(f"{const.APPDIR}/Components/Pages/Home.razor", "a") as homeRazorFile:
 ''')
 
 # Build the IPA
-# NuGet.config file cannot be in the build directory currently due to https://github.com/dotnet/aspnetcore/issues/41397
-# shutil.copy('./MauiNuGet.config', './app/Nuget.config')
-shutil.copy('./MauiNuGet.config', './app/Nuget.config')
+# NuGet.config file cannot be in the build directory due same cause as to https://github.com/dotnet/aspnetcore/issues/41397
+shutil.copy('./MauiNuGet.config', './Nuget.config')
 precommands.execute(['/p:_RequireCodeSigning=false', '/p:ApplicationId=net.dot.mauiblazortesting'])
 
 output_dir = const.PUBDIR
