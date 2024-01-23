a9438791bc20b3c5749f4aef1658ccfd7fbc78e3
Cameron Aavik <caaavik@microsoft.com>
2024-01-24 03:35:50 +1000

    More bug fixes for scenarios runs

--- scripts/run_performance_job.py
+++ scripts/run_performance_job.py
@@ -532,10 +532,11 @@ def run_performance_job(args: RunPerformanceJobArgs):
 
     if args.dotnet_version_link is not None:
         with urllib.request.urlopen(args.dotnet_version_link) as response:
-            values = json.loads(response)
-            dotnet_version_check = values["dotnet_version"]
-            version_check = values["version"]
-            ci_setup_arguments.dotnet_versions = [dotnet_version_check] if dotnet_version_check else [version_check]
+            values = json.loads(response.read().decode('utf-8'))
+            if "dotnet_version" in values:
+                ci_setup_arguments.dotnet_versions = [values["dotnet_version"]]
+            else:
+                ci_setup_arguments.dotnet_versions = [values["version"]]
 
     if args.pgo_run_type == "nodynamicpgo":
         ci_setup_arguments.pgo_status = "nodynamicpgo"
@@ -730,9 +731,9 @@ def run_performance_job(args: RunPerformanceJobArgs):
         os.environ["RuntimeFlavor"] = args.runtime_flavor or ''
         os.environ["HybridGlobalization"] = str(args.hybrid_globalization)
 
-        RunCommand([*(python.split(" ")), "-m", "pip", "install", "--upgrade", "pip"]).run()
-        RunCommand([*(python.split(" ")), "-m", "pip", "install", "urllib3==1.26.18"]).run()
-        RunCommand([*(python.split(" ")), "-m", "pip", "install", "requests"]).run()
+        RunCommand([*(python.split(" ")), "-m", "pip", "install", "--user", "--upgrade", "pip"]).run()
+        RunCommand([*(python.split(" ")), "-m", "pip", "install", "--user", "urllib3==1.26.18"]).run()
+        RunCommand([*(python.split(" ")), "-m", "pip", "install", "--user", "requests"]).run()
 
         scenarios_path = os.path.join(args.performance_repo_dir, "src", "scenarios")
         script_path = os.path.join(args.performance_repo_dir, "scripts")
