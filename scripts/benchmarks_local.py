a213b6a332888c1b3c71708619ed1251fd8bce23
Parker Bibus <parkerbibus@microsoft.com>
2024-02-14 13:23:53 -0800

    Remove the EMSDK setup as it is already automatic and update the test-main.js file find for both wasm and browser directory paths.

--- scripts/benchmarks_local.py
+++ scripts/benchmarks_local.py
@@ -219,19 +219,6 @@ def generate_all_runtype_dependencies(parsed_args: Namespace, repo_path: str, co
         artifact_wasm_wasm = os.path.join(get_run_artifact_path(parsed_args, RunType.WasmInterpreter, commit), "wasm_bundle")
         artifact_wasm_aot = os.path.join(get_run_artifact_path(parsed_args, RunType.WasmAOT, commit), "wasm_bundle")
         if force_regenerate or not os.path.exists(artifact_wasm_wasm) or not os.path.exists(artifact_wasm_aot):
-            # browser folder was extracted from wasm folder here: https://github.com/dotnet/runtime/pull/95940, so we need to check both locations for which to use (Dec, 2023)
-            wasm_mono_folder = "browser"
-            if not os.path.exists(os.path.join(repo_path, "src", "mono", wasm_mono_folder)):
-                wasm_mono_folder = "wasm"
-            provision_wasm_command = [
-                "make",
-                "-C",
-                os.path.join("src", "mono", wasm_mono_folder),
-                "provision-wasm"
-            ]
-            RunCommand(provision_wasm_command, verbose=True).run(os.path.join(repo_path))
-            os.environ["EMSDK_PATH"] = os.path.join(repo_path, 'src', 'mono', wasm_mono_folder, 'emsdk')
-
             build_runtime_dependency(parsed_args, repo_path, "mono+libs", os_override="browser", arch_override="wasm", additional_args=[f'/p:AotHostArchitecture={parsed_args.architecture}', f'/p:AotHostOS={parsed_args.os}'])
             src_dir_dotnet_latest = os.path.join(repo_path, "artifacts", "BrowserWasm", "staging", "dotnet-latest")
             dest_dir_wasm_dotnet = os.path.join(repo_path, "artifacts", "bin", "wasm", "dotnet")
@@ -239,7 +226,8 @@ def generate_all_runtype_dependencies(parsed_args: Namespace, repo_path: str, co
             src_dir_built_nugets = os.path.join(repo_path, "artifacts", "BrowserWasm", "staging", "built-nugets")
             dest_dir_bin_wasm = os.path.join(repo_path, "artifacts", "bin", "wasm")
             copy_directory_contents(src_dir_built_nugets, dest_dir_bin_wasm)
-            src_file_test_main = os.path.join(repo_path, "src", "mono", wasm_mono_folder, "test-main.js")
+            # browser folder was extracted from wasm folder here: https://github.com/dotnet/runtime/pull/95940, so we need to check both locations for which to use (Dec, 2023)
+            src_file_test_main = glob.glob(os.path.join(repo_path, "src", "mono", "*", "test-main.js"))[0]
             dest_dir_wasm_data = os.path.join(repo_path, "artifacts", "bin", "wasm", "wasm-data")
             dest_file_test_main = os.path.join(dest_dir_wasm_data, "test-main.js")
             if not os.path.exists(dest_dir_wasm_data):
@@ -463,12 +451,12 @@ def generate_artifacts_for_commit(parsed_args: Namespace, repo_url: str, repo_di
 
         if not os.path.exists(repo_path):
             repo = Repo.clone_from(repo_url, repo_path) # type: ignore 'Type of "clone_from" is partially unknown', we know it is a method and returns a Repo
-            repo.git.checkout(commit)
+            repo.git.checkout(commit, '-f')
             repo.git.show('HEAD')
         else:
             repo = Repo(repo_path)
             repo.remotes.origin.fetch()
-            repo.git.checkout(commit)
+            repo.git.checkout(commit, '-f')
             repo.git.show('HEAD')
 
     # Determine what we need to generate for the local benchmarks
