f1e1e44a2b99945424e3837d6f42297ee1867ac2
Cameron Aavik <caaavik@microsoft.com>
2024-02-15 12:16:48 +1000

    Add VSTest Adapter

---
+++ src/benchmarks/micro/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Extensions;
+using System.Collections.Immutable;
+
+[assembly: MicroBenchmarks.RecommendedConfigSource]
+
+namespace MicroBenchmarks
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    class RecommendedConfigSourceAttribute : Attribute, IConfigSource
+    {
+        public RecommendedConfigSourceAttribute()
+        {
+            Config = RecommendedConfig.Create(
+                artifactsPath: new DirectoryInfo(Path.Combine(AppContext.BaseDirectory, "BenchmarkDotNet.Artifacts")),
+                mandatoryCategories: ImmutableHashSet.Create(Categories.Libraries, Categories.Runtime, Categories.ThirdParty));
+        }
+
+        public IConfig Config { get; }
+    }
+}
