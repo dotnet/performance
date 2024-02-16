e70b894002dcb4cbc2ec5ed998adcff5cea39aab
Parker Bibus <parkerbibus@microsoft.com>
2024-02-16 13:26:58 -0800

    Move device restart from in the test to an option in the pre.py.

--- src/scenarios/shared/androidinstrumentation.py
+++ src/scenarios/shared/androidinstrumentation.py
@@ -34,18 +34,6 @@ class AndroidInstrumentationHelper(object):
                     'reboot'
                 ]
 
-                waitForDeviceCmd = [
-                    adbpath,
-                    'wait-for-device'
-                ]
-
-                checkDeviceBootCmd = [
-                    adbpath,
-                    'shell',
-                    'getprop',
-                    'sys.boot_completed'
-                ]
-
                 installCmd = [
                     adbpath,
                     'install',
@@ -80,25 +68,6 @@ class AndroidInstrumentationHelper(object):
 
                 getLogger().info("Rebooting device to ensure we don't hit issue with being unable to install APK")
                 RunCommand(rebootCmd, verbose=True).run()
-
-                getLogger().info("Waiting for device to come back online")
-                RunCommand(waitForDeviceCmd, verbose=True).run()
-                
-                # Wait for the device to boot
-                getLogger().info("Waiting for device to boot")
-                boot_completed = False
-                boot_attempts = 0
-                while not boot_completed and boot_attempts < 10:
-                    time.sleep(5)
-                    boot_check = RunCommand(checkDeviceBootCmd, verbose=True)
-                    boot_check.run()
-                    boot_completed = boot_check.stdout.strip() == '1'
-                    boot_attempts += 1
-                    getLogger().info("Device not booted yet, waiting 5 seconds")
-
-                if not boot_completed:
-                    getLogger().error("Android device did not boot in a reasonable time")
-                    raise TimeoutError("Android device did not boot in a reasonable time")
 
                 getLogger().info("Installing APK")
                 RunCommand(installCmd, verbose=True).run()
