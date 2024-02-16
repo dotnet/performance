3e99f7a70220e27c481345f3efd7e1f3854d8707
Parker Bibus <parkerbibus@microsoft.com>
2024-02-16 13:12:01 -0800

    Update the sleep after device online to wait for boot a dynamic amount of time.

--- src/scenarios/shared/androidinstrumentation.py
+++ src/scenarios/shared/androidinstrumentation.py
@@ -39,6 +39,13 @@ class AndroidInstrumentationHelper(object):
                     'wait-for-device'
                 ]
 
+                checkDeviceBootCmd = [
+                    adbpath,
+                    'shell',
+                    'getprop',
+                    'sys.boot_completed'
+                ]
+
                 installCmd = [
                     adbpath,
                     'install',
@@ -76,7 +83,22 @@ class AndroidInstrumentationHelper(object):
 
                 getLogger().info("Waiting for device to come back online")
                 RunCommand(waitForDeviceCmd, verbose=True).run()
-                time.sleep(30)
+                
+                # Wait for the device to boot
+                getLogger().info("Waiting for device to boot")
+                boot_completed = False
+                boot_attempts = 0
+                while not boot_completed and boot_attempts < 10:
+                    time.sleep(5)
+                    boot_check = RunCommand(checkDeviceBootCmd, verbose=True)
+                    boot_check.run()
+                    boot_completed = boot_check.stdout.strip() == '1'
+                    boot_attempts += 1
+                    getLogger().info("Device not booted yet, waiting 5 seconds")
+
+                if not boot_completed:
+                    getLogger().error("Android device did not boot in a reasonable time")
+                    raise TimeoutError("Android device did not boot in a reasonable time")
 
                 getLogger().info("Installing APK")
                 RunCommand(installCmd, verbose=True).run()
