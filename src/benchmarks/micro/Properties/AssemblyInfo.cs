7cfd55df78ae732e0291f5b450e45d477b258817
Cameron Aavik <caaavik@microsoft.com>
2024-02-20 07:24:34 +1000

    Only use assembly config if in VSTest

--- src/benchmarks/micro/Properties/AssemblyInfo.cs
+++ src/benchmarks/micro/Properties/AssemblyInfo.cs
@@ -3,19 +3,24 @@ using System;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Extensions;
 using System.Collections.Immutable;
+using System.Reflection;
 
-[assembly: MicroBenchmarks.RecommendedConfigSource]
+[assembly: MicroBenchmarks.VSTestConfigSource]
 
 namespace MicroBenchmarks
 {
     [AttributeUsage(AttributeTargets.Assembly)]
-    class RecommendedConfigSourceAttribute : Attribute, IConfigSource
+    class VSTestConfigSourceAttribute : Attribute, IConfigSource
     {
-        public RecommendedConfigSourceAttribute()
+        public VSTestConfigSourceAttribute()
         {
-            Config = RecommendedConfig.Create(
-                artifactsPath: new DirectoryInfo(Path.Combine(AppContext.BaseDirectory, "BenchmarkDotNet.Artifacts")),
-                mandatoryCategories: ImmutableHashSet.Create(Categories.Libraries, Categories.Runtime, Categories.ThirdParty));
+            // We only want to set an assembly-level config when it isn't being set by the entry point
+            // We check for this by seeing if the calling assembly is the same as the executing assembly
+            Config = Assembly.GetCallingAssembly() == Assembly.GetExecutingAssembly()
+                ? ManualConfig.CreateEmpty()
+                : RecommendedConfig.Create(
+                    artifactsPath: new DirectoryInfo(Path.Combine(AppContext.BaseDirectory, "BenchmarkDotNet.Artifacts")),
+                    mandatoryCategories: ImmutableHashSet.Create(Categories.Libraries, Categories.Runtime, Categories.ThirdParty));
         }
 
         public IConfig Config { get; }
