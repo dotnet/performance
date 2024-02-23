45c66ea9b6334bb04eec530f73d9c40418e8ec2f
Petr <psfinaki@users.noreply.github.com>
2024-02-23 14:24:55 +0100

    Improve F# benchmarks (#3965)
    * Remove F# core ref
    
    * Dehardcode the dependency
    
    * Remove broken benchmark project
    
    * Remove commented out benchmarks
    
    * Cleanup
    
    * Setup proper package versions
    
    * fix a compile error
    
    ---------
    
    Co-authored-by: Jiri Cincura <jiri@cincura.net>

--- src/benchmarks/real-world/FSharp/Program.fs
+++ src/benchmarks/real-world/FSharp/Program.fs
@@ -6,14 +6,11 @@ open System.Collections.Immutable
 
 open FSharp.Compiler.CodeAnalysis
 open FSharp.Compiler.Diagnostics
-open FSharp.Compiler.EditorServices
 open FSharp.Compiler.Text
-open FSharp.Compiler.Tokenization
 
 open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
 open BenchmarkDotNet.Extensions
-
 
 [<Literal>]
 let FSharpCategory = "fsharp"
@@ -57,7 +54,7 @@ let prepareProject projectDirName =
         
     projectDir, projectOptions, checker
     
-let parseAndTypeCheckProject (projectDir, projectOptions, checker: FSharpChecker)  =    
+let parseAndTypeCheckProject (projectDir, projectOptions: FSharpProjectOptions, checker: FSharpChecker)  =    
 
     let result = checker.ParseAndCheckProject(projectOptions) |> Async.RunSynchronously
     
@@ -89,22 +86,6 @@ let typeCheckFileInProject projectDir projectOptions (checker: FSharpChecker) fi
 
 
 [<BenchmarkCategory(FSharpCategory)>]
-type FsPlusBenchmarks () =
-    
-    let projectDir, projectOptions, checker = prepareProject "FSharpPlus" |> parseAndTypeCheckProject
-   
-    [<Benchmark>]
-    member this.TypeCheckControlMonoid () =
-        let filename = "Control" ++ "Monoid.fs"
-        typeCheckFileInProject projectDir projectOptions checker filename
-        
-    [<Benchmark>]
-    member this.TypeCheckDataFree () =
-        let filename = "Data" ++ "Free.fs"
-        typeCheckFileInProject projectDir projectOptions checker filename
-
-
-[<BenchmarkCategory(FSharpCategory)>]
 type FsToolkitBenchmarks () =
     
     let projectDir, projectOptions, checker = prepareProject "FsToolkit.ErrorHandling"
@@ -118,7 +99,7 @@ type FsToolkitBenchmarks () =
             checker.ParseFile(file, contents, parsingOptions, cache=false)]
     
     [<Benchmark>]
-    member this.ParseAndTypeCheckProject() =
+    member _.ParseAndTypeCheckProject() =
         parseAndTypeCheckProject (projectDir, projectOptions, checker)
     
     [<IterationCleanup(Target = "ParseAndTypeCheckProject")>]
@@ -133,29 +114,6 @@ type FsToolkitBenchmarks () =
     [<Benchmark>]
     member _.ParseAllFilesInProjectParallel() =
         parseAllFiles |> Async.Parallel |> Async.RunSynchronously
-        
-    // [<Benchmark>]
-    // How to avoid cache?
-    member _.GetTooltip() =
-        let file, contents = sourceFiles |> List.find (fun (file, _) -> file.EndsWith "AsyncResult.fs")
-        let _parseResults, typeCheckAnswer = checker.ParseAndCheckFileInProject(file, 0, contents, projectOptions) |> Async.RunSynchronously        
-        match typeCheckAnswer with
-        | FSharpCheckFileAnswer.Succeeded checkFileResults ->
-            let result = checkFileResults.GetToolTip(173, 47, "        values |> Async.map (Result.requireHead error)", ["Result"; "requireHead"], FSharpTokenTag.Identifier)
-            // printfn $"%A{result}"
-            result
-        | _ -> failwith "Type checking failed"
-        
-    // [<Benchmark>]
-    member _.GetAutocompleteList() =
-        let file, contents = sourceFiles |> List.find (fun (file, _) -> file.EndsWith "AsyncResult.fs")
-        let parseResults, typeCheckAnswer = checker.ParseAndCheckFileInProject(file, 0, contents, projectOptions) |> Async.RunSynchronously        
-        match typeCheckAnswer with
-        | FSharpCheckFileAnswer.Succeeded checkFileResults ->
-            let result = checkFileResults.GetDeclarationListInfo(Some parseResults, 9, "    let inline retn (value: 'ok) : Async<Result<'ok, 'error>> = Ok value |> Async.", PartialLongName.Empty 82, (fun () -> []))
-            // Doesn't work, dunno why...
-            result
-        | _ -> failwith "Type checking failed"
 
 
 [<EntryPoint>]
