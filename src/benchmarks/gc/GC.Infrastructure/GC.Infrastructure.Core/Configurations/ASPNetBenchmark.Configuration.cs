d21bbdac26f3e58fa3b034e9e176894cba804544
Vincent Bu (Centific Technologies Inc) <v-bibu@microsoft.com>
2024-07-17 14:16:48 +0800

    get rid of output.path check logic

--- src/benchmarks/gc/GC.Infrastructure/GC.Infrastructure.Core/Configurations/ASPNetBenchmark.Configuration.cs
+++ src/benchmarks/gc/GC.Infrastructure/GC.Infrastructure.Core/Configurations/ASPNetBenchmark.Configuration.cs
@@ -62,12 +62,6 @@
                 throw new ArgumentNullException($"{nameof(ASPNetBenchmarksConfigurationParser)}: {nameof(configuration.Output)} is null. Check the syntax of the configuration.");
             }
 
-            // configuration.Output.Path can be null when creating suites for ene to end run. 
-            //if (string.IsNullOrEmpty(configuration.Output.Path))
-            //{
-            //    throw new ArgumentNullException($"{nameof(ASPNetBenchmarksConfigurationParser)}: {nameof(configuration.Output.Path)} is null or empty. Please specify an output path.");
-            //}
-
             if (configuration.Environment == null)
             {
                 throw new ArgumentNullException($"{nameof(ASPNetBenchmarksConfigurationParser)}: {nameof(configuration.Environment)} is null. Please add the environment item in the configuration.");
