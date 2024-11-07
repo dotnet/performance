f1d2354f98a2636e25e08b21ef253ec5bff4ef25
KubaD <kubadrazka+github@microsoft.com>
2024-11-07 18:53:08 +0100

    [M] Cleanup Reporting.Reporter tool (#4520)
    * Cleanup Reporting.Reporter tool
    
    * EnvironmentProvider creation in a RecommendedConfig moved to a static variable
    
    ---------
    
    Co-authored-by: Kuba Drazka <kubadrazka@microsoft.com>

--- src/tools/Reporting/Reporting/Reporter.cs
+++ src/tools/Reporting/Reporting/Reporter.cs
@@ -18,69 +18,89 @@ namespace Reporting;
 
 public class Reporter
 {
-    private Run run;
-    private Os os;
-    private Build build;
-    private readonly List<Test> tests = new();
-    protected IEnvironment environment;
+    private readonly static JsonSerializerSettings _jsonSerializerSettings = new()
+    {
+        ContractResolver = new DefaultContractResolver
+        {
+            NamingStrategy = new CamelCaseNamingStrategy() { ProcessDictionaryKeys = false }
+        },
+        Culture = _culture
+    };
 
-    private Reporter() { }
+    private readonly static CultureInfo _culture = CultureInfo.InvariantCulture;
+
+    public List<Test> Tests { get; private set; } = [];
+    public Run Run { get; private set; }
+    public Os Os { get; private set; }
+    public Build Build { get; private set; }
+    public bool InLab { get; }
+
+    public Reporter(IEnvironment environment = null)
+    {
+        environment ??= new EnvironmentProvider();
+        InLab = environment.IsLabEnvironment();
+        if (InLab)
+        {
+            InitializeFromEnvironment(environment);
+        }
+    }
+
+    [JsonConstructor]
+    public Reporter(Build build, Os os, Run run, List<Test> tests)
+    {
+        Build = build;
+        Os = os;
+        Run = run;
+        Tests = tests;
+        InLab = new EnvironmentProvider().IsLabEnvironment();
+    }
 
     public void AddTest(Test test)
     {
-        if (tests.Any(t => t.Name.Equals(test.Name)))
-            throw new Exception($"Duplicate test name, {test.Name}");
-        tests.Add(test);
-    }
-
-    /// <summary>
-    /// Get a Reporter. Relies on environment variables.
-    /// </summary>
-    /// <param name="environment">Optional environment variable provider</param>
-    /// <returns>A Reporter instance or null if the environment is incorrect.</returns>
-    public static Reporter CreateReporter(IEnvironment environment = null)
-    {
-        var ret = new Reporter();
-        ret.environment = environment == null ? new EnvironmentProvider() : environment;
-        if (ret.InLab)
+        if (Tests.Any(t => t.Name.Equals(test.Name)))
         {
-            ret.Init();
+            throw new Exception($"Duplicate test name, {test.Name}");
         }
 
-        return ret;
+        Tests.Add(test);
     }
 
-    private void Init()
+    public string GetJson()
+        => InLab ? JsonConvert.SerializeObject(this, Formatting.Indented, _jsonSerializerSettings) : null;
+
+    public string WriteResultTable()
     {
-        run = new Run
+        var ret = new StringBuilder();
+        foreach (var test in Tests)
         {
-            CorrelationId = environment.GetEnvironmentVariable("HELIX_CORRELATION_ID"),
-            PerfRepoHash = environment.GetEnvironmentVariable("PERFLAB_PERFHASH"),
-            Name = environment.GetEnvironmentVariable("PERFLAB_RUNNAME"),
-            Queue = environment.GetEnvironmentVariable("PERFLAB_QUEUE"),
-            WorkItemName = environment.GetEnvironmentVariable("HELIX_WORKITEM_FRIENDLYNAME"),
-        };
-        bool.TryParse(environment.GetEnvironmentVariable("PERFLAB_HIDDEN"), out var hidden);
-        run.Hidden = hidden;
-        var configs = environment.GetEnvironmentVariable("PERFLAB_CONFIGS");
-        if (!string.IsNullOrEmpty(configs)) // configs should be optional.
-        {
-            foreach (var kvp in configs.Split(';'))
+            var counterWidth = Math.Max(test.Counters.Max(c => c.Name.Length) + 1, 15);
+            var resultWidth = Math.Max(test.Counters.Max(c => c.Results.Max().ToString("F3", _culture).Length + c.MetricName.Length) + 2, 15);
+            ret.AppendLine(test.Name);
+            ret.AppendLine($"{LeftJustify("Metric", counterWidth)}|{LeftJustify("Average", resultWidth)}|{LeftJustify("Min", resultWidth)}|{LeftJustify("Max", resultWidth)}");
+            ret.AppendLine($"{new string('-', counterWidth)}|{new string('-', resultWidth)}|{new string('-', resultWidth)}|{new string('-', resultWidth)}");
+
+            var defaultCounter = test.Counters.Single(c => c.DefaultCounter);
+            ret.AppendLine(PrintCounter(defaultCounter, counterWidth, resultWidth));
+            var topCounters = test.Counters.Where(c => c.TopCounter && !c.DefaultCounter);
+            var restCounters = test.Counters.Where(c => !c.TopCounter && !c.DefaultCounter);
+            foreach (var counter in topCounters.Concat(restCounters))
             {
-                var split = kvp.Split('=');
-                run.Configurations.Add(split[0], split[1]);
+                ret.AppendLine(PrintCounter(counter, counterWidth, resultWidth));
             }
         }
+        return ret.ToString();
+    }
 
-        os = new Os()
-        {
-            Name = $"{RuntimeEnvironment.OperatingSystem} {RuntimeEnvironment.OperatingSystemVersion}",
-            MachineName = Environment.MachineName,
-            Architecture = RuntimeInformation.OSArchitecture.ToString(),
-            Locale = CultureInfo.CurrentUICulture.ToString()
-        };
+    private void InitializeFromEnvironment(IEnvironment environment)
+    {
+        Run = ParseRun(environment);
+        Os = DetectOs();
+        Build = ParseBuildInfo(environment);
+    }
 
-        build = new Build
+    private static Build ParseBuildInfo(IEnvironment environment)
+    {
+        var build = new Build()
         {
             Repo = environment.GetEnvironmentVariable("PERFLAB_REPO"),
             Branch = environment.GetEnvironmentVariable("PERFLAB_BRANCH"),
@@ -91,124 +111,141 @@ public class Reporter
             TimeStamp = DateTime.Parse(environment.GetEnvironmentVariable("PERFLAB_BUILDTIMESTAMP")),
         };
 
+
         foreach (DictionaryEntry entry in environment.GetEnvironmentVariables())
         {
             if (entry.Key.ToString().Equals("PERFLAB_TARGET_FRAMEWORKS", StringComparison.InvariantCultureIgnoreCase))
             {
                 build.AdditionalData["targetFrameworks"] = entry.Value.ToString();
             }
-            else if(entry.Key.ToString().EndsWith("version", true, CultureInfo.InvariantCulture))
+            else if (entry.Key.ToString().EndsWith("version", true, CultureInfo.InvariantCulture))
             {
                 // Special case the original two special cases, MAUI_VERSION is only needed because runtime based runs use MAUI_VERSION
-                if(entry.Key.ToString().Equals("DOTNET_VERSION", StringComparison.InvariantCultureIgnoreCase)){
+                if (entry.Key.ToString().Equals("DOTNET_VERSION", StringComparison.InvariantCultureIgnoreCase))
+                {
                     build.AdditionalData["productVersion"] = entry.Value.ToString();
-                } else if(entry.Key.ToString().Equals("MAUI_VERSION", StringComparison.InvariantCultureIgnoreCase)){
+                }
+                else if (entry.Key.ToString().Equals("MAUI_VERSION", StringComparison.InvariantCultureIgnoreCase))
+                {
                     build.AdditionalData["MAUIVERSION"] = entry.Value.ToString();
-                } else if(!string.IsNullOrWhiteSpace(entry.Value.ToString())){
+                }
+                else if (!string.IsNullOrWhiteSpace(entry.Value.ToString()))
+                {
                     build.AdditionalData[entry.Key.ToString()] = entry.Value.ToString();
                 }
             }
-            else if(entry.Key.ToString().Equals("DOTNET_ROOT", StringComparison.InvariantCultureIgnoreCase))
+            else if (entry.Key.ToString().Equals("DOTNET_ROOT", StringComparison.InvariantCultureIgnoreCase))
             {
-                (string installerHash, string sdkVersion) = GetDotNetVersionInfo(entry.Value.ToString());
+                (var installerHash, var sdkVersion) = GetDotNetVersionInfo(entry.Value.ToString());
                 if (installerHash is not null)
                     build.AdditionalData["installerHash"] = installerHash;
                 if (sdkVersion is not null)
                     build.AdditionalData["sdkVersion"] = sdkVersion;
             }
-            else if(entry.Key.ToString().StartsWith("PERFLAB_DATA_", true, CultureInfo.InvariantCulture))
+            else if (entry.Key.ToString().StartsWith("PERFLAB_DATA_", true, CultureInfo.InvariantCulture))
             {
                 build.AdditionalData[entry.Key.ToString().Substring("PERFLAB_DATA_".Length)] = entry.Value.ToString();
             }
         }
 
-        (string installerHash, string sdkVersion) GetDotNetVersionInfo(string dotnetRoot)
-        {
-            if (Path.Combine(dotnetRoot, "sdk") is string sdkRootPath && Directory.Exists(sdkRootPath))
-            {
-                try
-                {
-                    string versionFile = Directory
-                                            .EnumerateFiles(sdkRootPath, ".version", SearchOption.AllDirectories)
-                                            .FirstOrDefault();
-                    if (versionFile is not null)
-                    {
-                        string[] lines = File.ReadAllLines(versionFile);
-                        string installerHash = lines.Length > 0 ? lines[0] : null;
-                        string sdkVersion = lines.Length > 1 ? lines[1] : null;
+        return build;
+    }
 
-                        return (installerHash, sdkVersion);
-                    }
-                    else
-                    {
-                        Console.WriteLine ($"Failed to find .version file in {sdkRootPath} - {versionFile}");
-                    }
-                } catch (Exception ex) {
-                    Console.WriteLine($"Failed to extract dotnet versions from {sdkRootPath}: {ex.Message}");
+    private static (string installerHash, string sdkVersion) GetDotNetVersionInfo(string dotnetRoot)
+    {
+        if (Path.Combine(dotnetRoot, "sdk") is string sdkRootPath && Directory.Exists(sdkRootPath))
+        {
+            try
+            {
+                var versionFile = Directory
+                                        .EnumerateFiles(sdkRootPath, ".version", SearchOption.AllDirectories)
+                                        .FirstOrDefault();
+                if (versionFile is not null)
+                {
+                    var lines = File.ReadAllLines(versionFile);
+                    var installerHash = lines.Length > 0 ? lines[0] : null;
+                    var sdkVersion = lines.Length > 1 ? lines[1] : null;
+
+                    return (installerHash, sdkVersion);
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to find .version file in {sdkRootPath} - {versionFile}");
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to extract dotnet versions from {sdkRootPath}: {ex.Message}");
+            }
+        }
 
-            return (null, null);
-        }
+        return (null, null);
     }
-    public string GetJson()
-    {
-        if (!InLab)
+
+    private static Os DetectOs()
+        => new()
         {
-            return null;
-        }
-        var jsonobj = new
-        {
-            build,
-            os,
-            run,
-            tests,
+            Name = $"{RuntimeEnvironment.OperatingSystem} {RuntimeEnvironment.OperatingSystemVersion}",
+            MachineName = System.Environment.MachineName,
+            Architecture = RuntimeInformation.OSArchitecture.ToString(),
+            Locale = CultureInfo.CurrentUICulture.ToString()
         };
-        var settings = new JsonSerializerSettings();
-        var resolver = new DefaultContractResolver();
-        resolver.NamingStrategy = new CamelCaseNamingStrategy() { ProcessDictionaryKeys = false };
-        settings.ContractResolver = resolver;
-        return JsonConvert.SerializeObject(jsonobj, Formatting.Indented, settings);
+
+    private static Run ParseRun(IEnvironment environment)
+    {
+        var run = new Run
+        {
+            CorrelationId = environment.GetEnvironmentVariable("HELIX_CORRELATION_ID"),
+            PerfRepoHash = environment.GetEnvironmentVariable("PERFLAB_PERFHASH"),
+            Name = environment.GetEnvironmentVariable("PERFLAB_RUNNAME"),
+            Queue = environment.GetEnvironmentVariable("PERFLAB_QUEUE"),
+            WorkItemName = environment.GetEnvironmentVariable("HELIX_WORKITEM_FRIENDLYNAME"),
+        };
+
+        if (bool.TryParse(environment.GetEnvironmentVariable("PERFLAB_HIDDEN"), out var hidden) && hidden)
+        {
+            run.Hidden = true;
+        }
+
+        run.Configurations = ParseRunConfigs(environment);
+
+        return run;
     }
 
-    public string WriteResultTable()
+    private static IDictionary<string, string> ParseRunConfigs(IEnvironment environment)
     {
-        var ret = new StringBuilder();
-        foreach (var test in tests)
+        var configs = new Dictionary<string, string>();
+        var configsString = environment.GetEnvironmentVariable("PERFLAB_CONFIGS");
+        if (!string.IsNullOrEmpty(configsString)) // configs should be optional.
         {
-            var defaultCounter = test.Counters.Single(c => c.DefaultCounter);
-            var topCounters = test.Counters.Where(c => c.TopCounter && !c.DefaultCounter);
-            var restCounters = test.Counters.Where(c => !(c.TopCounter || c.DefaultCounter));
-            var counterWidth = Math.Max(test.Counters.Max(c => c.Name.Length) + 1, 15);
-            var resultWidth = Math.Max(test.Counters.Max(c => c.Results.Max().ToString("F3").Length + c.MetricName.Length) + 2, 15);
-            ret.AppendLine(test.Name);
-            ret.AppendLine($"{LeftJustify("Metric", counterWidth)}|{LeftJustify("Average",resultWidth)}|{LeftJustify("Min", resultWidth)}|{LeftJustify("Max",resultWidth)}");
-            ret.AppendLine($"{new string('-', counterWidth)}|{new string('-', resultWidth)}|{new string('-', resultWidth)}|{new string('-', resultWidth)}");
+            foreach (var kvp in configsString.Split([';'], StringSplitOptions.RemoveEmptyEntries))
+            {
+                var split = kvp.Split('=');
 
-            ret.AppendLine(Print(defaultCounter, counterWidth, resultWidth));
-            foreach(var counter in topCounters)
-            {
-                ret.AppendLine(Print(counter, counterWidth, resultWidth));
-            }
-            foreach (var counter in restCounters)
-            {
-                ret.AppendLine(Print(counter, counterWidth, resultWidth));
+                if (split.Length != 2)
+                {
+                    continue;
+                }
+
+                var key = split[0].Trim();
+                if (key.Length > 0)
+                {
+                    configs[key] = split[1];
+                }
             }
         }
-        return ret.ToString();
+
+        return configs;
     }
-    private string Print(Counter counter, int counterWidth, int resultWidth)
+
+    private static string LeftJustify(string str, int width)
+        => string.Format("{0,-" + width + "}", str);
+
+    private static string PrintCounter(Counter counter, int counterWidth, int resultWidth)
     {
-        var average = $"{counter.Results.Average():F3} {counter.MetricName}";
-        var max = $"{counter.Results.Max():F3} {counter.MetricName}";
-        var min = $"{counter.Results.Min():F3} {counter.MetricName}";
+        var average = ((FormattableString)$"{counter.Results.Average():F3} {counter.MetricName}").ToString(_culture);
+        var max = ((FormattableString)$"{counter.Results.Max():F3} {counter.MetricName}").ToString(_culture);
+        var min = ((FormattableString)$"{counter.Results.Min():F3} {counter.MetricName}").ToString(_culture);
         return $"{LeftJustify(counter.Name, counterWidth)}|{LeftJustify(average, resultWidth)}|{LeftJustify(min, resultWidth)}|{LeftJustify(max, resultWidth)}";
     }
-
-    private string LeftJustify(string str, int width)
-    {
-        return string.Format("{0,-" + width + "}", str);
-    }
-
-    public bool InLab => environment.GetEnvironmentVariable("PERFLAB_INLAB")?.Equals("1") ?? false;
 }
