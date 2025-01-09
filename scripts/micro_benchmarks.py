0fab2ae73cd939c34d2f506d958911edb97d270a
Parker Bibus <parkerbibus@microsoft.com>
2025-01-09 12:49:29 -0800

    Update the wasmnet10_0 runtime to include the underscore following the latest BDN update.

--- scripts/micro_benchmarks.py
+++ scripts/micro_benchmarks.py
@@ -275,7 +275,7 @@ def __get_benchmarkdotnet_arguments(framework: str, args: Any) -> List[str]:
         elif framework == "net9.0":
             run_args += ['--runtimes', 'wasmnet90']
         elif framework == "net10.0":
-            run_args += ['--runtimes', 'wasmnet100']
+            run_args += ['--runtimes', 'wasmnet10_0']
         else:
             raise ArgumentTypeError('Framework {} is not supported for wasm'.format(framework))
 
