7136b1aa4e315e46575af307a06d3b7ba2e1ec0e
Vincent Bu (Centific Technologies Inc) <v-bibu@microsoft.com>
2025-03-24 09:55:37 +0800

    remove conditional compilation

--- src/benchmarks/gc/GC.Infrastructure/GC.Infrastructure.Core/TraceCollection/TraceCollector.cs
+++ src/benchmarks/gc/GC.Infrastructure/GC.Infrastructure.Core/TraceCollection/TraceCollector.cs
@@ -45,7 +45,7 @@ namespace GC.Infrastructure.Core.TraceCollection
         internal static readonly Dictionary<CollectType, string> LinuxLocalRunCollectTypeMap = new()
         {
             { CollectType.gc, "--profile gc-collect" },
-            { CollectType.cpu, "" },
+            { CollectType.cpu, "--profile cpu-sampling" },
             { CollectType.verbose, "--clrevents gc+stack --clreventlevel verbose" }
         };
 
@@ -65,50 +65,52 @@ namespace GC.Infrastructure.Core.TraceCollection
         private readonly Process _traceProcess;
         private readonly string arguments;
         private readonly string _collectorPath;
-#if Windows
         private readonly Guid _sessionName;
-#endif
 
         private static void InstallTraceCollector(string dependenciesFolder)
         {
-#if Windows
-            string perfviewPath = Path.Combine(DependenciesFolder, "Perfview.exe");
-            if (File.Exists(perfviewPath))
+            if (OperatingSystem.IsWindows())
             {
-                return;
-            }
-            
-            // TODO: Make this URL configurable.
-            const string perfviewUrl = "https://github.com/microsoft/perfview/releases/download/v3.0.0/PerfView.exe";
-            using (HttpClient client = new())
-            {
-                HttpResponseMessage response = client.GetAsync(perfviewUrl).Result;
-                response.EnsureSuccessStatusCode();
-
-                using (FileStream writer = File.OpenWrite(perfviewPath))
+                string perfviewPath = Path.Combine(DependenciesFolder, "Perfview.exe");
+                if (File.Exists(perfviewPath))
                 {
-                    response.Content.ReadAsStream().CopyTo(writer);
+                    return;
+                }
+
+                // TODO: Make this URL configurable.
+                const string perfviewUrl = "https://github.com/microsoft/perfview/releases/download/v3.0.0/PerfView.exe";
+                using (HttpClient client = new())
+                {
+                    HttpResponseMessage response = client.GetAsync(perfviewUrl).Result;
+                    response.EnsureSuccessStatusCode();
+
+                    using (FileStream writer = File.OpenWrite(perfviewPath))
+                    {
+                        response.Content.ReadAsStream().CopyTo(writer);
+                    }
                 }
             }
-#else
-            string dotNetTracePath = Path.Combine(DependenciesFolder, "dotnet-trace");
-            if (File.Exists(dotNetTracePath))
+            if (OperatingSystem.IsLinux())
             {
-                return;
-            }
+                string dotNetTracePath = Path.Combine(DependenciesFolder, "dotnet-trace");
+                if (File.Exists(dotNetTracePath))
+                {
+                    return;
+                }
 
-            using (Process dotNetTraceInstaller = new())
-            {
-                dotNetTraceInstaller.StartInfo.FileName = "dotnet";
-                dotNetTraceInstaller.StartInfo.Arguments = $"tool install dotnet-trace --tool-path {dependenciesFolder}";
-                dotNetTraceInstaller.StartInfo.UseShellExecute = false;
-                dotNetTraceInstaller.StartInfo.CreateNoWindow = true;
-                dotNetTraceInstaller.StartInfo.RedirectStandardError = true;
-                dotNetTraceInstaller.StartInfo.RedirectStandardOutput = true;
-                dotNetTraceInstaller.Start();
-                dotNetTraceInstaller.WaitForExit();
+                using (Process dotNetTraceInstaller = new())
+                {
+                    dotNetTraceInstaller.StartInfo.FileName = "dotnet";
+                    dotNetTraceInstaller.StartInfo.Arguments = $"tool install dotnet-trace --tool-path {dependenciesFolder}";
+                    dotNetTraceInstaller.StartInfo.UseShellExecute = false;
+                    dotNetTraceInstaller.StartInfo.CreateNoWindow = true;
+                    dotNetTraceInstaller.StartInfo.RedirectStandardError = true;
+                    dotNetTraceInstaller.StartInfo.RedirectStandardOutput = true;
+                    dotNetTraceInstaller.Start();
+                    Console.WriteLine("install dotnet-trace");
+                    dotNetTraceInstaller.WaitForExit();
+                }
             }
-#endif
         }
 
         public TraceCollector(string name, string collectType, string outputPath, int? pid = null)
@@ -135,53 +137,56 @@ namespace GC.Infrastructure.Core.TraceCollection
             name = name.Replace("<", "");
             name = name.Replace(">", "");
 
-#if Windows
-            _collectorPath = Path.Combine(DependenciesFolder, "Perfview.exe");
-            _sessionName = Guid.NewGuid();
-
-            Name = Path.Combine(outputPath, $"{name}.etl");
-            string ALWAYS_ARGS = @$" /AcceptEULA /NoGUI /Merge:true";
-            arguments = $"{ALWAYS_ARGS} /sessionName:{_sessionName} {WindowsCollectTypeMap[_collectType]} /LogFile:{Path.Combine(outputPath, name)}.txt /DataFile:{Name}";
-            string command = $"start {arguments}";
-
-            _traceProcess = new();
-            _traceProcess.StartInfo.FileName = _collectorPath;
-            _traceProcess.StartInfo.Arguments = command;
-            _traceProcess.StartInfo.UseShellExecute = false;
-            _traceProcess.StartInfo.CreateNoWindow = true;
-            _traceProcess.StartInfo.RedirectStandardError = true;
-            _traceProcess.StartInfo.RedirectStandardOutput = true;
-            _traceProcess.Start();
-
-            // Give PerfView about a second to get started.
-            Thread.Sleep(1000);
-
-#else
-            if (pid == null)
+            if (OperatingSystem.IsWindows())
             {
-                throw new Exception($"{nameof(TraceCollector)}: Must provide prcoess id in Linux case");
-            }
+                _collectorPath = Path.Combine(DependenciesFolder, "Perfview.exe");
+                _sessionName = Guid.NewGuid();
 
-            if (_collectType != CollectType.none && !LinuxLocalRunCollectTypeMap.Keys.Contains(_collectType))
+                Name = Path.Combine(outputPath, $"{name}.etl");
+                string ALWAYS_ARGS = @$" /AcceptEULA /NoGUI /Merge:true";
+                arguments = $"{ALWAYS_ARGS} /sessionName:{_sessionName} {WindowsCollectTypeMap[_collectType]} /LogFile:{Path.Combine(outputPath, name)}.txt /DataFile:{Name}";
+                string command = $"start {arguments}";
+
+                _traceProcess = new();
+                _traceProcess.StartInfo.FileName = _collectorPath;
+                _traceProcess.StartInfo.Arguments = command;
+                _traceProcess.StartInfo.UseShellExecute = false;
+                _traceProcess.StartInfo.CreateNoWindow = true;
+                _traceProcess.StartInfo.RedirectStandardError = true;
+                _traceProcess.StartInfo.RedirectStandardOutput = true;
+                _traceProcess.Start();
+
+                // Give PerfView about a second to get started.
+                Thread.Sleep(1000);
+            }
+            
+            if (OperatingSystem.IsLinux())
             {
-                throw new Exception($"{nameof(TraceCollector)}: Trace collect type {collectType} is not supported for Linux local run.");
+                if (pid == null)
+                {
+                    throw new Exception($"{nameof(TraceCollector)}: Must provide prcoess id in Linux case");
+                }
+
+                if (_collectType != CollectType.none && !LinuxLocalRunCollectTypeMap.Keys.Contains(_collectType))
+                {
+                    throw new Exception($"{nameof(TraceCollector)}: Trace collect type {collectType} is not supported for Linux local run.");
+                }
+
+                _collectorPath = Path.Combine(DependenciesFolder, "dotnet-trace");
+
+                Name = Path.Combine(outputPath, $"{name}.nettrace");
+                arguments = $"-p {pid} -o {Name} {LinuxLocalRunCollectTypeMap[_collectType]}";
+                string command = $"collect {arguments}";
+
+                _traceProcess = new();
+                _traceProcess.StartInfo.FileName = _collectorPath;
+                _traceProcess.StartInfo.Arguments = command;
+                _traceProcess.StartInfo.UseShellExecute = false;
+                _traceProcess.StartInfo.CreateNoWindow = true;
+                _traceProcess.StartInfo.RedirectStandardError = true;
+                _traceProcess.StartInfo.RedirectStandardOutput = true;
+                _traceProcess.Start();
             }
-
-            _collectorPath = Path.Combine(DependenciesFolder, "dotnet-trace");
-
-            Name = Path.Combine(outputPath, $"{name}.nettrace");
-            arguments = $"-p {pid} -o {Name} {LinuxLocalRunCollectTypeMap[_collectType]}";
-            string command = $"collect {arguments}";
-
-            _traceProcess = new();
-            _traceProcess.StartInfo.FileName = _collectorPath;
-            _traceProcess.StartInfo.Arguments = command;
-            _traceProcess.StartInfo.UseShellExecute = false;
-            _traceProcess.StartInfo.CreateNoWindow = true;
-            _traceProcess.StartInfo.RedirectStandardError = true;
-            _traceProcess.StartInfo.RedirectStandardOutput = true;
-            _traceProcess.Start();
-#endif
         }
 
         private void Dispose(bool disposing)
@@ -193,60 +198,63 @@ namespace GC.Infrastructure.Core.TraceCollection
             }
 
             // TODO: Parameterize the wait for exit time.
-
-#if Windows
-            if (!disposedValue)
+            if (OperatingSystem.IsWindows())
             {
-                using (Process stopProcess = new())
+                if (!disposedValue)
                 {
-                    stopProcess.StartInfo.FileName = _collectorPath;
-                    string command = $"stop {arguments}";
-                    stopProcess.StartInfo.Arguments = command;
-                    stopProcess.StartInfo.UseShellExecute = false;
-                    stopProcess.StartInfo.CreateNoWindow = true;
-                    stopProcess.StartInfo.RedirectStandardInput = true;
-                    stopProcess.StartInfo.RedirectStandardError = true;
-                    stopProcess.Start();
-                    stopProcess.WaitForExit(200_000);
-                    _traceProcess?.Dispose();
-                }
+                    using (Process stopProcess = new())
+                    {
+                        stopProcess.StartInfo.FileName = _collectorPath;
+                        string command = $"stop {arguments}";
+                        stopProcess.StartInfo.Arguments = command;
+                        stopProcess.StartInfo.UseShellExecute = false;
+                        stopProcess.StartInfo.CreateNoWindow = true;
+                        stopProcess.StartInfo.RedirectStandardInput = true;
+                        stopProcess.StartInfo.RedirectStandardError = true;
+                        stopProcess.Start();
+                        stopProcess.WaitForExit(200_000);
+                        _traceProcess?.Dispose();
+                    }
 
-                // Clean up any dangling ETW sessions for both the Kernel and the session.
-                using (Process stopLogmanKernelProcess = new())
-                {
-                    stopLogmanKernelProcess.StartInfo.FileName = "logman";
-                    string etsStopCommand = $"-ets stop {_sessionName}Kernel";
-                    stopLogmanKernelProcess.StartInfo.Arguments = etsStopCommand;
-                    stopLogmanKernelProcess.StartInfo.UseShellExecute = false;
-                    stopLogmanKernelProcess.StartInfo.RedirectStandardOutput = false;
-                    stopLogmanKernelProcess.StartInfo.RedirectStandardError = false;
-                    stopLogmanKernelProcess.StartInfo.CreateNoWindow = true;
-                    stopLogmanKernelProcess.Start();
-                    stopLogmanKernelProcess.WaitForExit(5_000);
-                }
+                    // Clean up any dangling ETW sessions for both the Kernel and the session.
+                    using (Process stopLogmanKernelProcess = new())
+                    {
+                        stopLogmanKernelProcess.StartInfo.FileName = "logman";
+                        string etsStopCommand = $"-ets stop {_sessionName}Kernel";
+                        stopLogmanKernelProcess.StartInfo.Arguments = etsStopCommand;
+                        stopLogmanKernelProcess.StartInfo.UseShellExecute = false;
+                        stopLogmanKernelProcess.StartInfo.RedirectStandardOutput = false;
+                        stopLogmanKernelProcess.StartInfo.RedirectStandardError = false;
+                        stopLogmanKernelProcess.StartInfo.CreateNoWindow = true;
+                        stopLogmanKernelProcess.Start();
+                        stopLogmanKernelProcess.WaitForExit(5_000);
+                    }
 
-                using (Process stopLogmanProcess = new())
-                {
-                    stopLogmanProcess.StartInfo.FileName = "logman";
-                    string etsStopCommand = $"-ets stop {_sessionName}";
-                    stopLogmanProcess.StartInfo.Arguments = etsStopCommand;
-                    stopLogmanProcess.StartInfo.UseShellExecute = false;
-                    stopLogmanProcess.StartInfo.RedirectStandardOutput = false;
-                    stopLogmanProcess.StartInfo.RedirectStandardError = false;
-                    stopLogmanProcess.StartInfo.CreateNoWindow = true;
-                    stopLogmanProcess.Start();
-                    stopLogmanProcess.WaitForExit(5_000);
-                }
+                    using (Process stopLogmanProcess = new())
+                    {
+                        stopLogmanProcess.StartInfo.FileName = "logman";
+                        string etsStopCommand = $"-ets stop {_sessionName}";
+                        stopLogmanProcess.StartInfo.Arguments = etsStopCommand;
+                        stopLogmanProcess.StartInfo.UseShellExecute = false;
+                        stopLogmanProcess.StartInfo.RedirectStandardOutput = false;
+                        stopLogmanProcess.StartInfo.RedirectStandardError = false;
+                        stopLogmanProcess.StartInfo.CreateNoWindow = true;
+                        stopLogmanProcess.Start();
+                        stopLogmanProcess.WaitForExit(5_000);
+                    }
 
-                disposedValue = true;
+                    disposedValue = true;
+                }
             }
-#else
-            if (!disposedValue)
+
+            if (OperatingSystem.IsLinux())
             {
-                _traceProcess.WaitForExit();
-                _traceProcess.Dispose();
+                if (!disposedValue)
+                {
+                    _traceProcess.WaitForExit();
+                    _traceProcess.Dispose();
+                }
             }
-#endif
         }
 
 
