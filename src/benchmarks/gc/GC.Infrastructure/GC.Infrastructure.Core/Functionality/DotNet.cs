8f9759a49e93b8758bb49333a5186f5e108b0507
Daisy Zhou (Centific Technologies Inc) <v-wazho@microsoft.com>
2025-03-26 16:43:00 +0800

    add GCTool Command to GC Infra

---
+++ src/benchmarks/gc/GC.Infrastructure/GC.Infrastructure.Core/Functionality/DotNet.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC.Infrastructure.Core.Functionality
+{
+    public static class DotNetInfrastructure
+    {
+        private static readonly List<string> ValidRIDList = new()
+        {
+            "win-x64", "win-x86", "win-arm64",
+            "linux-x64", "linux-musl-x64",
+            "linux-arm64", "linux-musl-arm64",
+            "linux-arm", "linux-musl-arm",
+            "osx-x64", "osx-arm64",
+        };
+
+        public static readonly string CurrentRID = RuntimeInformation.RuntimeIdentifier;
+
+        public static void CheckRID(string targetRID)
+        {
+            if (!ValidRIDList.Contains(targetRID))
+            {
+                throw new ArgumentException($"{nameof(DotNetInfrastructure)}: The given RID {targetRID} is invalid");
+            }
+        }
+
+        public static string GetExcutableFileExtensionByRID(string targetRID)
+        {
+            CheckRID(targetRID);
+            if (targetRID.StartsWith("win"))
+            {
+                return ".exe";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        //public static string GetCompressionExtensionByRID(string targetRID)
+        //{
+        //    CheckRID(targetRID);
+        //    if (targetRID.StartsWith("win"))
+        //    {
+        //        return ".zip";
+        //    }
+        //    else
+        //    {
+        //        return ".tar.gz";
+        //    }
+        //}
+
+        //public static string GetDotNetExecutableFromEnv(Dictionary<string, string> env, string? targetRID = null)
+        //{
+        //    if (!env.ContainsKey("DOTNET_ROOT"))
+        //    {
+        //        throw new Exception($"{nameof(DotNetInfrastructure)}: Please set DOTNET_ROOT");
+        //    }
+
+        //    string dotNetRoot = env["DOTNET_ROOT"];
+        //    if (string.IsNullOrEmpty(targetRID))
+        //    {
+        //        targetRID = CurrentRID;
+        //    }
+        //    string exeExtension = GetExcutableFileExtensionByRID(targetRID);
+        //    string dotNetExe = Path.Combine(dotNetRoot, $"dotnet{exeExtension}");
+
+        //    return dotNetExe;
+        //}
+
+        //public static void ActiveDotNetDumpGeneratingEnvironment(Dictionary<string, string> env,
+        //                                                         string dumpPath)
+        //{
+        //    env["DOTNET_DbgEnableMiniDump"] = "1";
+        //    env["DOTNET_DbgMiniDumpType"] = "4";
+        //    env["DOTNET_DbgMiniDumpName"] = dumpPath;
+        //}
+    }
+}
---
+++ src/benchmarks/gc/GC.Infrastructure/GC.Infrastructure/Commands/ReliabilityFrameworkTest/ReliabilityFrameworkTestCreateTestSuiteCommand.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+using GC.Infrastructure.Core.Configurations;
+using GC.Infrastructure.Core.Configurations.ReliabilityFrameworkTest;
+using GC.Infrastructure.Core.TaskRunner.ReliabilityFrameworkTest;
+using GC.Infrastructure.Core.Functionality;
+
+namespace GC.Infrastructure.Commands.ReliabilityFrameworkTest
+{
+    public sealed class ReliabilityFrameworkTestCreateTestsSuiteCommand :
+        Command<ReliabilityFrameworkTestCreateTestsSuiteCommand.ReliabilityFrameworkTestCreateTestSuiteSettings>
+    {
+        private static readonly string RID = RuntimeInformation.RuntimeIdentifier;
+        
+        private static readonly string _baseSuitePath = Path.Combine("Commands", "RunCommand", "BaseSuite", "ReliabilityFramework");
+        public sealed class ReliabilityFrameworkTestCreateTestSuiteSettings : CommandSettings
+        {
+            [Description("Path to Configuration.")]
+            [CommandOption("-c|--configuration")]
+            public required string ConfigurationPath { get; init; }
+        }
+
+        public override int Execute([NotNull] CommandContext context,
+                                    [NotNull] ReliabilityFrameworkTestCreateTestSuiteSettings settings)
+        {
+            AnsiConsole.Write(new Rule("Create Test Suite For Reliability Framework Test"));
+            AnsiConsole.WriteLine();
+
+            ConfigurationChecker.VerifyFile(settings.ConfigurationPath,
+                                            nameof(ReliabilityFrameworkTestCreateTestSuiteSettings));
+            ReliabilityFrameworkTestCreateTestSuiteConfiguration configuration =
+                ReliabilityFrameworkTestCreateTestSuiteConfigurationParser.Parse(settings.ConfigurationPath);
+
+            Directory.CreateDirectory(configuration.OutputFolder);
+
+            List<string> configNameList = new() { "loh", "poh", "non_induced", "finalization" };
+
+            List<string> gcModeList = new() { "Datas", "Server", "Workstation" };
+
+            // Build ReliabilityFramework.dll, gcperfsim.dll and Tests
+            ReliabilityFrameworkTestSuiteCreator.CreateTestingAssets(configuration.ReliabilityFrameworkDll,
+                                                                     RID,
+                                                                     configuration.CoreRoot,
+                                                                     configuration.OutputFolder,
+                                                                     configuration.GCPerfSimDll,
+                                                                     configuration.TestFolder);
+
+            // Create config file
+            string platformFolder = Path.Combine(configuration.OutputFolder, RID);
+            Directory.CreateDirectory(platformFolder);
+            foreach (string configName in configNameList)
+            {
+                string configFolder = Path.Combine(platformFolder, configName);
+                Directory.CreateDirectory(configFolder);
+                foreach (string gcMode in gcModeList)
+                {
+                    string gcModeFolder = Path.Combine(configFolder, gcMode);
+                    Directory.CreateDirectory(gcModeFolder);
+
+                    AnsiConsole.WriteLine($"====== Generate {configName}.config for {gcMode} mode ======");
+
+                    string configSuffix =
+                        configuration.EnableStressMode switch
+                        {
+                            true => "-stress",
+                            false => ""
+                        };
+                    string configPath = Path.Combine(gcModeFolder,
+                                                     $"{configName}-{gcMode}-{RID}{configSuffix}.config");
+                    ReliabilityFrameworkTestSuiteCreator.GenerateTestConfig(RID,
+                                                                            _baseSuitePath,
+                                                                            configName,
+                                                                            configuration.EnableStressMode,
+                                                                            gcMode,
+                                                                            configPath);
+
+                    AnsiConsole.WriteLine($"====== Generate testing script for {gcMode} mode ======");
+                    string osName = RID.Split("-")
+                    .FirstOrDefault("");
+                    string scriptExtension =
+                        osName switch
+                        {
+                            "win" => ".ps1",
+                            "linux" => ".sh"
+                        };
+                    string scriptPath = Path.Combine(gcModeFolder, $"TestingScript-{configName}-{gcMode}{scriptExtension}");
+                    //string clrtestFolderForCorerun = ReliabilityFrameworkTestSuiteCreator.GetGCTestFolder(
+                    //    configuration.RuntimeRoot, configuration.CoreRunBuildConfig, RID);
+
+                    //string coreRoot = Path.Combine(clrtestFolderForCorerun, "Tests", "Core_Root");
+                    ReliabilityFrameworkTestSuiteCreator.GenerateTestScript(RID,
+                                                                            _baseSuitePath,
+                                                                            configuration.CoreRoot,
+                                                                            configPath,
+                                                                            gcMode,
+                                                                            configuration.OutputFolder,
+                                                                            scriptPath);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
