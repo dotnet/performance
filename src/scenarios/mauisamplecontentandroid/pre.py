31ce0780c2c6a87c3e12450d522493d3b0af791a
Matous Kozak <55735845+matouskozak@users.noreply.github.com>
2025-04-17 19:54:50 +0100

    [Android] Add MAUI Android Sample Content template app + refactor TTFD naming (#4837)
    Add new Maui Sample Content Android scenario, support for extra arguments to the dotnet new command, and refactored MAUI Blazor Android naming so that TTFD tag is only included in startup measurements.

---
+++ src/scenarios/mauisamplecontentandroid/pre.py
@@ -0,0 +1,43 @@
+'''
+pre-command
+'''
+import shutil
+import sys
+from performance.logger import setup_loggers, getLogger
+from shared import const
+from shared.mauisharedpython import remove_aab_files, install_latest_maui
+from shared.precommands import PreCommands
+from shared.versionmanager import versions_write_json, get_sdk_versions
+from test import EXENAME
+
+setup_loggers(True)
+logger = getLogger(__name__)
+logger.info(f"Starting pre-command for MAUI Sample Content template app (dotnet new maui --sample-content)")
+
+precommands = PreCommands()
+
+install_latest_maui(precommands)
+precommands.print_dotnet_info()
+
+# Setup the Maui folder
+precommands.new(template='maui',
+                output_dir=const.APPDIR,
+                bin_dir=const.BINDIR,
+                exename=EXENAME,
+                working_directory=sys.path[0],
+                no_restore=False,
+                extra_args=['--sample-content'])
+
+# Build the APK
+precommands.execute([])
+
+# Remove the aab files as we don't need them, this saves space
+output_dir = const.PUBDIR
+if precommands.output:
+    output_dir = precommands.output
+remove_aab_files(output_dir)
+
+# Extract the versions of used SDKs from the linked folder DLLs
+version_dict = get_sdk_versions(rf".\{const.APPDIR}\obj\Release\{precommands.framework}\android-arm64\linked")
+versions_write_json(version_dict, rf"{output_dir}\versions.json")
+print(f"Versions: {version_dict} from location " + rf".\{const.APPDIR}\obj\Release\{precommands.framework}\android-arm64\linked")
