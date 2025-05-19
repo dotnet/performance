6f98a414beeb7c623456788a8db0b9c7398f0162
Jacob Crawley <66950962+jacob-crawley@users.noreply.github.com>
2025-05-19 22:31:31 +0100

    SVE microbenchmarks with string operations (#4841)
    * SVE microbenchmarks with string operations
    
    Microbenchmarking tests on string operations (len, indexof, cmp) to
    compare the runtimes across scalar, Vector128 and SVE implementations.
    
    * Filtering string benchmarks to only run on aarch64
    
    * Add check for SVE support in string benchmarks
    
    Include a 'Sve.IsSupported' check on each SVE benchmark so they
    still run on machines that dont have support for SVE.
    
    * Add filter to benchmarks for SVE support
    
    Adds a custom filter to each benchmark class which means it can only
    be run if SVE is supported on the machine
    
    * Naming changes to string benchmarks
    
    Addressing upstream comments to renmae 'Scenario' param in StrCmp to
    'Modify', and all scalar methos names have been renamed to 'Scalar()'.

---
+++ src/benchmarks/micro/sve/StrIndexOf.cs
@@ -0,0 +1,189 @@
+#pragma warning disable SYSLIB5003
+
+using System;
+using System.Numerics;
+using System.Linq;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.Arm;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Extensions;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Filters;
+using MicroBenchmarks;
+
+namespace SveBenchmarks
+{
+    [BenchmarkCategory(Categories.Runtime)]
+    [OperatingSystemsArchitectureFilter(allowed: true, System.Runtime.InteropServices.Architecture.Arm64)]
+    [Config(typeof(Config))]
+    public class StrIndexOf
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                AddFilter(new SimpleFilter(_ => Sve.IsSupported));
+            }
+        }
+
+        [Params(15, 127, 527, 10015)]
+        public int Size;
+
+        private char[] _array;
+        private char _searchValue;
+
+        [GlobalSetup]
+        public virtual void Setup()
+        {
+            _array = Enumerable.Range(1, Size)
+                                .Select(i => (char) i)
+                                .ToArray();
+            _searchValue = _array[Size / 2];
+        }
+
+        [Benchmark]
+        public int Scalar()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == _searchValue)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        [Benchmark]
+        public unsafe int Vector128IndexOf()
+        {
+            int incr = Vector128<ushort>.Count;
+            int i = 0;
+
+
+            fixed (char* arr_ptr = _array)
+            {
+                Vector128<ushort> target = Vector128.Create((ushort)_searchValue);
+
+                for (; i <= Size - incr; i += incr)
+                {
+                    Vector128<ushort> vals = Vector128.Load(((ushort*)arr_ptr) + i);
+
+                    // Compare each vector value with the target
+                    Vector128<ushort> cmp = Vector128.Equals(vals, target);
+
+                    ushort cmpSum = Vector128.Sum<ushort>(cmp);
+
+                    if (cmpSum > 0)
+                    {
+                        // find index of matching item
+                        for (int j = 0; j < incr; j++)
+                        {
+                            if (cmp.GetElement(j) == ushort.MaxValue)
+                            {
+                                return i + j;
+                            }
+                        }
+                    }
+                }
+
+                // Search the remaining values
+                for (; i < Size; i++)
+                {
+                    if (_array[i] == _searchValue)
+                        return i;
+                }
+
+                return -1;
+            }
+        }
+
+        [Benchmark]
+        public unsafe int SveIndexOf()
+        {
+            if (Sve.IsSupported)
+            {
+                int i = 0;
+
+                fixed (char* arr_ptr = _array)
+                {
+                    Vector<ushort> target = new Vector<ushort>((ushort)_searchValue);
+                    var pLoop = (Vector<ushort>)Sve.CreateWhileLessThanMask16Bit(i, Size);
+
+                    for (; Sve.TestFirstTrue(Sve.CreateTrueMaskUInt16(), pLoop);  i += (int)Sve.Count16BitElements())
+                    {
+                        Vector<ushort> vals = Sve.LoadVector(pLoop, ((ushort*)arr_ptr) + i);
+                        Vector<ushort> cmpVec = Sve.CompareEqual(vals, target);
+
+                        ushort cmpSum = (ushort)Sve.AddAcross(cmpVec).ToScalar();
+
+                        if (cmpSum > 0)
+                        {
+                            // find index of matching item
+                            for (int j = 0; j < Vector<ushort>.Count; j++)
+                            {
+                                if (cmpVec.GetElement(j) == 1)
+                                {
+                                    return i + j;
+                                }
+                            }
+                        }
+
+                        pLoop = (Vector<ushort>)Sve.CreateWhileLessThanMask16Bit(i, Size);
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        [Benchmark]
+        public unsafe int SveTail()
+        {
+            if (Sve.IsSupported)
+            {
+                int i = 0;
+
+                fixed (char* arr_ptr = _array)
+                {
+                    Vector<ushort> target = new Vector<ushort>((ushort)_searchValue);
+                    var pLoop = (Vector<ushort>)Sve.CreateTrueMaskInt16();
+
+
+                    for (; (Size - i) > (int)Sve.Count16BitElements(); i += (int)Sve.Count16BitElements())
+                    {
+                        Vector<ushort> vals = Sve.LoadVector(pLoop, ((ushort*)arr_ptr) + i);
+                        Vector<ushort> cmpVec = Sve.CompareEqual(vals, target);
+
+                        ushort cmpSum = (ushort)Sve.AddAcross(cmpVec).ToScalar();
+
+                        if (cmpSum > 0)
+                        {
+                            // find index of matching item
+                            for (int j = 0; j < Vector<ushort>.Count; j++)
+                            {
+                                if (cmpVec.GetElement(j) == 1)
+                                {
+                                    return i + j;
+                                }
+                            }
+                        }
+                    }
+
+                    for (; i < Size; i++)
+                    {
+                        if (_array[i] == _searchValue)
+                            return i;
+                    }
+
+                    return -1;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
+
+#pragma warning restore SYSLIB5003
