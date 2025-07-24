7847ca40fdcefea5f27185ceee974b7ee167ff29
Vincent Bu (Centific Technologies Inc) <v-bibu@microsoft.com>
2025-07-24 16:05:35 +0800

    add benchmarks

---
+++ src/benchmarks/gc/GCPerfSim/CustomEvents.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+
+enum CustomEventID: int
+{
+    PrivateMemoryAtGCStart = 1,
+    PrivateMemoryAtGCEnd = 2
+}
+
+
+[EventSource(Name = "CustomEvents")]
+class CustomEvents : EventSource
+{
+    public static CustomEvents Log = new CustomEvents();
+
+    [Event((int)CustomEventID.PrivateMemoryAtGCStart)]
+    public void PrivateMemoryAtGCStart(long PrivateMemory) => WriteEvent((int)CustomEventID.PrivateMemoryAtGCStart, PrivateMemory);
+
+    [Event((int)CustomEventID.PrivateMemoryAtGCEnd)]
+    public void PrivateMemoryAtGCEnd(long PrivateMemory) => WriteEvent((int)CustomEventID.PrivateMemoryAtGCEnd, PrivateMemory);
+}
+
+class GCEventListener : EventListener
+{
+    protected override void OnEventSourceCreated(EventSource eventSource)
+    {
+        if (String.Compare(eventSource.Name, "Microsoft-Windows-DotNETRuntime") == 0)
+        {
+            EnableEvents(eventSource, EventLevel.Informational, (EventKeywords)0x1); // GC keyword
+        }
+    }
+
+    protected override void OnEventWritten(EventWrittenEventArgs eventData)
+    {
+        if (String.CompareOrdinal(eventData.EventName, "GCStart_V2") == 0)
+        {
+            long privateMemory = Process.GetCurrentProcess().PrivateMemorySize64;
+            CustomEvents.Log.PrivateMemoryAtGCStart(privateMemory);
+        }
+        if (String.CompareOrdinal(eventData.EventName, "GCEnd_V1") == 0)
+        {
+            long privateMemory = Process.GetCurrentProcess().PrivateMemorySize64;
+            CustomEvents.Log.PrivateMemoryAtGCEnd(privateMemory);
+        }
+    }
+}
--- src/benchmarks/gc/GCPerfSim/Harness.cs
+++ src/benchmarks/gc/GCPerfSim/Harness.cs
@@ -1,7 +1,40 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Running;
+using System.Linq;
+
+public static class ConsoleArgs
+{
+    public static string loh = "-tc 28 -tagb 540 -tlgb 2 -lohar 0 -pohar 0 -sohsr 100-4000 -lohsr 102400-204800 -pohsr 100-204800 -sohsi 50 -lohsi 0 -pohsi 0 -sohpi 0 -lohpi 0 -sohfi 0 -lohfi 0 -pohfi 0 -ramb 20 -rlmb 2 -allocType reference -testKind time";
+}
+
+[SimpleJob(launchCount: 1, warmupCount: 2, iterationCount: 3)]
+[EtwProfiler]
+[MemoryDiagnoser]
+public class CustomEventsBenchmarks
+{
+    private readonly string[] args = ConsoleArgs.loh.Split(' ')
+        .ToArray(); 
+
+    [Benchmark(Baseline = true)]
+    public void RunTest()
+    {
+        MemoryAlloc.Test(args);
+    }
+
+    [Benchmark]
+    public void RunTestWithCustomEvents()
+    {
+        using var listener = new GCEventListener();
+        MemoryAlloc.Test(args);
+    }
+}
+
 public static class Harness
 {
     public static void Main(string[] args)
     {
-        MemoryAlloc.Test(args);
+        //MemoryAlloc.Test(args);
+        var summary = BenchmarkRunner.Run<CustomEventsBenchmarks>();
     }
 }
