94b9753f162f55f610c02e23181f6d6248f3d1f3
prozolic <42107886+prozolic@users.noreply.github.com>
2025-08-23 19:28:27 +0900

    Add benchmark for ImmutableArrayExtensions

---
+++ src/benchmarks/micro/libraries/System.Linq/ImmutableArrayExtensions.cs
@@ -0,0 +1,34 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace System.Linq.Tests
+{
+    [BenchmarkCategory(Categories.Libraries, Categories.LINQ)]
+    public class ImmutableArrayExtensions
+    {
+        private ImmutableArray<int> _immutableArray;
+
+        [GlobalSetup]
+        public void GlobalSetup() => _immutableArray = ImmutableArray.CreateRange(Enumerable.Range(0, LinqTestData.Size));
+
+        public IEnumerable<object> Arguments()
+        {
+            yield return LinqTestData.IEnumerable;
+            yield return LinqTestData.ICollection;
+            yield return LinqTestData.IList;
+            yield return LinqTestData.Array;
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Arguments))]
+        [MemoryRandomization]
+        public bool SequenceEqual(LinqTestData input) => _immutableArray.SequenceEqual(input.Collection);
+
+    }
+}
