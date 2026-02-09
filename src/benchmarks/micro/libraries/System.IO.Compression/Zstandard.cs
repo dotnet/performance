6418f2c9f482a71f542fa6ab779710c59226d9f6
Radek Zikmund <r.zikmund.rz@gmail.com>
2026-02-09 18:55:05 +0100

    Address code review feedback

--- src/benchmarks/micro/libraries/System.IO.Compression/Zstandard.cs
+++ src/benchmarks/micro/libraries/System.IO.Compression/Zstandard.cs
@@ -1,12 +1,9 @@
-
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 using BenchmarkDotNet.Attributes;
 using MicroBenchmarks;
-
-#if NET11_0_OR_GREATER
 
 namespace System.IO.Compression
 {
@@ -72,5 +69,3 @@ namespace System.IO.Compression
             => compressLevel == CompressionLevel.Optimal ? 11 : compressLevel == CompressionLevel.Fastest ? 1 : 0;
     }
 }
-
-#endif
