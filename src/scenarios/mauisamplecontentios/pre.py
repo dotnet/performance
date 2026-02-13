f728a51d20d2d82629a13142a78bf2f6354cf3b2
Matous Kozak <55735845+matouskozak@users.noreply.github.com>
2026-02-13 19:19:05 +0100

    Add MAUI --sample-content test app for iOS (#5117)
    * Add MAUI -sc test app for iOS
    * Reduce iOS payload size to fit into 2GB helix limit with CoreCLR
    * Move from testing different codegens for Android to Default configurations to simplify the measurements.

---
+++ src/scenarios/mauisamplecontentios/pre.py
@@ -0,0 +1,47 @@
+'''
+pre-command
+'''
+import shutil
+import sys
+import subprocess
+from performance.logger import setup_loggers, getLogger
+from shared import const
+from shared.mauisharedpython import remove_aab_files, install_latest_maui, MauiNuGetConfigContext
+from shared.precommands import PreCommands
+from shared.versionmanager import versions_write_json, get_sdk_versions
+from test import EXENAME
+
+setup_loggers(True)
+logger = getLogger(__name__)
+logger.info(f"Starting pre-command for MAUI Sample Content iOS template app (dotnet new maui --sample-content)")
+
+precommands = PreCommands()
+
+# Use context manager to temporarily merge MAUI's NuGet feeds into repo config
+# This ensures dotnet package search, dotnet new, and dotnet build/publish have access to MAUI packages
+with MauiNuGetConfigContext(precommands.framework):
+    install_latest_maui(precommands)
+    precommands.print_dotnet_info()
+    # Setup the Maui folder - will use merged NuGet.config with MAUI feeds
+    precommands.new(template='maui',
+                    output_dir=const.APPDIR,
+                    bin_dir=const.BINDIR,
+                    exename=EXENAME,
+                    working_directory=sys.path[0],
+                    no_restore=False,
+                    extra_args=['--sample-content'])
+    
+    # Build the IPA - will use merged NuGet.config
+    precommands.execute(['/p:EnableCodeSigning=false', '/p:ApplicationId=net.dot.mauisamplecontenttesting'])
+    # NuGet.config is automatically restored after this block
+
+# Remove the aab files as we don't need them, this saves space
+output_dir = const.PUBDIR
+if precommands.output:
+    output_dir = precommands.output
+remove_aab_files(output_dir)
+
+# Extract the versions of used SDKs from the linked folder DLLs
+version_dict = get_sdk_versions(rf"./{const.APPDIR}/obj/Release/{precommands.framework}/ios-arm64/linked", False)
+versions_write_json(version_dict, rf"{output_dir}/versions.json")
+print(f"Versions: {version_dict} from location " + rf"./{const.APPDIR}/obj/Release/{precommands.framework}/ios-arm64/linked")
