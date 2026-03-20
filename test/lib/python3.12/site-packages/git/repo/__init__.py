f2bc9a6c57d9d92576f928493f9ed4d070caa804
Drew Scoggins <anscoggi@microsoft.com>
2026-03-20 09:57:43 -0700

    Remove test files

--- test/lib/python3.12/site-packages/git/repo/__init__.py
+++
@@ -1,6 +0,0 @@
-# This module is part of GitPython and is released under the
-# 3-Clause BSD License: https://opensource.org/license/bsd-3-clause/
-
-"""Initialize the Repo package."""
-
-from .base import Repo as Repo  # noqa: F401
