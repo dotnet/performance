70f40eb736abc8f62b1556275ba2f4e4d7cba4d0
Vincent Bu (Centific Technologies Inc) <v-bibu@microsoft.com>
2026-05-25 10:25:48 +0800

    average multi microbenchmarks itertations

---
+++ src/benchmarks/gc/GC.Infrastructure/GC.Infrastructure.Core/Presentation/Microbenchmarks/Json.cs
@@ -0,0 +1,15 @@
+﻿using GC.Infrastructure.Core.Analysis.Microbenchmarks;
+using GC.Infrastructure.Core.Configurations.Microbenchmarks;
+using Newtonsoft.Json;
+
+namespace GC.Infrastructure.Core.Presentation.Microbenchmarks
+{
+    public static class Json
+    {
+        public static void Generate(MicrobenchmarkConfiguration configuration, List<MicrobenchmarkComparisonResults> comparisonResultsGroupedByName, string path)
+        {
+            string json = JsonConvert.SerializeObject(comparisonResultsGroupedByName);
+            File.WriteAllText(path, json);
+        }
+    }
+}
